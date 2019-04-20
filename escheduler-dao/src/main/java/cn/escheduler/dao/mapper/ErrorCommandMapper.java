0c894c6824130f492fb482f3f69e2800a3bb3062
qiaozhanwei <qiaozhanwei@analysys.com.cn>
2019-04-20 16:46:51 +0800

    command and task monitor dev

--- escheduler-dao/src/main/java/cn/escheduler/dao/mapper/ErrorCommandMapper.java
+++ escheduler-dao/src/main/java/cn/escheduler/dao/mapper/ErrorCommandMapper.java
@@ -19,11 +19,13 @@ package cn.escheduler.dao.mapper;
 import cn.escheduler.common.enums.*;
 import cn.escheduler.dao.model.Command;
 import cn.escheduler.dao.model.ErrorCommand;
+import cn.escheduler.dao.model.ExecuteStatusCount;
 import org.apache.ibatis.annotations.*;
 import org.apache.ibatis.type.EnumOrdinalTypeHandler;
 import org.apache.ibatis.type.JdbcType;
 
 import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -40,6 +42,18 @@ public interface ErrorCommandMapper {
     @Options(useGeneratedKeys = true,keyProperty = "errorCommand.id")
     @SelectKey(statement = "SELECT LAST_INSERT_ID()", keyProperty = "errorCommand.id", before = false, resultType = int.class)
     int insert(@Param("errorCommand") ErrorCommand errorCommand);
+
+    @Results(value = {
+            @Result(property = "state", column = "state", typeHandler = EnumOrdinalTypeHandler.class, javaType = ExecutionStatus.class, jdbcType = JdbcType.TINYINT),
+            @Result(property = "count", column = "count", javaType = Integer.class, jdbcType = JdbcType.INTEGER),
+    })
+    @SelectProvider(type = ErrorCommandMapperProvider.class, method = "countCommandState")
+    List<ExecuteStatusCount> countCommandState(
+            @Param("userId") int userId,
+            @Param("userType") UserType userType,
+            @Param("startTime") Date startTime,
+            @Param("endTime") Date endTime,
+            @Param("projectId") int projectId);
 
 
 }
