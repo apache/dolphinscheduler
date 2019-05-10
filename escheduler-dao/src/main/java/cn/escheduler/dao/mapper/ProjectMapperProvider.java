1101b5f4e7f863b3e8fee45e989ada3ce96ed142
qiaozhanwei <qiaozhanwei@analysys.com.cn>
2019-05-10 17:21:59 +0800

    project process define count and process instance running count

--- escheduler-dao/src/main/java/cn/escheduler/dao/mapper/ProjectMapperProvider.java
+++ escheduler-dao/src/main/java/cn/escheduler/dao/mapper/ProjectMapperProvider.java
@@ -157,7 +157,8 @@ public class ProjectMapperProvider {
         return new SQL() {{
             SELECT("p.*");
             SELECT("u.user_name as user_name");
-
+            SELECT("(SELECT COUNT(*) FROM t_escheduler_process_definition AS def WHERE def.project_id = p.id) AS def_count");
+            SELECT("(SELECT COUNT(*) FROM t_escheduler_process_definition def, t_escheduler_process_instance inst WHERE def.id = inst.process_definition_id AND def.project_id = p.id AND inst.state=1 ) as inst_running_count");
             FROM(TABLE_NAME + " p");
             JOIN("t_escheduler_user u on u.id=p.user_id");
             WHERE("p.id in " +
@@ -199,7 +200,8 @@ public class ProjectMapperProvider {
         return new SQL() {{
             SELECT("p.*");
             SELECT("u.user_name as user_name");
-
+            SELECT("(SELECT COUNT(*) FROM t_escheduler_process_definition AS def WHERE def.project_id = p.id) AS def_count");
+            SELECT("(SELECT COUNT(*) FROM t_escheduler_process_definition def, t_escheduler_process_instance inst WHERE def.id = inst.process_definition_id AND def.project_id = p.id AND inst.state=1 ) as inst_running_count");
             FROM(TABLE_NAME + " p");
             JOIN("t_escheduler_user u on p.user_id = u.id");
 
