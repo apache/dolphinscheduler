beaa6c6aa076b4414a1dd3fc6ab7e81907a80f79
lenboo <denny1011@163.com>
2019-07-09 14:37:42 +0800

    update

--- escheduler-api/src/main/java/cn/escheduler/api/utils/ZookeeperMonitor.java
+++ escheduler-api/src/main/java/cn/escheduler/api/utils/ZookeeperMonitor.java
@@ -7,7 +7,6 @@ import cn.escheduler.server.ResInfo;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import sun.jvm.hotspot.opto.MachSafePointNode;
 
 import java.util.ArrayList;
 import java.util.Date;
