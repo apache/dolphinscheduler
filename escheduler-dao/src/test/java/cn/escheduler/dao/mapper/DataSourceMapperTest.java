39219ecbb21fb5c1f91d5231e08d270b2b2ab3ea
lidongdai <escheduler@outlook.com>
2019-08-27 11:45:21 +0800

    add unit test

--- escheduler-dao/src/test/java/cn/escheduler/dao/mapper/DataSourceMapperTest.java
+++ escheduler-dao/src/test/java/cn/escheduler/dao/mapper/DataSourceMapperTest.java
@@ -21,13 +21,14 @@ import cn.escheduler.dao.datasource.ConnectionFactory;
 import cn.escheduler.dao.model.DataSource;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Date;
 import java.util.List;
 
-@Ignore
+/**
+ * datasource mapper test
+ */
 public class DataSourceMapperTest {
 
 
