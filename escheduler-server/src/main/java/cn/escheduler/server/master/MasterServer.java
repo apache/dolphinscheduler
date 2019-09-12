2c4bdb12db22b548e48ec3de01e1ebb4b71de1cd
乔占卫 <825193156@qq.com>
2019-09-12 11:24:55 +0800

    service start exception modify (#787)
    * service start exception modify
    
    * master，worker start modify
    
    * .env update

--- escheduler-server/src/main/java/cn/escheduler/server/master/MasterServer.java
+++ escheduler-server/src/main/java/cn/escheduler/server/master/MasterServer.java
@@ -33,6 +33,7 @@ import org.quartz.SchedulerException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
@@ -90,10 +91,9 @@ public class MasterServer extends AbstractServer {
      * master server not use web service
      */
     public static void main(String[] args) {
-        SpringApplicationBuilder app = new SpringApplicationBuilder(MasterServer.class);
+        SpringApplication app = new SpringApplication(MasterServer.class);
 
-        app.web(WebApplicationType.NONE)
-                .run(args);
+        app.run(args);
     }
 
 
