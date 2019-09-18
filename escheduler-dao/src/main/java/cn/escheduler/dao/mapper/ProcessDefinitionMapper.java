e051a4f7a98c2dcbd6aa6f07d2a4f0a81247f3ca
黄聪 <389401879@qq.com>
2019-09-18 20:08:35 +0800

    [BUG][#731]repair swagger annotation，interface path:/escheduler/projects/{projectName}/process/batch-delete and /escheduler/projects/{projectName}/process/delete  (#764)
    * 增加工作流导出导入功能，前端定时器表达式的秒和分钟的默认值从*修改成0
    
    * 修改工作流导出导入功能由excel改为json减少依赖
    
    * [BUG][#731]repair swagger annotation ,interface path:/escheduler/projects/{projectName}/process/batch-delete and /escheduler/projects/{projectName}/process/delete
    
    * [BUG][#771] The edit timer did not assign the original data
    
    * [FEATURE][#236] Cross-project dependency
    
    * [FEATURE][#236] Cross-project dependency(增加跨项目依赖)

--- escheduler-dao/src/main/java/cn/escheduler/dao/mapper/ProcessDefinitionMapper.java
+++ escheduler-dao/src/main/java/cn/escheduler/dao/mapper/ProcessDefinitionMapper.java
@@ -255,4 +255,26 @@ public interface ProcessDefinitionMapper {
     int updateReceiversAndCcById(@Param("receivers") String receivers,
                                  @Param("receiversCc") String receiversCc,
                                  @Param("processDefinitionId") int processDefinitionId);
+
+    /**
+     * query all
+     * @return
+     */
+    @Results(value = {@Result(property = "id", column = "id", id = true, javaType = Integer.class, jdbcType = JdbcType.INTEGER),
+            @Result(property = "name", column = "name", javaType = String.class, jdbcType = JdbcType.VARCHAR),
+            @Result(property = "version", column = "version", javaType = Integer.class, jdbcType = JdbcType.TINYINT),
+            @Result(property = "releaseState", column = "release_state",  typeHandler = EnumOrdinalTypeHandler.class, javaType = ReleaseState.class, jdbcType = JdbcType.TINYINT),
+            @Result(property = "projectId", column = "project_id", javaType = Integer.class, jdbcType = JdbcType.INTEGER),
+            @Result(property = "userId", column = "user_id", javaType = Integer.class, jdbcType = JdbcType.INTEGER),
+            @Result(property = "desc", column = "desc", javaType = String.class, jdbcType = JdbcType.VARCHAR),
+            @Result(property = "createTime", column = "create_time", javaType = Timestamp.class, jdbcType = JdbcType.DATE),
+            @Result(property = "updateTime", column = "update_time", javaType = Timestamp.class, jdbcType = JdbcType.DATE),
+            @Result(property = "flag", column = "flag", typeHandler = EnumOrdinalTypeHandler.class, javaType = Flag.class, jdbcType = JdbcType.TINYINT),
+            @Result(property = "userName", column = "user_name", javaType = String.class, jdbcType = JdbcType.VARCHAR),
+            @Result(property = "timeout", column = "timeout", javaType = Integer.class, jdbcType = JdbcType.INTEGER),
+            @Result(property = "tenantId", column = "tenant_id", javaType = Integer.class, jdbcType = JdbcType.INTEGER),
+            @Result(property = "projectName", column = "project_name", javaType = String.class, jdbcType = JdbcType.VARCHAR)
+    })
+    @SelectProvider(type = ProcessDefinitionMapperProvider.class, method = "queryAll")
+    List<ProcessDefinition> queryAll();
 }
