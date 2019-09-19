43b1bf8844146029b478c3a0f2831b33d5ed4b9d
Love EvenWong <39328362+LoveEvenWong@users.noreply.github.com>
2019-09-19 12:28:13 +0800

    flink task support(flink 任务支持) (#711)
    * flink任务支持
    
    * flink任务支持
    
    * Update zh_CN.js
    
    * Update FlinkArgsUtils.java
    
    * Update .escheduler_env.sh

--- escheduler-common/src/main/java/cn/escheduler/common/utils/TaskParametersUtils.java
+++ escheduler-common/src/main/java/cn/escheduler/common/utils/TaskParametersUtils.java
@@ -19,6 +19,7 @@ package cn.escheduler.common.utils;
 import cn.escheduler.common.enums.TaskType;
 import cn.escheduler.common.task.AbstractParameters;
 import cn.escheduler.common.task.dependent.DependentParameters;
+import cn.escheduler.common.task.flink.FlinkParameters;
 import cn.escheduler.common.task.mr.MapreduceParameters;
 import cn.escheduler.common.task.procedure.ProcedureParameters;
 import cn.escheduler.common.task.python.PythonParameters;
@@ -63,6 +64,8 @@ public class TaskParametersUtils {
           return JSONUtils.parseObject(parameter, PythonParameters.class);
         case DEPENDENT:
           return JSONUtils.parseObject(parameter, DependentParameters.class);
+        case FLINK:
+          return JSONUtils.parseObject(parameter, FlinkParameters.class);
         default:
           return null;
       }
