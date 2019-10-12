ae995fcc1b13af00d323b09bb56c22ace6d1921c
khadgarmage <khadgar.mage@outlook.com>
2019-10-12 12:00:17 +0800

    dockerfile (#993)

--- dockerfile/startup.sh
+++ dockerfile/startup.sh
@@ -5,7 +5,7 @@ if [ `netstat -anop|grep mysql|wc -l` -gt 0 ];then
                 echo "MySQL is Running."
 else
 	MYSQL_ROOT_PWD="root@123"
-        ESZ_DB="escheduler"
+        ESZ_DB="dolphinscheduler"
 	echo "启动mysql服务"
 	chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
 	find /var/lib/mysql -type f -exec touch {} \; && service mysql restart $ sleep 10
@@ -15,55 +15,55 @@ else
 
 		echo "设置mysql权限"
 		mysql --user=root --password=$MYSQL_ROOT_PWD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PWD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-		echo "创建escheduler数据库"
+		echo "创建dolphinscheduler数据库"
 		mysql --user=root --password=$MYSQL_ROOT_PWD -e "CREATE DATABASE IF NOT EXISTS \`$ESZ_DB\` CHARACTER SET utf8 COLLATE utf8_general_ci; FLUSH PRIVILEGES;"
 		echo "导入mysql数据"
-		nohup /opt/escheduler/script/create_escheduler.sh &
+		nohup /opt/dolphinscheduler/script/create-dolphinscheduler.sh &
 		sleep 90
 	fi
-	
-	if [ `mysql --user=root --password=$MYSQL_ROOT_PWD -s -r -e  "SELECT count(TABLE_NAME) FROM information_schema.TABLES WHERE TABLE_SCHEMA='escheduler';" | grep -v count` -eq 38 ];then
+
+	if [ `mysql --user=root --password=$MYSQL_ROOT_PWD -s -r -e  "SELECT count(TABLE_NAME) FROM information_schema.TABLES WHERE TABLE_SCHEMA='dolphinscheduler';" | grep -v count` -eq 38 ];then
 		echo "\`$ESZ_DB\` 表个数正确"
 	else
 		echo "\`$ESZ_DB\` 表个数不正确"
 		mysql --user=root --password=$MYSQL_ROOT_PWD  -e "DROP DATABASE \`$ESZ_DB\`;"
-		echo "创建escheduler数据库"
+		echo "创建dolphinscheduler数据库"
                 mysql --user=root --password=$MYSQL_ROOT_PWD -e "CREATE DATABASE IF NOT EXISTS \`$ESZ_DB\` CHARACTER SET utf8 COLLATE utf8_general_ci; FLUSH PRIVILEGES;"
                 echo "导入mysql数据"
-                nohup /opt/escheduler/script/create_escheduler.sh &
+                nohup /opt/dolphinscheduler/script/create-dolphinscheduler.sh &
                 sleep 90
 	fi
 fi
 
-/opt/zookeeper/bin/zkServer.sh restart 
+/opt/zookeeper/bin/zkServer.sh restart
 
 sleep 90
 
 echo "启动api-server"
-/opt/escheduler/bin/escheduler-daemon.sh stop api-server
-/opt/escheduler/bin/escheduler-daemon.sh start api-server
+/opt/dolphinscheduler/bin/dolphinscheduler-daemon.sh stop api-server
+/opt/dolphinscheduler/bin/dolphinscheduler-daemon.sh start api-server
 
 
 
 echo "启动master-server"
-/opt/escheduler/bin/escheduler-daemon.sh stop master-server
-python /opt/escheduler/script/del_zk_node.py 127.0.0.1 /escheduler/masters
-/opt/escheduler/bin/escheduler-daemon.sh start master-server
+/opt/dolphinscheduler/bin/dolphinscheduler-daemon.sh stop master-server
+python /opt/dolphinscheduler/script/del-zk-node.py 127.0.0.1 /dolphinscheduler/masters
+/opt/dolphinscheduler/bin/dolphinscheduler-daemon.sh start master-server
 
 echo "启动worker-server"
-/opt/escheduler/bin/escheduler-daemon.sh stop worker-server
-python /opt/escheduler/script/del_zk_node.py 127.0.0.1 /escheduler/workers
-/opt/escheduler/bin/escheduler-daemon.sh start worker-server
+/opt/dolphinscheduler/bin/dolphinscheduler-daemon.sh stop worker-server
+python /opt/dolphinscheduler/script/del-zk-node.py 127.0.0.1 /dolphinscheduler/workers
+/opt/dolphinscheduler/bin/dolphinscheduler-daemon.sh start worker-server
 
 
 echo "启动logger-server"
-/opt/escheduler/bin/escheduler-daemon.sh stop logger-server
-/opt/escheduler/bin/escheduler-daemon.sh start logger-server
+/opt/dolphinscheduler/bin/dolphinscheduler-daemon.sh stop logger-server
+/opt/dolphinscheduler/bin/dolphinscheduler-daemon.sh start logger-server
 
 
 echo "启动alert-server"
-/opt/escheduler/bin/escheduler-daemon.sh stop alert-server
-/opt/escheduler/bin/escheduler-daemon.sh start alert-server
+/opt/dolphinscheduler/bin/dolphinscheduler-daemon.sh stop alert-server
+/opt/dolphinscheduler/bin/dolphinscheduler-daemon.sh start alert-server
 
 
 
@@ -72,7 +72,7 @@ echo "启动alert-server"
 echo "启动nginx"
 /etc/init.d/nginx stop
 nginx &
-	
+
 
 while true
 do
--- sql/create/release-1.2.0_schema/postgresql/dolphinscheduler_dml.sql
+++ sql/create/release-1.2.0_schema/postgresql/dolphinscheduler_dml.sql
@@ -1,5 +1,5 @@
--- Records of t_escheduler_user,user : admin , password : escheduler123
-INSERT INTO "t_escheduler_user" VALUES ('1', 'admin', '055a97b5fcd6d120372ad1976518f371', '0', 'xxx@qq.com', 'xx', '0', '2018-03-27 15:48:50', '2018-10-24 17:40:22');
+-- Records of t_escheduler_user,user : admin , password : dolphinscheduler123
+INSERT INTO "t_escheduler_user" VALUES ('1', 'admin', '7ad2410b2f4c074479a8937a28a22b8f', '0', 'xxx@qq.com', 'xx', '0', '2018-03-27 15:48:50', '2018-10-24 17:40:22');
 INSERT INTO "t_escheduler_alertgroup" VALUES (1, 'escheduler管理员告警组', '0', 'escheduler管理员告警组','2018-11-29 10:20:39', '2018-11-29 10:20:39');
 INSERT INTO "t_escheduler_relation_user_alertgroup" VALUES ('1', '1', '1', '2018-11-29 10:22:33', '2018-11-29 10:22:33');
 
