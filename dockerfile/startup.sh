10f55bf8b09b1907cf00ae00e86f3b5441c054fb
qiaozhanwei <qiaozhanwei@outlook.com>
2019-11-06 15:05:13 +0800

    dockerfile optimization (#1165)
    * add ConnectionFactoryTest and ConnectionFactory read datasource from appliction.yml
    
    * .escheduler_env.sh to dolphinscheduler_env.sh
    
    * dao yml assembly to conf directory
    
    * table name modify
    
    * entity title table  name modify
    
    * logback log name modify
    
    * running through the big process
    
    * running through the big process error modify
    
    * logback log name modify
    
    * data_source.properties rename
    
    * logback log name modify
    
    * install.sh optimization
    
    * install.sh optimization
    
    * command count modify
    
    * command state update
    
    * countCommandState sql update
    
    * countCommandState sql update
    
    * remove application.yml file
    
    * master.properties modify
    
    * install.sh modify
    
    * install.sh modify
    
    * api server startup modify
    
    * the current user quits and the session is completely emptied. bug fix
    
    * remove pom package resources
    
    * checkQueueNameExist method update
    
    * checkQueueExist
    
    * install.sh error output update
    
    * signOut error update
    
    * ProcessDao is null bug fix
    
    * install.sh add mail.user
    
    * request url variables replace
    
    * process define import bug fix
    
    * process define import export bug fix
    
    * processdefine import export bug fix
    
    * down log suffix format modify
    
    * import export process define contains crontab error bug fix
    
    * add Flink local mode
    
    * ProcessDao is null bug fix
    
    * loadAverage display problem bug fix
    
    * MasterServer rename Server
    
    * rollback .env
    
    * rollback .env
    
    * MasterServer rename Server
    
    * the task is abnormal and task is running bug fix
    
    * owners and administrators can delete
    
    * dockerfile optimization
    
    * dockerfile optimization
    
    * dockerfile optimization

--- dockerfile/startup.sh
+++ dockerfile/startup.sh
@@ -5,31 +5,31 @@ if [ `netstat -anop|grep mysql|wc -l` -gt 0 ];then
                 echo "MySQL is Running."
 else
 	MYSQL_ROOT_PWD="root@123"
-        ESZ_DB="dolphinscheduler"
-	echo "启动mysql服务"
+    ESZ_DB="dolphinscheduler"
+	echo "start mysql service"
 	chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
 	find /var/lib/mysql -type f -exec touch {} \; && service mysql restart $ sleep 10
 	if [ ! -f /nohup.out ];then
-		echo "设置mysql密码"
+		echo "setting mysql password"
 		mysql --user=root --password=root -e "UPDATE mysql.user set authentication_string=password('$MYSQL_ROOT_PWD') where user='root'; FLUSH PRIVILEGES;"
 
-		echo "设置mysql权限"
+		echo "setting mysql permission"
 		mysql --user=root --password=$MYSQL_ROOT_PWD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PWD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-		echo "创建dolphinscheduler数据库"
+		echo "create dolphinscheduler database"
 		mysql --user=root --password=$MYSQL_ROOT_PWD -e "CREATE DATABASE IF NOT EXISTS \`$ESZ_DB\` CHARACTER SET utf8 COLLATE utf8_general_ci; FLUSH PRIVILEGES;"
-		echo "导入mysql数据"
+		echo "import mysql data"
 		nohup /opt/dolphinscheduler/script/create-dolphinscheduler.sh &
 		sleep 90
 	fi
 
 	if [ `mysql --user=root --password=$MYSQL_ROOT_PWD -s -r -e  "SELECT count(TABLE_NAME) FROM information_schema.TABLES WHERE TABLE_SCHEMA='dolphinscheduler';" | grep -v count` -eq 38 ];then
-		echo "\`$ESZ_DB\` 表个数正确"
+		echo "\`$ESZ_DB\` the number of tables is correct"
 	else
-		echo "\`$ESZ_DB\` 表个数不正确"
+		echo "\`$ESZ_DB\` the number of tables is incorrect"
 		mysql --user=root --password=$MYSQL_ROOT_PWD  -e "DROP DATABASE \`$ESZ_DB\`;"
-		echo "创建dolphinscheduler数据库"
+		echo "create dolphinscheduler database"
                 mysql --user=root --password=$MYSQL_ROOT_PWD -e "CREATE DATABASE IF NOT EXISTS \`$ESZ_DB\` CHARACTER SET utf8 COLLATE utf8_general_ci; FLUSH PRIVILEGES;"
-                echo "导入mysql数据"
+                echo "import mysql data"
                 nohup /opt/dolphinscheduler/script/create-dolphinscheduler.sh &
                 sleep 90
 	fi
@@ -39,29 +39,29 @@ fi
 
 sleep 90
 
-echo "启动api-server"
+echo "start api-server"
 /opt/dolphinscheduler/bin/dolphinscheduler-daemon.sh stop api-server
 /opt/dolphinscheduler/bin/dolphinscheduler-daemon.sh start api-server
 
 
 
-echo "启动master-server"
+echo "start master-server"
 /opt/dolphinscheduler/bin/dolphinscheduler-daemon.sh stop master-server
 python /opt/dolphinscheduler/script/del-zk-node.py 127.0.0.1 /dolphinscheduler/masters
 /opt/dolphinscheduler/bin/dolphinscheduler-daemon.sh start master-server
 
-echo "启动worker-server"
+echo "start worker-server"
 /opt/dolphinscheduler/bin/dolphinscheduler-daemon.sh stop worker-server
 python /opt/dolphinscheduler/script/del-zk-node.py 127.0.0.1 /dolphinscheduler/workers
 /opt/dolphinscheduler/bin/dolphinscheduler-daemon.sh start worker-server
 
 
-echo "启动logger-server"
+echo "start logger-server"
 /opt/dolphinscheduler/bin/dolphinscheduler-daemon.sh stop logger-server
 /opt/dolphinscheduler/bin/dolphinscheduler-daemon.sh start logger-server
 
 
-echo "启动alert-server"
+echo "start alert-server"
 /opt/dolphinscheduler/bin/dolphinscheduler-daemon.sh stop alert-server
 /opt/dolphinscheduler/bin/dolphinscheduler-daemon.sh start alert-server
 
@@ -69,7 +69,7 @@ echo "启动alert-server"
 
 
 
-echo "启动nginx"
+echo "start nginx"
 /etc/init.d/nginx stop
 nginx &
 
