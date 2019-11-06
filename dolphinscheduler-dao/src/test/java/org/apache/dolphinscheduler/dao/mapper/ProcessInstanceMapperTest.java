625442bdb2a3780006ddfa1fc9af458e94bfe00b
Tboy <technoboy@yeah.net>
2019-11-06 19:14:37 +0800

    fix api server debug mode bug (#1157)
    * move updateTaskState into try/catch block in case of exception
    
    * fix NPE
    
    * using conf.getInt instead of getString
    
    * for AbstractZKClient, remove the log, for it will print the same log message in createZNodePath.
    for AlertDao, correct the spelling.
    
    * duplicate
    
    * refactor getTaskWorkerGroupId
    
    * add friendly log
    
    * update hearbeat thread num = 1
    
    * fix the bug when worker execute task using queue. and remove checking Tenant user anymore in TaskScheduleThread
    
    * 1. move verifyTaskInstanceIsNull after taskInstance
    2. keep verifyTenantIsNull/verifyTaskInstanceIsNull clean and readable
    
    * fix the message
    
    * delete before check to avoid KeeperException$NoNodeException
    
    * fix the message
    
    * check processInstance state before delete tenant
    
    * check processInstance state before delete worker group
    
    * refactor
    
    * merge api constants into common constatns
    
    * update the resource perm
    
    * update the dataSource perm
    
    * fix CheckUtils.checkUserParams method
    
    * update AlertGroupService, extends from BaseService, remove duplicate methods
    
    * refactor
    
    * modify method name
    
    * add hasProjectAndPerm method
    
    * using checkProject instead of getResultStatus
    
    * delete checkAuth method, using hasProjectAndPerm instead.
    
    * correct spelling
    
    * add transactional for deleteWorkerGroupById
    
    * add Transactional for deleteProcessInstanceById method
    
    * change sqlSessionTemplate singleton
    
    * change sqlSessionTemplate singleton and reformat code
    
    * fix unsuitable error message
    
    * update shutdownhook methods
    
    * fix worker log bug
    
    * fix api server debug mode bug
    
    * upgrade zk version

--- dolphinscheduler-dao/src/test/java/org/apache/dolphinscheduler/dao/mapper/ProcessInstanceMapperTest.java
+++ dolphinscheduler-dao/src/test/java/org/apache/dolphinscheduler/dao/mapper/ProcessInstanceMapperTest.java
@@ -26,7 +26,6 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mortbay.jetty.servlet.AbstractSessionIdManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
