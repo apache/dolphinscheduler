0ad41d9f86bd0720f47c01997b7deebe8109606c
Tboy <technoboy@yeah.net>
2019-11-08 17:38:00 +0800

    add Transactional for createSession method (#1186)
    * move updateTaskState into try/catch block in case of exception
    
    * fix NPE
    
    * using conf.getInt instead of getString
    
    * for AbstractZKClient, remove the log, for it will print the same log message in createZNodePath.
    for AlertDao, correct the spelling.
    
    * duplicate
    
    * refactor getTaskWorkerGroupId
    
    * add friendly log
    
    * update hearbeat thread num = 1
    
    * fix the bug when worker execute task using queue. and remove checking Tenant user anymore in TaskScheduleThread
    
    * 1. move verifyTaskInstanceIsNull after taskInstance
    2. keep verifyTenantIsNull/verifyTaskInstanceIsNull clean and readable
    
    * fix the message
    
    * delete before check to avoid KeeperException$NoNodeException
    
    * fix the message
    
    * check processInstance state before delete tenant
    
    * check processInstance state before delete worker group
    
    * refactor
    
    * merge api constants into common constatns
    
    * update the resource perm
    
    * update the dataSource perm
    
    * fix CheckUtils.checkUserParams method
    
    * update AlertGroupService, extends from BaseService, remove duplicate methods
    
    * refactor
    
    * modify method name
    
    * add hasProjectAndPerm method
    
    * using checkProject instead of getResultStatus
    
    * delete checkAuth method, using hasProjectAndPerm instead.
    
    * correct spelling
    
    * add transactional for deleteWorkerGroupById
    
    * add Transactional for deleteProcessInstanceById method
    
    * change sqlSessionTemplate singleton
    
    * change sqlSessionTemplate singleton and reformat code
    
    * fix unsuitable error message
    
    * update shutdownhook methods
    
    * fix worker log bug
    
    * fix api server debug mode bug
    
    * upgrade zk version
    
    * delete this line ,for zkClient.close() will do the whole thing
    
    * fix master server shutdown error
    
    * degrade zk version and add FourLetterWordMain class
    
    * fix PathChildrenCache not close
    
    * add Transactional for createSession method

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/SessionService.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/SessionService.java
@@ -28,6 +28,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -80,6 +81,7 @@ public class SessionService extends BaseService{
    * @param ip
    * @return
    */
+  @Transactional(rollbackFor = Exception.class)
   public String createSession(User user, String ip) {
     Session session = null;
 
