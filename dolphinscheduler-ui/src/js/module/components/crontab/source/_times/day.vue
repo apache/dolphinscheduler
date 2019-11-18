40a4d6e6ea7af7b0562566bd816d4e82e466d154
break60 <790061044@qq.com>
2019-11-18 16:59:44 +0800

    Modify crontab style

--- dolphinscheduler-ui/src/js/module/components/crontab/source/_times/day.vue
+++ dolphinscheduler-ui/src/js/module/components/crontab/source/_times/day.vue
@@ -516,6 +516,11 @@
 
 <style lang="scss" rel="stylesheet/scss">
   .day-model {
-
+    .ans-radio-group-vertical {
+      .ans-radio-wrapper {
+        margin: 5px 0;
+        display: inline-block
+      }
+    }
   }
 </style>
