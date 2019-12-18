a024b448f6b8b1512fa3b6d78fa245330d23c87c
break60 <790061044@qq.com>
2019-12-18 17:38:26 +0800

    webpack upgrade to 4

--- dolphinscheduler-ui/build/webpack.config.prod.js
+++ dolphinscheduler-ui/build/webpack.config.prod.js
@@ -19,10 +19,9 @@ const webpack = require('webpack')
 const merge = require('webpack-merge')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const { baseConfig } = require('./config')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const ProgressPlugin = require('progress-bar-webpack-plugin')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 const resolve = dir =>
   path.resolve(__dirname, '..', dir)
@@ -32,100 +31,10 @@ const config = merge.smart(baseConfig, {
   output: {
     filename: 'js/[name].[chunkhash:7].js'
   },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          hotReload: false // Open hot overload
-        }
-      },
-      {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract({
-          use: [
-            'css-loader',
-            {
-              loader: 'postcss-loader',
-              options: {
-                plugins: (loader) => [
-                  require('autoprefixer')({
-                    overrideBrowserslist: [
-                      "Android 4.1",
-                      "iOS 7.1",
-                      "Chrome > 31",
-                      "ff > 31",
-                      "ie >= 8"
-                    ]      
-                  }),
-                  require('cssnano')
-                ]
-              }
-            }
-          ],
-          fallback: ['vue-style-loader']
-        })
-      },
-      {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract({
-          use: [
-            'css-loader',
-            'sass-loader',
-            {
-              loader: 'postcss-loader',
-              options: {
-                plugins: (loader) => [
-                  require('autoprefixer')({
-                    overrideBrowserslist: [
-                      "Android 4.1",
-                      "iOS 7.1",
-                      "Chrome > 31",
-                      "ff > 31",
-                      "ie >= 8"
-                    ]
-                  }),
-                  require('cssnano')
-                ]
-              }
-            }
-          ],
-          fallback: ['vue-style-loader']
-        })
-      }
-    ]
-  },
   plugins: [
     new ProgressPlugin(),
-    new ExtractTextPlugin({ filename: 'css/[name].[contenthash:7].css', allChunks: true }),
-    new webpack.optimize.CommonsChunkPlugin({ name: 'common', filename: 'js/[name].[hash:7].js' }),
+    new MiniCssExtractPlugin({ filename: 'css/[name].[contenthash:7].css' }),
     new webpack.optimize.OccurrenceOrderPlugin(),
-    new OptimizeCssAssetsPlugin({
-      assetNameRegExp: /\.css$/g,
-      cssProcessor: require('cssnano'),
-      cssProcessorOptions: { discardComments: { removeAll: true } },
-      canPrint: true
-    }),
-    new UglifyJSPlugin({
-      parallel: true,
-      sourceMap: true,
-      uglifyOptions: {
-        compress: {
-          warnings: false,
-          drop_debugger: true,
-          drop_console: true,
-          pure_funcs: ['console.log']// remove console
-        },
-        comments: function (n, c) {
-          /*! IMPORTANT: Please preserve 3rd-party library license info, inspired from @allex/amd-build-worker/config/jsplumb.js */
-          var text = c.value, type = c.type
-          if (type === 'comment2') {
-            return /^!|@preserve|@license|@cc_on|MIT/i.test(text)
-          }
-        }
-      }
-    }),
     new CopyWebpackPlugin([
       {
         from: resolve('src/lib'),
@@ -136,7 +45,34 @@ const config = merge.smart(baseConfig, {
         to: resolve('dist/images')
       },
     ]),
-  ]
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            warnings: false,
+            drop_console: true,
+            drop_debugger: true,
+            pure_funcs: ['console.log']
+          }
+        },
+        cache: true,
+        parallel: true,
+        sourceMap: false,
+        exclude: /node_modules/,
+        extractComments: (astNode, comment) => {
+          if (/^!|@preserve|@license|@cc_on|MIT/i.test(comment.value)) {
+            return true
+          }
+          return false
+        }
+      }),
+    ],
+    sideEffects: true
+  },
+  mode: 'production'
 })
 
 module.exports = config
