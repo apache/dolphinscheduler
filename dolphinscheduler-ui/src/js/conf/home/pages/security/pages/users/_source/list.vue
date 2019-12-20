70a7c8d55d0a657a17418e86398c8db6be022d66
break60 <790061044@qq.com>
2019-12-20 11:48:31 +0800

    Remove iconfont, awesome, replace with ans-ui icon

--- dolphinscheduler-ui/src/js/conf/home/pages/security/pages/users/_source/list.vue
+++ dolphinscheduler-ui/src/js/conf/home/pages/security/pages/users/_source/list.vue
@@ -91,11 +91,11 @@
                 <a href="javascript:" @click="_authUdfFunc(item,$index)">{{$t('UDF Function')}}</a>
               </div>
               <template slot="reference">
-                <x-button type="warning" shape="circle" size="xsmall" data-toggle="tooltip" :title="$t('Authorize')" icon="iconfont icon-yonghu1" :disabled="item.userType === 'ADMIN_USER'"></x-button>
+                <x-button type="warning" shape="circle" size="xsmall" data-toggle="tooltip" :title="$t('Authorize')" icon="ans-icon-user-empty" :disabled="item.userType === 'ADMIN_USER'"></x-button>
               </template>
             </x-poptip>
 
-            <x-button type="info" shape="circle" size="xsmall" data-toggle="tooltip" icon="iconfont icon-bianjixiugai" :title="$t('Edit')" @click="_edit(item)">
+            <x-button type="info" shape="circle" size="xsmall" data-toggle="tooltip" icon="ans-icon-edit" :title="$t('Edit')" @click="_edit(item)">
             </x-button>
             <x-poptip
                     :ref="'poptip-delete-' + $index"
@@ -114,7 +114,7 @@
                         data-toggle="tooltip"
                         :title="$t('delete')"
                         :disabled="item.userType === 'ADMIN_USER'"
-                        icon="iconfont icon-shanchu">
+                        icon="ans-icon-trash">
                 </x-button>
               </template>
             </x-poptip>
