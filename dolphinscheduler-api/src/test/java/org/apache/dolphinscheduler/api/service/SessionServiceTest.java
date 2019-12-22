011d72e86d8e53320c432c649a4b99046ee10348
lilin <samz406@foxmail.com>
2019-12-22 14:58:38 +0800

    update SessionServiceTest

--- dolphinscheduler-api/src/test/java/org/apache/dolphinscheduler/api/service/SessionServiceTest.java
+++ dolphinscheduler-api/src/test/java/org/apache/dolphinscheduler/api/service/SessionServiceTest.java
@@ -16,17 +16,24 @@
  */
 package org.apache.dolphinscheduler.api.service;
 
-import org.apache.dolphinscheduler.api.ApiApplicationServer;
-import org.apache.dolphinscheduler.common.enums.UserType;
-import org.apache.dolphinscheduler.dao.entity.User;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.dolphinscheduler.api.ApiApplicationServer;
+import org.apache.dolphinscheduler.common.Constants;
+import org.apache.dolphinscheduler.common.enums.UserType;
+import org.apache.dolphinscheduler.dao.entity.Session;
+import org.apache.dolphinscheduler.dao.entity.User;
+import org.apache.dolphinscheduler.dao.mapper.SessionMapper;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockCookie;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -38,15 +45,104 @@ public class SessionServiceTest {
     @Autowired
     private SessionService sessionService;
 
+    @Autowired
+    private SessionMapper sessionMapper;
+
+    @Before
+    public void setUp() {
+        removeSession();
+    }
+
+
+    @After
+    public void after(){
+
+        removeSession();
+    }
+
+    /**
+     * create session
+     */
     @Test
-    public void createSession(){
+    public void testGetSession(){
 
-        User loginUser = new User();
-        loginUser.setId(1);
-        loginUser.setUserType(UserType.GENERAL_USER);
+        //add session
+        String sessionId = getSessionId();
+        // get sessionId from  header
+        MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
+        mockHttpServletRequest.addHeader(Constants.SESSION_ID,sessionId);
+        mockHttpServletRequest.addHeader("HTTP_X_FORWARDED_FOR","127.0.0.1");
+        //query
+        Session session = sessionService.getSession(mockHttpServletRequest);
+        Assert.assertNotNull(session);
+        logger.info("session ip {}",session.getIp());
 
-        String session = sessionService.createSession(loginUser, "127.0.0.1");
-        Assert.assertTrue(StringUtils.isNotEmpty(session));
+        // get sessionId from cookie
+        mockHttpServletRequest = new MockHttpServletRequest();
+        mockHttpServletRequest.addHeader("HTTP_X_FORWARDED_FOR","127.0.0.1");
+        MockCookie mockCookie = new MockCookie(Constants.SESSION_ID,sessionId);
+        mockHttpServletRequest.setCookies(mockCookie);
+        //query
+        session = sessionService.getSession(mockHttpServletRequest);
+        Assert.assertNotNull(session);
+        logger.info("session ip {}",session.getIp());
+        Assert.assertEquals(session.getIp(),"127.0.0.1");
 
+
+    }
+
+    /**
+     * create session
+     */
+    @Test
+    public void testCreateSession(){
+
+        String ip = "127.0.0.1";
+        User user = new User();
+        user.setUserType(UserType.GENERAL_USER);
+        user.setId(88888888);
+        String sessionId = sessionService.createSession(user, ip);
+        logger.info("createSessionId is "+sessionId);
+        Assert.assertTrue(StringUtils.isNotEmpty(sessionId));
+    }
+    /**
+     * sign out
+     * remove ip restrictions
+     */
+    @Test
+    public void testSignOut(){
+
+        int userId = 88888888;
+        String ip = "127.0.0.1";
+        //create session
+        getSessionId();
+        User user = new User();
+        user.setId(userId);
+        //signOut
+        sessionService.signOut(ip ,user);
+        //check exist
+        Session session = sessionMapper.queryByUserIdAndIp(userId, ip);
+        Assert.assertNull(session);
+    }
+
+
+
+    private String getSessionId(){
+
+        String ip = "127.0.0.1";
+        User user = new User();
+        user.setId(88888888);
+        return sessionService.createSession(user, ip);
+    }
+
+    /**
+     * remove  session
+     */
+    private void removeSession(){
+
+        String ip = "127.0.0.1";
+        User user = new User();
+        user.setId(88888888);
+        sessionService.signOut( ip,user);
     }
 }
