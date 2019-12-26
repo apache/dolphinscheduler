5d7fddf3a614b9eb11c7e7453785acfec183c312
zjc <zhaojinchao@ricebook.com>
2019-12-26 16:34:35 +0800

    remove

--- dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/entity/Command.java
+++ dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/entity/Command.java
@@ -20,14 +20,10 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import lombok.Data;
 import org.apache.dolphinscheduler.common.enums.*;
 
 import java.util.Date;
 
-/**
- * command
- */
 @TableName("t_ds_command")
 public class Command {
     @TableId(value = "id", type = IdType.AUTO)
