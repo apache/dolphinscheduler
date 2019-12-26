04c7aa123dd44f21ccd2bfde46eefa16fcb3eac2
chenxingchun <438044805@qq.com>
2019-12-26 09:27:58 +0800

    common/parameterUtils unit test

--- dolphinscheduler-common/src/test/java/org/apache/dolphinscheduler/common/utils/ParameterUtilsTest.java
+++ dolphinscheduler-common/src/test/java/org/apache/dolphinscheduler/common/utils/ParameterUtilsTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.dolphinscheduler.common.utils;
 
 import com.alibaba.fastjson.JSONObject;
@@ -11,17 +27,13 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.util.*;
-
 import static org.apache.dolphinscheduler.common.Constants.PARAMETER_FORMAT_TIME;
 import static org.apache.dolphinscheduler.common.utils.placeholder.TimePlaceholderUtils.replacePlaceholders;
 
 
 public class ParameterUtilsTest {
-
     public static final Logger logger = LoggerFactory.getLogger(ParameterUtilsTest.class);
-
 
     /**
      * Test convertParameterPlaceholders
@@ -51,11 +63,7 @@ public class ParameterUtilsTest {
         // replace time $[...] form, eg. $[yyyyMMdd]
         Date cronTimeStr = DateUtils.parseDate("20191220145900", new String[]{PARAMETER_FORMAT_TIME});
         Assert.assertEquals(parameterString, replacePlaceholders(parameterString, cronTimeStr, true));
-
-
     }
-
-
 
     /**
      * Test curingGlobalParams
@@ -104,11 +112,9 @@ public class ParameterUtilsTest {
         globalParamList.add(property3);
         globalParamList.add(property4);
 
-
         String result5 = ParameterUtils.curingGlobalParams(globalParamMap, globalParamList, CommandType.START_CURRENT_TASK_PROCESS, scheduleTime);
         Assert.assertEquals(result5,JSONUtils.toJsonString(globalParamList));
     }
-
 
     /**
      * Test handleEscapes
