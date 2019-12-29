779decc96dd2e832f9cd14b8a0f44b483080683a
break60 <790061044@qq.com>
2019-12-29 10:23:10 +0800

    Increase script text box to zoom in and fix log loading #1547 、#1247 (#1604)
    * Increase script text box to zoom in and fix log loading
    
    * Timing operation adds default value for start and end dates

--- dolphinscheduler-ui/src/js/conf/home/pages/dag/_source/formModel/log.vue
+++ dolphinscheduler-ui/src/js/conf/home/pages/dag/_source/formModel/log.vue
@@ -257,8 +257,8 @@
       _rtParam () {
         return {
           taskInstId: this.stateId || this.logId,
-          skipLineNum: parseInt(`${this.loadingIndex ? this.loadingIndex + '0000' : 0}`),
-          limit: parseInt(`${this.loadingIndex ? this.loadingIndex + 1 : 1}0000`)
+          skipLineNum: parseInt(`${this.loadingIndex ? this.loadingIndex + '000' : 0}`),
+          limit: parseInt(`${this.loadingIndex ? this.loadingIndex + 1 : 1}000`)
         }
       }
     },
@@ -316,7 +316,7 @@
           }
           .refresh-log {
             >i {
-              font-size: 24px;
+              font-size: 20px;
               vertical-align: middle;
               transform: scale(1);
             }
@@ -353,7 +353,7 @@
             font-weight: bold;
             resize:none;
             line-height: 1.6;
-            padding: 6px;
+            padding: 0px;
           }
         }
       }
