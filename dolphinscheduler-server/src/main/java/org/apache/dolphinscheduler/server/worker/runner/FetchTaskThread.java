c37fa16f278ad9bf41dfa117abe7a32af0d8fd7f
gabry.wu <wu_shao_jie@qq.com>
2020-01-07 17:35:38 +0800

    why not invoke checkWorkerGroup in advance  (#1747)
    * issue https://github.com/apache/incubator-dolphinscheduler/issues/1743
    
    * issue https://github.com/apache/incubator-dolphinscheduler/issues/1743

--- dolphinscheduler-server/src/main/java/org/apache/dolphinscheduler/server/worker/runner/FetchTaskThread.java
+++ dolphinscheduler-server/src/main/java/org/apache/dolphinscheduler/server/worker/runner/FetchTaskThread.java
@@ -186,6 +186,10 @@ public class FetchTaskThread implements Runnable{
                         continue;
                     }
 
+                    if(!checkWorkerGroup(taskInstance, OSUtils.getHost())){
+                        continue;
+                    }
+
                     Tenant tenant = processDao.getTenantForProcess(taskInstance.getProcessInstance().getTenantId(),
                             taskInstance.getProcessDefine().getUserId());
 
@@ -202,11 +206,6 @@ public class FetchTaskThread implements Runnable{
                     taskInstance.getProcessInstance().setTenantCode(tenant.getTenantCode());
 
                     logger.info("worker fetch taskId : {} from queue ", taskInstId);
-
-
-                    if(!checkWorkerGroup(taskInstance, OSUtils.getHost())){
-                        continue;
-                    }
 
                     // local execute path
                     String execLocalPath = getExecLocalPath();
