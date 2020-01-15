d02422d030b46e61238224ae328241b27c98cd15
break60 <790061044@qq.com>
2020-01-15 13:55:25 +0800

    Requests the previous page when there is no data on the list page(#1768) (#1832)
    * fix udfs assignment and task instance page jump #1789
    
    * jest add coverage display
    
    * Fix data echo, style, popup cannot be closed and license
    
    * Fix data echo, style, popup cannot be closed and license
    
    * fix
    
    * Requests the previous page when there is no data on the list page

--- dolphinscheduler-ui/src/js/conf/home/pages/datasource/pages/list/index.vue
+++ dolphinscheduler-ui/src/js/conf/home/pages/datasource/pages/list/index.vue
@@ -124,10 +124,14 @@
       _getList (flag) {
         this.isLoading = !flag
         this.getDatasourcesListP(this.searchParams).then(res => {
-          this.datasourcesList = []
-          this.datasourcesList = res.totalList
-          this.total = res.total
-          this.isLoading = false
+          if(this.searchParams.pageNo>1 && res.totalList.length == 0) {
+            this.searchParams.pageNo = this.searchParams.pageNo -1
+          } else {
+            this.datasourcesList = []
+            this.datasourcesList = res.totalList
+            this.total = res.total
+            this.isLoading = false
+          }
         }).catch(e => {
           this.isLoading = false
         })
