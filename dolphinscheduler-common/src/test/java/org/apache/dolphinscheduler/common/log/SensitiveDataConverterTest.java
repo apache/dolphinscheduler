7729e0b6b0d0f5aba7542de6533ffeac5e0a61c3
dailidong <dailidong66@gmail.com>
2020-02-06 00:22:21 +0800

    add UT

--- dolphinscheduler-common/src/test/java/org/apache/dolphinscheduler/common/log/SensitiveDataConverterTest.java
+++ dolphinscheduler-common/src/test/java/org/apache/dolphinscheduler/common/log/SensitiveDataConverterTest.java
@@ -17,13 +17,19 @@
 package org.apache.dolphinscheduler.common.log;
 
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.spi.IThrowableProxy;
+import ch.qos.logback.classic.spi.LoggerContextVO;
 import org.apache.dolphinscheduler.common.Constants;
 import org.apache.dolphinscheduler.common.utils.SensitiveLogUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
 
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -36,26 +42,106 @@ public class SensitiveDataConverterTest {
      */
     private final Pattern pwdPattern = Pattern.compile(Constants.DATASOURCE_PASSWORD_REGEX);
 
+    private final String logMsg = "{\"address\":\"jdbc:mysql://192.168.xx.xx:3306\"," +
+            "\"database\":\"carbond\"," +
+            "\"jdbcUrl\":\"jdbc:mysql://192.168.xx.xx:3306/ods\"," +
+            "\"user\":\"view\"," +
+            "\"password\":\"view1\"}";
+
+    private final String maskLogMsg = "{\"address\":\"jdbc:mysql://192.168.xx.xx:3306\"," +
+            "\"database\":\"carbond\"," +
+            "\"jdbcUrl\":\"jdbc:mysql://192.168.xx.xx:3306/ods\"," +
+            "\"user\":\"view\"," +
+            "\"password\":\"******\"}";
+    @Test
+    public void convert() {
+        SensitiveDataConverter sensitiveDataConverter = new SensitiveDataConverter();
+        String result = sensitiveDataConverter.convert(new ILoggingEvent() {
+            @Override
+            public String getThreadName() {
+                return null;
+            }
+
+            @Override
+            public Level getLevel() {
+                return Level.INFO;
+            }
+
+            @Override
+            public String getMessage() {
+                return null;
+            }
+
+            @Override
+            public Object[] getArgumentArray() {
+                return new Object[0];
+            }
+
+            @Override
+            public String getFormattedMessage() {
+                return logMsg;
+            }
+
+            @Override
+            public String getLoggerName() {
+                return null;
+            }
+
+            @Override
+            public LoggerContextVO getLoggerContextVO() {
+                return null;
+            }
+
+            @Override
+            public IThrowableProxy getThrowableProxy() {
+                return null;
+            }
+
+            @Override
+            public StackTraceElement[] getCallerData() {
+                return new StackTraceElement[0];
+            }
+
+            @Override
+            public boolean hasCallerData() {
+                return false;
+            }
+
+            @Override
+            public Marker getMarker() {
+                return null;
+            }
+
+            @Override
+            public Map<String, String> getMDCPropertyMap() {
+                return null;
+            }
+
+            @Override
+            public Map<String, String> getMdc() {
+                return null;
+            }
+
+            @Override
+            public long getTimeStamp() {
+                return 0;
+            }
+
+            @Override
+            public void prepareForDeferredProcessing() {
+
+            }
+        });
+
+        Assert.assertEquals(maskLogMsg, passwordHandler(pwdPattern, logMsg));
+
+    }
 
     /**
      * mask sensitive logMsg - sql task datasource password
      */
     @Test
     public void testPwdLogMsgConverter() {
-
-        String logMsg = "{\"address\":\"jdbc:mysql://192.168.xx.xx:3306\"," +
-                "\"database\":\"carbond\"," +
-                "\"jdbcUrl\":\"jdbc:mysql://192.168.xx.xx:3306/ods\"," +
-                "\"user\":\"view\"," +
-                "\"password\":\"view1\"}";
-
-        String maskLogMsg = "{\"address\":\"jdbc:mysql://192.168.xx.xx:3306\"," +
-                "\"database\":\"carbond\"," +
-                "\"jdbcUrl\":\"jdbc:mysql://192.168.xx.xx:3306/ods\"," +
-                "\"user\":\"view\"," +
-                "\"password\":\"******\"}";
-
-
         logger.info("parameter : {}", logMsg);
         logger.info("parameter : {}", passwordHandler(pwdPattern, logMsg));
 
@@ -87,6 +173,7 @@ public class SensitiveDataConverterTest {
 
         return sb.toString();
     }
+
 
 
 }
