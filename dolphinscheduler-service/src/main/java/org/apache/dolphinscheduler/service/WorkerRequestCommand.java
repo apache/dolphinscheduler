3fc6026898710147dcd54d56d218f8716ed25de8
qiaozhanwei <qiaozhanwei@outlook.com>
2020-02-10 18:01:56 +0800

    1，remove dolphinscheduler-rpc module  2，add dolphinscheduler-remote module 3，add dolphinscheduler-service module 4，refactor LoggerServer module (#1925)
    * 1，remove dolphinscheduler-rpc module
    2，add dolphinscheduler-remote module
    3，add dolphinscheduler-service module
    4，refactor LoggerServer module
    
    * ProcessUtils modify

---
+++ dolphinscheduler-service/src/main/java/org/apache/dolphinscheduler/service/WorkerRequestCommand.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.dolphinscheduler.service;
+
+import org.apache.dolphinscheduler.remote.command.Command;
+import org.apache.dolphinscheduler.remote.command.CommandType;
+import org.apache.dolphinscheduler.remote.utils.FastJsonSerializer;
+
+import java.io.Serializable;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ *  view log request command
+ */
+public class WorkerRequestCommand implements Serializable {
+
+    private static final AtomicLong REQUEST = new AtomicLong(1);
+
+    private String path;
+
+    public WorkerRequestCommand() {
+    }
+
+    public WorkerRequestCommand(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public Command convert2Command(){
+        Command command = new Command(REQUEST.getAndIncrement());
+        command.setType(CommandType.WORKER_REQUEST);
+        byte[] body = FastJsonSerializer.serialize(this);
+        command.setBody(body);
+        return command;
+    }
+}
