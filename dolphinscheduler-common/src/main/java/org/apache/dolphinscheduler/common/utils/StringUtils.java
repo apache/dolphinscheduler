c447bb489f5da45ca234a236551474778b24f2e3
khadgarmage <khadgar.mage@outlook.com>
2020-02-10 13:52:32 +0800

    add stringutils ut (#1921)
    * add stringutils ut

--- dolphinscheduler-common/src/main/java/org/apache/dolphinscheduler/common/utils/StringUtils.java
+++ dolphinscheduler-common/src/main/java/org/apache/dolphinscheduler/common/utils/StringUtils.java
@@ -16,17 +16,7 @@
  */
 package org.apache.dolphinscheduler.common.utils;
 
-
-import java.nio.charset.StandardCharsets;
-import java.util.Iterator;
-import java.util.Objects;
-import java.util.regex.Pattern;
-
-
 public class StringUtils {
-
-    public static final int INDEX_NOT_FOUND = -1;
-
     public static final String EMPTY = "";
 
     public static boolean isEmpty(final CharSequence cs) {
@@ -37,119 +27,14 @@ public class StringUtils {
         return !isEmpty(cs);
     }
 
-    public static boolean isBlank(CharSequence cs){
-        int strLen;
-        if (cs == null || (strLen = cs.length()) == 0) {
+    public static boolean isBlank(String s){
+        if (isEmpty(s)) {
             return true;
         }
-        for (int i = 0; i < strLen; i++) {
-            if (Character.isWhitespace(cs.charAt(i)) == false) {
-                return false;
-            }
-        }
-        return true;
+        return s.trim().length() == 0;
     }
 
-    public static boolean isNotBlank(CharSequence str){
-        return !isBlank(str);
-    }
-
-    public static String substringBefore(final String str, final String separator) {
-        if (isBlank(str) || separator == null) {
-            return str;
-        }
-        if (separator.isEmpty()) {
-            return EMPTY;
-        }
-        final int pos = str.indexOf(separator);
-        if (pos == INDEX_NOT_FOUND) {
-            return str;
-        }
-        return str.substring(0, pos);
-    }
-
-    public static String substringAfter(final String str, final String separator) {
-        if (isBlank(str)) {
-            return str;
-        }
-        if (separator == null) {
-            return EMPTY;
-        }
-        final int pos = str.indexOf(separator);
-        if (pos == INDEX_NOT_FOUND) {
-            return EMPTY;
-        }
-        return str.substring(pos + separator.length());
-    }
-
-    public static String substringAfterLast(final String str, final String separator) {
-        if (isEmpty(str)) {
-            return str;
-        }
-        if (isEmpty(separator)) {
-            return EMPTY;
-        }
-        final int pos = str.lastIndexOf(separator);
-        if (pos == INDEX_NOT_FOUND || pos == str.length() - separator.length()) {
-            return EMPTY;
-        }
-        return str.substring(pos + separator.length());
-    }
-
-    public static String getUtf8String(byte[] bytes){
-        return new String(bytes, StandardCharsets.UTF_8);
-    }
-
-    public static byte[] getUtf8Bytes(String str){
-        return str.getBytes(StandardCharsets.UTF_8);
-    }
-
-    public static boolean hasChinese(String str) {
-        if (str == null) {
-            return false;
-        }
-        Pattern pattern = Pattern.compile("[\\u4E00-\\u9FBF]+");
-        return pattern.matcher(str).find();
-    }
-
-    public static boolean hasSpace(String str) {
-        if (str == null) {
-            return false;
-        }
-        int len = str.length();
-        for (int i = 0; i < len; i++) {
-            if (str.charAt(i) == ' ') {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static String join(final Iterable<?> iterable, final String separator){
-        Iterator<?> iterator = iterable.iterator();
-        if (iterator == null) {
-            return null;
-        }
-        if (!iterator.hasNext()) {
-            return EMPTY;
-        }
-        final Object first = iterator.next();
-        if (!iterable.iterator().hasNext()) {
-            return Objects.toString(first, "");
-        }
-        final StringBuilder buf = new StringBuilder(64);
-        if (first != null) {
-            buf.append(first);
-        }
-        while (iterator.hasNext()) {
-            if (separator != null) {
-                buf.append(separator);
-            }
-            final Object obj = iterator.next();
-            if (obj != null) {
-                buf.append(obj);
-            }
-        }
-        return buf.toString();
+    public static boolean isNotBlank(String s){
+        return !isBlank(s);
     }
 }
