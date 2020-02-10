1f11ea4a87456cf64889f79126982a99b7cf5df1
Yeleights <amarantine@my.com>
2020-02-10 14:19:40 +0800

    modify mysql 1.2.2 ddl.sql&dml.sql

--- sql/upgrade/1.2.2_schema/mysql/dolphinscheduler_dml.sql
+++ sql/upgrade/1.2.2_schema/mysql/dolphinscheduler_dml.sql
@@ -14,12 +14,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
 */
-SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
-SET FOREIGN_KEY_CHECKS=0;
-UPDATE QRTZ_CRON_TRIGGERS SET SCHED_NAME='DolphinScheduler' WHERE SCHED_NAME='EasyScheduler';
-UPDATE QRTZ_TRIGGERS SET SCHED_NAME='DolphinScheduler' WHERE SCHED_NAME='EasyScheduler';
-UPDATE QRTZ_FIRED_TRIGGERS SET SCHED_NAME='DolphinScheduler' WHERE SCHED_NAME='EasyScheduler';
-UPDATE QRTZ_JOB_DETAILS SET SCHED_NAME='DolphinScheduler' WHERE SCHED_NAME='EasyScheduler';
-UPDATE QRTZ_JOB_DETAILS SET JOB_CLASS_NAME='org.apache.dolphinscheduler.dao.quartz.ProcessScheduleJob' WHERE JOB_CLASS_NAME='cn.escheduler.server.quartz.ProcessScheduleJob';
-UPDATE QRTZ_LOCKS SET SCHED_NAME='DolphinScheduler' WHERE SCHED_NAME='EasyScheduler';
-UPDATE QRTZ_SCHEDULER_STATE SET SCHED_NAME='DolphinScheduler' WHERE SCHED_NAME='EasyScheduler';
