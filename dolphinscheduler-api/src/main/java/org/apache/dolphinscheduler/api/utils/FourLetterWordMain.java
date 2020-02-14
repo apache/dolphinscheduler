f288f229516a030582fa9bb23ba7c4cb61512ac9
Jave-Chen <baicai.chen@gmail.com>
2020-02-14 10:39:02 +0800

    Fix socket bug: read before sock connect. Add UT for FourLetterWordMain. (#1954)
    * Fix socket bug: read before sock connect. Add UT for FourLetterWordMain.
    
    * Fix code smell
    
       Remove  @Ignore and  @Test from testCmd().
    
    * correct comment

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/utils/FourLetterWordMain.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/utils/FourLetterWordMain.java
@@ -17,7 +17,6 @@
 
 package org.apache.dolphinscheduler.api.utils;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,12 +28,16 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
-
+import java.util.Objects;
 
 public class FourLetterWordMain {
 
     private static final int DEFAULT_SOCKET_TIMEOUT = 5000;
     protected static final Logger LOG = LoggerFactory.getLogger(FourLetterWordMain.class);
+
+    private FourLetterWordMain() {
+        throw new IllegalStateException("FourLetterWordMain class");
+    }
 
     /**
      * Send the 4letterword
@@ -48,6 +51,7 @@ public class FourLetterWordMain {
             throws IOException {
         return send4LetterWord(host, port, cmd, DEFAULT_SOCKET_TIMEOUT);
     }
+
     /**
      * Send the 4letterword
      * @param host the destination host
@@ -59,30 +63,33 @@ public class FourLetterWordMain {
      */
     public static String send4LetterWord(String host, int port, String cmd, int timeout)
             throws IOException {
+        Objects.requireNonNull(cmd, "cmd must not be null");
         LOG.info("connecting to {} {}", host, port);
         InetSocketAddress hostaddress= host != null ? new InetSocketAddress(host, port) :
                 new InetSocketAddress(InetAddress.getByName(null), port);
         
-        try (Socket sock = new Socket();
-             OutputStream outstream = sock.getOutputStream();
-             BufferedReader reader =
-                    new BufferedReader(
-                            new InputStreamReader(sock.getInputStream()))) {
+        try (Socket sock = new Socket()) {
             sock.setSoTimeout(timeout);
             sock.connect(hostaddress, timeout);
+            OutputStream outstream = sock.getOutputStream();
             outstream.write(cmd.getBytes());
             outstream.flush();
             // this replicates NC - close the output stream before reading
             sock.shutdownOutput();
 
-            StringBuilder sb = new StringBuilder();
-            String line;
-            while((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
+            try (BufferedReader reader =
+                        new BufferedReader(
+                            new InputStreamReader(sock.getInputStream()))) {
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+                return sb.toString();
             }
-            return sb.toString();
         } catch (SocketTimeoutException e) {
             throw new IOException("Exception while executing four letter word: " + cmd, e);
         }
     }
+
 }
