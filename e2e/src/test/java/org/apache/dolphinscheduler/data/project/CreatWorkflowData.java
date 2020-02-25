623465e5e5027f8b5618a57f28e37cc77a9c8d09
chenxingchun <438044805@qq.com>
2020-02-25 14:26:08 +0800

    add ui-test

---
+++ e2e/src/test/java/org/apache/dolphinscheduler/data/project/CreatWorkflowData.java
@@ -0,0 +1,13 @@
+package org.apache.dolphinscheduler.data.project;
+
+public class CreatWorkflowData {
+    //input shell task name
+    public static final String SHELL_TASK_NAME = "shell task description test";
+
+    //input shell task description
+    public static final String SHELL_TASK_DESCRIPTION = "shell task description test";
+
+    //input shell script
+    public static final String SHELL_SCRIPT = "echo 1111111";
+
+}
