fef6d68b2d68d19dbbc74e81437a157f0b8af715
qiaozhanwei <qiaozhanwei@analysys.com.cn>
2020-03-03 17:50:53 +0800

    create buildKillCommand

--- dolphinscheduler-server/src/main/java/org/apache/dolphinscheduler/server/master/dispatch/executor/NettyExecutorManager.java
+++ dolphinscheduler-server/src/main/java/org/apache/dolphinscheduler/server/master/dispatch/executor/NettyExecutorManager.java
@@ -22,6 +22,7 @@ import org.apache.dolphinscheduler.remote.NettyRemotingClient;
 import org.apache.dolphinscheduler.remote.command.Command;
 import org.apache.dolphinscheduler.remote.command.CommandType;
 import org.apache.dolphinscheduler.remote.command.TaskExecuteRequestCommand;
+import org.apache.dolphinscheduler.remote.command.TaskKillRequestCommand;
 import org.apache.dolphinscheduler.remote.config.NettyClientConfig;
 import org.apache.dolphinscheduler.remote.utils.FastJsonSerializer;
 import org.apache.dolphinscheduler.remote.utils.Host;
@@ -132,7 +133,7 @@ public class NettyExecutorManager extends AbstractExecutorManager<Boolean>{
 
     @Override
     public void executeDirectly(ExecutionContext context) throws ExecuteException {
-        Command command = buildCommand(context);
+        Command command = buildKillCommand(context);
         Host host = context.getHost();
         doExecute(host,command);
     }
@@ -144,6 +145,28 @@ public class NettyExecutorManager extends AbstractExecutorManager<Boolean>{
      */
     private Command buildCommand(ExecutionContext context) {
         TaskExecuteRequestCommand requestCommand = new TaskExecuteRequestCommand();
+        ExecutorType executorType = context.getExecutorType();
+        switch (executorType){
+            case WORKER:
+                TaskExecutionContext taskExecutionContext = context.getContext();
+                requestCommand.setTaskExecutionContext(FastJsonSerializer.serializeToString(taskExecutionContext));
+                break;
+            case CLIENT:
+                break;
+            default:
+                throw new IllegalArgumentException("invalid executor type : " + executorType);
+
+        }
+        return requestCommand.convert2Command();
+    }
+
+    /**
+     *  build command
+     * @param context context
+     * @return command
+     */
+    private Command buildKillCommand(ExecutionContext context) {
+        TaskKillRequestCommand requestCommand = new TaskKillRequestCommand();
         ExecutorType executorType = context.getExecutorType();
         switch (executorType){
             case WORKER:
