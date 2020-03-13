5e46ce1f0c96ce9730026872e90b5f17393a32ee
liwenhe1993 <411934049@qq.com>
2020-03-13 14:39:48 +0800

    Toolbar of the DAG has not prompt

--- dolphinscheduler-ui/src/js/conf/home/pages/dag/_source/dag.vue
+++ dolphinscheduler-ui/src/js/conf/home/pages/dag/_source/dag.vue
@@ -25,7 +25,7 @@
              :key="v"
              v-for="(item,v) in tasksTypeList"
              @mousedown="_getDagId(v)">
-          <div data-toggle="tooltip" :title="item.description">
+          <div data-toggle="tooltip" :title="item.desc">
             <div class="icos" :class="'icos-' + v" ></div>
           </div>
         </div>
