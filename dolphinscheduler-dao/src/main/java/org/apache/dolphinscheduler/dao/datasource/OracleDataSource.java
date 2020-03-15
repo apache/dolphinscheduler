8c8c96a00fd02723d898107e40fad4a3e2501f25
wu_shao_jie@qq.com <wu_shao_jie@qq.com>
2020-03-15 17:06:58 +0800

    Adapting partial code(file name start with O) to the sonar cloud rule

--- dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/datasource/OracleDataSource.java
+++ dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/datasource/OracleDataSource.java
@@ -38,7 +38,7 @@ public class OracleDataSource extends BaseDataSource {
     @Override
     public String getJdbcUrl() {
         String jdbcUrl = getAddress();
-        if (jdbcUrl.lastIndexOf("/") != (jdbcUrl.length() - 1)) {
+        if (jdbcUrl.lastIndexOf('/') != (jdbcUrl.length() - 1)) {
             jdbcUrl += "/";
         }
 
