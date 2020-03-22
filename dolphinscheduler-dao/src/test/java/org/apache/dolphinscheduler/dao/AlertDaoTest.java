df32b9dd7257ca79ea03df08fe4cd951a441453e
韦韬晟 <tswstarplanet@apache.org>
2020-03-22 10:49:42 +0800

    add unit test

--- dolphinscheduler-dao/src/test/java/org/apache/dolphinscheduler/dao/AlertDaoTest.java
+++ dolphinscheduler-dao/src/test/java/org/apache/dolphinscheduler/dao/AlertDaoTest.java
@@ -16,6 +16,7 @@
  */
 package org.apache.dolphinscheduler.dao;
 
+import org.apache.dolphinscheduler.common.enums.AlertStatus;
 import org.apache.dolphinscheduler.common.enums.AlertType;
 import org.apache.dolphinscheduler.common.enums.ShowType;
 import org.apache.dolphinscheduler.dao.entity.Alert;
@@ -40,11 +41,19 @@ public class AlertDaoTest {
                 "\"get the alarm exception.！，interface error，exception information：timed out\", \"request address：http://blog.csdn.net/dreamInTheWorld/article/details/78539286\"]");
         alert.setAlertType(AlertType.EMAIL);
         alert.setAlertGroupId(1);
+        alert.setAlertStatus(AlertStatus.WAIT_EXECUTION);
         alertDao.addAlert(alert);
+
+
         List<Alert> alerts = alertDao.listWaitExecutionAlert();
         Assert.assertNotNull(alerts);
         Assert.assertNotEquals(0, alerts.size());
         int id = alerts.get(0).getId();
+        AlertStatus alertStatus = alerts.get(0).getAlertStatus();
+        alertDao.updateAlert(AlertStatus.EXECUTION_SUCCESS, "", id);
+
+        alerts = alertDao.listWaitExecutionAlert();
+        Assert.assertNotEquals(alertStatus, alerts.get(0).getAlertStatus());
         alertDao.getAlertMapper().deleteById(id);
     }
 }
