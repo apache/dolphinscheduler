a8cccc812f9ee6c1bd09b01f06bfba3ac5712d93
孙朝和 <649790970@qq.com>
2020-03-26 23:25:15 +0800

    resolve shell task build command nullpointexception (#2267)
    Co-authored-by: sunchaohe <sunzhaohe@linklogis.com>

--- dolphinscheduler-server/src/main/java/org/apache/dolphinscheduler/server/worker/task/shell/ShellTask.java
+++ dolphinscheduler-server/src/main/java/org/apache/dolphinscheduler/server/worker/task/shell/ShellTask.java
@@ -133,7 +133,6 @@ public class ShellTask extends AbstractTask {
 
     String script = shellParameters.getRawScript().replaceAll("\\r\\n", "\n");
 
-
     /**
      *  combining local and global parameters
      */
@@ -142,13 +141,9 @@ public class ShellTask extends AbstractTask {
             shellParameters.getLocalParametersMap(),
             taskProps.getCmdTypeIfComplement(),
             taskProps.getScheduleTime());
-//    if (paramsMap != null){
-//      script = ParameterUtils.convertParameterPlaceholders(script, ParamUtils.convert(paramsMap));
-//    }
 
-    //new
 //    replace variable TIME with $[YYYYmmddd...] in shell file when history run job and batch complement job
-    if (paramsMap != null) {
+    if(paramsMap != null && taskProps.getScheduleTime()!=null) {
       String dateTime = DateUtils.format(taskProps.getScheduleTime(), Constants.PARAMETER_FORMAT_TIME);
       Property p = new Property();
       p.setValue(dateTime);
@@ -176,7 +171,5 @@ public class ShellTask extends AbstractTask {
   public AbstractParameters getParameters() {
     return shellParameters;
   }
-
-
 
 }
