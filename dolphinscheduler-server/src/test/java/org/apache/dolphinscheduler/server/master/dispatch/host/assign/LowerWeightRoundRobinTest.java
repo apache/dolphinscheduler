57f349d1169b486d386056128548a03df0fd36f0
Tboy <guo.jiwei@immomo.com>
2020-03-26 10:20:20 +0800

    Refactor worker (#2312)
    * let quartz use the same datasource
    
    * move master/worker config from dao.properties to each config
    add master/worker registry test
    
    * move mybatis config from application.properties to SpringConnectionFactory
    
    * move mybatis-plus config from application.properties to SpringConnectionFactory
    
    * refactor TaskCallbackService
    
    * add ZookeeperNodeManagerTest
    
    * add NettyExecutorManagerTest
    
    * refactor TaskKillProcessor
    
    * add RandomSelectorTest, RoundRobinSelectorTest, TaskCallbackServiceTest
    
    * add RoundRobinHostManagerTest, ExecutorDispatcherTest

--- dolphinscheduler-server/src/test/java/org/apache/dolphinscheduler/server/master/dispatch/host/assign/LowerWeightRoundRobinTest.java
+++ dolphinscheduler-server/src/test/java/org/apache/dolphinscheduler/server/master/dispatch/host/assign/LowerWeightRoundRobinTest.java
@@ -14,11 +14,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.dolphinscheduler.server.master.dispatch.host;
+package org.apache.dolphinscheduler.server.master.dispatch.host.assign;
 
 import org.apache.dolphinscheduler.remote.utils.Host;
-import org.apache.dolphinscheduler.server.master.dispatch.host.assign.HostWeight;
-import org.apache.dolphinscheduler.server.master.dispatch.host.assign.LowerWeightRoundRobin;
 import org.junit.Test;
 
 import java.util.ArrayList;
