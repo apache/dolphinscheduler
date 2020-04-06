bf7b72c09e24b46630629ca7ea3670f286f4a641
songqh <songquanhe@foxmail.com>
2020-04-06 13:23:11 +0800

    remove duplicate code

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/ExecutorService.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/ExecutorService.java
@@ -540,7 +540,6 @@ public class ExecutorService extends BaseService{
                         processDefineId, schedule);
             }
         }
-
         return processService.createCommand(command);
     }
 
