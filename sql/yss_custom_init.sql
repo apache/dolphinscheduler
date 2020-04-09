3f57b7cb337c7aabe278cb8d6de04e37f47be327
BoYiZhang <zhangboyi_mx@163.com>
2020-04-09 15:56:30 +0800

    add  calendar  sql

--- sql/yss_custom_init.sql
+++ sql/yss_custom_init.sql
@@ -1,24 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for macos10.15 (x86_64)
+--
+-- Host: localhost    Database: dolphinscheduler
+-- ------------------------------------------------------
+-- Server version	5.7.28
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
----------------------------  Custom SQL start  :  create by ZhangLong  --------------------------
+--
+-- Table structure for table `t_yss_calendar`
+--
 
 DROP TABLE IF EXISTS `t_yss_calendar`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `t_yss_calendar` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'key',
   `name` varchar(100) NOT NULL COMMENT 'calendar name',
   `start_time` datetime NOT NULL COMMENT 'start time',
-  `end_time` datetime  NOT NULL COMMENT 'end time',
-  `flag` tinyint(4) DEFAULT NULL COMMENT '0 not available, 1 available',
+  `end_time` datetime NOT NULL COMMENT 'end time',
+  `release_state` tinyint(4) DEFAULT NULL COMMENT '0 not available, 1 available',
   `desc` varchar(200) DEFAULT NULL COMMENT 'calendar description',
   `user_id` int(11) DEFAULT NULL COMMENT 'creator id',
   `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'create time',
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'update time',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `calendar_name_unique` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `t_yss_calendar`
+--
+
+LOCK TABLES `t_yss_calendar` WRITE;
+/*!40000 ALTER TABLE `t_yss_calendar` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_yss_calendar` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_yss_calendar_details`
+--
 
 DROP TABLE IF EXISTS `t_yss_calendar_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `t_yss_calendar_details` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'key',
   `calendar_id` int(11) NOT NULL COMMENT 'calendar id',
@@ -27,15 +61,27 @@ CREATE TABLE `t_yss_calendar_details` (
   `user_id` int(11) DEFAULT NULL COMMENT 'creator id',
   `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'create time',
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'update time',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `calendar_details_index` (`calendar_id`,`stamp`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `t_yss_calendar_details`
+--
 
+LOCK TABLES `t_yss_calendar_details` WRITE;
+/*!40000 ALTER TABLE `t_yss_calendar_details` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_yss_calendar_details` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-UPDATE `t_ds_user` SET `user_password` = '21232f297a57a5a743894a0e4a801fc3' WHERE (`id` = '1');
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-
-
-
----------------------------   Custom SQL end  :  create by ZhangLong     --------------------------
-
+-- Dump completed on 2020-04-09 15:55:35
