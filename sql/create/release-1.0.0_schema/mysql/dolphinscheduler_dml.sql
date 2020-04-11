4677d94add4571003cd82c70da00476537c6f818
BoYiZhang <zhangboyi_mx@163.com>
2020-04-12 01:30:45 +0800

    update default password

--- sql/create/release-1.0.0_schema/mysql/dolphinscheduler_dml.sql
+++ sql/create/release-1.0.0_schema/mysql/dolphinscheduler_dml.sql
@@ -16,7 +16,7 @@
 */
 
 -- Records of t_escheduler_user,user : admin , password : dolphinscheduler123
-INSERT INTO `t_escheduler_user` VALUES ('1', 'admin', '7ad2410b2f4c074479a8937a28a22b8f', '0', 'xxx@qq.com', 'xx', '0', '2018-03-27 15:48:50', '2018-10-24 17:40:22');
+INSERT INTO `t_escheduler_user` VALUES ('1', 'admin', '21232f297a57a5a743894a0e4a801fc3', '0', 'xxx@qq.com', 'xx', '0', '2018-03-27 15:48:50', '2018-10-24 17:40:22');
 INSERT INTO `t_escheduler_alertgroup` VALUES (1, 'default admin warning group', '0', 'default admin warning group','2018-11-29 10:20:39', '2018-11-29 10:20:39');
 INSERT INTO `t_escheduler_relation_user_alertgroup` VALUES ('1', '1', '1', '2018-11-29 10:22:33', '2018-11-29 10:22:33');
 
