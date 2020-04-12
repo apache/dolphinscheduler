3c9ba0a3f300da91fc1c4e71a927ea0c2f21515a
Han Gao <dhangao@hotmail.com>
2020-04-12 09:39:57 +0800

    api server exception management and code optimization (#397) (#2397)
    * api server exception management and code optimization (#397)
    
    * add unit test for exception handler.
    
    * fix some style and naming issues
    
    Co-authored-by: dailidong <dailidong66@gmail.com>

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/enums/Status.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/enums/Status.java
@@ -27,6 +27,8 @@ public enum Status {
 
     SUCCESS(0, "success", "成功"),
 
+    INTERNAL_SERVER_ERROR_ARGS(10000, "Internal Server Error: {0}", "服务端异常: {0}"),
+
     REQUEST_PARAMS_NOT_VALID_ERROR(10001, "request parameter {0} is not valid", "请求参数[{0}]无效"),
     TASK_TIMEOUT_PARAMS_ERROR(10002, "task timeout parameter is not valid", "任务超时参数无效"),
     USER_NAME_EXIST(10003, "user name already exists", "用户名已存在"),
