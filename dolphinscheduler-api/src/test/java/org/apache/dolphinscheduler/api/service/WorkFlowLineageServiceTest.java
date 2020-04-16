ece442495524f3bd673f3e194fc250ef98a477d5
LiemLin <linhaiqiang@126.com>
2020-04-16 10:02:20 +0800

    add apache license

--- dolphinscheduler-api/src/test/java/org/apache/dolphinscheduler/api/service/WorkFlowLineageServiceTest.java
+++ dolphinscheduler-api/src/test/java/org/apache/dolphinscheduler/api/service/WorkFlowLineageServiceTest.java
@@ -17,28 +17,34 @@
 package org.apache.dolphinscheduler.api.service;
 
 import org.apache.dolphinscheduler.common.Constants;
+import org.apache.dolphinscheduler.common.utils.EncryptionUtils;
 import org.apache.dolphinscheduler.dao.entity.WorkFlowLineage;
 import org.apache.dolphinscheduler.dao.entity.WorkFlowRelation;
+import org.apache.dolphinscheduler.dao.mapper.WorkFlowLineageMapper;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class WorkFlowLineageServiceTest {
 
     @InjectMocks
-    WorkFlowLineageService workFlowLineageService;
+    private WorkFlowLineageService workFlowLineageService;
+
+    @Mock
+    private WorkFlowLineageMapper workFlowLineageMapper;
 
     @Test
     public void testQueryWorkFlowLineageByName() {
         String searchVal = "test";
+        when(workFlowLineageMapper.queryByName(searchVal, 1)).thenReturn(getWorkFlowLineages());
         Map<String, Object> result = workFlowLineageService.queryWorkFlowLineageByName(searchVal,1);
         List<WorkFlowLineage> workFlowLineageList = (List<WorkFlowLineage>)result.get(Constants.DATA_LIST);
         Assert.assertTrue(workFlowLineageList.size()>0);
@@ -50,12 +56,33 @@ public class WorkFlowLineageServiceTest {
         Set<Integer> ids = new HashSet<>();
         ids.add(1);
         ids.add(2);
+
+        when(workFlowLineageMapper.queryByIds(ids, 1)).thenReturn(getWorkFlowLineages());
+        when(workFlowLineageMapper.querySourceTarget(1)).thenReturn(getWorkFlowRelation());
         Map<String, Object> result = workFlowLineageService.queryWorkFlowLineageByIds(ids,1);
         Map<String, Object> workFlowLists = (Map<String, Object>)result.get(Constants.DATA_LIST);
         List<WorkFlowLineage> workFlowLineages = (List<WorkFlowLineage>)workFlowLists.get("workFlowList");
         List<WorkFlowRelation> workFlowRelations = (List<WorkFlowRelation>)workFlowLists.get("workFlowRelationList");
         Assert.assertTrue(workFlowLineages.size()>0);
         Assert.assertTrue(workFlowRelations.size()>0);
+    }
+
+    private List<WorkFlowLineage> getWorkFlowLineages() {
+        List<WorkFlowLineage> workFlowLineages = new ArrayList<>();
+        WorkFlowLineage workFlowLineage = new WorkFlowLineage();
+        workFlowLineage.setWorkFlowId(1);
+        workFlowLineage.setWorkFlowName("testdag");
+        workFlowLineages.add(workFlowLineage);
+        return workFlowLineages;
+    }
+
+    private List<WorkFlowRelation> getWorkFlowRelation(){
+        List<WorkFlowRelation> workFlowRelations = new ArrayList<>();
+        WorkFlowRelation workFlowRelation = new WorkFlowRelation();
+        workFlowRelation.setSourceWorkFlowId(1);
+        workFlowRelation.setTargetWorkFlowId(2);
+        workFlowRelations.add(workFlowRelation);
+        return workFlowRelations;
     }
 
 }
