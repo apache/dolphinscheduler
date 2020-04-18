0b36315a1880625e0d4c0ca56f024056d3106151
zixi0825 <649790970@qq.com>
2020-04-18 11:28:08 +0800

    solve code spell problem (#2419)
    * solve code spell problem
    
    * remove controller test
    
    * add controller test
    
    * sovle spell problem and sovle conflict
    
    * sovle spell problem and sovle conflict
    
    * sovle spell problem and add test
    
    Co-authored-by: sunchaohe <sunzhaohe@linklogis.com>

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/controller/ProjectController.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/controller/ProjectController.java
@@ -234,7 +234,8 @@ public class ProjectController extends BaseController {
      * @param projectName project name
      * @return import result code
      */
-    @ApiOperation(value = "importProcessDefinition", notes = "EXPORT_PROCCESS_DEFINITION_NOTES")
+
+    @ApiOperation(value = "importProcessDefinition", notes= "EXPORT_PROCESS_DEFINITION_NOTES")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "file", value = "RESOURCE_FILE", required = true, dataType = "MultipartFile")
     })
