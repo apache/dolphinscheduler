6c6f4784653d936b750b196fef3714f788a72d55
kezhenxu94 <kezhenxu94@163.com>
2020-04-29 23:59:30 +0800

    Add eslint to check frontend codes style, closes #1463

--- dolphinscheduler-ui/src/js/module/util/index.js
+++ dolphinscheduler-ui/src/js/module/util/index.js
@@ -51,14 +51,13 @@ export function findComponentDownward (context, componentName) {
  *  uuid('v-ani-%{s}-translate')  // -> v-ani-xxx
  */
 export function uuid (prefix) {
-  let id = Math.floor(Math.random() * 10000).toString(36)
+  const id = Math.floor(Math.random() * 10000).toString(36)
   return prefix ? (
     ~prefix.indexOf('%{s}') ? (
       prefix.replace(/%\{s\}/g, id)
     ) : (prefix + id)
   ) : id
 }
-
 
 /**
  * template
@@ -92,4 +91,3 @@ export function template (string, ...args) {
     }
   })
 }
-
