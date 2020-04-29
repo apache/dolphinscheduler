043a20f2c72993b63b3963b0057fce3c6a83d00b
qiaozhanwei <qiaozhanwei@outlook.com>
2020-04-29 17:36:21 +0800

    No master don't create command #2571 (#2575)
    * dispatch task fail will set task status failed
    
    * 1，no worker condition , master will while ture wait for worker startup
    2，worker response task status sync wait for result
    
    * 1，no worker condition , master will while ture wait for worker startup
    2，worker response task status sync wait for result
    
    * 1，no worker condition , master will while ture wait for worker startup
    2，worker response task status sync wait for result
    
    * 1，no worker condition , master will while ture wait for worker startup
    2，worker response task status sync wait for result
    
    * 1，no worker condition , master will while ture wait for worker startup
    2，worker response task status sync wait for result
    
    * 1，no worker condition , master will while ture wait for worker startup
    2，worker response task status sync wait for result
    
    * 1，no worker condition , master will while ture wait for worker startup
    2，worker response task status sync wait for result
    
    * 1，no worker condition , master will while ture wait for worker startup
    2，worker response task status sync wait for result
    
    * 1，no worker condition , master will while ture wait for worker startup
    2，worker response task status sync wait for result
    
    * 1，no worker condition , master will while ture wait for worker startup
    2，worker response task status sync wait for result
    
    * 1，no worker condition , master will while ture wait for worker startup
    2，worker response task status sync wait for result
    
    * 1，task status statistics and process status statistics bug fix (#2357)
    2，worker group bug fix
    
    * 1，task status statistics and process status statistics bug fix (#2357)
    2，worker group bug fix
    
    * 1，task status statistics and process status statistics bug fix (#2357)
    2，worker group bug fix
    
    * 1，task status statistics and process status statistics bug fix (#2357)
    2，worker group bug fix
    
    * send mail error, #2466 bug fix
    
    * send mail error, #2466 bug fix
    
    * send mail error, #2466 bug fix
    
    * send mail error, #2466 bug fix
    
    * #2486 bug fix
    
    * host and workergroup compatible
    
    * EnterpriseWeChatUtils modify
    
    * EnterpriseWeChatUtils modify
    
    * EnterpriseWeChatUtils modify
    
    * #2499 bug fix
    
    * add comment
    
    * revert comment
    
    * revert comment
    
    * #2499 buf fix
    
    * #2499 bug fix
    
    * #2499 bug fix
    
    * #2499 bug fix
    
    * #2499 bug fix
    
    * #2499 bug fix
    
    * #2499 bug fix
    
    * no valid worker group，master can kill task directly
    
    * no valid worker group，master can kill task directly
    
    * no valid worker group，master can kill task directly
    
    * no valid worker group，master can kill task directly
    
    * no valid worker group，master can kill task directly
    
    * no valid worker group，master can kill task directly
    
    * no valid worker group，master can kill task directly
    
    * no valid worker group，master can kill task directly
    
    * no valid worker group，master can kill task directly
    
    * No master don't create command #2571
    
    * No master don't create command #2571
    
    Co-authored-by: qiaozhanwei <qiaozhanwei@analysys.com.cn>

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/ExecutorService.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/ExecutorService.java
@@ -21,6 +21,7 @@ import org.apache.dolphinscheduler.api.enums.ExecuteType;
 import org.apache.dolphinscheduler.api.enums.Status;
 import org.apache.dolphinscheduler.common.Constants;
 import org.apache.dolphinscheduler.common.enums.*;
+import org.apache.dolphinscheduler.common.model.Server;
 import org.apache.dolphinscheduler.common.utils.CollectionUtils;
 import org.apache.dolphinscheduler.common.utils.DateUtils;
 import org.apache.dolphinscheduler.common.utils.JSONUtils;
@@ -59,7 +60,7 @@ public class ExecutorService extends BaseService{
     private ProcessDefinitionMapper processDefinitionMapper;
 
     @Autowired
-    private ProcessDefinitionService processDefinitionService;
+    private MonitorService monitorService;
 
 
     @Autowired
@@ -123,6 +124,14 @@ public class ExecutorService extends BaseService{
             return result;
         }
 
+        // check master server exists
+        List<Server> masterServers = monitorService.getServerListFromZK(true);
+
+
+        if (masterServers.size() == 0) {
+            putMsg(result, Status.MASTER_NOT_EXISTS);
+            return result;
+        }
         /**
          * create command
          */
