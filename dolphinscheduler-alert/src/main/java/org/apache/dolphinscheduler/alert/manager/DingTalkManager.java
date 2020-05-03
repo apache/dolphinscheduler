75e8a532d397d67665062c7c4c5bad6a69620ad5
xiaozhaoying <stanfen@126.com>
2020-05-03 16:53:02 +0800

    Create DingTalkManager.java

---
+++ dolphinscheduler-alert/src/main/java/org/apache/dolphinscheduler/alert/manager/DingTalkManager.java
@@ -0,0 +1,37 @@
+package org.apache.dolphinscheduler.alert.manager;
+
+import org.apache.dolphinscheduler.alert.utils.Constants;
+import org.apache.dolphinscheduler.alert.utils.DingTalkUtils;
+import org.apache.dolphinscheduler.dao.entity.Alert;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Ding Talk Manager
+ */
+public class DingTalkManager {
+    private static final Logger logger = LoggerFactory.getLogger(EnterpriseWeChatManager.class);
+
+    public Map<String,Object> send(Alert alert) {
+        Map<String,Object> retMap = new HashMap<>();
+        retMap.put(Constants.STATUS, false);
+        logger.info("send message {}",alert);
+        try {
+            String msg = buildMessage(alert);
+            DingTalkUtils.sendDingTalkMsg(msg, Constants.UTF_8);
+        } catch (IOException e) {
+            logger.error(e.getMessage(),e);
+        }
+        retMap.put(Constants.STATUS, true);
+        return retMap;
+    }
+
+    private String buildMessage(Alert alert) {
+        String msg = alert.getContent();
+        return msg;
+    }
+}
