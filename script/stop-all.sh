14d676b4b43abf841469c17bbce3d511709dcc59
dailidong <dailidong66@gmail.com>
2020-05-18 18:04:24 +0800

    update worker group config (#2746)
    * fix worker group config no effect
    
    * remove codehaus janino jar
    the license about janino maybe not compatiable with Apache v2
    
    * Merge remote-tracking branch 'upstream/dev-1.3.0' into dev-1.3.0
    
    # Conflicts:
    #	dolphinscheduler-server/src/main/resources/config/install_config.conf
    
    * Update datasource.properties

--- script/stop-all.sh
+++ script/stop-all.sh
@@ -21,6 +21,17 @@ workDir=`cd ${workDir};pwd`
 
 source $workDir/../conf/config/install_config.conf
 
+declare -A workersGroupMap=()
+
+workersGroup=(${workers//,/ })
+for workerGroup in ${workersGroup[@]}
+do
+  echo $workerGroup;
+  worker=`echo $workerGroup|awk -F':' '{print $1}'`
+  groupName=`echo $workerGroup|awk -F':' '{print $2}'`
+  workersGroupMap+=([$worker]=$groupName)
+done
+
 mastersHost=(${masters//,/ })
 for master in ${mastersHost[@]}
 do
@@ -29,7 +40,7 @@ do
 
 done
 
-for worker in ${!workersGroup[*]}
+for worker in ${!workersGroupMap[*]}
 do
   echo "$worker worker server is stopping"
   ssh -p $sshPort $worker  "cd $installPath/; sh bin/dolphinscheduler-daemon.sh stop worker-server;"
