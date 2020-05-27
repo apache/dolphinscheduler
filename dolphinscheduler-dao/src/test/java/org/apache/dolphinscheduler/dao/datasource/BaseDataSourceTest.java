16a59666cd132aae2b365268a0f2d5449c535394
taoyuxin <taoyuxin@inspur.com>
2020-05-27 20:37:14 +0800

    Remove some unit test.

--- dolphinscheduler-dao/src/test/java/org/apache/dolphinscheduler/dao/datasource/BaseDataSourceTest.java
+++ dolphinscheduler-dao/src/test/java/org/apache/dolphinscheduler/dao/datasource/BaseDataSourceTest.java
@@ -110,53 +110,5 @@ public class BaseDataSourceTest {
     //set fake other
     db2DataSource.setOther("charset=UTF-8");
     Assert.assertEquals("jdbc:db2://127.0.0.1:50000/test:charset=UTF-8", db2DataSource.getJdbcUrl());
-
-    OracleDataSource oracleDataSource = new OracleDataSource();
-    oracleDataSource.setType(DbConnectType.ORACLE_SERVICE_NAME);
-    oracleDataSource.setAddress("jdbc:oracle:thin:@//127.0.0.1:1521");
-    oracleDataSource.setDatabase("test");
-    oracleDataSource.setPassword("123456");
-    oracleDataSource.setUser("test");
-    Assert.assertEquals("jdbc:oracle:thin:@//127.0.0.1:1521/test", oracleDataSource.getJdbcUrl());
-    //set fake principal
-    oracleDataSource.setPrincipal("fake principal");
-    Assert.assertEquals("jdbc:oracle:thin:@//127.0.0.1:1521/test", oracleDataSource.getJdbcUrl());
-    //set fake other
-    oracleDataSource.setOther("charset=UTF-8");
-    Assert.assertEquals("jdbc:oracle:thin:@//127.0.0.1:1521/test?charset=UTF-8", oracleDataSource.getJdbcUrl());
-
-    OracleDataSource oracleDataSource2 = new OracleDataSource();
-    oracleDataSource2.setAddress("jdbc:oracle:thin:@127.0.0.1:1521");
-    oracleDataSource2.setDatabase("orcl");
-    oracleDataSource2.setPassword("123456");
-    oracleDataSource2.setUser("test");
-    oracleDataSource2.setType(DbConnectType.ORACLE_SID);
-    Assert.assertEquals("jdbc:oracle:thin:@127.0.0.1:1521:orcl", oracleDataSource2.getJdbcUrl());
-    //set fake principal
-    oracleDataSource2.setPrincipal("fake principal");
-    Assert.assertEquals("jdbc:oracle:thin:@127.0.0.1:1521:orcl", oracleDataSource2.getJdbcUrl());
-    //set fake other
-    oracleDataSource2.setOther("charset=UTF-8");
-    Assert.assertEquals("jdbc:oracle:thin:@127.0.0.1:1521:orcl?charset=UTF-8", oracleDataSource2.getJdbcUrl());
-
-  }
-
-  @Test
-  public void testAppendDatabase() {
-    OracleDataSource oracleDataSource = new OracleDataSource();
-    oracleDataSource.setAddress("jdbc:oracle:thin:@//127.0.0.1:1521");
-    oracleDataSource.setDatabase("test");
-    oracleDataSource.setType(DbConnectType.ORACLE_SERVICE_NAME);
-    StringBuilder jdbcUrl = new StringBuilder(oracleDataSource.getAddress());
-    oracleDataSource.appendDatabase(jdbcUrl);
-    Assert.assertEquals("jdbc:oracle:thin:@//127.0.0.1:1521/test", jdbcUrl.toString());
-
-    OracleDataSource oracleDataSource2 = new OracleDataSource();
-    oracleDataSource2.setAddress("jdbc:oracle:thin:@127.0.0.1:1521");
-    oracleDataSource2.setDatabase("orcl");
-    oracleDataSource2.setType(DbConnectType.ORACLE_SID);
-    StringBuilder jdbcUrl2 = new StringBuilder(oracleDataSource2.getAddress());
-    oracleDataSource2.appendDatabase(jdbcUrl2);
-    Assert.assertEquals("jdbc:oracle:thin:@127.0.0.1:1521:orcl", jdbcUrl2.toString());
   }
 }
