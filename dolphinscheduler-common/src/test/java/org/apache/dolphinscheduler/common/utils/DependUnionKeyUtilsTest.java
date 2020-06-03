0d8c0152d2e9f4581abf1ef9af5280aa52da452e
Rubik-W <whm_777@163.com>
2020-06-03 11:24:06 +0800

    fix: UT bug

--- dolphinscheduler-common/src/test/java/org/apache/dolphinscheduler/common/utils/DependUnionKeyUtilsTest.java
+++ dolphinscheduler-common/src/test/java/org/apache/dolphinscheduler/common/utils/DependUnionKeyUtilsTest.java
@@ -47,48 +47,48 @@ public class DependUnionKeyUtilsTest {
 
     @Test
     public void testRemoveMarkWord() {
-        Assert.assertEquals("127.0.0.1:db1:table1", DependUnionKeyUtils.removeMarkWord("127.0.0.1:db1:table1#depend"));
-        Assert.assertEquals("127.0.0.1:db1:table2", DependUnionKeyUtils.removeMarkWord("127.0.0.1:db1:table2#target"));
+        Assert.assertEquals("127.0.0.1:db1:table1", DependUnionKeyUtils.removeMarkWord("127.0.0.1:db1:table1#table_depend"));
+        Assert.assertEquals("127.0.0.1:db1:table2", DependUnionKeyUtils.removeMarkWord("127.0.0.1:db1:table2#table_target"));
         Assert.assertEquals(null, DependUnionKeyUtils.removeMarkWord(null));
     }
 
     @Test
     public void testBuildDependTableUnionKey1() {
-        Assert.assertEquals("127.0.0.1:db1:table1#depend", DependUnionKeyUtils.buildDependTableUnionKey("127.0.0.1", "db1", "table1"));
+        Assert.assertEquals("127.0.0.1:db1:table1#table_depend", DependUnionKeyUtils.buildDependTableUnionKey("127.0.0.1", "db1", "table1"));
     }
 
     @Test
     public void testBuildDependTableUnionKey2() {
-        Assert.assertEquals("127.0.0.1:db1:table1#depend", DependUnionKeyUtils.buildDependTableUnionKey("127.0.0.1", null, "db1.table1"));
+        Assert.assertEquals("127.0.0.1:db1:table1#table_depend", DependUnionKeyUtils.buildDependTableUnionKey("127.0.0.1", null, "db1.table1"));
     }
 
     @Test
     public void testBuildDependTableUnionKey3() {
-        Assert.assertEquals("127.0.0.1:db1:table1#depend,127.0.0.1:db1:table2#depend", DependUnionKeyUtils.buildDependTableUnionKey("127.0.0.1", "db1", Arrays.asList("table1", "table2")));
+        Assert.assertEquals("127.0.0.1:db1:table1#table_depend,127.0.0.1:db1:table2#table_depend", DependUnionKeyUtils.buildDependTableUnionKey("127.0.0.1", "db1", Arrays.asList("table1", "table2")));
 
         List<String> list = null;
         Assert.assertEquals(null, DependUnionKeyUtils.buildDependTableUnionKey("127.0.0.1", "db1", list));
-        Assert.assertEquals("127.0.0.1:db1:table1#depend,", DependUnionKeyUtils.buildDependTableUnionKey("127.0.0.1", "db1", Arrays.asList("table1", null)));
+        Assert.assertEquals("127.0.0.1:db1:table1#table_depend,", DependUnionKeyUtils.buildDependTableUnionKey("127.0.0.1", "db1", Arrays.asList("table1", null)));
     }
 
     @Test
     public void testBuildTargetTableUnionKey1() {
-        Assert.assertEquals("127.0.0.1:db1:table1#target", DependUnionKeyUtils.buildTargetTableUnionKey("127.0.0.1", "db1", "table1"));
+        Assert.assertEquals("127.0.0.1:db1:table1#table_target", DependUnionKeyUtils.buildTargetTableUnionKey("127.0.0.1", "db1", "table1"));
     }
 
     @Test
     public void testBuildTargetTableUnionKey2() {
-        Assert.assertEquals("127.0.0.1:db1:table1#target", DependUnionKeyUtils.buildTargetTableUnionKey("127.0.0.1", null, "db1.table1"));
+        Assert.assertEquals("127.0.0.1:db1:table1#table_target", DependUnionKeyUtils.buildTargetTableUnionKey("127.0.0.1", null, "db1.table1"));
     }
 
     @Test
     public void testBuildTargetTableUnionKey3() {
-        Assert.assertEquals("127.0.0.1:db1:table1#target,127.0.0.1:db1:table2#target", DependUnionKeyUtils.buildTargetTableUnionKey("127.0.0.1", "db1", Arrays.asList("table1", "table2")));
+        Assert.assertEquals("127.0.0.1:db1:table1#table_target,127.0.0.1:db1:table2#table_target", DependUnionKeyUtils.buildTargetTableUnionKey("127.0.0.1", "db1", Arrays.asList("table1", "table2")));
     }
 
     @Test
     public void testBuildTargetTableUnionKey4() {
-        Assert.assertEquals("127.0.0.1:db1:table1#target,127.0.0.1:db1:table2#target", DependUnionKeyUtils.buildTargetTableUnionKey("127.0.0.1", "db1", new String[]{"table1", "table2"}));
+        Assert.assertEquals("127.0.0.1:db1:table1#table_target,127.0.0.1:db1:table2#table_target", DependUnionKeyUtils.buildTargetTableUnionKey("127.0.0.1", "db1", new String[]{"table1", "table2"}));
     }
 
     @Test
@@ -96,23 +96,23 @@ public class DependUnionKeyUtilsTest {
         TaskNode taskNode = new TaskNode();
         taskNode.setType(TaskType.SQL.toString());
         SqlParameters sqlParameters = new SqlParameters();
-        sqlParameters.setTargetNodeKeys("127.0.0.1:db1:table1#target");
+        sqlParameters.setTargetNodeKeys("127.0.0.1:db1:table1#table_target");
         taskNode.setParams(JSONUtils.toJsonString(sqlParameters));
-        Assert.assertTrue(DependUnionKeyUtils.existDependRelation(taskNode, new String[]{"127.0.0.1:db1:table1#depend"}));
+        Assert.assertTrue(DependUnionKeyUtils.existDependRelation(taskNode, new String[]{"127.0.0.1:db1:table1#table_depend"}));
 
         Assert.assertTrue(!DependUnionKeyUtils.existDependRelation(taskNode, null));
     }
 
     @Test
     public void testExistDependRelation2() {
-        Assert.assertTrue(DependUnionKeyUtils.existDependRelation("127.0.0.1:db1:table1#target", new String[]{"127.0.0.1:db1:table1#depend"}));
-        Assert.assertTrue(!DependUnionKeyUtils.existDependRelation("127.0.0.1:db1:table2#target", new String[]{"127.0.0.1:db1:table1#depend"}));
-        Assert.assertTrue(!DependUnionKeyUtils.existDependRelation("127.0.0.1:db1:table2#target", null));
+        Assert.assertTrue(DependUnionKeyUtils.existDependRelation("127.0.0.1:db1:table1#table_target", new String[]{"127.0.0.1:db1:table1#table_depend"}));
+        Assert.assertTrue(!DependUnionKeyUtils.existDependRelation("127.0.0.1:db1:table2#table_target", new String[]{"127.0.0.1:db1:table1#table_depend"}));
+        Assert.assertTrue(!DependUnionKeyUtils.existDependRelation("127.0.0.1:db1:table2#table_target", null));
     }
 
     @Test
     public void testReplaceMarkWordToTarget1() {
-        Assert.assertArrayEquals(new String[]{"127.0.0.1:db1:table1#target"}, DependUnionKeyUtils.replaceMarkWordToTarget(new String[]{"127.0.0.1:db1:table1#depend"}));
+        Assert.assertArrayEquals(new String[]{"127.0.0.1:db1:table1#table_target"}, DependUnionKeyUtils.replaceMarkWordToTarget(new String[]{"127.0.0.1:db1:table1#table_depend"}));
 
         String[] array = null;
         Assert.assertArrayEquals(null, DependUnionKeyUtils.replaceMarkWordToTarget(array));
@@ -120,6 +120,6 @@ public class DependUnionKeyUtilsTest {
 
     @Test
     public void testReplaceMarkWordToTarget2() {
-        Assert.assertEquals("127.0.0.1:db1:table1#target", DependUnionKeyUtils.replaceMarkWordToTarget("127.0.0.1:db1:table1#depend"));
+        Assert.assertEquals("127.0.0.1:db1:table1#table_target", DependUnionKeyUtils.replaceMarkWordToTarget("127.0.0.1:db1:table1#table_depend"));
     }
 }
