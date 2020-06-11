41137300240ee91b76f87d46c3d2d246dd48ddfa
zhanglong <zhanglong@ysstech.com>
2020-06-11 22:26:23 +0800

    add http task timeout & test unit

--- dolphinscheduler-ui/src/js/module/i18n/locale/en_US.js
+++ dolphinscheduler-ui/src/js/module/i18n/locale/en_US.js
@@ -599,5 +599,11 @@ export default {
   'Unauthorized or deleted resources': 'Unauthorized or deleted resources',
   'Please delete all non-existent resources': 'Please delete all non-existent resources',
   'Enable': 'Enable',
-  'Disable': 'Disable'
+  'Disable': 'Disable',
+  'Timeout Settings': '超时设置',
+  'Connect Timeout':'连接超时',
+  'Socket Timeout':'Socket超时',
+  'Connect timeout be a positive integer': '连接超时必须为数字',
+  'Socket Timeout be a positive integer': 'Socket超时必须为数字',
+  'ms':'毫秒'
 }
