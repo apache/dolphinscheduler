6e0b464531d322c2ff60470d3ed63d025d8c970f
xsbai <XS.bai93@gmail.com>
2020-06-20 22:22:55 +0800

    fix issue #3018 [BUG]i18n incomplete (#3022)
    Co-authored-by: dailidong <dailidong66@gmail.com>

--- dolphinscheduler-ui/src/js/module/i18n/locale/zh_CN.js
+++ dolphinscheduler-ui/src/js/module/i18n/locale/zh_CN.js
@@ -184,6 +184,7 @@ export default {
   Tenant: '租户',
   Email: '邮件',
   Phone: '手机',
+  'User Type':'用户类型',
   'Please enter phone number': '请输入手机',
   'Please enter email': '请输入邮箱',
   'Please enter the correct email format': '请输入正确的邮箱格式',
