9407ec457ac8498343e3b04d970ebac1878ae7ca
CalvinKirs <acm_master@163.com>
2020-06-21 10:26:16 +0800

    lowering

--- dolphinscheduler-common/src/main/java/org/apache/dolphinscheduler/common/utils/CollectionUtils.java
+++ dolphinscheduler-common/src/main/java/org/apache/dolphinscheduler/common/utils/CollectionUtils.java
@@ -98,8 +98,8 @@ public class CollectionUtils {
         }
         String[] strings = str.split(separator);
         Map<String, String> map = new HashMap<>(strings.length);
-        for (String string : strings) {
-            String[] strArray = string.split("=");
+        for (int i = 0; i < strings.length; i++) {
+            String[] strArray = strings[i].split("=");
             if (strArray.length != 2) {
                 return emptyMap;
             }
