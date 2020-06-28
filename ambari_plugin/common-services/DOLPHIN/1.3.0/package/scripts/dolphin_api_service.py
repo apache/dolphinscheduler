e5beff80c4f69dceea96e6434ec6edb190137e35
zhangchunyang <zhangchunyang0123@gmail.com>
2020-06-28 11:37:59 +0800

    [bugfix] remove Conflicting configuration sudo and user (#3038)
    Co-authored-by: zhangchunyang1024 <zhangchunyang0123@gamil.com>

--- ambari_plugin/common-services/DOLPHIN/1.3.0/package/scripts/dolphin_api_service.py
+++ ambari_plugin/common-services/DOLPHIN/1.3.0/package/scripts/dolphin_api_service.py
@@ -26,7 +26,8 @@ class DolphinApiService(Script):
         import params
         env.set_params(params)
         self.install_packages(env)
-        Execute(('chmod', '-R', '777', params.dolphin_home), user=params.dolphin_user, sudo=True)
+        Execute(('chmod', '-R', '777', params.dolphin_home))
+        Execute(('chown', '-R', params.dolphin_user + ":" + params.dolphin_group,  params.dolphin_home))
 
     def configure(self, env):
         import params
