f57d71de4100d199176a23dd7a25635a6d70d207
linhaojie <linhaojie@lizhi.fm>
2020-06-30 10:00:06 +0800

    support multi drag

--- dolphinscheduler-ui/src/js/conf/home/pages/dag/_source/plugIn/jsPlumbHandle.js
+++ dolphinscheduler-ui/src/js/conf/home/pages/dag/_source/plugIn/jsPlumbHandle.js
@@ -34,6 +34,7 @@ import {
   rtTargetarrArr
 } from './util'
 import mStart from '@/conf/home/pages/projects/pages/definition/pages/list/_source/start'
+import multiDrag from './multiDrag'
 
 const JSP = function () {
   this.dag = {}
@@ -92,6 +93,9 @@ JSP.prototype.init = function ({ dag, instance, options }) {
   if (this.config.isNewNodes) {
     DragZoom.init()
   }
+
+  // support multi drag
+  multiDrag()
 }
 
 /**
