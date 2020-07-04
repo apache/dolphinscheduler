5293a4f3bbae5f9a75a2afc7ae97970137ff43b8
lgcareer <18610854716@163.com>
2020-07-04 15:31:55 +0800

    merge from 1.3.1-release

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/dto/resources/Directory.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/dto/resources/Directory.java
@@ -1,5 +1,3 @@
-package org.apache.dolphinscheduler.api.dto.resources;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -16,6 +14,8 @@ package org.apache.dolphinscheduler.api.dto.resources;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.apache.dolphinscheduler.api.dto.resources;
+
 /**
  * directory
  */
