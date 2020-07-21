8740b01be53f0a49a7f8e6af3ae40c2757933d50
LEI SHENG <xuanfeng1992@gmail.com>
2020-07-21 18:54:07 +0800

    [Fix-3240][worker] Cannot find a (Map) Key deserializer for type (#3245)
    * fix the bug #2923 (Hive JDBC connection parameter ignored) , the hive jdbc connection url ;
    
    * fix the bug index out of bound and add some test case
    
    * add the Licensed
    
    * fix the checkstyle and import StringBuilder
    
    * put HiveDataSourceTest.java to root pom maven-surefire-plugin to avoiding the sonar 0.0% Coverage.
    
    * Update HiveDataSource.java
    
    * fix the bug #3240  [worker] Cannot find a (Map) Key deserializer for type
    
    * [DS-3245][fix]  change toString  use JsonString
    
    - change the method of  UdfFunc.toString use JsonString
    
    This closes #3245
    
    * [DS-3245][fix]  change toString  use JsonString
    
    - fix the junit test for full
    
    This closes #3245
    
    * [DS-3245][fix]  change toString  use JsonString
    
    - fix the sonarcloud merge error, Swap these 2 arguments so they are in the correct order: expected value, actual value.
    
    This closes #3245
    
    * [DS-3245][fix]  move the Class UdfFuncDeserializer to UdfFunc.java
    
    move the class UdfFuncDeserializer.java to UdfFunc.java
    
    This closes #3245
    
    * [DS-3245][fix]  move the Class UdfFuncDeserializer to UdfFunc.java
    
    add blank line
    
    This closes #3245
    
    * [DS-3245][fix]  add the unit test of UdfFunc
    
    add the unit test of UdfFunc.java
    
    This closes #3245
    
    * [DS-3245][fix]  add the unit test of UdfFunc
    
    remove the UdfFuncTest.java from maven-surefire-plugin list
    
    This closes #3245
    
    * [DS-3245][fix]  add the unit test of UdfFunc
    
    add the UdfFuncTest to the root pom maven-surefire-plugin
    
    This closes #3245
    
    Co-authored-by: dailidong <dailidong66@gmail.com>

---
+++ dolphinscheduler-dao/src/test/java/org/apache/dolphinscheduler/dao/entity/UdfFuncTest.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.dolphinscheduler.dao.entity;
+
+import org.apache.dolphinscheduler.dao.entity.UdfFunc.UdfFuncDeserializer;
+import java.io.IOException;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UdfFuncTest {
+
+  /**
+   * test to String
+   */
+  @Test
+  public void testUdfFuncToString() {
+    
+    UdfFunc udfFunc = new UdfFunc();
+    udfFunc.setResourceName("dolphin_resource_update");
+    udfFunc.setResourceId(2);
+    udfFunc.setClassName("org.apache.dolphinscheduler.test.mrUpdate");
+
+    Assert.assertEquals("{\"id\":0,\"userId\":0,\"funcName\":null,\"className\":\"org.apache.dolphinscheduler.test.mrUpdate\",\"argTypes\":null,\"database\":null,"
+            + "\"description\":null,\"resourceId\":2,\"resourceName\":\"dolphin_resource_update\",\"type\":null,\"createTime\":null,\"updateTime\":null}"
+        , udfFunc.toString());
+  }
+
+  /**
+   * test UdfFuncDeserializer.deserializeKey
+   *
+   * @throws IOException
+   */
+  @Test
+  public void testUdfFuncDeserializer() throws IOException {
+
+    // UdfFuncDeserializer.deserializeKey key is null
+    UdfFuncDeserializer udfFuncDeserializer = new UdfFuncDeserializer();
+    Assert.assertNull(udfFuncDeserializer.deserializeKey(null, null));
+
+    //
+    UdfFunc udfFunc = new UdfFunc();
+    udfFunc.setResourceName("dolphin_resource_update");
+    udfFunc.setResourceId(2);
+    udfFunc.setClassName("org.apache.dolphinscheduler.test.mrUpdate");
+
+    Assert.assertNotNull(udfFuncDeserializer.deserializeKey(udfFunc.toString(), null));
+  }
+
+}
