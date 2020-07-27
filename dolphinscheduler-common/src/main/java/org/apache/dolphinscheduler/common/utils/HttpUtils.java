5cd1ab0397eebcc0bd66c88b256ac96a53da7f76
felix.Wang <felix@thinkingdata.cn>
2020-07-27 15:19:23 +0800

    check style

--- dolphinscheduler-common/src/main/java/org/apache/dolphinscheduler/common/utils/HttpUtils.java
+++ dolphinscheduler-common/src/main/java/org/apache/dolphinscheduler/common/utils/HttpUtils.java
@@ -34,85 +34,85 @@ import java.io.IOException;
  */
 public class HttpUtils {
 
-	public static final Logger logger = LoggerFactory.getLogger(HttpUtils.class);
+    public static final Logger logger = LoggerFactory.getLogger(HttpUtils.class);
 
-	/**
-	 * get http request content
-	 *
-	 * @param url url
-	 * @return http get request response content
-	 */
-	public static String get(String url) {
-		boolean kerberState = PropertyUtils.getBoolean(Constants.HADOOP_SECURITY_AUTHENTICATION_STARTUP_STATE, false);
-		if (kerberState) {
-			String responseContent;
-			KerberosHttpClient kerberosHttpClient = new KerberosHttpClient(
-					PropertyUtils.getString(Constants.LOGIN_USER_KEY_TAB_USERNAME),
-					PropertyUtils.getString(Constants.LOGIN_USER_KEY_TAB_PATH),
-					PropertyUtils.getString(Constants.JAVA_SECURITY_KRB5_CONF_PATH), true);
-			responseContent = kerberosHttpClient.get(url,
-					PropertyUtils.getString(Constants.LOGIN_USER_KEY_TAB_USERNAME));
-			return responseContent;
+    /**
+     * get http request content
+     *
+     * @param url url
+     * @return http get request response content
+     */
+    public static String get(String url) {
+	boolean kerberState = PropertyUtils.getBoolean(Constants.HADOOP_SECURITY_AUTHENTICATION_STARTUP_STATE, false);
+	if (kerberState) {
+	    String responseContent;
+	    KerberosHttpClient kerberosHttpClient = new KerberosHttpClient(
+		    PropertyUtils.getString(Constants.LOGIN_USER_KEY_TAB_USERNAME),
+		    PropertyUtils.getString(Constants.LOGIN_USER_KEY_TAB_PATH),
+		    PropertyUtils.getString(Constants.JAVA_SECURITY_KRB5_CONF_PATH), true);
+	    responseContent = kerberosHttpClient.get(url,
+		    PropertyUtils.getString(Constants.LOGIN_USER_KEY_TAB_USERNAME));
+	    return responseContent;
 
+	} else {
+	    CloseableHttpClient httpclient = HttpClients.createDefault();
+
+	    HttpGet httpget = new HttpGet(url);
+	    /** set timeout、request time、socket timeout */
+	    RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(Constants.HTTP_CONNECT_TIMEOUT)
+		    .setConnectionRequestTimeout(Constants.HTTP_CONNECTION_REQUEST_TIMEOUT)
+		    .setSocketTimeout(Constants.SOCKET_TIMEOUT).setRedirectsEnabled(true).build();
+	    httpget.setConfig(requestConfig);
+	    return getResponseContentString(httpget, httpclient);
+	}
+
+    }
+
+    /**
+     * get http response content
+     *
+     * @param httpget    httpget
+     * @param httpClient httpClient
+     * @return http get request response content
+     */
+    public static String getResponseContentString(HttpGet httpget, CloseableHttpClient httpClient) {
+	String responseContent = null;
+	CloseableHttpResponse response = null;
+	try {
+	    response = httpClient.execute(httpget);
+	    // check response status is 200
+	    if (response.getStatusLine().getStatusCode() == 200) {
+		HttpEntity entity = response.getEntity();
+		if (entity != null) {
+		    responseContent = EntityUtils.toString(entity, Constants.UTF_8);
 		} else {
-			CloseableHttpClient httpclient = HttpClients.createDefault();
-
-			HttpGet httpget = new HttpGet(url);
-			/** set timeout、request time、socket timeout */
-			RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(Constants.HTTP_CONNECT_TIMEOUT)
-					.setConnectionRequestTimeout(Constants.HTTP_CONNECTION_REQUEST_TIMEOUT)
-					.setSocketTimeout(Constants.SOCKET_TIMEOUT).setRedirectsEnabled(true).build();
-			httpget.setConfig(requestConfig);
-			return getResponseContentString(httpget, httpclient);
+		    logger.warn("http entity is null");
 		}
-
-	}
-
-	/**
-	 * get http response content
-	 *
-	 * @param httpget    httpget
-	 * @param httpClient httpClient
-	 * @return http get request response content
-	 */
-	public static String getResponseContentString(HttpGet httpget, CloseableHttpClient httpClient) {
-		String responseContent = null;
-		CloseableHttpResponse response = null;
-		try {
-			response = httpClient.execute(httpget);
-			// check response status is 200
-			if (response.getStatusLine().getStatusCode() == 200) {
-				HttpEntity entity = response.getEntity();
-				if (entity != null) {
-					responseContent = EntityUtils.toString(entity, Constants.UTF_8);
-				} else {
-					logger.warn("http entity is null");
-				}
-			} else {
-				logger.error("http get:{} response status code is not 200!", response.getStatusLine().getStatusCode());
-			}
-		} catch (IOException ioe) {
-			logger.error(ioe.getMessage(), ioe);
-		} finally {
-			try {
-				if (response != null) {
-					EntityUtils.consume(response.getEntity());
-					response.close();
-				}
-			} catch (IOException e) {
-				logger.error(e.getMessage(), e);
-			}
-			if (!httpget.isAborted()) {
-				httpget.releaseConnection();
-				httpget.abort();
-			}
-			try {
-				httpClient.close();
-			} catch (IOException e) {
-				logger.error(e.getMessage(), e);
-			}
+	    } else {
+		logger.error("http get:{} response status code is not 200!", response.getStatusLine().getStatusCode());
+	    }
+	} catch (IOException ioe) {
+	    logger.error(ioe.getMessage(), ioe);
+	} finally {
+	    try {
+		if (response != null) {
+		    EntityUtils.consume(response.getEntity());
+		    response.close();
 		}
-		return responseContent;
+	    } catch (IOException e) {
+		logger.error(e.getMessage(), e);
+	    }
+	    if (!httpget.isAborted()) {
+		httpget.releaseConnection();
+		httpget.abort();
+	    }
+	    try {
+		httpClient.close();
+	    } catch (IOException e) {
+		logger.error(e.getMessage(), e);
+	    }
 	}
+	return responseContent;
+    }
 
 }
