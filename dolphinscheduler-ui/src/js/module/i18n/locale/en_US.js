941bb4dce48a486b0f002d8fa98d14a0d9782040
xsbai <xs.bai93@qq.com>
2020-07-28 11:01:34 +0800

    Update en_US.js
    Add  'Please select the waterdrop resources'

--- dolphinscheduler-ui/src/js/module/i18n/locale/en_US.js
+++ dolphinscheduler-ui/src/js/module/i18n/locale/en_US.js
@@ -619,6 +619,7 @@ export default {
   'User name length is between 3 and 39': 'User name length is between 3 and 39',
   'Please Enter Url': 'Please Enter Url eg. 127.0.0.1:7077',
   'Master': 'Master',
+  'Please select the waterdrop resources':'Please select the waterdrop resources',
   zkDirectory: 'zkDirectory',
   'Directory detail': 'Directory detail',
   'Connection name': 'Connection name',
