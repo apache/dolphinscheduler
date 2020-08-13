26ee30d61b4dcf8ec0aada07932e35f67088b902
break60 <790061044@qq.com>
2020-08-13 17:13:28 +0800

    [fixBug-3058][ui]Fix connection abnormalities in historical workflow instance data

--- dolphinscheduler-ui/src/js/conf/home/pages/dag/_source/formModel/tasks/spark.vue
+++ dolphinscheduler-ui/src/js/conf/home/pages/dag/_source/formModel/tasks/spark.vue
@@ -335,10 +335,8 @@
           resourceIdArr = isResourceId.map(item=>{
             return item.id
           })
-          Array.prototype.diff = function(a) {
-            return this.filter(function(i) {return a.indexOf(i) < 0;});
-          };
-          let diffSet = this.resourceList.diff(resourceIdArr);
+          let diffSet
+          diffSet = _.xorWith(this.resourceList, resourceIdArr, _.isEqual)
           let optionsCmp = []
           if(diffSet.length>0) {
             diffSet.forEach(item=>{
@@ -525,7 +523,7 @@
         // Non-null objects represent backfill
         if (!_.isEmpty(o)) {
           this.mainClass = o.params.mainClass || ''
-          if(o.params.mainJar.res) {
+          if(!o.params.mainJar.id) {
             this.marjarId(o.params.mainJar.res)
           } else if(o.params.mainJar.res=='') {
             this.mainJar = ''
