ddfc2d004420e7d988764a06057ebdc315d27196
xingchun-chen <55787491+xingchun-chen@users.noreply.github.com>
2020-08-17 14:10:46 +0800

    Update BrowserCommon.java

--- e2e/src/test/java/org/apache/dolphinscheduler/common/BrowserCommon.java
+++ e2e/src/test/java/org/apache/dolphinscheduler/common/BrowserCommon.java
@@ -23,7 +23,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import redis.clients.jedis.Jedis;
-
+import org.openqa.selenium.JavascriptExecutor;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
