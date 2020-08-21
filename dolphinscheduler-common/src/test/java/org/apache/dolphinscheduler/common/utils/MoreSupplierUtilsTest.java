70aeca5e20a15905dd3d422d48a4a3ef36eaa781
Yichao Yang <1048262223@qq.com>
2020-08-21 17:57:53 +0800

    ImportOrder

--- dolphinscheduler-common/src/test/java/org/apache/dolphinscheduler/common/utils/MoreSupplierUtilsTest.java
+++ dolphinscheduler-common/src/test/java/org/apache/dolphinscheduler/common/utils/MoreSupplierUtilsTest.java
@@ -17,14 +17,14 @@
 
 package org.apache.dolphinscheduler.common.utils;
 
+import org.apache.dolphinscheduler.common.utils.MoreSupplierUtils.LazyCloseableSupplier;
+import org.apache.dolphinscheduler.common.utils.MoreSupplierUtils.LazyCloseableThrowableSupplier;
+
 import java.io.Closeable;
 import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.function.Supplier;
-
-import org.apache.dolphinscheduler.common.utils.MoreSupplierUtils.LazyCloseableSupplier;
-import org.apache.dolphinscheduler.common.utils.MoreSupplierUtils.LazyCloseableThrowableSupplier;
 
 import org.junit.Assert;
 import org.junit.Test;
