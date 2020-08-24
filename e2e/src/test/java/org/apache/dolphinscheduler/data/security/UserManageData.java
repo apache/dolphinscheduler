779ead6e538b6c8a535f36ba5c91d6c80351f769
xingchun-chen <55787491+xingchun-chen@users.noreply.github.com>
2020-08-24 17:16:20 +0800

    [test-2995][e2e]Optimize waiting time  (#3571)
    * Add task connection 
    
    * Optimize test cases
     
    
    * Modify variable format
     
     
    
    * Optimize test cases
     
    
    * Update BrowserCommon.java
    
    * Update BrowserCommon.java
    
    * Update WorkflowDefineLocator.java
    
    * Optimize waiting time
     
    
    * Optimize wait time
     
    
    * Optimize wait time
     
    
    * Optimize wait time
     
    
    * Optimize wait time
     
    
    * Optimize wait time
     
    
    * Optimize wait time
     
    
    * Optimize wait time
     
    
    * Optimize wait time
     
    
    * Optimize wait time
     
    
    * Optimize project wait time
     
    
    * Optimize wait time
     
    
    * Optimize wait time
     
    
    Co-authored-by: chenxingchun <438044805@qq.com>

--- e2e/src/test/java/org/apache/dolphinscheduler/data/security/UserManageData.java
+++ e2e/src/test/java/org/apache/dolphinscheduler/data/security/UserManageData.java
@@ -31,6 +31,9 @@ public class UserManageData  {
 
     public static final String USER_MANAGE = "用户管理 - DolphinScheduler";
 
+    public static final String CREATE_USER_BUTTON = "创建用户";
+
+
     /**
      * edit user
      */
