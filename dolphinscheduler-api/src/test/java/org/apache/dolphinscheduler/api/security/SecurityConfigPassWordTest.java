840426a53eced111984bfb7da3ada6975688fc32
geosmart <geosmart@hotmail.com>
2020-09-01 10:55:14 +0800

    [FEATURE-736] add unit test for SecurityConfig

--- dolphinscheduler-api/src/test/java/org/apache/dolphinscheduler/api/security/SecurityConfigPassWordTest.java
+++ dolphinscheduler-api/src/test/java/org/apache/dolphinscheduler/api/security/SecurityConfigPassWordTest.java
@@ -23,6 +23,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -31,7 +32,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @TestPropertySource(properties = {
         "security.authentication.type=PASSWORD",
 })
-public class SecurityConfigTest {
+@ActiveProfiles("api")
+public class SecurityConfigPassWordTest {
 
     @Autowired
     private SecurityConfig securityConfig;
