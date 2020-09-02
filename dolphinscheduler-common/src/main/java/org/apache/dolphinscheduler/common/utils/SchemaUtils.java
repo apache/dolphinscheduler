8ddabf60992a6e6c1391672ce353e1345a9b2585
Yichao Yang <1048262223@qq.com>
2020-09-02 15:59:47 +0800

    [Improvement][common] Add UnsupportedOperationException for utils construct (#3381)
    * [Improvement][common] Add UnsupportedOperationException for utils construct
    
    * Fix checkstyle

--- dolphinscheduler-common/src/main/java/org/apache/dolphinscheduler/common/utils/SchemaUtils.java
+++ dolphinscheduler-common/src/main/java/org/apache/dolphinscheduler/common/utils/SchemaUtils.java
@@ -14,10 +14,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.dolphinscheduler.common.utils;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+package org.apache.dolphinscheduler.common.utils;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -29,113 +27,123 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Metadata related common classes
- *
  */
 public class SchemaUtils {
 
-	private static final Logger logger = LoggerFactory.getLogger(SchemaUtils.class);
-	private static Pattern p = Pattern.compile("\\s*|\t|\r|\n");
+    private static final Logger logger = LoggerFactory.getLogger(SchemaUtils.class);
+    private static Pattern p = Pattern.compile("\\s*|\t|\r|\n");
 
-	/**
-	 * Gets upgradable schemas for all upgrade directories
-	 * @return all schema list
-	 */
-	@SuppressWarnings("unchecked")
-	public static List<String> getAllSchemaList() {
-		List<String> schemaDirList = new ArrayList<>();
-		File[] schemaDirArr = FileUtils.getAllDir("sql/upgrade");
-		if(schemaDirArr == null || schemaDirArr.length == 0) {
-			return null;
-		}
+    private SchemaUtils() {
+        throw new UnsupportedOperationException("Construct SchemaUtils");
+    }
 
-		for(File file : schemaDirArr) {
-			schemaDirList.add(file.getName());
-		}
+    /**
+     * Gets upgradable schemas for all upgrade directories
+     *
+     * @return all schema list
+     */
+    @SuppressWarnings("unchecked")
+    public static List<String> getAllSchemaList() {
+        List<String> schemaDirList = new ArrayList<>();
+        File[] schemaDirArr = FileUtils.getAllDir("sql/upgrade");
+        if (schemaDirArr == null || schemaDirArr.length == 0) {
+            return null;
+        }
 
-		Collections.sort(schemaDirList , new Comparator() {
-			@Override
-			public int compare(Object o1 , Object o2){
-				try {
-					String dir1 = String.valueOf(o1);
-					String dir2 = String.valueOf(o2);
-					String version1 = dir1.split("_")[0];
-					String version2 = dir2.split("_")[0];
-					if(version1.equals(version2)) {
-						return 0;
-					}
+        for (File file : schemaDirArr) {
+            schemaDirList.add(file.getName());
+        }
 
-					if(SchemaUtils.isAGreatVersion(version1, version2)) {
-						return 1;
-					}
+        Collections.sort(schemaDirList, new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                try {
+                    String dir1 = String.valueOf(o1);
+                    String dir2 = String.valueOf(o2);
+                    String version1 = dir1.split("_")[0];
+                    String version2 = dir2.split("_")[0];
+                    if (version1.equals(version2)) {
+                        return 0;
+                    }
 
-					return -1;
+                    if (SchemaUtils.isAGreatVersion(version1, version2)) {
+                        return 1;
+                    }
 
-				} catch (Exception e) {
-					logger.error(e.getMessage(),e);
-					throw new RuntimeException(e);
-				}
-			}
-		});
+                    return -1;
 
-		return schemaDirList;
-	}
+                } catch (Exception e) {
+                    logger.error(e.getMessage(), e);
+                    throw new RuntimeException(e);
+                }
+            }
+        });
 
-	/**
-	 * Determine whether schemaVersion is higher than version
-	 * @param schemaVersion schema version
-	 * @param version version
-	 * @return  Determine whether schemaVersion is higher than version
-	 */
-	public static boolean isAGreatVersion(String schemaVersion, String version) {
-		if(StringUtils.isEmpty(schemaVersion) || StringUtils.isEmpty(version)) {
-			throw new RuntimeException("schemaVersion or version is empty");
-		}
+        return schemaDirList;
+    }
 
-		String[] schemaVersionArr = schemaVersion.split("\\.");
-		String[] versionArr = version.split("\\.");
-		int arrLength = Math.min(schemaVersionArr.length, versionArr.length);
-		for(int i = 0 ; i < arrLength ; i++) {
-			if(Integer.parseInt(schemaVersionArr[i]) > Integer.parseInt(versionArr[i])) {
-				return true;
-			}else if(Integer.parseInt(schemaVersionArr[i]) < Integer.parseInt(versionArr[i])) {
-				return false;
-			}
-		}
+    /**
+     * Determine whether schemaVersion is higher than version
+     *
+     * @param schemaVersion schema version
+     * @param version version
+     * @return Determine whether schemaVersion is higher than version
+     */
+    public static boolean isAGreatVersion(String schemaVersion, String version) {
+        if (StringUtils.isEmpty(schemaVersion) || StringUtils.isEmpty(version)) {
+            throw new RuntimeException("schemaVersion or version is empty");
+        }
 
-		// If the version and schema version is the same from 0 up to the arrlength-1 element,whoever has a larger arrLength has a larger version number
-		return schemaVersionArr.length > versionArr.length;
-	}
+        String[] schemaVersionArr = schemaVersion.split("\\.");
+        String[] versionArr = version.split("\\.");
+        int arrLength = Math.min(schemaVersionArr.length, versionArr.length);
+        for (int i = 0; i < arrLength; i++) {
+            if (Integer.parseInt(schemaVersionArr[i]) > Integer.parseInt(versionArr[i])) {
+                return true;
+            } else if (Integer.parseInt(schemaVersionArr[i]) < Integer.parseInt(versionArr[i])) {
+                return false;
+            }
+        }
 
-	/**
-	 * Gets the current software version number of the system
-	 * @return current software version
-	 */
-	public static String getSoftVersion() {
-		String soft_version;
-		try {
-			soft_version = FileUtils.readFile2Str(new FileInputStream(new File("sql/soft_version")));
-			soft_version = replaceBlank(soft_version);
-		} catch (FileNotFoundException e) {
-			logger.error(e.getMessage(),e);
-			throw new RuntimeException("Failed to get the product version description file. The file could not be found", e);
-		}
-		return soft_version;
-	}
+        // If the version and schema version is the same from 0 up to the arrlength-1 element,whoever has a larger arrLength has a larger version number
+        return schemaVersionArr.length > versionArr.length;
+    }
 
-	/**
-	 * Strips the string of space carriage returns and tabs
-	 * @param str string
-	 * @return string removed blank
-	 */
-	public static String replaceBlank(String str) {
-		String dest = "";
-		if (str!=null) {
+    /**
+     * Gets the current software version number of the system
+     *
+     * @return current software version
+     */
+    public static String getSoftVersion() {
+        String softVersion;
+        try {
+            softVersion = FileUtils.readFile2Str(new FileInputStream(new File("sql/soft_version")));
+            softVersion = replaceBlank(softVersion);
+        } catch (FileNotFoundException e) {
+            logger.error(e.getMessage(), e);
+            throw new RuntimeException("Failed to get the product version description file. The file could not be found", e);
+        }
+        return softVersion;
+    }
 
-			Matcher m = p.matcher(str);
-			dest = m.replaceAll("");
-		}
-		return dest;
-	}
+    /**
+     * Strips the string of space carriage returns and tabs
+     *
+     * @param str string
+     * @return string removed blank
+     */
+    public static String replaceBlank(String str) {
+        String dest = "";
+        if (str != null) {
+
+            Matcher m = p.matcher(str);
+            dest = m.replaceAll("");
+        }
+        return dest;
+    }
 }
