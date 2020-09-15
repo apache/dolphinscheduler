02bd10d6b5d74c2056b0e1b83334ac7114b1af03
qiaozhanwei <qiaozhanwei@analysys.com.cn>
2020-09-15 15:51:06 +0800

    [Fix-3616][Server] when worker akc/response master exception , async retry

--- dolphinscheduler-server/src/main/java/org/apache/dolphinscheduler/server/worker/cache/ResponceCache.java
+++ dolphinscheduler-server/src/main/java/org/apache/dolphinscheduler/server/worker/cache/ResponceCache.java
@@ -25,7 +25,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class ResponceCache {
 
-    private static volatile ResponceCache instance = null;
+    private static  ResponceCache instance = null;
 
     private ResponceCache(){}
 
