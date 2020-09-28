de2dad5944dc3f5aa6b1b60e865f984dc451b882
baoliang <baoliang@analysys.com.cn>
2020-09-28 10:29:02 +0800

    change proess service test to executor service

--- dolphinscheduler-api/src/test/java/org/apache/dolphinscheduler/api/service/ExecutorService2Test.java
+++ dolphinscheduler-api/src/test/java/org/apache/dolphinscheduler/api/service/ExecutorService2Test.java
@@ -22,7 +22,9 @@ import org.apache.dolphinscheduler.common.enums.CommandType;
 import org.apache.dolphinscheduler.common.enums.Priority;
 import org.apache.dolphinscheduler.common.enums.ReleaseState;
 import org.apache.dolphinscheduler.common.enums.RunMode;
+import org.apache.dolphinscheduler.common.enums.WarningType;
 import org.apache.dolphinscheduler.common.model.Server;
+import org.apache.dolphinscheduler.common.utils.JSONUtils;
 import org.apache.dolphinscheduler.dao.entity.*;
 import org.apache.dolphinscheduler.dao.mapper.ProcessDefinitionMapper;
 import org.apache.dolphinscheduler.dao.mapper.ProjectMapper;
@@ -42,6 +44,8 @@ import java.util.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.times;
+
+import com.alibaba.fastjson.JSONObject;
 
 /**
  * test for ExecutorService
@@ -259,4 +263,74 @@ public class ExecutorService2Test {
         result.put(Constants.STATUS, Status.SUCCESS);
         return result;
     }
+
+    @Test
+    public void testCreateSubCommand() {
+
+        ProcessService processService1 = new ProcessService();
+        ProcessInstance parentInstance = new ProcessInstance();
+        parentInstance.setProcessDefinitionId(1);
+        parentInstance.setWarningType(WarningType.SUCCESS);
+        parentInstance.setWarningGroupId(0);
+
+        TaskInstance task = new TaskInstance();
+        task.setTaskJson("{params:{processDefinitionId:100}}");
+        task.setId(10);
+
+        ProcessInstance childInstance = null;
+        ProcessInstanceMap instanceMap = new ProcessInstanceMap();
+        instanceMap.setParentProcessInstanceId(1);
+        instanceMap.setParentTaskInstanceId(10);
+        Command command = null;
+
+        //father history: start; child null == command type: start
+        parentInstance.setHistoryCmd("START_PROCESS");
+        parentInstance.setCommandType(CommandType.START_PROCESS);
+        command = processService1.createSubProcessCommand(
+                parentInstance, childInstance, instanceMap, task
+        );
+        Assert.assertEquals(CommandType.START_PROCESS, command.getCommandType());
+
+        //father history: start,start failure; child null == command type: start
+        parentInstance.setCommandType(CommandType.START_FAILURE_TASK_PROCESS);
+        parentInstance.setHistoryCmd("START_PROCESS,START_FAILURE_TASK_PROCESS");
+        command = processService1.createSubProcessCommand(
+                parentInstance, childInstance, instanceMap, task
+        );
+        Assert.assertEquals(CommandType.START_PROCESS, command.getCommandType());
+
+
+        //father history: scheduler,start failure; child null == command type: scheduler
+        parentInstance.setCommandType(CommandType.START_FAILURE_TASK_PROCESS);
+        parentInstance.setHistoryCmd("SCHEDULER,START_FAILURE_TASK_PROCESS");
+        command = processService1.createSubProcessCommand(
+                parentInstance, childInstance, instanceMap, task
+        );
+        Assert.assertEquals(CommandType.SCHEDULER, command.getCommandType());
+
+        //father history: complement,start failure; child null == command type: complement
+        parentInstance.setCommandType(CommandType.START_FAILURE_TASK_PROCESS);
+        parentInstance.setHistoryCmd("COMPLEMENT_DATA,START_FAILURE_TASK_PROCESS");
+        parentInstance.setCommandParam("{complementStartDate:'2020-01-01',complementEndDate:'2020-01-10'}");
+        command = processService1.createSubProcessCommand(
+                parentInstance, childInstance, instanceMap, task
+        );
+        Assert.assertEquals(CommandType.COMPLEMENT_DATA, command.getCommandType());
+
+        JSONObject complementDate = JSONUtils.parseObject(command.getCommandParam());
+        Assert.assertEquals("2020-01-01", String.valueOf(complementDate.get(Constants.CMDPARAM_COMPLEMENT_DATA_START_DATE)));
+        Assert.assertEquals("2020-01-10", String.valueOf(complementDate.get(Constants.CMDPARAM_COMPLEMENT_DATA_END_DATE)));
+
+        //father history: start,failure,start failure; child not null == command type: start failure
+        childInstance = new ProcessInstance();
+        parentInstance.setCommandType(CommandType.START_FAILURE_TASK_PROCESS);
+        parentInstance.setHistoryCmd("START_PROCESS,START_FAILURE_TASK_PROCESS");
+        command = processService1.createSubProcessCommand(
+                parentInstance, childInstance, instanceMap, task
+        );
+        Assert.assertEquals(CommandType.START_FAILURE_TASK_PROCESS, command.getCommandType());
+
+
+    }
+
 }
