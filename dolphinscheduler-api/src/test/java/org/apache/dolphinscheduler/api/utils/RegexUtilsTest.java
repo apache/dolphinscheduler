85b1741c885c8b9e80cdb106f86dfe7bbfbb54d7
hermeshephaestus <turingdojo@163.com>
2020-10-02 00:57:57 +0800

    bug fixed #3840

---
+++ dolphinscheduler-api/src/test/java/org/apache/dolphinscheduler/api/utils/RegexUtilsTest.java
@@ -0,0 +1,11 @@
+package org.apache.dolphinscheduler.api.utils;
+
+/**
+ * @author cf.ge@tuya.com
+ * @version 1.0
+ * @company Tuya Inc.
+ * @description todo
+ * @date 2020-10-02 00:55
+ */
+public class RegexUtilsTest {
+}
