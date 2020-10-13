3fbf912dc00bbd11819c083e8f990b4158f996cd
break60 <790061044@qq.com>
2020-10-13 10:05:37 +0800

    fix

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/utils/RegexUtils.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/utils/RegexUtils.java
@@ -15,33 +15,6 @@
  * limitations under the License.
  */
 
-package org.apache.dolphinscheduler.dao.mapper;
-
-import org.apache.dolphinscheduler.dao.entity.AlertPluginInstance;
-
-import org.apache.ibatis.annotations.Param;
-
-import java.util.List;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface AlertPluginInstanceMapper extends BaseMapper<AlertPluginInstance> {
-
-    /**
-     * query all alert plugin instance
-     *
-     * @return AlertPluginInstance list
-     */
-    List<AlertPluginInstance> queryAllAlertPluginInstanceList();
-
-    /**
-     * query by alert group id
-     *
-     * @param alertGroupId
-     * @return AlertPluginInstance list
-     */
-    List<AlertPluginInstance> queryByAlertGroupId(@Param("alertGroupId") int alertGroupId);
-
 package org.apache.dolphinscheduler.api.utils;
 
 import java.util.regex.Matcher;
--- dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/mapper/AlertPluginInstanceMapper.java
+++ dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/mapper/AlertPluginInstanceMapper.java
@@ -41,34 +41,4 @@ public interface AlertPluginInstanceMapper extends BaseMapper<AlertPluginInstanc
      * @return AlertPluginInstance list
      */
     List<AlertPluginInstance> queryByAlertGroupId(@Param("alertGroupId") int alertGroupId);
-
-package org.apache.dolphinscheduler.api.utils;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-/**
- * This is Regex expression utils.
- */
-public class RegexUtils {
-
-    /**
-     * check number regex expression
-     */
-    private static final String CHECK_NUMBER = "^-?\\d+(\\.\\d+)?$";
-
-    private RegexUtils() {
-    }
-
-    /**
-     * check if the input is number
-     *
-     * @param str input
-     * @return
-     */
-    public static boolean isNumeric(String str) {
-        Pattern pattern = Pattern.compile(CHECK_NUMBER);
-        Matcher isNum = pattern.matcher(str);
-        return isNum.matches();
-    }
 }
