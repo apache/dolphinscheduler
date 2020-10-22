71e1d9cd43487aa125195b9d1b155a20989280ba
lgcareer <18610854716@163.com>
2020-10-22 23:38:47 +0800

    [Fix-#3618][server] resolve task executed finished but not release the file handle (#3975)
    * [Fix-#3618][server] resolve task executed finished but not release the file handle
    
    * [Fix-#3618][server] resolve task executed finished but not release the file handle

--- dolphinscheduler-server/src/main/java/org/apache/dolphinscheduler/server/worker/task/AbstractCommandExecutor.java
+++ dolphinscheduler-server/src/main/java/org/apache/dolphinscheduler/server/worker/task/AbstractCommandExecutor.java
@@ -319,12 +319,16 @@ public abstract class AbstractCommandExecutor {
      * clear
      */
     private void clear() {
+
+        List<String> markerList = new ArrayList<>();
+        markerList.add(ch.qos.logback.classic.ClassicConstants.FINALIZE_SESSION_MARKER.toString());
+
         if (!logBuffer.isEmpty()) {
             // log handle
             logHandler.accept(logBuffer);
-
             logBuffer.clear();
         }
+        logHandler.accept(markerList);
     }
 
     /**
