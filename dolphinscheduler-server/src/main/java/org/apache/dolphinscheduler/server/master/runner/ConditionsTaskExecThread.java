6caac0f36623cee4fa1cb2edfc35c7db87b08aa9
bao liang <29528966+lenboo@users.noreply.github.com>
2020-10-27 17:37:59 +0800

    [FIX-3929] condition task would post wrong tasks when failover. (#3999)
    * fix #3966 sub process doesnot send alert mail after process instance ending.
    
    * fix bug 3964: sub_process The timeout warning does not take effect
    add timeout warning for sub_process/dependent task.
    
    * fix code smell
    
    * fix code smell
    
    * fix code smell
    
    * update worker group inherit from parent
    
    * remove stdout in logback configuration
    
    * fix bug #3929 condition task would post error when failover.
    
    * remove unused test
    
    * add comments
    
    * add skip node judge
    
    Co-authored-by: baoliang <baoliang@analysys.com.cn>

--- dolphinscheduler-server/src/main/java/org/apache/dolphinscheduler/server/master/runner/ConditionsTaskExecThread.java
+++ dolphinscheduler-server/src/main/java/org/apache/dolphinscheduler/server/master/runner/ConditionsTaskExecThread.java
@@ -37,7 +37,6 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class ConditionsTaskExecThread extends MasterBaseTaskExecThread {
 
-
     /**
      * dependent parameters
      */
@@ -131,7 +130,6 @@ public class ConditionsTaskExecThread extends MasterBaseTaskExecThread {
         this.dependentParameters = JSONUtils.parseObject(this.taskInstance.getDependency(), DependentParameters.class);
     }
 
-
     /**
      * depend result for depend item
      * @param item
@@ -154,6 +152,5 @@ public class ConditionsTaskExecThread extends MasterBaseTaskExecThread {
                 Constants.DEPENDENT_SPLIT, item.getDepTasks(), dependResult);
         return dependResult;
     }
-
 
 }
