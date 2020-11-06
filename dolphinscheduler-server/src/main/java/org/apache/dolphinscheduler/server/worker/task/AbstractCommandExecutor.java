e826c4a5763f808971cb2f9211a1ba66ba7629ad
baoliang <baoliang@analysys.com.cn>
2020-11-06 11:44:34 +0800

    merge from 1.3.3-release

--- dolphinscheduler-server/src/main/java/org/apache/dolphinscheduler/server/worker/task/AbstractCommandExecutor.java
+++ dolphinscheduler-server/src/main/java/org/apache/dolphinscheduler/server/worker/task/AbstractCommandExecutor.java
@@ -202,6 +202,9 @@ public abstract class AbstractCommandExecutor {
         return result;
     }
 
+    public String getVarPool() {
+        return varPool.toString();
+    }
 
     /**
      * cancel application
