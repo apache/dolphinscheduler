f4ef86c50bc25cb39d48565025651b775433b934
江蓠 <acm_master@163.com>
2020-11-13 11:16:05 +0800

    [FEATURE#4029] alert spi support script alert
    this closes #4029

---
+++ dolphinscheduler-alert-plugin/dolphinscheduler-alert-script/src/main/java/org/apache/dolphinscheduler/plugin/alert/script/ScriptParamsConstants.java
@@ -0,0 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.dolphinscheduler.plugin.alert.script;
+
+/**
+ * ScriptParamsConstants
+ */
+public class ScriptParamsConstants {
+
+    static final String SCRIPT_TYPE = "script.type";
+
+    static final String NAME_SCRIPT_TYPE = "scriptType";
+
+    static final String SCRIPT_PATH = "script.path";
+
+    static final String NAME_SCRIPT_PATH = "scriptPath";
+
+    static final String SCRIPT_USER_PARAMS = "script.user.params";
+
+    static final String NAME_SCRIPT_USER_PARAMS = "scriptUserParams";
+}
