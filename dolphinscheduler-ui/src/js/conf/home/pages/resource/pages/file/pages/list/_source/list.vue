465a1dbcc3ad2c1d0290c323aa7054da6af2c9e9
chengshiwen <chengshiwen0103@gmail.com>
2020-11-26 17:30:34 +0800

    improvement: resovle download url with resolveURL to prevent change of apiPrefix

--- dolphinscheduler-ui/src/js/conf/home/pages/resource/pages/file/pages/list/_source/list.vue
+++ dolphinscheduler-ui/src/js/conf/home/pages/resource/pages/file/pages/list/_source/list.vue
@@ -162,7 +162,7 @@
         }
       },
       _downloadFile (item) {
-        downloadFile('/dolphinscheduler/resources/download', {
+        downloadFile('resources/download', {
           id: item.id
         })
       },
