520f8da9c59b4f186ede0bbb38f7938b12d5adf3
Mujuzi Moses <mujuzimoses@gmail.com>
2020-12-03 16:57:38 +0300

    Fixed UnitTest/check code style

--- dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/upgrade/DolphinSchedulerManager.java
+++ dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/upgrade/DolphinSchedulerManager.java
@@ -16,12 +16,12 @@
  */
 package org.apache.dolphinscheduler.dao.upgrade;
 
-import java.util.List;
-
 import org.apache.dolphinscheduler.common.enums.DbType;
 import org.apache.dolphinscheduler.common.utils.SchemaUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 /**
  * upgrade manager
@@ -109,18 +109,18 @@ public class DolphinSchedulerManager {
             }
             // The target version of the upgrade
             String schemaVersion = "";
-			String schemaDir = schemaList.get(schemaList.size() - 1);
-			schemaVersion = schemaDir.split("_")[0];
-			if (SchemaUtils.isAGreatVersion(schemaVersion, version)) {
-				logger.info("upgrade DolphinScheduler metadata version from {} to {}", version, schemaVersion);
-				logger.info("Begin upgrading DolphinScheduler's table structure");
-				upgradeDao.upgradeDolphinScheduler(schemaDir);
-				if ("1.3.0".equals(schemaVersion)) {
-					upgradeDao.upgradeDolphinSchedulerWorkerGroup();
-				}
-				version = schemaVersion;
-			}
-			
+            String schemaDir = schemaList.get(schemaList.size() - 1);
+                schemaVersion = schemaDir.split("_")[0];
+                if(SchemaUtils.isAGreatVersion(schemaVersion , version)) {
+                    logger.info("upgrade DolphinScheduler metadata version from {} to {}", version, schemaVersion);
+                    logger.info("Begin upgrading DolphinScheduler's table structure");
+                    upgradeDao.upgradeDolphinScheduler(schemaDir);
+                    if ("1.3.0".equals(schemaVersion)) {
+                        upgradeDao.upgradeDolphinSchedulerWorkerGroup();
+                    }
+                    version = schemaVersion;
+                }
+
         }
 
         // Assign the value of the version field in the version table to the version of the product
