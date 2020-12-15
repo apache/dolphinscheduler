c699890290455a7032cb5f0e45000a3bfca31710
CalvinKirs <acm_master@163.com>
2020-12-15 15:06:52 +0800

    [FIX-3177]Task time parameter parsing error when system.datetime !=null $[datetime] = system.datetime else $[datetime] = current time

--- dolphinscheduler-server/src/main/java/org/apache/dolphinscheduler/server/worker/task/shell/ShellTask.java
+++ dolphinscheduler-server/src/main/java/org/apache/dolphinscheduler/server/worker/task/shell/ShellTask.java
@@ -136,20 +136,8 @@ public class ShellTask extends AbstractTask {
             shellParameters.getLocalParametersMap(),
             CommandType.of(taskExecutionContext.getCmdTypeIfComplement()),
             taskExecutionContext.getScheduleTime());
-
         // replace variable TIME with $[YYYYmmddd...] in shell file when history run job and batch complement job
-        if (paramsMap != null) {
-            if (taskExecutionContext.getScheduleTime() != null) {
-                String dateTime = DateUtils.format(taskExecutionContext.getScheduleTime(), Constants.PARAMETER_FORMAT_TIME);
-                Property p = new Property();
-                p.setValue(dateTime);
-                p.setProp(Constants.PARAMETER_SHECDULE_TIME);
-                paramsMap.put(Constants.PARAMETER_SHECDULE_TIME, p);
-            }
-        }
-
-        script = ParameterUtils.convertParameterPlaceholders2(script, ParamUtils.convert(paramsMap));
-
+        script = ParameterUtils.convertParameterPlaceholders(script, ParamUtils.convert(paramsMap));
         shellParameters.setRawScript(script);
 
         logger.info("raw script : {}", shellParameters.getRawScript());
