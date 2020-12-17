6489af869ffd5878a9519c816110a91735431ca8
felix.wang <59079269+felix-thinkingdata@users.noreply.github.com>
2020-12-17 17:39:47 +0800

    [FIX][UI ]  security user state  tenantCode User Type Display abnormal (#4255)
    * fix user state error
    
    * fix userType  error
    
    * fix tenantCode  error

--- dolphinscheduler-ui/src/js/conf/home/pages/security/pages/users/_source/list.vue
+++ dolphinscheduler-ui/src/js/conf/home/pages/security/pages/users/_source/list.vue
@@ -21,14 +21,18 @@
         <el-table-column type="index" :label="$t('#')" width="50"></el-table-column>
         <el-table-column prop="userName" :label="$t('User Name')"></el-table-column>
         <el-table-column :label="$t('User Type')">
-          {{userType === 'GENERAL_USER' ? `${$t('Ordinary users')}` : `${$t('Administrator')}`}}
+          <template slot-scope="scope">
+            <span>{{scope.row.userType === 'GENERAL_USER'? `${$t('Ordinary users')}` : `${$t('Administrator')}`}}</span>
+          </template>
         </el-table-column>
-        <el-table-column prop="tenantName" :label="$t('Tenant')" width="160"></el-table-column>
+        <el-table-column prop="tenantCode" :label="$t('Tenant')" width="160"></el-table-column>
         <el-table-column prop="queue" :label="$t('Queue')"></el-table-column>
         <el-table-column prop="email" :label="$t('Email')" min-width="120"></el-table-column>
         <el-table-column prop="phone" :label="$t('Phone')" min-width="90"></el-table-column>
-        <el-table-column :label="$t('State')">
-          {{state === 1 ? `${$t('Enable')}` : `${$t('Disable')}`}}
+        <el-table-column  :label="$t('State')">
+          <template slot-scope="scope">
+            <span>{{scope.row.state === 1? `${$t('Enable')}` : `${$t('Disable')}`}}</span>
+          </template>
         </el-table-column>
         <el-table-column :label="$t('Create Time')" min-width="120">
           <template slot-scope="scope">
