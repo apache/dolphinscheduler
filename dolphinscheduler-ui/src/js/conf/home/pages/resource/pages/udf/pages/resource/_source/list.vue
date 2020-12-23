e2b21a7ec03e267838d0a5d064a7e175a52a12a1
break60 <790061044@qq.com>
2020-12-23 09:39:33 +0800

    [Improvement][ui]List vacancy optimization and icon icon repair (#4286)
    * [Improvement][ui] List vacancy optimization and icon icon repair

--- dolphinscheduler-ui/src/js/conf/home/pages/resource/pages/udf/pages/resource/_source/list.vue
+++ dolphinscheduler-ui/src/js/conf/home/pages/resource/pages/udf/pages/resource/_source/list.vue
@@ -40,7 +40,11 @@
             {{_rtSize(scope.row.size)}}
           </template>
         </el-table-column>
-        <el-table-column prop="description" :label="$t('Description')" width="200"></el-table-column>
+        <el-table-column :label="$t('Description')" width="200">
+          <template slot-scope="scope">
+            <span>{{scope.row.description | filterNull}}</span>
+          </template>
+        </el-table-column>
         <el-table-column :label="$t('Create Time')" min-width="120">
           <template slot-scope="scope">
             <span>{{scope.row.createTime | formatDate}}</span>
