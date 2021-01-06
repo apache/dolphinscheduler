363b19ee99bb47d9af6d23211c6f488277ceb698
Segun Ogundipe <davephenoms@gmail.com>
2021-01-06 18:19:51 +0100

    chore: Update test cases to increase coverage

--- dolphinscheduler-api/src/test/java/org/apache/dolphinscheduler/api/controller/ResourcesControllerTest.java
+++ dolphinscheduler-api/src/test/java/org/apache/dolphinscheduler/api/controller/ResourcesControllerTest.java
@@ -58,7 +58,6 @@ public class ResourcesControllerTest extends AbstractControllerTest {
         logger.info(mvcResult.getResponse().getContentAsString());
     }
 
-
     @Test
     public void testQueryResourceListPaging() throws Exception {
         MultiValueMap<String, String> paramsMap = new LinkedMultiValueMap<>();
@@ -80,7 +79,6 @@ public class ResourcesControllerTest extends AbstractControllerTest {
         Assert.assertEquals(Status.SUCCESS.getCode(), result.getCode().intValue());
         logger.info(mvcResult.getResponse().getContentAsString());
     }
-
 
     @Test
     public void testVerifyResourceName() throws Exception {
@@ -110,7 +108,6 @@ public class ResourcesControllerTest extends AbstractControllerTest {
         paramsMap.add("skipLineNum", "2");
         paramsMap.add("limit", "100");
 
-
         MvcResult mvcResult = mockMvc.perform(get("/resources/view")
                 .header(SESSION_ID, sessionId)
                 .params(paramsMap))
@@ -134,7 +131,6 @@ public class ResourcesControllerTest extends AbstractControllerTest {
         paramsMap.add("description", "test");
         paramsMap.add("content", "echo 1111");
 
-
         MvcResult mvcResult = mockMvc.perform(post("/resources/online-create")
                 .header(SESSION_ID, sessionId)
                 .params(paramsMap))
@@ -154,7 +150,6 @@ public class ResourcesControllerTest extends AbstractControllerTest {
         MultiValueMap<String, String> paramsMap = new LinkedMultiValueMap<>();
         paramsMap.add("id", "1");
         paramsMap.add("content", "echo test_1111");
-
 
         MvcResult mvcResult = mockMvc.perform(post("/resources/update-content")
                 .header(SESSION_ID, sessionId)
@@ -208,7 +203,6 @@ public class ResourcesControllerTest extends AbstractControllerTest {
         logger.info(mvcResult.getResponse().getContentAsString());
     }
 
-
     @Test
     public void testCreateUdfFunc() throws Exception {
 
@@ -220,7 +214,6 @@ public class ResourcesControllerTest extends AbstractControllerTest {
         paramsMap.add("database", "database");
         paramsMap.add("description", "description");
         paramsMap.add("resourceId", "1");
-
 
         MvcResult mvcResult = mockMvc.perform(post("/resources/udf-func/create")
                 .header(SESSION_ID, sessionId)
@@ -254,7 +247,6 @@ public class ResourcesControllerTest extends AbstractControllerTest {
         logger.info(mvcResult.getResponse().getContentAsString());
     }
 
-
     @Test
     public void testUpdateUdfFunc() throws Exception {
 
@@ -281,7 +273,6 @@ public class ResourcesControllerTest extends AbstractControllerTest {
         logger.info(mvcResult.getResponse().getContentAsString());
     }
 
-
     @Test
     public void testQueryUdfFuncList() throws Exception {
         MultiValueMap<String, String> paramsMap = new LinkedMultiValueMap<>();
@@ -302,7 +293,6 @@ public class ResourcesControllerTest extends AbstractControllerTest {
         logger.info(mvcResult.getResponse().getContentAsString());
     }
 
-
     @Test
     public void testQueryResourceList() throws Exception {
         MultiValueMap<String, String> paramsMap = new LinkedMultiValueMap<>();
@@ -320,7 +310,6 @@ public class ResourcesControllerTest extends AbstractControllerTest {
         Assert.assertEquals(Status.SUCCESS.getCode(), result.getCode().intValue());
         logger.info(mvcResult.getResponse().getContentAsString());
     }
-
 
     @Test
     public void testVerifyUdfFuncName() throws Exception {
@@ -376,7 +365,6 @@ public class ResourcesControllerTest extends AbstractControllerTest {
         logger.info(mvcResult.getResponse().getContentAsString());
     }
 
-
     @Test
     public void testAuthorizedUDFFunction() throws Exception {
         MultiValueMap<String, String> paramsMap = new LinkedMultiValueMap<>();
@@ -413,7 +401,6 @@ public class ResourcesControllerTest extends AbstractControllerTest {
         logger.info(mvcResult.getResponse().getContentAsString());
     }
 
-
     @Test
     public void testDeleteUdfFunc() throws Exception {
         MultiValueMap<String, String> paramsMap = new LinkedMultiValueMap<>();
@@ -431,7 +418,6 @@ public class ResourcesControllerTest extends AbstractControllerTest {
         Assert.assertEquals(Status.SUCCESS.getCode(), result.getCode().intValue());
         logger.info(mvcResult.getResponse().getContentAsString());
     }
-
 
     @Test
     public void testDeleteResource() throws Exception {
