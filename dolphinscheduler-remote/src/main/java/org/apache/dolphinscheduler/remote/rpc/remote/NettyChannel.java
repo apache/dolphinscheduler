31738b5d2d0485f53d16e15c76454d44e8232a09
CalvinKirs <kirs@apache.org>
2021-01-13 14:00:38 +0800

    rpc

---
+++ dolphinscheduler-remote/src/main/java/org/apache/dolphinscheduler/remote/rpc/remote/NettyChannel.java
@@ -0,0 +1,8 @@
+package org.apache.dolphinscheduler.remote.rpc.remote;
+
+/**
+ * @author jiangli
+ * @date 2021-01-13 13:51
+ */
+public class NettyChannel {
+}
---
+++ dolphinscheduler-remote/src/main/java/org/apache/dolphinscheduler/remote/rpc/remote/NettyClientHandler.java
@@ -0,0 +1,40 @@
+package org.apache.dolphinscheduler.remote.rpc.remote;
+
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @author jiangli
+ * @date 2021-01-13 13:33
+ */
+@ChannelHandler.Sharable
+public class NettyClientHandler extends ChannelInboundHandlerAdapter {
+
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        super.channelRegistered(ctx);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        super.channelRead(ctx, msg);
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        super.userEventTriggered(ctx, evt);
+    }
+}
