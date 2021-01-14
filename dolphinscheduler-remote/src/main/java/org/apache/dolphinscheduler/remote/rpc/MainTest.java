2e4e73c5e0e811d06189f421412c77053955f4ce
CalvinKirs <kirs@apache.org>
2021-01-15 06:47:02 +0800

    rpc

--- dolphinscheduler-remote/src/main/java/org/apache/dolphinscheduler/remote/rpc/MainTest.java
+++ dolphinscheduler-remote/src/main/java/org/apache/dolphinscheduler/remote/rpc/MainTest.java
@@ -24,7 +24,7 @@ public class MainTest {
         Host host = new Host("127.0.0.1", 12366);
 
         IRpcClient rpcClient = new RpcClient();
-        IUserService userService = rpcClient.create(UserService.class);
+        UserService userService = rpcClient.create(UserService.class);
         String result = userService.say("calvin");
         System.out.println("我是你爸爸吧"+result);
 
