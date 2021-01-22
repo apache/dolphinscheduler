ff67386ce3ab12e6c56ac3fa8b30543a320c604b
CalvinKirs <kirs@apache.org>
2021-01-22 10:05:01 +0800

    update

--- dolphinscheduler-remote/src/main/java/org/apache/dolphinscheduler/remote/rpc/client/ConsumerInterceptor.java
+++ dolphinscheduler-remote/src/main/java/org/apache/dolphinscheduler/remote/rpc/client/ConsumerInterceptor.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.dolphinscheduler.remote.rpc.client;
 
 import org.apache.dolphinscheduler.remote.exceptions.RemotingException;
@@ -5,7 +22,6 @@ import org.apache.dolphinscheduler.remote.rpc.Invoker;
 import org.apache.dolphinscheduler.remote.rpc.base.Rpc;
 import org.apache.dolphinscheduler.remote.rpc.common.RpcRequest;
 import org.apache.dolphinscheduler.remote.rpc.common.RpcResponse;
-import org.apache.dolphinscheduler.remote.rpc.filter.FilterChain;
 import org.apache.dolphinscheduler.remote.rpc.remote.NettyClient;
 import org.apache.dolphinscheduler.remote.utils.Host;
 
@@ -24,23 +40,17 @@ public class ConsumerInterceptor {
     private Invoker invoker;
 
 
-    private FilterChain filterChain;
-
     private Host host;
 
     private NettyClient nettyClient = NettyClient.getInstance();
 
-    public ConsumerInterceptor(Invoker invoker, Host host) {
-        this.filterChain = new FilterChain(invoker);
-        this.invoker = this.filterChain.buildFilterChain();
+    public ConsumerInterceptor(Host host) {
         this.host = host;
     }
-
 
     @RuntimeType
     public Object intercept(@AllArguments Object[] args, @Origin Method method) throws RemotingException {
         RpcRequest request = buildReq(args, method);
-
 
         String serviceName = method.getDeclaringClass().getName() + method.getName();
         ConsumerConfig consumerConfig = ConsumerConfigCache.getConfigByServersName(serviceName);
@@ -52,7 +62,7 @@ public class ConsumerInterceptor {
         int retries = consumerConfig.getRetries();
 
         while (retries-- > 0) {
-            RpcResponse rsp = (RpcResponse) nettyClient.sendMsg(host, request, async);
+            RpcResponse rsp = nettyClient.sendMsg(host, request, async);
             //success
             if (null != rsp && rsp.getStatus() == 0) {
                 return rsp.getResult();
