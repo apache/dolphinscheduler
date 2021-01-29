81e2b82788d635c4085fad9723ad427b0f65e689
CalvinKirs <kirs@apache.org>
2021-01-29 10:39:28 +0800

    Store plug-in information when loading plug-in, query database when removing alert

--- dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/PluginDao.java
+++ dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/PluginDao.java
@@ -58,22 +58,22 @@ public class PluginDao extends AbstractBaseDao {
      *
      * @param pluginDefine new pluginDefine
      */
-    public void addOrUpdatePluginDefine(PluginDefine pluginDefine) {
+    public int addOrUpdatePluginDefine(PluginDefine pluginDefine) {
         requireNonNull(pluginDefine, "pluginDefine is null");
         requireNonNull(pluginDefine.getPluginName(), "pluginName is null");
         requireNonNull(pluginDefine.getPluginType(), "pluginType is null");
 
         List<PluginDefine> pluginDefineList = pluginDefineMapper.queryByNameAndType(pluginDefine.getPluginName(), pluginDefine.getPluginType());
         if (pluginDefineList == null || pluginDefineList.size() == 0) {
-            pluginDefineMapper.insert(pluginDefine);
-        } else {
-            PluginDefine currPluginDefine = pluginDefineList.get(0);
-            if (!currPluginDefine.getPluginParams().equals(pluginDefine.getPluginParams())) {
-                currPluginDefine.setUpdateTime(pluginDefine.getUpdateTime());
-                currPluginDefine.setPluginParams(pluginDefine.getPluginParams());
-                pluginDefineMapper.updateById(currPluginDefine);
-            }
+            return pluginDefineMapper.insert(pluginDefine);
         }
+        PluginDefine currPluginDefine = pluginDefineList.get(0);
+        if (!currPluginDefine.getPluginParams().equals(pluginDefine.getPluginParams())) {
+            currPluginDefine.setUpdateTime(pluginDefine.getUpdateTime());
+            currPluginDefine.setPluginParams(pluginDefine.getPluginParams());
+            pluginDefineMapper.updateById(currPluginDefine);
+        }
+        return currPluginDefine.getId();
     }
 
     /**
