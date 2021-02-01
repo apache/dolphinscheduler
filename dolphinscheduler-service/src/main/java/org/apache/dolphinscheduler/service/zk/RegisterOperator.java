c61a47c3eea1db1c5bfaecf218583915cadf84be
lgcareer <18610854716@163.com>
2021-02-01 11:50:04 +0800

    [Improvement-4624] Fix check code error

--- dolphinscheduler-service/src/main/java/org/apache/dolphinscheduler/service/zk/RegisterOperator.java
+++ dolphinscheduler-service/src/main/java/org/apache/dolphinscheduler/service/zk/RegisterOperator.java
@@ -14,19 +14,21 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.dolphinscheduler.service.zk;
+
+import static org.apache.dolphinscheduler.common.Constants.*;
 
 import org.apache.dolphinscheduler.common.Constants;
 import org.apache.dolphinscheduler.common.enums.ZKNodeType;
 import org.apache.dolphinscheduler.common.utils.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Set;
 
-import static org.apache.dolphinscheduler.common.Constants.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 /**
  * register operator
@@ -35,43 +37,45 @@ import static org.apache.dolphinscheduler.common.Constants.*;
 public class RegisterOperator extends ZookeeperCachedOperator {
 
     private final Logger logger = LoggerFactory.getLogger(ZookeeperCachedOperator.class);
+
     /**
-     *
      * @return get dead server node parent path
      */
-    protected String getDeadZNodeParentPath(){
+    protected String getDeadZNodeParentPath() {
         return getZookeeperConfig().getDsRoot() + Constants.ZOOKEEPER_DOLPHINSCHEDULER_DEAD_SERVERS;
     }
 
     /**
-     *  remove dead server by host
-     * @param host host
+     * remove dead server by host
+     *
+     * @param host       host
      * @param serverType serverType
      * @throws Exception
      */
     public void removeDeadServerByHost(String host, String serverType) throws Exception {
         List<String> deadServers = super.getChildrenKeys(getDeadZNodeParentPath());
-        for(String serverPath : deadServers){
-            if(serverPath.startsWith(serverType+UNDERLINE+host)){
+        for (String serverPath : deadServers) {
+            if (serverPath.startsWith(serverType + UNDERLINE + host)) {
                 String server = getDeadZNodeParentPath() + SINGLE_SLASH + serverPath;
                 super.remove(server);
-                logger.info("{} server {} deleted from zk dead server path success" , serverType , host);
+                logger.info("{} server {} deleted from zk dead server path success", serverType, host);
             }
         }
     }
 
     /**
-     *  get host ip, string format: masterParentPath/ip
+     * get host ip, string format: masterParentPath/ip
+     *
      * @param path path
      * @return host ip, string format: masterParentPath/ip
      */
     protected String getHostByEventDataPath(String path) {
-        if(StringUtils.isEmpty(path)){
+        if (StringUtils.isEmpty(path)) {
             logger.error("empty path!");
             return "";
         }
         String[] pathArray = path.split(SINGLE_SLASH);
-        if(pathArray.length < 1){
+        if (pathArray.length < 1) {
             logger.error("parse ip error: {}", path);
             return "";
         }
@@ -83,9 +87,9 @@ public class RegisterOperator extends ZookeeperCachedOperator {
      * opType(add): if find dead server , then add to zk deadServerPath
      * opType(delete): delete path from zk
      *
-     * @param zNode   		  node path
-     * @param zkNodeType	  master or worker
-     * @param opType		  delete or add
+     * @param zNode      node path
+     * @param zkNodeType master or worker
+     * @param opType     delete or add
      * @throws Exception errors
      */
     public void handleDeadServer(String zNode, ZKNodeType zkNodeType, String opType) throws Exception {
@@ -93,17 +97,17 @@ public class RegisterOperator extends ZookeeperCachedOperator {
         String type = (zkNodeType == ZKNodeType.MASTER) ? MASTER_PREFIX : WORKER_PREFIX;
 
         //check server restart, if restart , dead server path in zk should be delete
-        if(opType.equals(DELETE_ZK_OP)){
+        if (opType.equals(DELETE_ZK_OP)) {
             removeDeadServerByHost(host, type);
 
-        }else if(opType.equals(ADD_ZK_OP)){
+        } else if (opType.equals(ADD_ZK_OP)) {
             String deadServerPath = getDeadZNodeParentPath() + SINGLE_SLASH + type + UNDERLINE + host;
-            if(!super.isExisted(deadServerPath)){
+            if (!super.isExisted(deadServerPath)) {
                 //add dead server info to zk dead server path : /dead-servers/
 
-                super.persist(deadServerPath,(type + UNDERLINE + host));
+                super.persist(deadServerPath, (type + UNDERLINE + host));
 
-                logger.info("{} server dead , and {} added to zk dead server path success" ,
+                logger.info("{} server dead , and {} added to zk dead server path success",
                         zkNodeType.toString(), zNode);
             }
         }
@@ -114,9 +118,9 @@ public class RegisterOperator extends ZookeeperCachedOperator {
      * opType(add): if find dead server , then add to zk deadServerPath
      * opType(delete): delete path from zk
      *
-     * @param zNodeSet        node path set
-     * @param zkNodeType	  master or worker
-     * @param opType		  delete or add
+     * @param zNodeSet   node path set
+     * @param zkNodeType master or worker
+     * @param opType     delete or add
      * @throws Exception errors
      */
     public void handleDeadServer(Set<String> zNodeSet, ZKNodeType zkNodeType, String opType) throws Exception {
@@ -125,23 +129,22 @@ public class RegisterOperator extends ZookeeperCachedOperator {
         for (String zNode : zNodeSet) {
             String host = getHostByEventDataPath(zNode);
             //check server restart, if restart , dead server path in zk should be delete
-            if(opType.equals(DELETE_ZK_OP)){
+            if (opType.equals(DELETE_ZK_OP)) {
                 removeDeadServerByHost(host, type);
 
-            }else if(opType.equals(ADD_ZK_OP)){
+            } else if (opType.equals(ADD_ZK_OP)) {
                 String deadServerPath = getDeadZNodeParentPath() + SINGLE_SLASH + type + UNDERLINE + host;
-                if(!super.isExisted(deadServerPath)){
+                if (!super.isExisted(deadServerPath)) {
                     //add dead server info to zk dead server path : /dead-servers/
 
-                    super.persist(deadServerPath,(type + UNDERLINE + host));
+                    super.persist(deadServerPath, (type + UNDERLINE + host));
 
-                    logger.info("{} server dead , and {} added to zk dead server path success" ,
+                    logger.info("{} server dead , and {} added to zk dead server path success",
                             zkNodeType.toString(), zNode);
                 }
             }
 
         }
-
 
     }
 }
