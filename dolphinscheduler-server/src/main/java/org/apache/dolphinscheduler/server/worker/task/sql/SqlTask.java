5ba2116961dfd4e9118a82431ec8ba37109414ef
WongBin <abc13579d@163.com>
2021-02-09 16:15:29 +0800

    Update SqlTask.java

--- dolphinscheduler-server/src/main/java/org/apache/dolphinscheduler/server/worker/task/sql/SqlTask.java
+++ dolphinscheduler-server/src/main/java/org/apache/dolphinscheduler/server/worker/task/sql/SqlTask.java
@@ -180,7 +180,7 @@ public class SqlTask extends AbstractTask {
                 extractPreQuerySqlParams(con, sqlBind);
             }
         } catch (Exception e) {
-            throw new ServiceException("execute pre-query-sql error:"+preStatements, e);
+            throw new ServiceException("execute pre-query-sql error:" + preStatements, e);
         }
     }
 
