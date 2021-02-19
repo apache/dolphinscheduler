ac80bdbb7e693adcdfc62303e96745811522aa47
william@work <416934181@qq.com>
2021-02-19 13:50:16 +0800

    roll back changes of tab alignment

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/ExecutorService.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/ExecutorService.java
@@ -37,21 +37,21 @@ public interface ExecutorService {
     /**
      * execute process instance
      *
-     * @param loginUser               login user
-     * @param projectName             project name
-     * @param processDefinitionId     process Definition Id
-     * @param cronTime                cron time
-     * @param commandType             command type
-     * @param failureStrategy         failuer strategy
-     * @param startNodeList           start nodelist
-     * @param taskDependType          node dependency type
-     * @param warningType             warning type
-     * @param warningGroupId          notify group id
+     * @param loginUser login user
+     * @param projectName project name
+     * @param processDefinitionId process Definition Id
+     * @param cronTime cron time
+     * @param commandType command type
+     * @param failureStrategy failuer strategy
+     * @param startNodeList start nodelist
+     * @param taskDependType node dependency type
+     * @param warningType warning type
+     * @param warningGroupId notify group id
      * @param processInstancePriority process instance priority
-     * @param workerGroup             worker group name
-     * @param runMode                 run mode
-     * @param timeout                 timeout
-     * @param startParams             the global param values which pass to new process instance
+     * @param workerGroup worker group name
+     * @param runMode run mode
+     * @param timeout timeout
+     * @param startParams the global param values which pass to new process instance
      * @return execute process instance code
      */
     Map<String, Object> execProcessInstance(User loginUser, String projectName,
@@ -66,7 +66,7 @@ public interface ExecutorService {
      * check whether the process definition can be executed
      *
      * @param processDefinition process definition
-     * @param processDefineId   process definition id
+     * @param processDefineId process definition id
      * @return check result code
      */
     Map<String, Object> checkProcessDefinitionValid(ProcessDefinition processDefinition, int processDefineId);
@@ -74,10 +74,10 @@ public interface ExecutorService {
     /**
      * do action to process instance：pause, stop, repeat, recover from pause, recover from stop
      *
-     * @param loginUser         login user
-     * @param projectName       project name
+     * @param loginUser login user
+     * @param projectName project name
      * @param processInstanceId process instance id
-     * @param executeType       execute type
+     * @param executeType execute type
      * @return execute result code
      */
     Map<String, Object> execute(User loginUser, String projectName, Integer processInstanceId, ExecuteType executeType);
