831ebc6938975cf9f0c4cc7caa2c17dc63f7ccb4
felix.Wang <felix@thinkingdata.cn>
2021-02-19 10:41:11 +0800

    fix format2Duration when parameter has one is null

--- dolphinscheduler-common/src/test/java/org/apache/dolphinscheduler/common/utils/DateUtilsTest.java
+++ dolphinscheduler-common/src/test/java/org/apache/dolphinscheduler/common/utils/DateUtilsTest.java
@@ -196,4 +196,12 @@ public class DateUtilsTest {
 
     }
 
+    @Test
+    public void testNullDuration() {
+        // days hours minutes seconds
+        Date d1 = DateUtils.stringToDate("2020-01-20 11:00:00");
+        Date d2 = null;
+        Assert.assertEquals("",DateUtils.format2Duration(d1,d2));
+    }
+
 }
