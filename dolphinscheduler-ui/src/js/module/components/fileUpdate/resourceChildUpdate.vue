4c18e520b01eb264d577d47c7dc1584b366e17af
Shiwen Cheng <chengshiwen0103@gmail.com>
2021-02-20 08:43:12 +0800

    [Fix-4796][UI] Fix child file upload failure and file id mixed problem (#4797)

--- dolphinscheduler-ui/src/js/module/components/fileUpdate/resourceChildUpdate.vue
+++ dolphinscheduler-ui/src/js/module/components/fileUpdate/resourceChildUpdate.vue
@@ -70,7 +70,7 @@
             <template slot="content">
               <div class="file-update-box">
                 <template v-if="progress === 0">
-                  <input name="file" id="file" type="file" class="file-update">
+                  <input ref="file" name="file" type="file" class="file-update" @change="_onChange">
                   <el-button size="mini">{{$t('Upload')}}<em class="el-icon-upload"></em></el-button>
                 </template>
                 <div class="progress-box" v-if="progress !== 0">
@@ -214,7 +214,7 @@
         this.progress = 0
         this.file = ''
         this.currentDir = localStore.getItem('currentDir')
-        this.pid = -1
+        this.pid = this.id
         this.dragOver = false
       },
       /**
@@ -226,16 +226,15 @@
         this.name = file.name
         this.dragOver = false
       },
+      _onChange () {
+        let file = this.$refs.file.files[0]
+        this.file = file
+        this.name = file.name
+        this.$refs.file.value = null
+      },
       close () {
         this.$emit('closeResourceChildUpdate')
       }
-    },
-    mounted () {
-      $('#file').change(() => {
-        let file = $('#file')[0].files[0]
-        this.file = file
-        this.name = file.name
-      })
     },
     components: { mPopup, mListBoxF, mProgressBar }
   }
