2db53fc46770cbcc2afb627f0fe0f14b7dcfd79c
Simon <zhangshiming@cvte.com>
2021-03-01 15:01:17 +0800

    [Feature][JsonSplit] fix processDefinitionService ut and bug (#4894)
    * Modify Project and ProjectUser Mapper
    
    * Modify Project and ProjectUser Mapper
    
    * project_code is bigint(20)
    
    * modify ERROR name
    
    * modify saveProcessDefine, remove the duplicate code with createTaskAndRelation
    
    * modify import/export processdefinition, add genProcessData
    
    * fix ut and bug
    
    * code style

--- dolphinscheduler-common/src/test/java/org/apache/dolphinscheduler/common/utils/VarPoolUtilsTest.java
+++ dolphinscheduler-common/src/test/java/org/apache/dolphinscheduler/common/utils/VarPoolUtilsTest.java
@@ -28,8 +28,10 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 public class VarPoolUtilsTest {
-    
+
     private static final Logger logger = LoggerFactory.getLogger(VarPoolUtilsTest.class);
 
     @Test
@@ -37,53 +39,122 @@ public class VarPoolUtilsTest {
         String varPool = "p1,66$VarPool$p2,69$VarPool$";
         ConcurrentHashMap<String, Object> propToValue = new ConcurrentHashMap<String, Object>();
         VarPoolUtils.convertVarPoolToMap(propToValue, varPool);
-        Assert.assertEquals((String)propToValue.get("p1"), "66");
-        Assert.assertEquals((String)propToValue.get("p2"), "69");
+        Assert.assertEquals((String) propToValue.get("p1"), "66");
+        Assert.assertEquals((String) propToValue.get("p2"), "69");
         logger.info(propToValue.toString());
     }
-    
+
     @Test
     public void testConvertPythonScriptPlaceholders() throws Exception {
         String rawScript = "print(${p1});\n${setShareVar(${p1},3)};\n${setShareVar(${p2},4)};";
         rawScript = VarPoolUtils.convertPythonScriptPlaceholders(rawScript);
         Assert.assertEquals(rawScript, "print(${p1});\n"
-            + "print(\"${{setValue({},{})}}\".format(\"p1\",3));\n"
-            + "print(\"${{setValue({},{})}}\".format(\"p2\",4));");
+                + "print(\"${{setValue({},{})}}\".format(\"p1\",3));\n"
+                + "print(\"${{setValue({},{})}}\".format(\"p2\",4));");
         logger.info(rawScript);
     }
 
     @Test
     public void testSetTaskNodeLocalParams() throws Exception {
         String taskJson = "{\"id\":\"tasks-66199\",\"name\":\"file-shell\",\"desc\":null,\"type\":\"SHELL\","
-                        + "\"runFlag\":\"NORMAL\",\"loc\":null,\"maxRetryTimes\":0,\"retryInterval\":1,\""
-                        +  "params\":{\"rawScript\":\"sh n-1/n-1-1/run.sh\",\""
-                        + "localParams\":[{\"prop\":\"k1\",\"direct\":\"IN\",\"type\":\"VARCHAR\",\"value\":\"v1\"},{\"prop\":\"k2\",\"direct\":\"IN\",\"type\":\"VARCHAR\",\"value\":\"v2\"},"
-                        + "{\"prop\":\"k3\",\"direct\":\"IN\",\"type\":\"VARCHAR\",\"value\":\"v3\"}],\""
-                        + "resourceList\":[{\"id\":\"dolphinschedule-code\",\"res\":\"n-1/n-1-1/dolphinscheduler-api-server.log\"},"
-                        + "{\"id\":\"mr-code\",\"res\":\"n-1/n-1-1/hadoop-mapreduce-examples-2.7.4.jar\"},"
-                        + "{\"id\":\"run\",\"res\":\"n-1/n-1-1/run.sh\"}]},\"preTasks\":[],\"extras\":null,\"depList\":[],\""
-                        + "dependence\":{},\"conditionResult\":{\"successNode\":[\"\"],\"failedNode\":[\"\"]},\"taskInstancePriority\":\"MEDIUM\",\""
-                        + "workerGroup\":\"default\",\"workerGroupId\":null,\"timeout\":{\"strategy\":\"\",\"interval\":null,\"enable\":false},\"delayTime\":0}";
-        String changeTaskJson = "{\"id\":\"tasks-66199\",\"name\":\"file-shell\",\"desc\":null,\"type\":\"SHELL\","
-                        + "\"runFlag\":\"NORMAL\",\"loc\":null,\"maxRetryTimes\":0,\"retryInterval\":1,\""
-                        +  "params\":{\"rawScript\":\"sh n-1/n-1-1/run.sh\",\""
-                        + "localParams\":[{\"prop\":\"k1\",\"direct\":\"IN\",\"type\":\"VARCHAR\",\"value\":\"k1-value-change\"},"
-                        + "{\"prop\":\"k2\",\"direct\":\"IN\",\"type\":\"VARCHAR\",\"value\":\"k2-value-change\"},"
-                        + "{\"prop\":\"k3\",\"direct\":\"IN\",\"type\":\"VARCHAR\",\"value\":\"v3\"}],\""
-                        + "resourceList\":[{\"id\":\"dolphinschedule-code\",\"res\":\"n-1/n-1-1/dolphinscheduler-api-server.log\"},"
-                        + "{\"id\":\"mr-code\",\"res\":\"n-1/n-1-1/hadoop-mapreduce-examples-2.7.4.jar\"},"
-                        + "{\"id\":\"run\",\"res\":\"n-1/n-1-1/run.sh\"}]},\"preTasks\":[],\"extras\":null,\"depList\":[],\""
-                        + "dependence\":{},\"conditionResult\":{\"successNode\":[\"\"],\"failedNode\":[\"\"]},\"taskInstancePriority\":\"MEDIUM\",\""
-                        + "workerGroup\":\"default\",\"workerGroupId\":null,\"timeout\":{\"strategy\":\"\",\"interval\":null,\"enable\":false},\"delayTime\":0}";
+                + "\"runFlag\":\"NORMAL\",\"loc\":null,\"maxRetryTimes\":0,\"retryInterval\":1,\""
+                + "params\":{\"rawScript\":\"sh n-1/n-1-1/run.sh\",\""
+                + "localParams\":[{\"prop\":\"k1\",\"direct\":\"IN\",\"type\":\"VARCHAR\",\"value\":\"v1\"},{\"prop\":\"k2\",\"direct\":\"IN\",\"type\":\"VARCHAR\",\"value\":\"v2\"},"
+                + "{\"prop\":\"k3\",\"direct\":\"IN\",\"type\":\"VARCHAR\",\"value\":\"v3\"}],\""
+                + "resourceList\":[{\"id\":\"dolphinschedule-code\",\"res\":\"n-1/n-1-1/dolphinscheduler-api-server.log\"},"
+                + "{\"id\":\"mr-code\",\"res\":\"n-1/n-1-1/hadoop-mapreduce-examples-2.7.4.jar\"},"
+                + "{\"id\":\"run\",\"res\":\"n-1/n-1-1/run.sh\"}]},\"preTasks\":[],\"extras\":null,\"depList\":[],\""
+                + "dependence\":{},\"conditionResult\":{\"successNode\":[\"\"],\"failedNode\":[\"\"]},\"taskInstancePriority\":\"MEDIUM\",\""
+                + "workerGroup\":\"default\",\"workerGroupId\":null,\"timeout\":{\"strategy\":\"\",\"interval\":null,\"enable\":false},\"delayTime\":0}";
+
+        String changeTaskJson = "{"
+                + "    \"id\":\"tasks-66199\","
+                + "    \"code\":null,"
+                + "    \"version\":0,"
+                + "    \"name\":\"file-shell\","
+                + "    \"desc\":null,"
+                + "    \"type\":\"SHELL\","
+                + "    \"runFlag\":\"NORMAL\","
+                + "    \"loc\":null,"
+                + "    \"maxRetryTimes\":0,"
+                + "    \"retryInterval\":1,"
+                + "    \"params\":{"
+                + "        \"rawScript\":\"sh n-1/n-1-1/run.sh\","
+                + "        \"localParams\":["
+                + "            {"
+                + "                \"prop\":\"k1\","
+                + "                \"direct\":\"IN\","
+                + "                \"type\":\"VARCHAR\","
+                + "                \"value\":\"k1-value-change\""
+                + "            },"
+                + "            {"
+                + "                \"prop\":\"k2\","
+                + "                \"direct\":\"IN\","
+                + "                \"type\":\"VARCHAR\","
+                + "                \"value\":\"k2-value-change\""
+                + "            },"
+                + "            {"
+                + "                \"prop\":\"k3\","
+                + "                \"direct\":\"IN\","
+                + "                \"type\":\"VARCHAR\","
+                + "                \"value\":\"v3\""
+                + "            }"
+                + "        ],"
+                + "        \"resourceList\":["
+                + "            {"
+                + "                \"id\":\"dolphinschedule-code\","
+                + "                \"res\":\"n-1/n-1-1/dolphinscheduler-api-server.log\""
+                + "            },"
+                + "            {"
+                + "                \"id\":\"mr-code\","
+                + "                \"res\":\"n-1/n-1-1/hadoop-mapreduce-examples-2.7.4.jar\""
+                + "            },"
+                + "            {"
+                + "                \"id\":\"run\","
+                + "                \"res\":\"n-1/n-1-1/run.sh\""
+                + "            }"
+                + "        ]"
+                + "    },"
+                + "    \"preTasks\":["
+                + ""
+                + "    ],"
+                + "    \"preTaskNodeList\":null,"
+                + "    \"extras\":null,"
+                + "    \"depList\":["
+                + ""
+                + "    ],"
+                + "    \"dependence\":{"
+                + ""
+                + "    },"
+                + "    \"conditionResult\":{"
+                + "        \"successNode\":["
+                + "            \"\""
+                + "        ],"
+                + "        \"failedNode\":["
+                + "            \"\""
+                + "        ]"
+                + "    },"
+                + "    \"taskInstancePriority\":\"MEDIUM\","
+                + "    \"workerGroup\":\"default\","
+                + "    \"workerGroupId\":null,"
+                + "    \"timeout\":{"
+                + "        \"strategy\":\"\","
+                + "        \"interval\":null,"
+                + "        \"enable\":false"
+                + "    },"
+                + "    \"delayTime\":0"
+                + "}";
+
+        ObjectNode jsonNodes = JSONUtils.parseObject(changeTaskJson);
         Map<String, Object> propToValue = new HashMap<String, Object>();
-        propToValue.put("k1","k1-value-change");
-        propToValue.put("k2","k2-value-change");
+        propToValue.put("k1", "k1-value-change");
+        propToValue.put("k2", "k2-value-change");
 
-        TaskNode taskNode = JSONUtils.parseObject(taskJson,TaskNode.class);
+        TaskNode taskNode = JSONUtils.parseObject(taskJson, TaskNode.class);
 
-        VarPoolUtils.setTaskNodeLocalParams(taskNode,propToValue);
+        VarPoolUtils.setTaskNodeLocalParams(taskNode, propToValue);
 
-        Assert.assertEquals(changeTaskJson,JSONUtils.toJsonString(taskNode));
+        Assert.assertEquals(JSONUtils.toJsonString(jsonNodes), JSONUtils.toJsonString(taskNode).trim());
 
     }
 
