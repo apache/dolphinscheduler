f3fcf33807c610f335fd3ef3643e17ba735c45c6
hailin0 <hailin0@yeah.net>
2021-03-01 23:44:28 +0800

    fix

--- script/dolphinscheduler-daemon.sh
+++ script/dolphinscheduler-daemon.sh
@@ -98,7 +98,7 @@ else
 fi
 
 if [ "$SKYWALKING_ENABLE" = "true" ]; then
-  SKYWALKING_OPTS="-javaagent:$DOLPHINSCHEDULER_HOME/skywalking-agent/apm-agent.jar -DSW_AGENT_NAME=dolphinscheduler::$command -DSW_LOGGING_FILE_NAME=skywalking-dolphinscheduler-$command.log"
+  SKYWALKING_OPTS="-javaagent:$DOLPHINSCHEDULER_HOME/skywalking-agent/skywalking-agent.jar -DSW_AGENT_NAME=dolphinscheduler::$command -DSW_LOGGING_FILE_NAME=skywalking-dolphinscheduler-$command.log"
 
   export DOLPHINSCHEDULER_OPTS="$DOLPHINSCHEDULER_OPTS $SKYWALKING_OPTS"
   echo "Info: Skywalking enabled opts: $SKYWALKING_OPTS"
