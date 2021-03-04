7a896b9e3dcd545f635a1bd6f87fda1ee7fb76a9
Shiwen Cheng <chengshiwen0103@gmail.com>
2021-03-04 10:05:24 +0800

    [Fix-4905][K8s] Fix incorrect host problem in minikube (#4906)
    * [Fix][K8s] Fix incorrect host problem in minikube
    
    * [Improvement][K8s] Rename MASTER_WORKER_STS_PATTERN to STS_PATTERN

---
+++ dolphinscheduler-common/src/test/java/org/apache/dolphinscheduler/common/CommonTest.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.dolphinscheduler.common;
+
+import static org.junit.Assert.assertTrue;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+import org.junit.Test;
+
+/**
+ * CommonTest
+ */
+public class CommonTest {
+
+    public static void setFinalStatic(Field field, Object newValue) throws NoSuchFieldException, IllegalAccessException {
+        field.setAccessible(true);
+        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+        field.set(null, newValue);
+    }
+
+    @Test
+    public void testSetFinalStatic() throws Exception {
+        setFinalStatic(Constants.class.getDeclaredField("KUBERNETES_MODE"), true);
+        assertTrue(Constants.KUBERNETES_MODE);
+    }
+
+}
