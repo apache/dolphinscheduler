c4bd1e85c6d918d8f8679b7c78d41cd52294f65d
lijufeng <920347627@qq.com>
2021-03-15 18:03:33 +0800

    fix upload file bug

--- dolphinscheduler-ui/src/js/module/components/fileUpdate/fileChildUpdate.vue
+++ dolphinscheduler-ui/src/js/module/components/fileUpdate/fileChildUpdate.vue
@@ -174,8 +174,8 @@
           formData.append('file', this.file)
           formData.append('type', this.type)
           formData.append('name', this.name)
-          formData.append('pid', this.pid)
-          formData.append('currentDir', this.currentDir)
+          formData.append('pid', localStore.getItem('pid') || this.id)
+          formData.append('currentDir', localStore.getItem('currentDir') || this.currentDir)
           formData.append('description', this.description)
           io.post('resources/create', res => {
             this.$message.success(res.msg)
