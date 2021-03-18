eafa8ac76a768f1833e731d3a47de04671f5aae4
chengshiwen <chengshiwen0103@gmail.com>
2021-03-18 17:54:06 +0800

    [Fix-5089][UI] Fix code mirror cannot be displayed normally in sqoop task

--- dolphinscheduler-ui/src/js/conf/home/pages/dag/_source/formModel/tasks/sqoop.vue
+++ dolphinscheduler-ui/src/js/conf/home/pages/dag/_source/formModel/tasks/sqoop.vue
@@ -223,7 +223,7 @@
         </m-list-box>
       </template>
       <template v-if="sourceType === 'MYSQL'">
-        <m-list-box v-if="srcQueryType === '1'">
+        <m-list-box v-show="srcQueryType === '1'">
           <div slot="text">{{$t('SQL Statement')}}</div>
           <div slot="content">
             <div class="form-mirror">
@@ -710,10 +710,32 @@
       getSriptBoxValue (val) {
         editor.setValue(val)
       },
+      _onSwitch (is) {
+        if (is) {
+          this.jobType = 'CUSTOM'
+          this.isCustomTask = true
+          setTimeout(() => {
+            this._handlerShellEditor()
+          }, 200)
+        } else {
+          this.jobType = 'TEMPLATE'
+          this.isCustomTask = false
+          if (this.srcQueryType === '1') {
+            setTimeout(() => {
+              this._handlerEditor()
+            }, 200)
+          }
+        }
+      },
       _handleQueryType (o) {
         this.sourceMysqlParams.srcQueryType = this.srcQueryType
         this._getTargetTypeList(this.sourceType)
         this.targetType = this.targetTypeList[0].code
+        if (this.srcQueryType === '1') {
+          setTimeout(() => {
+            this._handlerEditor()
+          }, 200)
+        }
       },
 
       _handleModelTypeChange (a) {
