f16d988766ab9d828569583af59e9a32ddcdae3a
小清 <62982788+597365581@users.noreply.github.com>
2021-03-19 17:00:38 +0800

    Update HadoopUtilsTest.java

--- dolphinscheduler-common/src/test/java/org/apache/dolphinscheduler/common/utils/HadoopUtilsTest.java
+++ dolphinscheduler-common/src/test/java/org/apache/dolphinscheduler/common/utils/HadoopUtilsTest.java
@@ -15,6 +15,7 @@
  * limitations under the License.
  */
 package org.apache.dolphinscheduler.common.utils;
+
 import java.lang.reflect.Method;
 import org.apache.dolphinscheduler.common.enums.ResourceType;
 import org.apache.hadoop.conf.Configuration;
