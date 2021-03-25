48763f56b0a4a1a5615ba8711da9f0676dd75391
chengshiwen <chengshiwen0103@gmail.com>
2021-03-25 12:15:58 +0800

    [Improvement][SQL] Add Unique for name in table t_ds_worker_group

--- sql/dolphinscheduler_mysql.sql
+++ sql/dolphinscheduler_mysql.sql
@@ -790,11 +790,12 @@ CREATE TABLE `t_ds_user` (
 DROP TABLE IF EXISTS `t_ds_worker_group`;
 CREATE TABLE `t_ds_worker_group` (
   `id` bigint(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `name` varchar(256) NULL DEFAULT NULL COMMENT 'worker group name',
+  `name` varchar(256) NOT NULL COMMENT 'worker group name',
   `addr_list` text NULL DEFAULT NULL COMMENT 'worker addr list. split by [,]',
   `create_time` datetime NULL DEFAULT NULL COMMENT 'create time',
   `update_time` datetime NULL DEFAULT NULL COMMENT 'update time',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_unique` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
