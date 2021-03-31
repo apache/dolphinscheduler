80f92155616650d8af4cc7dda04cdbaf21026e9c
Shiwen Cheng <chengshiwen0103@gmail.com>
2021-03-31 20:47:02 +0800

    [1.3.6-prepare][Build] Optimize long release name and remove unused nginx release (#5187)
    * [1.3.6-prepare][Build] Optimize long release name and remove unused nginx release
    
    * [1.3.6-prepare][Build] Upgrade spring boot version
    
    * [1.3.6-prepare][Build] Support page /swagger-ui.html
    
    * [1.3.6-prepare][Improvement] Improve code style

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/configuration/AppConfiguration.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/configuration/AppConfiguration.java
@@ -19,16 +19,20 @@ package org.apache.dolphinscheduler.api.configuration;
 import org.apache.dolphinscheduler.api.interceptor.LocaleChangeInterceptor;
 import org.apache.dolphinscheduler.api.interceptor.LoginHandlerInterceptor;
 
+import java.util.Locale;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
-
-import java.util.Locale;
 
 /**
  * application configuration
@@ -82,13 +86,18 @@ public class AppConfiguration implements WebMvcConfigurer {
     // i18n
     registry.addInterceptor(localeChangeInterceptor());
     // login
-    registry.addInterceptor(loginInterceptor()).addPathPatterns(LOGIN_INTERCEPTOR_PATH_PATTERN).excludePathPatterns(LOGIN_PATH_PATTERN,"/swagger-resources/**", "/webjars/**", "/v2/**", "/doc.html", "*.html", "/ui/**");
+    registry.addInterceptor(loginInterceptor())
+            .addPathPatterns(LOGIN_INTERCEPTOR_PATH_PATTERN)
+            .excludePathPatterns(LOGIN_PATH_PATTERN,
+                    "/swagger-resources/**", "/webjars/**", "/v2/**",
+                    "/doc.html", "/swagger-ui.html", "*.html", "/ui/**");
   }
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+    registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
     registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     registry.addResourceHandler("/ui/**").addResourceLocations("file:ui/");
   }
