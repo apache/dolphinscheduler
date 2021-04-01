dd36da8d1f2c9d37172be493f83221819a26c51f
JinyLeeChina <42576980+JinyLeeChina@users.noreply.github.com>
2021-04-01 17:45:44 +0800

    [Feature][JsonSplit] fix process bug (#5193)
    * transform taskCode from long to string
    
    * fix process bug
    
    * code review
    
    * code review
    
    * code review
    
    Co-authored-by: JinyLeeChina <297062848@qq.com>

--- dolphinscheduler-server/src/main/java/org/apache/dolphinscheduler/server/master/runner/MasterExecThread.java
+++ dolphinscheduler-server/src/main/java/org/apache/dolphinscheduler/server/master/runner/MasterExecThread.java
@@ -65,6 +65,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
