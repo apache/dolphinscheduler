01ac122e55ee384ed3727d8fdfe1a876894466b7
zhuangchong <zhuangchong6@163.com>
2021-04-14 11:54:35 +0800

    update DolphinSchedulerManagerTest.testUpgradeDolphinScheduler method.

--- dolphinscheduler-dao/src/test/java/org/apache/dolphinscheduler/dao/upgrade/DolphinSchedulerManagerTest.java
+++ dolphinscheduler-dao/src/test/java/org/apache/dolphinscheduler/dao/upgrade/DolphinSchedulerManagerTest.java
@@ -17,16 +17,39 @@
 
 package org.apache.dolphinscheduler.dao.upgrade;
 
+import org.apache.dolphinscheduler.common.utils.FileUtils;
+
+import java.io.File;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 /**
  * dolphinshceduler manager test
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ FileUtils.class })
 public class DolphinSchedulerManagerTest {
 
     @Test
     public void testUpgradeDolphinScheduler() throws Exception {
+        String rootDir = System.getProperty("user.dir") + "/../";
+        PowerMockito.mockStatic(FileUtils.class);
+        File[] files = new File[4];
+
+        files[0] = new File(rootDir + "sql/upgrade/1.0.1_schema");
+        files[1] = new File(rootDir + "sql/upgrade/1.0.2_schema");
+        files[2] = new File(rootDir + "sql/upgrade/1.1.0_schema");
+        files[3] = new File(rootDir + "sql/upgrade/1.2.0_schema");
+        files[4] = new File(rootDir + "sql/upgrade/1.3.0_schema");
+        files[5] = new File(rootDir + "sql/upgrade/1.3.2_schema");
+        files[6] = new File(rootDir + "sql/upgrade/1.3.4_schema");
+        PowerMockito.when(FileUtils.getAllDir("sql/upgrade")).thenReturn(files);
+
         DolphinSchedulerManager dolphinSchedulerManager = new DolphinSchedulerManager();
         dolphinSchedulerManager.upgradeDolphinScheduler();
         Assert.assertNotNull(dolphinSchedulerManager.upgradeDao);
