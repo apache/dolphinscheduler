eca4dd0fae74f721b9e633c7da8bc5f8f6d1b7ac
CalvinKirs <kirs@apache.org>
2021-04-27 11:09:46 +0800

    init register rep

--- dolphinscheduler-register/dolphinscheduler-register-api/src/main/java/org/apache/dolphinscheduler/plugin/register/api/RegisterListener.java
+++ dolphinscheduler-register/dolphinscheduler-register-api/src/main/java/org/apache/dolphinscheduler/plugin/register/api/RegisterListener.java
@@ -1,4 +1,4 @@
-package org.apache.dolphinscheduler.plugin.register.api;/*
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -15,22 +15,7 @@ package org.apache.dolphinscheduler.plugin.register.api;/*
  * limitations under the License.
  */
 
-public interface RegisterCenter {
+package org.apache.dolphinscheduler.plugin.register.api;
 
-    void register();
-
-    void upRegister();
-
-    String get(String key);
-
-    void remove(String key);
-
-    void persist(String key, String value);
-
-    void update(String key, String value);
-
-
-    void remove();
-
-
+public class RegisterListener {
 }
