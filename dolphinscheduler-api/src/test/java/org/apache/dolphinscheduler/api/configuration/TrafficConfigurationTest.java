30eb4997c127cfb51fb268250a7398d65c4f9e4e
CalvinKirs <kirs@apache.org>
2021-06-01 12:55:04 +0800

    fix err

--- dolphinscheduler-api/src/test/java/org/apache/dolphinscheduler/api/configuration/TrafficConfigurationTest.java
+++ dolphinscheduler-api/src/test/java/org/apache/dolphinscheduler/api/configuration/TrafficConfigurationTest.java
@@ -20,7 +20,6 @@ package org.apache.dolphinscheduler.api.configuration;
 import org.apache.commons.collections.MapUtils;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
