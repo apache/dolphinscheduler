c86c59c20d159f2726effbacebed7a4883c7e089
kyoty <echohlne@gmail.com>
2021-06-09 22:20:13 +0800

    Update dolphinscheduler_ddl.sql

--- sql/upgrade/1.4.0_schema/postgresql/dolphinscheduler_ddl.sql
+++ sql/upgrade/1.4.0_schema/postgresql/dolphinscheduler_ddl.sql
@@ -112,6 +112,96 @@ delimiter ;
 SELECT uc_dolphin_T_t_ds_task_instance_A_var_pool();
 DROP FUNCTION IF EXISTS uc_dolphin_T_t_ds_task_instance_A_var_pool();
 
+-- uc_dolphin_T_t_ds_task_instance_A_task_code
+delimiter d//
+CREATE OR REPLACE FUNCTION uc_dolphin_T_t_ds_task_instance_A_task_code() RETURNS void AS $$
+BEGIN
+       IF NOT EXISTS (SELECT 1 FROM information_schema.COLUMNS
+          WHERE TABLE_NAME='t_ds_task_instance'
+                            AND COLUMN_NAME ='task_code')
+      THEN
+         ALTER TABLE t_ds_task_instance ADD COLUMN task_code bigint NOT NULL;
+       END IF;
+END;
+$$ LANGUAGE plpgsql;
+d//
+
+delimiter ;
+SELECT uc_dolphin_T_t_ds_task_instance_A_task_code();
+DROP FUNCTION IF EXISTS uc_dolphin_T_t_ds_task_instance_A_task_code();
+
+-- uc_dolphin_T_t_ds_task_instance_A_task_definition_version
+delimiter d//
+CREATE OR REPLACE FUNCTION uc_dolphin_T_t_ds_task_instance_A_task_definition_version() RETURNS void AS $$
+BEGIN
+       IF NOT EXISTS (SELECT 1 FROM information_schema.COLUMNS
+          WHERE TABLE_NAME='t_ds_task_instance'
+                            AND COLUMN_NAME ='task_definition_version')
+      THEN
+         ALTER TABLE t_ds_task_instance ADD COLUMN task_definition_version int DEFAULT NULL;
+       END IF;
+END;
+$$ LANGUAGE plpgsql;
+d//
+
+delimiter ;
+SELECT uc_dolphin_T_t_ds_task_instance_A_task_definition_version();
+DROP FUNCTION IF EXISTS uc_dolphin_T_t_ds_task_instance_A_task_definition_version();
+
+-- uc_dolphin_T_t_ds_task_instance_A_task_params
+delimiter d//
+CREATE OR REPLACE FUNCTION uc_dolphin_T_t_ds_task_instance_A_task_params() RETURNS void AS $$
+BEGIN
+       IF NOT EXISTS (SELECT 1 FROM information_schema.COLUMNS
+          WHERE TABLE_NAME='t_ds_task_instance'
+                            AND COLUMN_NAME ='task_params')
+      THEN
+         ALTER TABLE t_ds_task_instance ADD COLUMN task_params text;
+       END IF;
+END;
+$$ LANGUAGE plpgsql;
+d//
+
+delimiter ;
+SELECT uc_dolphin_T_t_ds_task_instance_A_task_params();
+DROP FUNCTION IF EXISTS uc_dolphin_T_t_ds_task_instance_A_task_params();
+
+-- uc_dolphin_T_t_ds_process_instance_A_process_definition_code
+delimiter d//
+CREATE OR REPLACE FUNCTION uc_dolphin_T_t_ds_process_instance_A_process_definition_code() RETURNS void AS $$
+BEGIN
+       IF NOT EXISTS (SELECT 1 FROM information_schema.COLUMNS
+          WHERE TABLE_NAME='t_ds_process_instance'
+                            AND COLUMN_NAME ='process_definition_code')
+      THEN
+         ALTER TABLE t_ds_process_instance ADD COLUMN process_definition_code bigint DEFAULT NULL;
+       END IF;
+END;
+$$ LANGUAGE plpgsql;
+d//
+
+delimiter ;
+SELECT uc_dolphin_T_t_ds_process_instance_A_process_definition_code();
+DROP FUNCTION IF EXISTS uc_dolphin_T_t_ds_process_instance_A_process_definition_code();
+
+-- uc_dolphin_T_t_ds_process_instance_A_process_definition_version
+delimiter d//
+CREATE OR REPLACE FUNCTION uc_dolphin_T_t_ds_process_instance_A_process_definition_version() RETURNS void AS $$
+BEGIN
+       IF NOT EXISTS (SELECT 1 FROM information_schema.COLUMNS
+          WHERE TABLE_NAME='t_ds_process_instance'
+                            AND COLUMN_NAME ='process_definition_version')
+      THEN
+         ALTER TABLE t_ds_process_instance ADD COLUMN process_definition_version int DEFAULT NULL;
+       END IF;
+END;
+$$ LANGUAGE plpgsql;
+d//
+
+delimiter ;
+SELECT uc_dolphin_T_t_ds_process_instance_A_process_definition_version();
+DROP FUNCTION IF EXISTS uc_dolphin_T_t_ds_process_instance_A_process_definition_version();
+
 -- uc_dolphin_T_t_ds_process_instance_A_var_pool
 delimiter d//
 CREATE OR REPLACE FUNCTION uc_dolphin_T_t_ds_process_instance_A_var_pool() RETURNS void AS $$
@@ -129,6 +219,60 @@ d//
 delimiter ;
 SELECT uc_dolphin_T_t_ds_process_instance_A_var_pool();
 DROP FUNCTION IF EXISTS uc_dolphin_T_t_ds_process_instance_A_var_pool();
+
+-- uc_dolphin_T_t_ds_project_A_code
+delimiter d//
+CREATE OR REPLACE FUNCTION uc_dolphin_T_t_ds_project_A_code() RETURNS void AS $$
+BEGIN
+       IF NOT EXISTS (SELECT 1 FROM information_schema.COLUMNS
+          WHERE TABLE_NAME='t_ds_project'
+                            AND COLUMN_NAME ='code')
+      THEN
+         ALTER TABLE t_ds_project ADD COLUMN code bigint NOT NULL;
+       END IF;
+END;
+$$ LANGUAGE plpgsql;
+d//
+
+delimiter ;
+SELECT uc_dolphin_T_t_ds_project_A_code();
+DROP FUNCTION IF EXISTS uc_dolphin_T_t_ds_project_A_code();
+
+-- uc_dolphin_T_t_ds_process_definition_A_code
+delimiter d//
+CREATE OR REPLACE FUNCTION uc_dolphin_T_t_ds_process_definition_A_code() RETURNS void AS $$
+BEGIN
+       IF NOT EXISTS (SELECT 1 FROM information_schema.COLUMNS
+          WHERE TABLE_NAME='t_ds_process_definition'
+                            AND COLUMN_NAME ='code')
+      THEN
+         ALTER TABLE t_ds_process_definition ADD COLUMN code bigint NOT NULL;
+       END IF;
+END;
+$$ LANGUAGE plpgsql;
+d//
+
+delimiter ;
+SELECT uc_dolphin_T_t_ds_process_definition_A_code();
+DROP FUNCTION IF EXISTS uc_dolphin_T_t_ds_process_definition_A_code();
+
+-- uc_dolphin_T_t_ds_process_definition_A_project_code
+delimiter d//
+CREATE OR REPLACE FUNCTION uc_dolphin_T_t_ds_process_definition_A_project_code() RETURNS void AS $$
+BEGIN
+       IF NOT EXISTS (SELECT 1 FROM information_schema.COLUMNS
+          WHERE TABLE_NAME='t_ds_process_definition'
+                            AND COLUMN_NAME ='project_code')
+      THEN
+         ALTER TABLE t_ds_process_definition ADD COLUMN project_code bigint NOT NULL;
+       END IF;
+END;
+$$ LANGUAGE plpgsql;
+d//
+
+delimiter ;
+SELECT uc_dolphin_T_t_ds_process_definition_A_project_code();
+DROP FUNCTION IF EXISTS uc_dolphin_T_t_ds_process_definition_A_project_code();
 
 -- uc_dolphin_T_t_ds_process_definition_A_modify_by
 delimiter d//
@@ -162,6 +306,197 @@ d//
 delimiter ;
 SELECT ct_dolphin_T_t_ds_process_definition_version();
 DROP FUNCTION IF EXISTS ct_dolphin_T_t_ds_process_definition_version();
+
+
+-- ct_dolphin_T_t_ds_task_definition
+delimiter d//
+CREATE OR REPLACE FUNCTION ct_dolphin_T_t_ds_task_definition() RETURNS void AS $$
+BEGIN
+    CREATE TABLE IF NOT EXISTS t_ds_task_definition (
+        id int NOT NULL  ,
+        code bigint NOT NULL,
+        name varchar(255) DEFAULT NULL ,
+        version int DEFAULT NULL ,
+        description text ,
+        project_code bigint DEFAULT NULL ,
+        user_id int DEFAULT NULL ,
+        task_type varchar(50) DEFAULT NULL ,
+        task_params text ,
+        flag int DEFAULT NULL ,
+        task_priority int DEFAULT NULL ,
+        worker_group varchar(255) DEFAULT NULL ,
+        fail_retry_times int DEFAULT NULL ,
+        fail_retry_interval int DEFAULT NULL ,
+        timeout_flag int DEFAULT NULL ,
+        timeout_notify_strategy int DEFAULT NULL ,
+        timeout int DEFAULT '0' ,
+        delay_time int DEFAULT '0' ,
+        resource_ids varchar(255) DEFAULT NULL ,
+        create_time timestamp DEFAULT NULL ,
+        update_time timestamp DEFAULT NULL ,
+        PRIMARY KEY (id) ,
+        CONSTRAINT task_definition_unique UNIQUE (name, project_code)
+    ) ;
+    create index task_definition_index on t_ds_task_definition (project_code,id);
+    CREATE SEQUENCE  t_ds_task_definition_id_sequence;
+    ALTER TABLE t_ds_task_definition ALTER COLUMN id SET DEFAULT NEXTVAL('t_ds_task_definition_id_sequence');
+    DROP SEQUENCE IF EXISTS t_ds_task_definition_log_id_sequence;
+
+END;
+$$ LANGUAGE plpgsql;
+d//
+
+delimiter ;
+SELECT ct_dolphin_T_t_ds_task_definition();
+DROP FUNCTION IF EXISTS ct_dolphin_T_t_ds_task_definition();
+
+-- ct_dolphin_T_t_ds_task_definition_log
+delimiter d//
+CREATE OR REPLACE FUNCTION ct_dolphin_T_t_ds_task_definition_log() RETURNS void AS $$
+BEGIN
+    CREATE TABLE IF NOT EXISTS t_ds_task_definition_log (
+        id int NOT NULL  ,
+        code bigint NOT NULL,
+        name varchar(255) DEFAULT NULL ,
+        version int DEFAULT NULL ,
+        description text ,
+        project_code bigint DEFAULT NULL ,
+        user_id int DEFAULT NULL ,
+        task_type varchar(50) DEFAULT NULL ,
+        task_params text ,
+        flag int DEFAULT NULL ,
+        task_priority int DEFAULT NULL ,
+        worker_group varchar(255) DEFAULT NULL ,
+        fail_retry_times int DEFAULT NULL ,
+        fail_retry_interval int DEFAULT NULL ,
+        timeout_flag int DEFAULT NULL ,
+        timeout_notify_strategy int DEFAULT NULL ,
+        timeout int DEFAULT '0' ,
+        delay_time int DEFAULT '0' ,
+        resource_ids varchar(255) DEFAULT NULL ,
+        operator int DEFAULT NULL ,
+        operate_time timestamp DEFAULT NULL ,
+        create_time timestamp DEFAULT NULL ,
+        update_time timestamp DEFAULT NULL ,
+        PRIMARY KEY (id)
+    ) ;
+    CREATE SEQUENCE  t_ds_task_definition_log_id_sequence;
+    ALTER TABLE t_ds_task_definition_log ALTER COLUMN id SET DEFAULT NEXTVAL('t_ds_task_definition_log_id_sequence');
+    DROP SEQUENCE IF EXISTS t_ds_process_task_relation_id_sequence;
+
+END;
+$$ LANGUAGE plpgsql;
+d//
+
+delimiter ;
+SELECT ct_dolphin_T_t_ds_task_definition_log();
+DROP FUNCTION IF EXISTS ct_dolphin_T_t_ds_task_definition_log();
+
+-- ct_dolphin_T_t_ds_process_task_relation
+delimiter d//
+CREATE OR REPLACE FUNCTION ct_dolphin_T_t_ds_process_task_relation() RETURNS void AS $$
+BEGIN
+    CREATE TABLE IF NOT EXISTS t_ds_process_task_relation (
+        id int NOT NULL  ,
+        name varchar(255) DEFAULT NULL ,
+        process_definition_version int DEFAULT NULL ,
+        project_code bigint DEFAULT NULL ,
+        process_definition_code bigint DEFAULT NULL ,
+        pre_task_code bigint DEFAULT NULL ,
+        pre_task_version int DEFAULT '0' ,
+        post_task_code bigint DEFAULT NULL ,
+        post_task_version int DEFAULT '0' ,
+        condition_type int DEFAULT NULL ,
+        condition_params text ,
+        create_time timestamp DEFAULT NULL ,
+        update_time timestamp DEFAULT NULL ,
+        PRIMARY KEY (id)
+    ) ;
+    CREATE SEQUENCE  t_ds_process_task_relation_id_sequence;
+    ALTER TABLE t_ds_process_task_relation ALTER COLUMN id SET DEFAULT NEXTVAL('t_ds_process_task_relation_id_sequence');
+    DROP SEQUENCE IF EXISTS t_ds_process_task_relation_log_id_sequence;
+
+END;
+$$ LANGUAGE plpgsql;
+d//
+
+delimiter ;
+SELECT ct_dolphin_T_t_ds_process_task_relation();
+DROP FUNCTION IF EXISTS ct_dolphin_T_t_ds_process_task_relation();
+
+-- ct_dolphin_T_t_ds_process_definition_log
+delimiter d//
+CREATE OR REPLACE FUNCTION ct_dolphin_T_t_ds_process_definition_log() RETURNS void AS $$
+BEGIN
+    CREATE TABLE IF NOT EXISTS t_ds_process_definition_log (
+        id int NOT NULL  ,
+        code bigint NOT NULL,
+        name varchar(255) DEFAULT NULL ,
+        version int DEFAULT NULL ,
+        description text ,
+        project_code bigint DEFAULT NULL ,
+        release_state int DEFAULT NULL ,
+        user_id int DEFAULT NULL ,
+        global_params text ,
+        locations text ,
+        connects text ,
+        warning_group_id int DEFAULT NULL ,
+        flag int DEFAULT NULL ,
+        timeout int DEFAULT '0' ,
+        tenant_id int DEFAULT '-1' ,
+        operator int DEFAULT NULL ,
+        operate_time timestamp DEFAULT NULL ,
+        create_time timestamp DEFAULT NULL ,
+        update_time timestamp DEFAULT NULL ,
+        PRIMARY KEY (id)
+    ) ;
+    CREATE SEQUENCE  t_ds_process_definition_log_id_sequence;
+    DROP SEQUENCE IF EXISTS t_ds_pt_ds_datasourcerocess_definition_log_id_sequence;
+    CREATE SEQUENCE  t_ds_process_definition_log_id_sequence;
+    ALTER TABLE t_ds_process_definition_log ALTER COLUMN id SET DEFAULT NEXTVAL('t_ds_process_definition_log_id_sequence');
+
+END;
+$$ LANGUAGE plpgsql;
+d//
+
+delimiter ;
+SELECT ct_dolphin_T_t_ds_process_definition_log();
+DROP FUNCTION IF EXISTS ct_dolphin_T_t_ds_process_definition_log();
+
+
+-- ct_dolphin_T_t_ds_process_task_relation_log
+delimiter d//
+CREATE OR REPLACE FUNCTION ct_dolphin_T_t_ds_process_task_relation_log() RETURNS void AS $$
+BEGIN
+    CREATE TABLE IF NOT EXISTS t_ds_process_task_relation_log (
+        id int NOT NULL  ,
+        name varchar(255) DEFAULT NULL ,
+        process_definition_version int DEFAULT NULL ,
+        project_code bigint DEFAULT NULL ,
+        process_definition_code bigint DEFAULT NULL ,
+        pre_task_code bigint DEFAULT NULL ,
+        pre_task_version int DEFAULT '0' ,
+        post_task_code bigint DEFAULT NULL ,
+        post_task_version int DEFAULT '0' ,
+        condition_type int DEFAULT NULL ,
+        condition_params text ,
+        operator int DEFAULT NULL ,
+        operate_time timestamp DEFAULT NULL ,
+        create_time timestamp DEFAULT NULL ,
+        update_time timestamp DEFAULT NULL ,
+        PRIMARY KEY (id)
+    ) ;
+    DROP SEQUENCE IF EXISTS t_ds_process_task_relation_log_id_sequence;
+    CREATE SEQUENCE  t_ds_process_task_relation_log_id_sequence;
+    ALTER TABLE t_ds_process_task_relation_log ALTER COLUMN id SET DEFAULT NEXTVAL('t_ds_process_task_relation_log_id_sequence');
+END;
+$$ LANGUAGE plpgsql;
+d//
+
+delimiter ;
+SELECT ct_dolphin_T_t_ds_process_task_relation_log();
+DROP FUNCTION IF EXISTS ct_dolphin_T_t_ds_process_task_relation_log();
+
 
 -- ----------------------------
 -- Table structure for t_ds_plugin_define
@@ -216,7 +551,7 @@ delimiter d//
 CREATE OR REPLACE FUNCTION uc_dolphin_T_t_ds_process_definition_version_A_warning_group_id() RETURNS void AS $$
 BEGIN
        IF NOT EXISTS (SELECT 1 FROM information_schema.COLUMNS
-          WHERE TABLE_NAME='t_ds_process_definition_version'
+          WHERE TABLE_NAME='3'
                             AND COLUMN_NAME ='warning_group_id')
       THEN
          ALTER TABLE t_ds_process_definition_version ADD COLUMN warning_group_id int4 DEFAULT NULL;
@@ -267,42 +602,6 @@ d//
 delimiter ;
 SELECT uc_dolphin_T_t_ds_alertgroup_A_create_user_id();
 DROP FUNCTION IF EXISTS uc_dolphin_T_t_ds_alertgroup_A_create_user_id();
-
--- uc_dolphin_T_t_ds_alertgroup_A_add_UN_groupName
-delimiter d//
-CREATE OR REPLACE FUNCTION uc_dolphin_T_t_ds_alertgroup_A_add_UN_groupName() RETURNS void AS $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_stat_all_indexes
-          WHERE relname='t_ds_alertgroup'
-                            AND indexrelname ='t_ds_alertgroup_name_UN')
-      THEN
-         ALTER TABLE t_ds_alertgroup ADD CONSTRAINT t_ds_alertgroup_name_UN UNIQUE (group_name);
-       END IF;
-END;
-$$ LANGUAGE plpgsql;
-d//
-
-delimiter ;
-SELECT uc_dolphin_T_t_ds_alertgroup_A_add_UN_groupName();
-DROP FUNCTION IF EXISTS uc_dolphin_T_t_ds_alertgroup_A_add_UN_groupName();
-
--- uc_dolphin_T_t_ds_datasource_A_add_UN_datasourceName
-delimiter d//
-CREATE OR REPLACE FUNCTION uc_dolphin_T_t_ds_datasource_A_add_UN_datasourceName() RETURNS void AS $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_stat_all_indexes
-          WHERE relname='t_ds_datasource'
-                            AND indexrelname ='t_ds_datasource_name_UN')
-      THEN
-         ALTER TABLE t_ds_datasource ADD CONSTRAINT t_ds_datasource_name_UN UNIQUE (name, type);
-       END IF;
-END;
-$$ LANGUAGE plpgsql;
-d//
-
-delimiter ;
-SELECT uc_dolphin_T_t_ds_datasource_A_add_UN_datasourceName();
-DROP FUNCTION IF EXISTS uc_dolphin_T_t_ds_datasource_A_add_UN_datasourceName();
 
 -- uc_dolphin_T_t_ds_schedules_A_add_timezone
 delimiter d//
