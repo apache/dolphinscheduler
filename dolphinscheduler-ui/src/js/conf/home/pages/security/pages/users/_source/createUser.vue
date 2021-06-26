bed5605139f54e2bfa54c10207d2cc6da2acffa8
Shiwen Cheng <chengshiwen0103@gmail.com>
2021-06-26 11:52:47 +0800

    Revert "Fix the bug `Edit User Info will always shows failed.` (#5627)"
    This reverts commit 0a5670f1958bfca56f84e42c1e11bb3150abcb4b.

--- dolphinscheduler-ui/src/js/conf/home/pages/security/pages/users/_source/createUser.vue
+++ dolphinscheduler-ui/src/js/conf/home/pages/security/pages/users/_source/createUser.vue
@@ -292,9 +292,8 @@
             this.userPassword = ''
             this.email = this.item.email
             this.phone = this.item.phone
-            this.state = this.item.state
             this.userState = this.item.state + '' || '1'
-            if (this.item.tenantId === 0 || this.item.tenantId) {
+            if (this.item.tenantId) {
               this.tenantId = this.item.tenantId
             }
             this.$nextTick(() => {
@@ -311,9 +310,8 @@
           this.userPassword = ''
           this.email = this.item.email
           this.phone = this.item.phone
-          this.state = this.item.state
           this.userState = this.state + '' || '1'
-          if (this.item.tenantId === 0 || this.item.tenantId) {
+          if (this.item.tenantId) {
             this.tenantId = this.item.tenantId
           }
           if (this.queueList.length > 0) {
