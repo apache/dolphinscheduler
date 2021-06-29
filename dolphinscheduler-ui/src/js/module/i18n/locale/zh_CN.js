b99bb19122944a4f5623e642d90ee57d87206eb6
flyto <1098377824@qq.com>
2021-06-29 18:56:50 +0800

    校验条件流程对应的任务是否存在

--- dolphinscheduler-ui/src/js/module/i18n/locale/zh_CN.js
+++ dolphinscheduler-ui/src/js/module/i18n/locale/zh_CN.js
@@ -688,5 +688,6 @@ export default {
   'The workflow canvas is abnormal and cannot be saved, please recreate': '该工作流画布异常，无法保存，请重新创建',
   Info: '提示',
   'Datasource userName': '所属用户',
-  'Resource userName': '所属用户'
+  'Resource userName': '所属用户',
+  'Error branch': '分支流转选择错误'
 }
