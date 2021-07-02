apache/dolphinscheduler

commit ab3a88b64b5ac7db905cffe76ff679756595f63b

[Fix-5699][UI] Fix update user error in user information (#5700)

dolphinscheduler-ui/src/js/conf/home/pages/security/pages/users/index.vue
@@ -60,7 +60,7 @@
 </template>
 <script>
   import _ from 'lodash'
-  import { mapActions } from 'vuex'
+  import { mapActions, mapMutations } from 'vuex'
   import mList from './_source/list'
   import mCreateUser from './_source/createUser'
   import mSpin from '@/module/components/spin/spin'
@@ -89,6 +89,7 @@
     mixins: [listUrlParamHandle],
     props: {},
     methods: {
+      ...mapMutations('user', ['setUserInfo']),
       ...mapActions('security', ['getUsersListP']),
       /**
        * Query
@@ -113,8 +114,9 @@
         this.item = item
         this.createUserDialog = true
       },
-      onUpdate () {
+      onUpdate (param) {
         this._debounceGET('false')
+        this.setUserInfo(param)
         this.createUserDialog = false
       },
       close () {

dolphinscheduler-ui/src/js/conf/home/pages/security/pages/users/_source/createUser.vue
@@ -138,7 +138,8 @@
       }
     },
     props: {
-      item: Object
+      item: Object,
+      fromUserInfo: Boolean
     },
     methods: {
       _ok () {
@@ -294,7 +295,7 @@
             this.phone = this.item.phone
             this.state = this.item.state
             this.userState = this.item.state + '' || '1'
-            if (this.item.tenantId === 0 || this.item.tenantId) {
+            if (this.fromUserInfo || this.item.tenantId) {
               this.tenantId = this.item.tenantId
             }
             this.$nextTick(() => {
@@ -313,7 +314,7 @@
           this.phone = this.item.phone
           this.state = this.item.state
           this.userState = this.state + '' || '1'
-          if (this.item.tenantId === 0 || this.item.tenantId) {
+          if (this.fromUserInfo || this.item.tenantId) {
             this.tenantId = this.item.tenantId
           }
           if (this.queueList.length > 0) {

dolphinscheduler-ui/src/js/conf/home/pages/user/pages/account/_source/info.vue
@@ -73,7 +73,7 @@
           v-if="createUserDialog"
           :visible.sync="createUserDialog"
           width="auto">
-          <m-create-user :item="item" @onUpdate="onUpdate" @close="close"></m-create-user>
+          <m-create-user :item="item" :from-user-info="true" @onUpdate="onUpdate" @close="close"></m-create-user>
         </el-dialog>
       </template>
     </m-list-box-f>
@@ -104,12 +104,7 @@
         this.createUserDialog = true
       },
       onUpdate (param) {
-        this.setUserInfo({
-          userName: param.userName,
-          userPassword: param.userPassword,
-          email: param.email,
-          phone: param.phone
-        })
+        this.setUserInfo(param)
         this.getUserInfo().finally(() => {
           this.createUserDialog = false
         })

commit 3805348b1a6899967d48462496e4d84053e3f19b

[Fix-5699][UI] Fix update user error in user information introduced by #5700

dolphinscheduler-ui/src/js/conf/home/pages/security/pages/users/index.vue
@@ -60,7 +60,7 @@
 </template>
 <script>
   import _ from 'lodash'
-  import { mapActions, mapMutations } from 'vuex'
+  import { mapActions } from 'vuex'
   import mList from './_source/list'
   import mCreateUser from './_source/createUser'
   import mSpin from '@/module/components/spin/spin'
@@ -89,7 +89,6 @@
     mixins: [listUrlParamHandle],
     props: {},
     methods: {
-      ...mapMutations('user', ['setUserInfo']),
       ...mapActions('security', ['getUsersListP']),
       /**
        * Query
@@ -114,9 +113,8 @@
         this.item = item
         this.createUserDialog = true
       },
-      onUpdate (param) {
+      onUpdate () {
         this._debounceGET('false')
-        this.setUserInfo(param)
         this.createUserDialog = false
       },
       close () {