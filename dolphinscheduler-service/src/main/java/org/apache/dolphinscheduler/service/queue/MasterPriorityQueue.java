d1d3ca1a5e7ff85034742ead93038d797524ef35
lenboo <baoliang.leon@gmail.com>
2021-08-17 09:03:27 +0800

    master refactor

--- dolphinscheduler-service/src/main/java/org/apache/dolphinscheduler/service/queue/MasterPriorityQueue.java
+++ dolphinscheduler-service/src/main/java/org/apache/dolphinscheduler/service/queue/MasterPriorityQueue.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.dolphinscheduler.service.queue;
 
 import org.apache.dolphinscheduler.common.model.Server;
@@ -25,23 +42,23 @@ public class MasterPriorityQueue implements TaskPriorityQueue<Server>{
     private HashMap<String, Integer> hostIndexMap = new HashMap<>();
 
     @Override
-    public void put(Server serverInfo) throws TaskPriorityQueueException {
+    public void put(Server serverInfo) {
         this.queue.put(serverInfo);
         refreshMasterList();
     }
 
     @Override
-    public Server take() throws TaskPriorityQueueException, InterruptedException {
+    public Server take() throws InterruptedException {
         return queue.take();
     }
 
     @Override
-    public Server poll(long timeout, TimeUnit unit) throws TaskPriorityQueueException, InterruptedException {
+    public Server poll(long timeout, TimeUnit unit) {
         return queue.poll();
     }
 
     @Override
-    public int size() throws TaskPriorityQueueException {
+    public int size() {
         return queue.size();
     }
 
