124c09eac4982ba95f3cb51d29bc8311ac9902b9
jon-qj <39773194+jon-qj@users.noreply.github.com>
2021-09-14 16:54:27 +0800

    Update DefaultZookeeperFactory.java
    delete the invalid code

--- dolphinscheduler-registry-plugin/dolphinscheduler-registry-zookeeper/src/main/java/org/apache/dolphinscheduler/plugin/registry/zookeeper/DefaultZookeeperFactory.java
+++ dolphinscheduler-registry-plugin/dolphinscheduler-registry-zookeeper/src/main/java/org/apache/dolphinscheduler/plugin/registry/zookeeper/DefaultZookeeperFactory.java
@@ -14,19 +14,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-package org.apache.dolphinscheduler.plugin.registry.zookeeper;
-
-import org.apache.curator.utils.ZookeeperFactory;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
-import org.apache.zookeeper.client.ZKClientConfig;
-
-public class DefaultZookeeperFactory implements ZookeeperFactory {
-    @Override
-    public ZooKeeper newZooKeeper(String s, int i, Watcher watcher, boolean b) throws Exception {
-        ZKClientConfig config = new ZKClientConfig();
-        config.setProperty(ZKClientConfig.ENABLE_CLIENT_SASL_KEY,"false");
-        return new ZooKeeper(s,i,watcher,b,config);
-    }
-}
+##
+##package org.apache.dolphinscheduler.plugin.registry.zookeeper;
+##
+##import org.apache.curator.utils.ZookeeperFactory;
+##import org.apache.zookeeper.Watcher;
+##import org.apache.zookeeper.ZooKeeper;
+##import org.apache.zookeeper.client.ZKClientConfig;
+##
+##public class DefaultZookeeperFactory implements ZookeeperFactory {
+##    @Override
+##    public ZooKeeper newZooKeeper(String s, int i, Watcher watcher, boolean b) throws Exception {
+##        ZKClientConfig config = new ZKClientConfig();
+##        config.setProperty(ZKClientConfig.ENABLE_CLIENT_SASL_KEY,"false");
+##        return new ZooKeeper(s,i,watcher,b,config);
+##    }
+##}
+##
