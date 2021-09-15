715a8f93ac1b677cddc6c36747d59dfc7efd1448
yinliyue <yinliyue@126.com>
2021-09-15 17:38:45 +0800

    fix: fix the  url mismatch between frontend and backend

--- dolphinscheduler-ui/src/js/conf/home/store/resource/actions.js
+++ dolphinscheduler-ui/src/js/conf/home/store/resource/actions.js
@@ -150,7 +150,7 @@ export default {
    */
   updateContent ({ state }, payload) {
     return new Promise((resolve, reject) => {
-      io.post(`resources/${payload.id}/update-content`, payload, res => {
+      io.post(`resources/${payload.id}`, payload, res => {
         resolve(res)
       }).catch(e => {
         reject(e)
