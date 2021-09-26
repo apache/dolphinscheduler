a874920bed77c917590fb8fcb08cb4b55b863ab8
JinYong Li <42576980+JinyLeeChina@users.noreply.github.com>
2021-09-26 16:36:54 +0800

    [Fix-6352] [ApiServer] fix process definition copy bug (#6357)
    * fix 6320 batch delete process definition bug
    
    * fix-6352 process definition copy bug
    
    Co-authored-by: JinyLeeChina <297062848@qq.com>

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/impl/ProcessDefinitionServiceImpl.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/impl/ProcessDefinitionServiceImpl.java
@@ -1335,6 +1335,14 @@ public class ProcessDefinitionServiceImpl extends BaseServiceImpl implements Pro
             List<ProcessTaskRelationLog> taskRelationList = processTaskRelations.stream().map(ProcessTaskRelationLog::new).collect(Collectors.toList());
             processDefinition.setProjectCode(targetProjectCode);
             if (isCopy) {
+                try {
+                    processDefinition.setCode(SnowFlakeUtils.getInstance().nextId());
+                } catch (SnowFlakeException e) {
+                    putMsg(result, Status.INTERNAL_SERVER_ERROR_ARGS);
+                    throw new ServiceException(Status.INTERNAL_SERVER_ERROR_ARGS);
+                }
+                processDefinition.setId(0);
+                processDefinition.setUserId(loginUser.getId());
                 processDefinition.setName(processDefinition.getName() + "_copy_" + DateUtils.getCurrentTimeStamp());
                 try {
                     result.putAll(createDagDefine(loginUser, taskRelationList, processDefinition, Lists.newArrayList()));
