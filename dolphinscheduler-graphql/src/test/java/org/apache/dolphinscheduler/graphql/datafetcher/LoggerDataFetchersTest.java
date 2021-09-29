0f37d2ecb0f2948f6308ba1b1cee028906c67529
mktb <1270874562@qq.com>
2021-09-29 17:40:36 +0800

    LoggerDataFetchersTest and LoginDataFetchersTest complete

--- dolphinscheduler-graphql/src/test/java/org/apache/dolphinscheduler/graphql/datafetcher/LoggerDataFetchersTest.java
+++ dolphinscheduler-graphql/src/test/java/org/apache/dolphinscheduler/graphql/datafetcher/LoggerDataFetchersTest.java
@@ -1,65 +1,31 @@
 package org.apache.dolphinscheduler.graphql.datafetcher;
 
-import org.apache.dolphinscheduler.api.enums.Status;
-import org.apache.dolphinscheduler.api.utils.Result;
-import org.apache.dolphinscheduler.common.utils.JSONUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 
 import java.util.HashMap;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class LoginDataFetchersTest extends AbstractDataFetchersTest {
+public class LoggerDataFetchersTest extends AbstractDataFetchersTest {
 
-    private static Logger logger = LoggerFactory.getLogger(LoginDataFetchersTest.class);
-
-    @Test
-    public void testLogin() throws Exception {
-        HashMap<String, String> paramsMap = new HashMap<>();
-        paramsMap.put("query",
-                "query login {\n" +
-                "    login(\n" +
-                "        userName: \"admin\"\n" +
-                "        userPassword: \"dolphinscheduler123\"\n" +
-                "        ip: \"127.0.0.1\"\n" +
-                "    ) {\n" +
-                "        code\n" +
-                "        msg\n" +
-                "        data\n" +
-                "        success\n" +
-                "        failed\n" +
-                "    }\n" +
-                "}");
-        paramsMap.put("variables",
-                "{}");
-
-        MvcResult mvcResult = mockMvc.perform(post("/graphql")
-                        .accept(MediaType.parseMediaType("*/*"))
-                        .contentType(MediaType.APPLICATION_JSON_UTF8)
-                        .content(toJson(paramsMap)))
-                .andExpect(status().isOk())
-                .andReturn();
-        System.out.println(mvcResult.getAsyncResult());
-        Assert.assertTrue(mvcResult.getAsyncResult().toString().contains("success=true"));
-        logger.info(mvcResult.getResponse().getContentAsString());
-    }
+    private static Logger logger = LoggerFactory.getLogger(LoggerDataFetchersTest.class);
 
     @Test
-    public void testSignOut() throws Exception {
+    public void testQueryLog() throws Exception {
         HashMap<String, String> paramsMap = new HashMap<>();
         paramsMap.put("query",
-                "mutation signOut {\n" +
-                        "        signOut(loginUser: { id: \"1\", sessionId: \"" + sessionId + "\" },\n" +
-                        "        ip: \"127.0.0.1\"\n" +
+                "query queryLog {\n" +
+                        "    queryLog(\n" +
+                        "        loginUser: { id: \"1\", sessionId: \"" + sessionId + "\" },\n" +
+                        "        taskInstanceId: 1,\n" +
+                        "        skipNum: 0,\n" +
+                        "        limit: 10\n" +
                         "    ) {\n" +
                         "        code\n" +
                         "        msg\n" +
@@ -78,7 +44,40 @@ public class LoginDataFetchersTest extends AbstractDataFetchersTest {
                 .andExpect(status().isOk())
                 .andReturn();
         System.out.println(mvcResult.getAsyncResult());
-        Assert.assertTrue(mvcResult.getAsyncResult().toString().contains("success=true"));
+        Assert.assertEquals("{data={queryLog={code=0, msg=success, data=[LOG-PATH]: logs/, [HOST]:  127.0.0.1\n" +
+                        ", success=true, failed=false}}}",
+                mvcResult.getAsyncResult().toString());
+        logger.info(mvcResult.getResponse().getContentAsString());
+    }
+
+    @Test
+    public void testDownloadTaskLog() throws Exception {
+        HashMap<String, String> paramsMap = new HashMap<>();
+        paramsMap.put("query",
+                "query downloadTaskLog {\n" +
+                        "    downloadTaskLog(\n" +
+                        "        loginUser: { id: \"1\", sessionId: \"" + sessionId + "\" },\n" +
+                        "        taskInstanceId: 1\n" +
+                        "    ) {\n" +
+                        "        code\n" +
+                        "        msg\n" +
+                        "        data\n" +
+                        "        success\n" +
+                        "        failed\n" +
+                        "    }\n" +
+                        "}");
+        paramsMap.put("variables",
+                "{}");
+
+        MvcResult mvcResult = mockMvc.perform(post("/graphql")
+                        .accept(MediaType.parseMediaType("*/*"))
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(toJson(paramsMap)))
+                .andExpect(status().isOk())
+                .andReturn();
+        System.out.println(mvcResult.getAsyncResult());
+        Assert.assertEquals("{data={downloadTaskLog={code=null, msg=null, data=null, success=null, failed=null}}}",
+                mvcResult.getAsyncResult().toString());
         logger.info(mvcResult.getResponse().getContentAsString());
     }
 }
