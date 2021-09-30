69f90ebc43341076e953aab359aaa6951a859c60
liuxuedi <liuxuedi@bytedance.com>
2021-09-30 23:48:56 +0800

    add header

--- docker/kubernetes/sumbit_shell_task.sh
+++ docker/kubernetes/sumbit_shell_task.sh
@@ -1,3 +1,20 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 curl -X POST -u admin:dolphinscheduler123 http://localhost:12345/dolphinscheduler/tenant/create -d 'tenantCode=ds&tenantName=ds&queueId=1&description='
 
 curl -X POST -u admin:dolphinscheduler123 http://127.0.0.1:12345/dolphinscheduler/users/update -d 'userName=admin&userPassword=&tenantId=1&email=xxx%40qq.com&queue=default&phone=&id=1'
