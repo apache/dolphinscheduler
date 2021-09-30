777155ad9daf1b8891674e59c543ae09cc4f852c
mktb <1270874562@qq.com>
2021-09-30 12:32:53 +0800

    complete test

---
+++ dolphinscheduler-graphql/src/test/java/org/apache/dolphinscheduler/graphql/datafetcher/UsersDataFetchersTest.java
@@ -0,0 +1,48 @@
+package org.apache.dolphinscheduler.graphql.datafetcher;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.HashMap;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class UsersDataFetchersTest extends AbstractDataFetchersTest {
+
+    private static Logger logger = LoggerFactory.getLogger(UsersDataFetchersTest.class);
+
+    @Test
+    public void testBatchActivateUser() throws Exception {
+        HashMap<String, String> paramsMap = new HashMap<>();
+        paramsMap.put("query",
+                "query batchActivateUser {\n" +
+                        "    batchActivateUser(\n" +
+                        "        loginUser: { id: \"1\", sessionId: \"" + sessionId + "\" }\n" +
+                        "        userNames: \"test, test1\"\n" +
+                        "    ) {\n" +
+                        "        code\n" +
+                        "        msg\n" +
+                        "        data\n" +
+                        "        success\n" +
+                        "        failed\n" +
+                        "    }\n" +
+                        "}");
+        paramsMap.put("variables",
+                "{}");
+
+        MvcResult mvcResult = mockMvc.perform(post("/graphql")
+                        .accept(MediaType.parseMediaType("*/*"))
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(toJson(paramsMap)))
+                .andExpect(status().isOk())
+                .andReturn();
+        System.out.println(mvcResult.getAsyncResult());
+        Assert.assertTrue(mvcResult.getAsyncResult().toString().contains("success=true"));
+        logger.info(mvcResult.getResponse().getContentAsString());
+    }
+}
---
+++ dolphinscheduler-graphql/src/test/java/org/apache/dolphinscheduler/graphql/datafetcher/WorkFlowLineageDataFetchersTest.java
@@ -0,0 +1,50 @@
+package org.apache.dolphinscheduler.graphql.datafetcher;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class WorkFlowLineageDataFetchersTest extends AbstractDataFetchersTest {
+
+    private static Logger logger = LoggerFactory.getLogger(WorkFlowLineageDataFetchersTest.class);
+
+    @Test
+    public  void testQueryWorkFlowLineageByCode() throws Exception {
+        HashMap<String, String> paramsMap = new HashMap<>();
+        paramsMap.put("query",
+                "query queryWorkFlowLineageByIds {\n" +
+                        "    queryWorkFlowLineageByCode(\n" +
+                        "        loginUser: { id: \"1\", sessionId: \"" + sessionId + "\" }\n" +
+                        "        projectCode: \"1\"\n" +
+                        "        workFlowCode: \"1\"\n" +
+                        "    ) {\n" +
+                        "        code\n" +
+                        "        msg\n" +
+                        "        data\n" +
+                        "        success\n" +
+                        "        failed\n" +
+                        "    }\n" +
+                        "}");
+        paramsMap.put("variables",
+                "{}");
+
+        MvcResult mvcResult = mockMvc.perform(post("/graphql")
+                        .accept(MediaType.parseMediaType("*/*"))
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(toJson(paramsMap)))
+                .andExpect(status().isOk())
+                .andReturn();
+        System.out.println(mvcResult.getAsyncResult());
+        Assert.assertTrue(mvcResult.getAsyncResult().toString().contains("success=true"));
+        logger.info(mvcResult.getResponse().getContentAsString());
+    }
+}
