e4dce4bc825f0cab24c5f7614ea630f036544cb2
CalvinKirs <kirs@apache.org>
2021-10-16 19:52:06 +0800

    [1.3.9-prepare]upgrade version

--- sql/dolphinscheduler_h2.sql
+++ sql/dolphinscheduler_h2.sql
@@ -803,7 +803,7 @@ CREATE TABLE `t_ds_version` (
 -- ----------------------------
 -- Records of t_ds_version
 -- ----------------------------
-INSERT INTO `t_ds_version` VALUES ('1', '1.3.8');
+INSERT INTO `t_ds_version` VALUES ('1', '1.3.9');
 
 
 -- ----------------------------
