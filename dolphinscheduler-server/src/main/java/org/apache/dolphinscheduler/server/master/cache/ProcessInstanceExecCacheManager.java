3c57e79b287fcf9f058d08f97c5c1a9f0759bd5a
ououtt <734164350@qq.com>
2021-10-20 14:55:23 +0800

    [Improvement-6540][server] Optimize the code related to the processInstanceExecMaps object (#6556)
    * [DS-6540][refactor-server] Optimize the code related to the processInstanceExecMaps object

---
+++ dolphinscheduler-server/src/main/java/org/apache/dolphinscheduler/server/master/cache/ProcessInstanceExecCacheManager.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.dolphinscheduler.server.master.cache;
+
+import org.apache.dolphinscheduler.server.master.runner.WorkflowExecuteThread;
+
+import java.util.Collection;
+
+/**
+ * cache of process instance id and WorkflowExecuteThread
+ */
+public interface ProcessInstanceExecCacheManager {
+
+    /**
+     * get WorkflowExecuteThread by process instance id
+     *
+     * @param processInstanceId processInstanceId
+     * @return WorkflowExecuteThread
+     */
+    WorkflowExecuteThread getByProcessInstanceId(int processInstanceId);
+
+    /**
+     * judge the process instance does it exist
+     *
+     * @param processInstanceId processInstanceId
+     * @return true - if process instance id exists in cache
+     */
+    boolean contains(int processInstanceId);
+
+    /**
+     * remove cache by process instance id
+     *
+     * @param processInstanceId processInstanceId
+     */
+    void removeByProcessInstanceId(int processInstanceId);
+
+    /**
+     * cache
+     *
+     * @param processInstanceId     processInstanceId
+     * @param workflowExecuteThread if it is null, will not be cached
+     */
+    void cache(int processInstanceId, WorkflowExecuteThread workflowExecuteThread);
+
+    /**
+     * get all WorkflowExecuteThread from cache
+     *
+     * @return all WorkflowExecuteThread in cache
+     */
+    Collection<WorkflowExecuteThread> getAll();
+}
