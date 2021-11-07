7e01f08a05a8f2cb7f6f3813d9dbbc1bc5185b58
wangxj <wangxj31>
2021-11-07 10:49:08 +0800

    add license header

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/impl/TaskGroupQueueServiceImpl.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/impl/TaskGroupQueueServiceImpl.java
@@ -1,10 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.dolphinscheduler.api.service.impl;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.apache.dolphinscheduler.api.enums.Status;
 import org.apache.dolphinscheduler.api.service.TaskGroupQueueService;
 import org.apache.dolphinscheduler.api.utils.PageInfo;
@@ -14,15 +26,21 @@ import org.apache.dolphinscheduler.dao.entity.User;
 import org.apache.dolphinscheduler.dao.mapper.TaskGroupQueueMapper;
 import org.apache.dolphinscheduler.dao.mapper.TaskInstanceMapper;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+
 /**
- *
+ * task group queue service
  */
-@Service("taskGroupQueueService")
+@Service
 public class TaskGroupQueueServiceImpl extends BaseServiceImpl implements TaskGroupQueueService {
 
     @Autowired
@@ -31,7 +49,7 @@ public class TaskGroupQueueServiceImpl extends BaseServiceImpl implements TaskGr
     @Autowired
     private TaskInstanceMapper taskInstanceMapper;
 
-    private static final Logger logger=LoggerFactory.getLogger(TaskGroupQueueServiceImpl.class);
+    private static final Logger logger = LoggerFactory.getLogger(TaskGroupQueueServiceImpl.class);
 
     /**
      * query tasks in task group queue by group id
@@ -40,11 +58,10 @@ public class TaskGroupQueueServiceImpl extends BaseServiceImpl implements TaskGr
      * @param groupId   group id
      * @param pageNo    page no
      * @param pageSize  page size
-
      * @return tasks list
      */
     @Override
-    public Map<String, Object>queryTasksByGroupId(User loginUser, Integer groupId, Integer pageNo, Integer pageSize) {
+    public Map<String, Object> queryTasksByGroupId(User loginUser, Integer groupId, Integer pageNo, Integer pageSize) {
         return this.doQuery(loginUser, pageNo, pageSize, null, groupId);
     }
 
@@ -58,7 +75,7 @@ public class TaskGroupQueueServiceImpl extends BaseServiceImpl implements TaskGr
      * @return tasks list
      */
     @Override
-    public Map<String, Object>queryTasksByProcessId(User loginUser, Integer pageNo, Integer pageSize, Integer processId) {
+    public Map<String, Object> queryTasksByProcessId(User loginUser, Integer pageNo, Integer pageSize, Integer processId) {
         return this.doQuery(loginUser, pageNo, pageSize, null, processId);
     }
 
@@ -71,21 +88,21 @@ public class TaskGroupQueueServiceImpl extends BaseServiceImpl implements TaskGr
      * @return tasks list
      */
     @Override
-    public Map<String, Object>queryAllTasks(User loginUser, Integer pageNo, Integer pageSize) {
+    public Map<String, Object> queryAllTasks(User loginUser, Integer pageNo, Integer pageSize) {
         return this.doQuery(loginUser, pageNo, pageSize, null, null);
     }
 
-    public Map<String, Object>doQuery(User loginUser, Integer pageNo, Integer pageSize,
-                                      Integer groupId,
-                                      Integer processId) {
-        Map<String, Object>result=new HashMap<>();
+    public Map<String, Object> doQuery(User loginUser, Integer pageNo, Integer pageSize,
+                                       Integer groupId,
+                                       Integer processId) {
+        Map<String, Object> result = new HashMap<>();
         if (isNotAdmin(loginUser, result)) {
             return result;
         }
-        Page<TaskGroupQueue>page=new Page<>(pageNo, pageSize);
-        IPage<TaskGroupQueue>taskGroupQueue=taskGroupQueueMapper.queryTaskGroupQueuePaging(page,groupId);
+        Page<TaskGroupQueue> page = new Page<>(pageNo, pageSize);
+        IPage<TaskGroupQueue> taskGroupQueue = taskGroupQueueMapper.queryTaskGroupQueuePaging(page, groupId);
 
-        PageInfo<TaskGroupQueue>pageInfo=new PageInfo<>(pageNo, pageSize);
+        PageInfo<TaskGroupQueue> pageInfo = new PageInfo<>(pageNo, pageSize);
         pageInfo.setTotal((int) taskGroupQueue.getTotal());
         pageInfo.setTotalList(taskGroupQueue.getRecords());
 
@@ -95,6 +112,7 @@ public class TaskGroupQueueServiceImpl extends BaseServiceImpl implements TaskGr
 
         return result;
     }
+
     /**
      * delete by task id
      *
@@ -104,6 +122,6 @@ public class TaskGroupQueueServiceImpl extends BaseServiceImpl implements TaskGr
 
     @Override
     public boolean deleteByTaskId(Integer taskId) {
-        return taskGroupQueueMapper.deleteByTaskId(taskId)==1;
+        return taskGroupQueueMapper.deleteByTaskId(taskId) == 1;
     }
 }
--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/impl/TaskGroupServiceImpl.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/impl/TaskGroupServiceImpl.java
@@ -1,9 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.dolphinscheduler.api.service.impl;
-
-import java.util.*;
-
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 
 import org.apache.dolphinscheduler.api.enums.Status;
 import org.apache.dolphinscheduler.api.service.TaskGroupQueueService;
@@ -15,15 +27,24 @@ import org.apache.dolphinscheduler.dao.entity.User;
 import org.apache.dolphinscheduler.dao.mapper.TaskGroupMapper;
 import org.apache.dolphinscheduler.service.process.ProcessService;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+
 /**
- *
+ * task Group Service
  */
-@Service("taskGroupService")
+@Service
 public class TaskGroupServiceImpl extends BaseServiceImpl implements TaskGroupService {
 
     @Autowired
@@ -35,7 +56,7 @@ public class TaskGroupServiceImpl extends BaseServiceImpl implements TaskGroupSe
     @Autowired
     private ProcessService processService;
 
-    private static final Logger logger=LoggerFactory.getLogger(TaskGroupServiceImpl.class);
+    private static final Logger logger = LoggerFactory.getLogger(TaskGroupServiceImpl.class);
 
     /**
      * create a Task group
@@ -47,31 +68,31 @@ public class TaskGroupServiceImpl extends BaseServiceImpl implements TaskGroupSe
      * @return the result code and msg
      */
     @Override
-    public Map<String, Object>createTaskGroup(User loginUser, String name, String description, Integer groupSize) {
-        Map<String, Object>result=new HashMap<>();
+    public Map<String, Object> createTaskGroup(User loginUser, String name, String description, Integer groupSize) {
+        Map<String, Object> result = new HashMap<>();
         if (isNotAdmin(loginUser, result)) {
             return result;
         }
-        if (name==null) {
+        if (name == null) {
             putMsg(result, Status.NAME_NULL);
             return result;
         }
-        if (groupSize<=0) {
+        if (groupSize <= 0) {
             putMsg(result, Status.TASK_GROUP_SIZE_ERROR);
             return result;
 
         }
-        TaskGroup taskGroup1=taskGroupMapper.queryByName(loginUser.getId(), name);
-        if (taskGroup1!=null) {
+        TaskGroup taskGroup1 = taskGroupMapper.queryByName(loginUser.getId(), name);
+        if (taskGroup1 != null) {
             putMsg(result, Status.TASK_GROUP_NAME_EXSIT);
             return result;
         }
-        TaskGroup taskGroup=new TaskGroup(0, name, description,
+        TaskGroup taskGroup = new TaskGroup(0, name, description,
                 groupSize, loginUser.getId());
-        Date date=new Date(System.currentTimeMillis());
+        Date date = new Date(System.currentTimeMillis());
         taskGroup.setCreateTime(date);
         taskGroup.setUpdateTime(date);
-        int insert=taskGroupMapper.insert(taskGroup);
+        int insert = taskGroupMapper.insert(taskGroup);
         logger.info("insert result:", insert);
         putMsg(result, Status.SUCCESS);
 
@@ -88,18 +109,18 @@ public class TaskGroupServiceImpl extends BaseServiceImpl implements TaskGroupSe
      * @return the result code and msg
      */
     @Override
-    public Map<String, Object>updateTaskGroup(User loginUser,Integer id, String name, String description, Integer groupSize) {
-        Map<String, Object>result=new HashMap<>();
+    public Map<String, Object> updateTaskGroup(User loginUser, Integer id, String name, String description, Integer groupSize) {
+        Map<String, Object> result = new HashMap<>();
         if (isNotAdmin(loginUser, result)) {
             return result;
         }
-        TaskGroup taskGroup=taskGroupMapper.selectById(id);
-        if (taskGroup.getStatus()!=0) {
+        TaskGroup taskGroup = taskGroupMapper.selectById(id);
+        if (taskGroup.getStatus() != 0) {
             putMsg(result, Status.TASK_GROUP_STATUS_ERROR);
             return result;
         }
-        TaskGroup taskGroup2=taskGroupMapper.queryByName(loginUser.getId(), name);
-        if (taskGroup2!=null&& !taskGroup2.getName().equals(name)) {
+        TaskGroup taskGroup2 = taskGroupMapper.queryByName(loginUser.getId(), name);
+        if (taskGroup2 != null && !taskGroup2.getName().equals(name)) {
             putMsg(result, Status.TASK_GROUP_NAME_EXSIT);
             return result;
         }
@@ -107,22 +128,23 @@ public class TaskGroupServiceImpl extends BaseServiceImpl implements TaskGroupSe
         taskGroup.setUpdateTime(new Date(System.currentTimeMillis()));
         taskGroup.setDescription(description);
         taskGroup.setName(name);
-        int i=taskGroupMapper.updateById(taskGroup);
-        logger.info("update result:{}",i);
+        int i = taskGroupMapper.updateById(taskGroup);
+        logger.info("update result:{}", i);
         putMsg(result, Status.SUCCESS);
         return result;
     }
 
     /**
      * get task group status
+     *
      * @param id task group id
      * @return is the task group available
      */
     @Override
     public boolean isTheTaskGroupAvailable(Integer id) {
-        TaskGroup taskGroup=taskGroupMapper.selectById(id);
+        TaskGroup taskGroup = taskGroupMapper.selectById(id);
 
-        return taskGroup.getStatus()==1;
+        return taskGroup.getStatus() == 1;
     }
 
     /**
@@ -134,8 +156,8 @@ public class TaskGroupServiceImpl extends BaseServiceImpl implements TaskGroupSe
      * @return the result code and msg
      */
     @Override
-    public Map<String, Object>queryAllTaskGroup(User loginUser, Integer pageNo, Integer pageSize) {
-        return this.doQuery(loginUser, pageNo, pageSize, loginUser.getId(), null,  null);
+    public Map<String, Object> queryAllTaskGroup(User loginUser, Integer pageNo, Integer pageSize) {
+        return this.doQuery(loginUser, pageNo, pageSize, loginUser.getId(), null, null);
     }
 
     /**
@@ -148,21 +170,22 @@ public class TaskGroupServiceImpl extends BaseServiceImpl implements TaskGroupSe
      * @return the result code and msg
      */
     @Override
-    public Map<String, Object>queryTaskGroupByStatus(User loginUser, Integer pageNo, Integer pageSize, Integer status) {
-        return this.doQuery(loginUser, pageNo, pageSize, loginUser.getId(), null,  status);
+    public Map<String, Object> queryTaskGroupByStatus(User loginUser, Integer pageNo, Integer pageSize, Integer status) {
+        return this.doQuery(loginUser, pageNo, pageSize, loginUser.getId(), null, status);
     }
 
     /**
      * query all task group by name
+     *
      * @param loginUser login user
-     * @param pageNo page no
-     * @param pageSize page size
-     * @param name name
+     * @param pageNo    page no
+     * @param pageSize  page size
+     * @param name      name
      * @return the result code and msg
      */
     @Override
-    public Map<String, Object>queryTaskGroupByName(User loginUser, Integer pageNo, Integer pageSize, String name) {
-        return this.doQuery(loginUser, pageNo, pageSize, loginUser.getId(),name,  null);
+    public Map<String, Object> queryTaskGroupByName(User loginUser, Integer pageNo, Integer pageSize, String name) {
+        return this.doQuery(loginUser, pageNo, pageSize, loginUser.getId(), name, null);
     }
 
     /**
@@ -173,38 +196,40 @@ public class TaskGroupServiceImpl extends BaseServiceImpl implements TaskGroupSe
      * @return the result code and msg
      */
     @Override
-    public Map<String, Object>queryTaskGroupById(User loginUser, Integer id) {
-        Map<String, Object>result=new HashMap<>();
+    public Map<String, Object> queryTaskGroupById(User loginUser, Integer id) {
+        Map<String, Object> result = new HashMap<>();
         if (isNotAdmin(loginUser, result)) {
             return result;
         }
-        TaskGroup taskGroup=taskGroupMapper.selectById(id);
+        TaskGroup taskGroup = taskGroupMapper.selectById(id);
         result.put(Constants.DATA_LIST, taskGroup);
         putMsg(result, Status.SUCCESS);
         return result;
     }
+
     /**
      * query
-     * @param pageNo    page no
-     * @param pageSize  page size
-     * @param userId user id
-     * @param name name
-     * @param status status
+     *
+     * @param pageNo   page no
+     * @param pageSize page size
+     * @param userId   user id
+     * @param name     name
+     * @param status   status
      * @return the result code and msg
      */
 
     @Override
-    public Map<String, Object>doQuery(User loginUser, Integer pageNo, Integer pageSize, Integer userId, String name, Integer status) {
-        Map<String, Object>result=new HashMap<>();
+    public Map<String, Object> doQuery(User loginUser, Integer pageNo, Integer pageSize, Integer userId, String name, Integer status) {
+        Map<String, Object> result = new HashMap<>();
         if (isNotAdmin(loginUser, result)) {
             return result;
         }
-        Page<TaskGroup>page=new Page<>(pageNo, pageSize);
-        IPage<TaskGroup>taskGroupPaging=taskGroupMapper.queryTaskGroupPaging(page, userId, name, status);
+        Page<TaskGroup> page = new Page<>(pageNo, pageSize);
+        IPage<TaskGroup> taskGroupPaging = taskGroupMapper.queryTaskGroupPaging(page, userId, name, status);
 
-        PageInfo<TaskGroup>pageInfo=new PageInfo<>(pageNo, pageSize);
-        int total=taskGroupPaging==null?0:(int) taskGroupPaging.getTotal();
-        List<TaskGroup>list=taskGroupPaging==null?new ArrayList<TaskGroup>():taskGroupPaging.getRecords();
+        PageInfo<TaskGroup> pageInfo = new PageInfo<>(pageNo, pageSize);
+        int total = taskGroupPaging == null ? 0 : (int) taskGroupPaging.getTotal();
+        List<TaskGroup> list = taskGroupPaging == null ? new ArrayList<TaskGroup>() : taskGroupPaging.getRecords();
         pageInfo.setTotal(total);
         pageInfo.setTotalList(list);
 
@@ -213,6 +238,7 @@ public class TaskGroupServiceImpl extends BaseServiceImpl implements TaskGroupSe
         putMsg(result, Status.SUCCESS);
         return result;
     }
+
     /**
      * close a task group
      *
@@ -222,19 +248,19 @@ public class TaskGroupServiceImpl extends BaseServiceImpl implements TaskGroupSe
      */
 
     @Override
-    public Map<String, Object>closeTaskGroup(User loginUser, Integer id) {
-        Map<String, Object>result=new HashMap<>();
+    public Map<String, Object> closeTaskGroup(User loginUser, Integer id) {
+        Map<String, Object> result = new HashMap<>();
         if (isNotAdmin(loginUser, result)) {
             return result;
         }
-        TaskGroup taskGroup=taskGroupMapper.selectById(id);
-        if (taskGroup.getUseSize()!=0||taskGroup.getStatus()==0) {
+        TaskGroup taskGroup = taskGroupMapper.selectById(id);
+        if (taskGroup.getUseSize() != 0 || taskGroup.getStatus() == 0) {
             putMsg(result, Status.TASK_GROUP_STATUS_ERROR);
             return result;
         }
         taskGroup.setStatus(0);
         taskGroup.setUpdateTime(new Date(System.currentTimeMillis()));
-        int update=taskGroupMapper.updateById(taskGroup);
+        int update = taskGroupMapper.updateById(taskGroup);
         putMsg(result, Status.SUCCESS);
         return result;
     }
@@ -247,19 +273,19 @@ public class TaskGroupServiceImpl extends BaseServiceImpl implements TaskGroupSe
      * @return the result code and msg
      */
     @Override
-    public Map<String, Object>startTaskGroup(User loginUser, Integer id) {
-        Map<String, Object>result=new HashMap<>();
+    public Map<String, Object> startTaskGroup(User loginUser, Integer id) {
+        Map<String, Object> result = new HashMap<>();
         if (isNotAdmin(loginUser, result)) {
             return result;
         }
-        TaskGroup taskGroup=taskGroupMapper.selectById(id);
-        if (taskGroup.getStatus()==1) {
+        TaskGroup taskGroup = taskGroupMapper.selectById(id);
+        if (taskGroup.getStatus() == 1) {
             putMsg(result, Status.TASK_GROUP_STATUS_ERROR);
             return result;
         }
         taskGroup.setStatus(1);
         taskGroup.setUpdateTime(new Date(System.currentTimeMillis()));
-        int update=taskGroupMapper.updateById(taskGroup);
+        int update = taskGroupMapper.updateById(taskGroup);
         putMsg(result, Status.SUCCESS);
         return result;
     }
@@ -273,8 +299,8 @@ public class TaskGroupServiceImpl extends BaseServiceImpl implements TaskGroupSe
      */
     @SuppressWarnings("checkstyle:WhitespaceAround")
     @Override
-    public Map<String, Object>wakeTaskcompulsively(User loginUser, Integer taskId) {
-        Map<String, Object>result=new HashMap<>();
+    public Map<String, Object> wakeTaskcompulsively(User loginUser, Integer taskId) {
+        Map<String, Object> result = new HashMap<>();
         if (isNotAdmin(loginUser, result)) {
             return result;
         }
