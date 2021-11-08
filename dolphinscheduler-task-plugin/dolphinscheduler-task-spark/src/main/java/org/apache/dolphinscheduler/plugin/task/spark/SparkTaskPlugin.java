0884ed71b630bc367e56d01e36b935aba7f7bb66
jon-qj <18351938808@sina.cn>
2021-11-08 11:42:17 +0800

    rename spark plugin's implemented function

--- dolphinscheduler-task-plugin/dolphinscheduler-task-spark/src/main/java/org/apache/dolphinscheduler/plugin/task/spark/SparkTaskPlugin.java
+++ dolphinscheduler-task-plugin/dolphinscheduler-task-spark/src/main/java/org/apache/dolphinscheduler/plugin/task/spark/SparkTaskPlugin.java
@@ -25,7 +25,7 @@ import com.google.common.collect.ImmutableList;
 public class SparkTaskPlugin implements DolphinSchedulerPlugin {
 
     @Override
-    public Iterable<TaskChannelFactory> getTaskChannelFactorys() {
+    public Iterable<TaskChannelFactory> getTaskChannelFactories() {
         return ImmutableList.of(new SparkTaskChannelFactory());
     }
 }
