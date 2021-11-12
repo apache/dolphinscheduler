e36d18b58890eb18a64ecc2a5181b37125a070f8
mask <39329477+Narcasserun@users.noreply.github.com>
2021-11-12 10:51:11 +0800

    [Fix-6792] resolve API and alert port conflicts (#6792) (#6815)
    * [Fix-6792]Multiple ApplicationRunners on classpath make SpringApplication.run only run one (#6792)

--- dolphinscheduler-alert/dolphinscheduler-alert-server/src/main/java/org/apache/dolphinscheduler/alert/AlertServer.java
+++ dolphinscheduler-alert/dolphinscheduler-alert-server/src/main/java/org/apache/dolphinscheduler/alert/AlertServer.java
@@ -38,10 +38,10 @@ import javax.annotation.PreDestroy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@EnableAutoConfiguration
 @ComponentScan(value = {
     "org.apache.dolphinscheduler.alert",
     "org.apache.dolphinscheduler.dao"
