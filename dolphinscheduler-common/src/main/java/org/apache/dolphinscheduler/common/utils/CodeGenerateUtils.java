da31a408001c98eed894f8704b018e4bea1608af
lijinyong <297061848@qq.com>
2021-11-19 15:00:27 +0800

    add algorithm from licenses file

--- dolphinscheduler-common/src/main/java/org/apache/dolphinscheduler/common/utils/CodeGenerateUtils.java
+++ dolphinscheduler-common/src/main/java/org/apache/dolphinscheduler/common/utils/CodeGenerateUtils.java
@@ -1,26 +1,13 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+/** Copyright 2010-2012 Twitter, Inc.*/
 package org.apache.dolphinscheduler.common.utils;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Objects;
 
+/**
+ *  Rewriting based on Twitter snowflake algorithm
+ */
 public class CodeGenerateUtils {
     // start timestamp
     private static final long START_TIMESTAMP = 1609430400000L; //2021-01-01 00:00:00
