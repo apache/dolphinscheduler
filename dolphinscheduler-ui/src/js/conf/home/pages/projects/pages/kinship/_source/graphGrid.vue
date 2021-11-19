b66990e31e38cf68c0d2cbe4e7d756e342647cae
songjianet <1778651752@qq.com>
2021-11-19 17:48:49 +0800

    [Feature][Workflow relationship] The page displays `null` as `-`.

--- dolphinscheduler-ui/src/js/conf/home/pages/projects/pages/kinship/_source/graphGrid.vue
+++ dolphinscheduler-ui/src/js/conf/home/pages/projects/pages/kinship/_source/graphGrid.vue
@@ -38,7 +38,20 @@
     },
     mounted () {
       const graphGrid = echarts.init(this.$refs['graph-grid'])
-      graphGrid.setOption(graphGridOption(this.locations, this.connects, this.sourceWorkFlowCode, this.isShowLabel), true)
+      graphGrid.setOption(
+        graphGridOption(
+          this.locations.map(item => {
+            item.crontab = item.crontab ? item.crontab : '-'
+            item.scheduleEndTime = item.scheduleEndTime ? item.scheduleEndTime : '-'
+            item.scheduleStartTime = item.scheduleStartTime ? item.scheduleStartTime : '-'
+            return item
+          }),
+          this.connects,
+          this.sourceWorkFlowCode,
+          this.isShowLabel
+        ),
+        true
+      )
       graphGrid.on('click', (params) => {
         // Jump to the definition page
         this.$router.push({ path: `/projects/${this.projectCode}/definition/list/${params.data.code}` })
