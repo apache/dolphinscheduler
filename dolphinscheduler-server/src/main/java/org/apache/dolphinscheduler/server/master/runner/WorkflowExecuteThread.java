37ae193edd15b231dea701e0abb9fd4cf24e3483
SbloodyS <sbloodys@qq.com>
2021-12-03 10:01:43 +0800

    fix bug 7037

--- dolphinscheduler-server/src/main/java/org/apache/dolphinscheduler/server/master/runner/WorkflowExecuteThread.java
+++ dolphinscheduler-server/src/main/java/org/apache/dolphinscheduler/server/master/runner/WorkflowExecuteThread.java
@@ -518,7 +518,7 @@ public class WorkflowExecuteThread implements Runnable {
     private boolean processStateChangeHandler(StateEvent stateEvent) {
         try {
             logger.info("process:{} state {} change to {}", processInstance.getId(), processInstance.getState(), stateEvent.getExecutionStatus());
-            if (processComplementData()) {
+            if (processInstance.getState() != ExecutionStatus.READY_STOP && processComplementData()) {
                 return true;
             }
             if (stateEvent.getExecutionStatus().typeIsFinished()) {
