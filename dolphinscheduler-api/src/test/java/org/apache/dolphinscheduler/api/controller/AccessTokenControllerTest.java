533a89c5e12ac8c4817de780f78ca0d0ba61e109
ouyangyewei <ouyangyewei@foxmail.com>
2021-12-14 17:49:20 +0800

    to feature 7401 (#7406)
    Co-authored-by: ouyangyewei <yewei.oyyw@alibaba-inc.com>

--- dolphinscheduler-api/src/test/java/org/apache/dolphinscheduler/api/controller/AccessTokenControllerTest.java
+++ dolphinscheduler-api/src/test/java/org/apache/dolphinscheduler/api/controller/AccessTokenControllerTest.java
@@ -60,6 +60,26 @@ public class AccessTokenControllerTest extends AbstractControllerTest {
     }
 
     @Test
+    public void testCreateTokenWhenTokenAbsent() throws Exception {
+        MultiValueMap<String, String> paramsMap = new LinkedMultiValueMap<>();
+        paramsMap.add("userId", "4");
+        paramsMap.add("expireTime", "2019-12-18 00:00:00");
+        paramsMap.add("token", null);
+
+        MvcResult mvcResult = this.mockMvc
+                .perform(post("/access-tokens")
+                .header("sessionId", this.sessionId)
+                .params(paramsMap))
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        Result result = JSONUtils.parseObject(mvcResult.getResponse().getContentAsString(), Result.class);
+        Assert.assertEquals(Status.SUCCESS.getCode(), result.getCode().intValue());
+        logger.info(mvcResult.getResponse().getContentAsString());
+    }
+
+    @Test
     public void testExceptionHandler() throws Exception {
         MultiValueMap<String, String> paramsMap = new LinkedMultiValueMap<>();
         paramsMap.add("userId","-1");
