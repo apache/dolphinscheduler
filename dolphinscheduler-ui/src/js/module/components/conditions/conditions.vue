5b22a381cb7e8a1ff1860fa4de8a75fc939af2ad
calvin <jianghuachinacom@163.com>
2021-12-17 14:40:40 +0800

    cherry-pick [Fix-7457] Support to search when pressing enter button in the search box

--- dolphinscheduler-ui/src/js/module/components/conditions/conditions.vue
+++ dolphinscheduler-ui/src/js/module/components/conditions/conditions.vue
@@ -27,12 +27,15 @@
             <el-button size="mini" @click="_ckQuery" icon="el-icon-search"></el-button>
           </div>
           <div class="list">
-            <el-input v-model="searchVal"
-                     @keyup.enter="_ckQuery"
-                     size="mini"
-                     :placeholder="$t('Please enter keyword')"
-                     type="text"
-                     style="width:180px;">
+            <el-input
+              v-model="searchVal"
+              @keyup.enter.native="_ckQuery"
+              size="mini"
+              :placeholder="$t('Please enter keyword')"
+              type="text"
+              style="width: 180px"
+              clearable
+            >
             </el-input>
           </div>
         </template>
