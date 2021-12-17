91e3423c724e9abbfb2e84fa54af0fd792e0361e
labbomb <739955946@qq.com>
2021-12-17 09:28:34 +0800

    feat: Added login pages and functions (#7456)

--- dolphinscheduler-ui-next/src/env.d.ts
+++ dolphinscheduler-ui-next/src/env.d.ts
@@ -22,11 +22,6 @@ declare module '*.vue' {
   export default component
 }
 
-declare module '*.scss' {
-  const classes: { readonly [key: string]: string }
-  export default classes
-}
-
 declare module '*.png'
 declare module '*.jpg'
 declare module '*.jpeg'
