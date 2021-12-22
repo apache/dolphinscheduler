39b15b221c4fc9f33a843448b7baaea077ed64a7
Kerwin <37063904+zhuangchong@users.noreply.github.com>
2021-12-23 00:54:44 +0800

    [Fix-#7480] [Standalone] add logger server (#7481)
    * Change build context to avoid cache
    
    Co-authored-by: kezhenxu94 <kezhenxu94@apache.org>

--- dolphinscheduler-standalone-server/src/main/java/org/apache/dolphinscheduler/StandaloneServer.java
+++ dolphinscheduler-standalone-server/src/main/java/org/apache/dolphinscheduler/StandaloneServer.java
@@ -17,6 +17,8 @@
 
 package org.apache.dolphinscheduler;
 
+import org.apache.dolphinscheduler.server.log.LoggerServer;
+
 import org.apache.curator.test.TestingServer;
 
 import org.springframework.boot.SpringApplication;
@@ -27,7 +29,7 @@ public class StandaloneServer {
     public static void main(String[] args) throws Exception {
         final TestingServer server = new TestingServer(true);
         System.setProperty("registry.zookeeper.connect-string", server.getConnectString());
-
+        new LoggerServer().start();
         SpringApplication.run(StandaloneServer.class, args);
     }
 }
