4f9ef699f9517906ddc2cf68709a108902140bd0
caishunfeng <534328519@qq.com>
2021-12-23 11:32:57 +0800

    fix 2.0.2 upgrade sql

--- dolphinscheduler-dao/src/main/resources/sql/upgrade/2.0.2_schema/mysql/dolphinscheduler_ddl.sql
+++ dolphinscheduler-dao/src/main/resources/sql/upgrade/2.0.2_schema/mysql/dolphinscheduler_ddl.sql
@@ -17,4 +17,22 @@
 
 SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
 
-alter table t_ds_process_instance add column if not exists `restart_time` datetime DEFAULT NULL COMMENT 'process instance restart time';
+-- uc_dolphin_T_t_ds_process_instance_A_restart_time
+drop PROCEDURE if EXISTS uc_dolphin_T_t_ds_process_instance_A_restart_time;
+delimiter d//
+CREATE PROCEDURE uc_dolphin_T_t_ds_process_instance_A_restart_time()
+   BEGIN
+       IF NOT EXISTS (SELECT 1 FROM information_schema.COLUMNS
+           WHERE TABLE_NAME='t_ds_process_instance'
+           AND TABLE_SCHEMA=(SELECT DATABASE())
+           AND COLUMN_NAME ='restart_time')
+   THEN
+         ALTER TABLE t_ds_process_instance ADD COLUMN `restart_time` datetime DEFAULT NULL COMMENT 'process instance restart time';
+       END IF;
+ END;
+
+d//
+
+delimiter ;
+CALL uc_dolphin_T_t_ds_process_instance_A_restart_time();
+DROP PROCEDURE uc_dolphin_T_t_ds_process_instance_A_restart_time;
