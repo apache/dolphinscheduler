5c57629781818c94f63422b002131f9ca4f7f236
songjianet <1778651752@qq.com>
2022-01-04 21:23:50 +0800

    [Feature][UI Next] Add password page. (#7795)

--- dolphinscheduler-ui-next/src/locales/modules/en_US.ts
+++ dolphinscheduler-ui-next/src/locales/modules/en_US.ts
@@ -31,6 +31,7 @@ const theme = {
 
 const profile = {
   profile: 'Profile',
+  password: 'Password',
   logout: 'Logout',
 }
 
@@ -76,10 +77,22 @@ const home = {
   state: 'State',
 }
 
+const password = {
+  edit_password: 'Edit Password',
+  password: 'Password',
+  confirm_password: 'Confirm Password',
+  password_tips: 'Please enter your password',
+  confirm_password_tips: 'Please enter your confirm password',
+  two_password_entries_are_inconsistent:
+    'Two Password Entries Are Inconsistent',
+  submit: 'Submit',
+}
+
 export default {
   login,
   theme,
   profile,
   menu,
   home,
+  password,
 }
