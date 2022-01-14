c5aeb180244700ac8cffd7ea16fbee0324c2d19f
devosend <devosend@gmail.com>
2022-01-14 19:37:46 +0800

    modify docstring of Engine

--- dolphinscheduler-python/pydolphinscheduler/src/pydolphinscheduler/core/engine.py
+++ dolphinscheduler-python/pydolphinscheduler/src/pydolphinscheduler/core/engine.py
@@ -35,7 +35,7 @@ class ProgramType(str):
 
 
 class Engine(Task):
-    """Task engine object, declare behavior for spark task to dolphinscheduler.
+    """Task engine object, declare behavior for engine task to dolphinscheduler.
 
     This is the parent class of spark, flink and mr tasks,
     and is used to provide the programType, mainClass and mainJar task parameters for reuse.
