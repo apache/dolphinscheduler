38b40e7ac49390164207104a4a788639b12696c4
labbomb <739955946@qq.com>
2022-01-20 22:25:16 +0800

    [Feature][UI Next]Improve menu bar function (#8141)
    * Improve menu bar function
    
    * Improve menu bar function

--- dolphinscheduler-ui-next/src/layouts/content/use-dataList.ts
+++ dolphinscheduler-ui-next/src/layouts/content/use-dataList.ts
@@ -44,9 +44,11 @@ import {
   SafetyOutlined,
   GroupOutlined
 } from '@vicons/antd'
+import { useMenuStore } from '@/store/menu/menu'
 
 export function useDataList() {
   const { t } = useI18n()
+  const menuStore = useMenuStore()
 
   const renderIcon = (icon: any) => {
     return () => h(NIcon, null, { default: () => h(icon) })
@@ -268,13 +270,11 @@ export function useDataList() {
         label: string
         key: string
         icon: any
-        isShowSide: boolean
       }) => {
         return {
           label: item.label,
           key: item.key,
           icon: item.icon,
-          isShowSide: item.isShowSide
         }
       }
     )
