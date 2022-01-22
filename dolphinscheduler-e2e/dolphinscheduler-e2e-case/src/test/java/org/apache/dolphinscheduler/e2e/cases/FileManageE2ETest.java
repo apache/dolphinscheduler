1dc26247c174aee44a817df09eaee9b4aa1ed1fd
SbloodyS <460888207@qq.com>
2022-01-22 09:57:13 +0800

    test

--- dolphinscheduler-e2e/dolphinscheduler-e2e-case/src/test/java/org/apache/dolphinscheduler/e2e/cases/FileManageE2ETest.java
+++ dolphinscheduler-e2e/dolphinscheduler-e2e-case/src/test/java/org/apache/dolphinscheduler/e2e/cases/FileManageE2ETest.java
@@ -308,18 +308,6 @@ public class FileManageE2ETest {
         final FileManagePage page = new FileManagePage(browser);
 
         page.downloadFile(testUnder1GBFileName);
-
-//        String downloadFilePath = String.format("/home/%s/Downloads/%s", tenant, testUnder1GBFileName);
-//        File file = new File(downloadFilePath);
-//
-//        new FluentWait<WebDriver>(page.driver())
-//                .withTimeout(Duration.ofSeconds(60))
-//                .pollingEvery(Duration.ofSeconds(1))
-//                .ignoring(NoSuchElementException.class)
-//                .until(webDriver -> file.exists());
-//
-//        if (!file.exists()) {
-//            throw new RuntimeException("download file failed");
-//        }
+        
     }
 }
