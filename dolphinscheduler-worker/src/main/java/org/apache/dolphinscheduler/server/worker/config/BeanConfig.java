25cb4a89f8a623cce517866fb901d5c6bc096a25
ruanwenjun <wenjun@apache.org>
2022-01-24 20:32:28 +0800

    Fix code smell

--- dolphinscheduler-worker/src/main/java/org/apache/dolphinscheduler/server/worker/config/BeanConfig.java
+++ dolphinscheduler-worker/src/main/java/org/apache/dolphinscheduler/server/worker/config/BeanConfig.java
@@ -19,18 +19,14 @@ package org.apache.dolphinscheduler.server.worker.config;
 
 import org.apache.dolphinscheduler.service.alert.AlertClientService;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeanConfig {
 
-    @Autowired
-    private WorkerConfig workerConfig;
-
     @Bean
-    public AlertClientService alertClientService() {
+    public AlertClientService alertClientService(WorkerConfig workerConfig) {
         return new AlertClientService(
                 workerConfig.getAlertListenHost(),
                 workerConfig.getAlertListenPort());
