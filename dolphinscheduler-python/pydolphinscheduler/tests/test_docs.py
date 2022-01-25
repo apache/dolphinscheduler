50e1eb84a25dc9f53a51e7d6ea9f691ede285f88
Jiajie Zhong <zhongjiajie955@hotmail.com>
2022-01-25 19:27:44 +0800

    Add test constants and path for more easier testing

--- dolphinscheduler-python/pydolphinscheduler/tests/test_docs.py
+++ dolphinscheduler-python/pydolphinscheduler/tests/test_docs.py
@@ -18,37 +18,42 @@
 """Test pydolphinscheduler docs."""
 
 import re
-from pathlib import Path
 
-path_code_base = Path(__file__).parent.parent.joinpath(
-    "src", "pydolphinscheduler", "tasks"
-)
-path_doc_base = Path(__file__).parent.parent.joinpath("docs", "source", "tasks")
+from tests.testing.constants import task_without_example
+from tests.testing.path import get_doc_tasks, get_tasks
 
 ignore_code_file = {"__init__.py"}
-
 ignore_doc_file = {"index.rst"}
 
 
 def test_without_missing_task_rst():
-    """Test without missing any task document by compare filename."""
-    code_files = {
-        file.stem
-        for file in path_code_base.iterdir()
-        if file.is_file() and file.name not in ignore_code_file
-    }
-    doc_files = {
-        file.stem
-        for file in path_doc_base.iterdir()
-        if file.is_file() and file.name not in ignore_doc_file
-    }
+    """Test without missing any task document by compare filename.
+
+    Avoiding add new type of tasks but without adding document about it.
+    """
+    code_files = {p.stem for p in get_tasks(ignore_name=ignore_code_file)}
+    doc_files = {p.stem for p in get_doc_tasks(ignore_name=ignore_doc_file)}
     assert code_files == doc_files
+
+
+def test_task_without_example():
+    """Test task document which without example.
+
+    Avoiding add new type of tasks but without adding example content describe how to use it.
+    """
+    task_without_example_detected = set()
+    pattern = re.compile("Example\n-------")
+
+    for doc in get_doc_tasks(ignore_name=ignore_doc_file):
+        search_result = pattern.search(doc.read_text())
+        if not search_result:
+            task_without_example_detected.add(doc.stem)
+    assert task_without_example == task_without_example_detected
 
 
 def test_doc_automodule_directive_name():
     """Test task document with correct name in directive automodule."""
     pattern = re.compile(".. automodule:: (.*)")
-    for doc in path_doc_base.iterdir():
-        if doc.is_file() and doc.name not in ignore_doc_file:
-            match_string = pattern.search(doc.read_text()).group(1)
-            assert f"pydolphinscheduler.tasks.{doc.stem}" == match_string
+    for doc in get_doc_tasks(ignore_name=ignore_doc_file):
+        match_string = pattern.search(doc.read_text()).group(1)
+        assert f"pydolphinscheduler.tasks.{doc.stem}" == match_string
