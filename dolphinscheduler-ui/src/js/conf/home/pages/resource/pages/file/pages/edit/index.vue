d5e66df79ef6f908a0413f4a1f087021c85dcaa8
xiangzihao <460888207@qq.com>
2022-01-26 09:45:21 +0800

    [E2E][CI] Add renaming file management E2E tests (#8160)

--- dolphinscheduler-ui/src/js/conf/home/pages/resource/pages/file/pages/edit/index.vue
+++ dolphinscheduler-ui/src/js/conf/home/pages/resource/pages/file/pages/edit/index.vue
@@ -27,8 +27,8 @@
               <textarea id="code-edit-mirror" name="code-edit-mirror"></textarea>
             </div>
             <div class="submit-c">
-              <el-button type="text" @click="close()" :disabled="disabled" size="small"> {{$t('Return')}} </el-button>
-              <el-button type="primary" :loading="spinnerLoading" @click="ok()" round size="small">{{spinnerLoading ? $t('Loading...') : $t('Save')}} </el-button>
+              <el-button id="btnCancel" type="text" @click="close()" :disabled="disabled" size="small"> {{$t('Return')}} </el-button>
+              <el-button id="btnSubmit" type="primary" :loading="spinnerLoading" @click="ok()" round size="small">{{spinnerLoading ? $t('Loading...') : $t('Save')}} </el-button>
             </div>
           </template>
           <m-no-data :msg="msg" v-if="msg"></m-no-data>
