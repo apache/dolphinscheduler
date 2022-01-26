b05ba4d1b9e6e66fb42f9cd5f7212e919c07725c
wind <caishunfeng@users.noreply.github.com>
2022-01-26 17:51:20 +0800

    fix host update processor init (#8205)
    Co-authored-by: caishunfeng <534328519@qq.com>

--- dolphinscheduler-worker/src/main/java/org/apache/dolphinscheduler/server/worker/processor/HostUpdateProcessor.java
+++ dolphinscheduler-worker/src/main/java/org/apache/dolphinscheduler/server/worker/processor/HostUpdateProcessor.java
@@ -45,17 +45,17 @@ public class HostUpdateProcessor implements NettyRequestProcessor {
     /**
      * task callback service
      */
-    private final TaskCallbackService taskCallbackService;
-
-    public HostUpdateProcessor() {
-        this.taskCallbackService = SpringApplicationContext.getBean(TaskCallbackService.class);
-    }
+    private TaskCallbackService taskCallbackService;
 
     @Override
     public void process(Channel channel, Command command) {
         Preconditions.checkArgument(CommandType.PROCESS_HOST_UPDATE_REQUEST == command.getType(), String.format("invalid command type : %s", command.getType()));
         HostUpdateCommand updateCommand = JSONUtils.parseObject(command.getBody(), HostUpdateCommand.class);
         logger.info("received host update command : {}", updateCommand);
+
+        if (taskCallbackService == null) {
+            taskCallbackService = SpringApplicationContext.getBean(TaskCallbackService.class);
+        }
         taskCallbackService.changeRemoteChannel(updateCommand.getTaskInstanceId(), new NettyRemoteChannel(channel, command.getOpaque()));
 
     }
