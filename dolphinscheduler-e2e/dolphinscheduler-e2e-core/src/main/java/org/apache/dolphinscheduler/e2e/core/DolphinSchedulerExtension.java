e24f4690bce6e890937fa28f0243646f4cef0660
SbloodyS <460888207@qq.com>
2022-01-26 22:32:13 +0800

    test

--- dolphinscheduler-e2e/dolphinscheduler-e2e-core/src/main/java/org/apache/dolphinscheduler/e2e/core/DolphinSchedulerExtension.java
+++ dolphinscheduler-e2e/dolphinscheduler-e2e-core/src/main/java/org/apache/dolphinscheduler/e2e/core/DolphinSchedulerExtension.java
@@ -119,9 +119,9 @@ final class DolphinSchedulerExtension
 
         Map<String, Object> prefs = new HashMap<>();
         prefs.put("download.default_directory", Constants.SELENIUM_CONTAINER_CHROME_DOWNLOAD_PATH);
-        prefs.put("download.prompt_for_download", false);
+        prefs.put("download", new HashMap<>().put("prompt_for_download", false));
         prefs.put("profile.default_content_settings.popups", 0);
-        prefs.put("safebrowsing.enabled", true);
+        prefs.put("safebrowsing", new HashMap<>().put("enabled", true));
 
 //        List<String> args = new ArrayList<>();
 //        args.add("--disable-extensions");
