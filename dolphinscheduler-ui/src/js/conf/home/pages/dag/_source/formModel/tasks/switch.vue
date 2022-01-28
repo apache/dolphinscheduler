48d83efe391959eb75af97983e52f3130847742f
Devosend <devosend@gmail.com>
2022-01-28 18:25:25 +0800

    [FIX-7514] press Enter key get "true" not new line (#7596)

--- dolphinscheduler-ui/src/js/conf/home/pages/dag/_source/formModel/tasks/switch.vue
+++ dolphinscheduler-ui/src/js/conf/home/pages/dag/_source/formModel/tasks/switch.vue
@@ -101,7 +101,10 @@
         this.keypress = () => {
           if (!editor.getOption('readOnly')) {
             editor.showHint({
-              completeSingle: false
+              completeSingle: false,
+              extraKeys: {
+                Enter: ''
+              }
             })
           }
         }
