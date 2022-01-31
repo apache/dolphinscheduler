6be220129fe214eeb56b9e774ef42c65b78bffa8
Amy0104 <97265214+Amy0104@users.noreply.github.com>
2022-01-31 12:24:16 +0800

    [Feature][UI Next] Add custom parameters to form (#8271)

--- dolphinscheduler-ui-next/src/components/form/fields/get-field.ts
+++ dolphinscheduler-ui-next/src/components/form/fields/get-field.ts
@@ -14,33 +14,48 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-import { formatValidate } from './utils'
-import getField from './fields/get-field'
+import * as Field from './index'
 import type { FormRules } from 'naive-ui'
-import type { IJsonItem } from './types'
+import type { IJsonItem } from '../types'
 
-export default function getElementByJson(
-  json: IJsonItem[],
-  fields: { [field: string]: any }
-) {
-  const rules: FormRules = {}
-  const initialValues: { [field: string]: any } = {}
-  const elements = []
-
-  for (let item of json) {
-    if (item.value) {
-      fields[item.field] = item.value
-      initialValues[item.field] = item.value
-    }
-    if (item.validate) rules[item.field] = formatValidate(item.validate)
-    elements.push({
-      label: item.name,
-      path: !item.children ? item.field : '',
-      showLabel: !!item.name,
-      widget: () => getField(item, fields, rules)
+const getField = (
+  item: IJsonItem,
+  fields: { [field: string]: any },
+  rules?: FormRules
+) => {
+  const { type, props = {}, field, options, children } = item
+  // TODO Support other widgets later
+  if (type === 'radio') {
+    return Field.renderRadio({
+      field,
+      fields,
+      props,
+      options
+    })
+  }
+  if (type === 'editor') {
+    return Field.renderEditor({
+      field,
+      fields,
+      props
     })
   }
 
-  return { rules, elements, initialValues }
+  if (type === 'custom-parameters') {
+    const params = {
+      field,
+      fields,
+      children,
+      props,
+      rules: []
+    }
+    if (rules) {
+      params.rules = rules[field] = []
+    }
+    return Field.renderCustomParameters(params)
+  }
+
+  return Field.renderInput({ field, fields, props })
 }
+
+export default getField
