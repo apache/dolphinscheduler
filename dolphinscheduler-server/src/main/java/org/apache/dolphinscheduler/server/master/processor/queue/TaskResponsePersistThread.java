c11f4b0463af9949398385873e23d698ead31071
SbloodyS <460888207@qq.com>
2022-02-17 13:44:24 +0800

    fix bug_8367

--- dolphinscheduler-server/src/main/java/org/apache/dolphinscheduler/server/master/processor/queue/TaskResponsePersistThread.java
+++ dolphinscheduler-server/src/main/java/org/apache/dolphinscheduler/server/master/processor/queue/TaskResponsePersistThread.java
@@ -105,15 +105,13 @@ public class TaskResponsePersistThread implements Runnable {
                                 taskResponseEvent.getExecutePath(),
                                 taskResponseEvent.getLogPath(),
                                 taskResponseEvent.getTaskInstanceId());
-                        logger.debug("changeTaskState in ACK , changed in meta:{} ,task instance state:{}, task " +
-                                        "response event state:{}, taskInstance id:{},taskInstance host:{}",
+                        logger.debug("changeTaskState in ACK , changed in meta:{} ,task instance state:{}, task response event state:{}, taskInstance id:{},taskInstance host:{}",
                                 result, taskInstance.getState(), taskResponseEvent.getState(), taskInstance.getId(), taskInstance.getHost());
                     }
                     // if taskInstance is null (maybe deleted) . retry will be meaningless . so ack success
                     DBTaskAckCommand taskAckCommand = new DBTaskAckCommand(ExecutionStatus.SUCCESS.getCode(), taskResponseEvent.getTaskInstanceId());
                     channel.writeAndFlush(taskAckCommand.convert2Command());
-                    logger.debug("worker ack master success, taskInstance id:{},taskInstance host:{}",
-                            taskInstance.getId(), taskInstance.getHost());
+                    logger.debug("worker ack master success, taskInstance id:{},taskInstance host:{}", taskInstance.getId(), taskInstance.getHost());
                 } catch (Exception e) {
                     result = false;
                     logger.error("worker ack master error", e);
--- dolphinscheduler-standalone-server/src/main/java/org/apache/dolphinscheduler/server/StandaloneServer.java
+++ dolphinscheduler-standalone-server/src/main/java/org/apache/dolphinscheduler/server/StandaloneServer.java
@@ -19,7 +19,6 @@ package org.apache.dolphinscheduler.server;
 
 import org.apache.dolphinscheduler.alert.AlertServer;
 import org.apache.dolphinscheduler.api.ApiApplicationServer;
-import org.apache.dolphinscheduler.server.log.LoggerServer;
 import org.apache.dolphinscheduler.server.master.MasterServer;
 import org.apache.dolphinscheduler.server.worker.WorkerServer;
 
