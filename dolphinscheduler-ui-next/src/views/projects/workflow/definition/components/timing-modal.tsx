86a11f53d369542e039f2e4cd80fb5e0962baac6
Devosend <devosend@gmail.com>
2022-02-17 09:13:53 +0800

    [Feature][UI Next] Add Crontab Component (#8409)
    * support crontab component
    
    * fix time-modal form data init bug

--- dolphinscheduler-ui-next/src/views/projects/workflow/definition/components/timing-modal.tsx
+++ dolphinscheduler-ui-next/src/views/projects/workflow/definition/components/timing-modal.tsx
@@ -15,7 +15,15 @@
  * limitations under the License.
  */
 
-import { defineComponent, PropType, toRefs, h, onMounted, ref } from 'vue'
+import {
+  defineComponent,
+  PropType,
+  toRefs,
+  h,
+  onMounted,
+  ref,
+  watch
+} from 'vue'
 import { useI18n } from 'vue-i18n'
 import Modal from '@/components/modal'
 import { useForm } from './use-form'
@@ -34,10 +42,12 @@ import {
   NInputGroup,
   NList,
   NListItem,
-  NThing
+  NThing,
+  NPopover
 } from 'naive-ui'
 import { ArrowDownOutlined, ArrowUpOutlined } from '@vicons/antd'
 import { timezoneList } from '@/utils/timezone'
+import Crontab from '@/components/crontab'
 
 const props = {
   row: {
@@ -59,6 +69,7 @@ export default defineComponent({
   props,
   emits: ['update:show', 'update:row', 'updateList'],
   setup(props, ctx) {
+    const crontabRef = ref()
     const parallelismRef = ref(false)
     const { t } = useI18n()
     const { timingState } = useForm()
@@ -173,8 +184,28 @@ export default defineComponent({
       getEnvironmentList()
     })
 
+    watch(
+      () => props.row,
+      () => {
+        timingState.timingForm.startEndTime = [
+          new Date(props.row.startTime),
+          new Date(props.row.endTime)
+        ]
+        timingState.timingForm.crontab = props.row.crontab
+        timingState.timingForm.timezoneId = props.row.timezoneId
+        timingState.timingForm.failureStrategy = props.row.failureStrategy
+        timingState.timingForm.warningType = props.row.warningType
+        timingState.timingForm.processInstancePriority =
+          props.row.processInstancePriority
+        timingState.timingForm.warningGroupId = props.row.warningGroupId
+        timingState.timingForm.workerGroup = props.row.workerGroup
+        timingState.timingForm.environmentCode = props.row.environmentCode
+      }
+    )
+
     return {
       t,
+      crontabRef,
       parallelismRef,
       hideModal,
       handleTiming,
@@ -216,10 +247,25 @@ export default defineComponent({
           </NFormItem>
           <NFormItem label={t('project.workflow.timing')} path='crontab'>
             <NInputGroup>
-              <NInput
-                style={{ width: '80%' }}
-                v-model:value={this.timingForm.crontab}
-              ></NInput>
+              <NPopover
+                trigger='click'
+                showArrow={false}
+                placement='bottom'
+                style={{ width: '500px' }}
+              >
+                {{
+                  trigger: () => (
+                    <NInput
+                      style={{ width: '80%' }}
+                      readonly={true}
+                      v-model:value={this.timingForm.crontab}
+                    ></NInput>
+                  ),
+                  default: () => (
+                    <Crontab v-model:value={this.timingForm.crontab} />
+                  )
+                }}
+              </NPopover>
               <NButton type='primary' ghost onClick={this.handlePreview}>
                 {t('project.workflow.execute_time')}
               </NButton>
