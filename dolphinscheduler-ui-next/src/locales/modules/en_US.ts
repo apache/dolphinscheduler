dd3e2905dce907eb27558b97c1f06aa47d9ca9d5
calvin <jianghuachinacom@163.com>
2022-02-21 18:33:57 +0800

    [Feature][UI Next] Add the form of sql task to the module of next ui. (#8471)
    * add the form of sql task
    
    * add the form of sql task
    
    * add the form of sql task
    
    * add the component of input group
    
    * add the component of input group
    
    * The form of sql task is done

--- dolphinscheduler-ui-next/src/locales/modules/en_US.ts
+++ dolphinscheduler-ui-next/src/locales/modules/en_US.ts
@@ -672,7 +672,17 @@ const project = {
     body_not_contains: 'Content does not contain',
     http_parameters_position: 'Http Parameters Position',
     target_task_name: 'Target Task Name',
-    target_task_name_tips: 'Please enter the Pigeon task name'
+    target_task_name_tips: 'Please enter the Pigeon task name',
+    datasource_type: 'Datasource types',
+    datasource: 'Datasource instances',
+    sql_type: 'SQL Type',
+    sql_type_query: 'Query',
+    sql_type_non_query: 'Non Query',
+    sql_statement: 'SQL Statement',
+    pre_sql_statement: 'Pre SQL Statement',
+    post_sql_statement: 'Post SQL Statement',
+    sql_input_placeholder: 'Please enter non-query sql.',
+    sql_empty_tips: 'The sql can not be empty.'
   }
 }
 
