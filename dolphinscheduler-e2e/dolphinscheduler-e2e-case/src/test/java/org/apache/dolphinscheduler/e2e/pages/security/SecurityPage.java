10d257d3b2b500b2eef7f8b763ff51e5ee7bc5b6
SbloodyS <460888207@qq.com>
2022-03-10 11:52:45 +0800

    test

--- dolphinscheduler-e2e/dolphinscheduler-e2e-case/src/test/java/org/apache/dolphinscheduler/e2e/pages/security/SecurityPage.java
+++ dolphinscheduler-e2e/dolphinscheduler-e2e-case/src/test/java/org/apache/dolphinscheduler/e2e/pages/security/SecurityPage.java
@@ -49,7 +49,7 @@ public class SecurityPage extends NavBarPage implements NavBarItem {
 //            "][2]//div[contains(@class, 'n-menu-item-content')]")
     @FindBys({
             @FindBy(className = "tab-vertical"),
-            @FindBy(xpath = "//div[contains(@class, 'n-menu-item')][2]//div[contains(@class, 'n-menu-item-content')"),
+            @FindBy(xpath = "//div[contains(@class, 'n-menu-item')][2]//div[contains(@class, 'n-menu-item-content')]"),
 //            @FindBy(className = "n-menu-item-content"),
     })
     private WebElement menUserManage;
