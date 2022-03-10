3848dd287885f586eae2c2a65de7851b16716b69
SbloodyS <460888207@qq.com>
2022-03-10 11:33:51 +0800

    test

--- dolphinscheduler-e2e/dolphinscheduler-e2e-case/src/test/java/org/apache/dolphinscheduler/e2e/pages/security/SecurityPage.java
+++ dolphinscheduler-e2e/dolphinscheduler-e2e-case/src/test/java/org/apache/dolphinscheduler/e2e/pages/security/SecurityPage.java
@@ -35,18 +35,22 @@ import lombok.Getter;
 
 @Getter
 public class SecurityPage extends NavBarPage implements NavBarItem {
-    @FindBys({
-        @FindBy(className = "tab-vertical"),
-        @FindBy(xpath = "//div[contains(@class, 'n-menu-item')][1]"),
-        @FindBy(className = "n-menu-item-content"),
-    })
+//    @FindBys({
+//        @FindBy(className = "tab-vertical"),
+//        @FindBy(xpath = "//div[contains(@class, 'n-menu-item')][1]"),
+//        @FindBy(className = "n-menu-item-content"),
+//    })
+    @FindBy(xpath = "//div[contains(@class, 'tab-vertical')]//div[contains(@class, 'n-menu-item')" +
+        "][1]//div[contains(@class, 'n-menu-item-content')]")
     private WebElement menuTenantManage;
 
-    @FindBys({
-        @FindBy(className = "tab-vertical"),
-        @FindBy(xpath = "//div[contains(@class, 'n-menu-item')][2]"),
-        @FindBy(className = "n-menu-item-content"),
-    })
+//    @FindBys({
+//        @FindBy(className = "tab-vertical"),
+//        @FindBy(xpath = "//div[contains(@class, 'n-menu-item')][2]"),
+//        @FindBy(className = "n-menu-item-content"),
+//    })
+    @FindBy(xpath = "//div[contains(@class, 'tab-vertical')]//div[contains(@class, 'n-menu-item')" +
+            "][2]//div[contains(@class, 'n-menu-item-content')]")
     private WebElement menUserManage;
 
     @FindBys({
