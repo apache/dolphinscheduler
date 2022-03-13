cf083bb087c5260078f9835e3b5f70e4d47ce74f
xiangzihao <460888207@qq.com>
2022-03-13 12:33:19 +0800

    [Feature-8844][E2E] Restore datasource center e2e test cases in ui-next (#8845)

--- dolphinscheduler-e2e/dolphinscheduler-e2e-case/src/test/java/org/apache/dolphinscheduler/e2e/cases/HiveDataSourceE2ETest.java
+++ dolphinscheduler-e2e/dolphinscheduler-e2e-case/src/test/java/org/apache/dolphinscheduler/e2e/cases/HiveDataSourceE2ETest.java
@@ -79,7 +79,8 @@ public class HiveDataSourceE2ETest {
 
         page.createDataSource(dataSourceType, dataSourceName, dataSourceDescription, ip, port, userName, hivePassword, database, jdbcParams);
 
-        new WebDriverWait(page.driver(), 10).until(ExpectedConditions.invisibilityOfElementLocated(new By.ById("dialogCreateDataSource")));
+        new WebDriverWait(page.driver(), 10).until(ExpectedConditions.invisibilityOfElementLocated(
+                new By.ByClassName("dialog-create-data-source")));
 
         await().untilAsserted(() -> assertThat(page.dataSourceItemsList())
             .as("DataSource list should contain newly-created database")
