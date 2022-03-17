78ff1fd1202d23dfe2387cac8a862734760d4a32
Kevin.Shin <mail4kevin@163.com>
2022-03-17 11:10:39 +0800

    fix https://github.com/apache/dolphinscheduler/issues/8889 (#8921)

--- dolphinscheduler-datasource-plugin/dolphinscheduler-datasource-oracle/src/main/java/org/apache/dolphinscheduler/plugin/datasource/oracle/OracleDataSourceClient.java
+++ dolphinscheduler-datasource-plugin/dolphinscheduler-datasource-oracle/src/main/java/org/apache/dolphinscheduler/plugin/datasource/oracle/OracleDataSourceClient.java
@@ -20,11 +20,16 @@ package org.apache.dolphinscheduler.plugin.datasource.oracle;
 import org.apache.dolphinscheduler.plugin.datasource.api.client.CommonDataSourceClient;
 import org.apache.dolphinscheduler.spi.datasource.BaseConnectionParam;
 import org.apache.dolphinscheduler.spi.enums.DbType;
+import org.apache.dolphinscheduler.spi.utils.Constants;
 
 public class OracleDataSourceClient extends CommonDataSourceClient {
 
     public OracleDataSourceClient(BaseConnectionParam baseConnectionParam, DbType dbType) {
         super(baseConnectionParam, dbType);
+    }
+
+    protected void setDefaultValidationQuery(BaseConnectionParam baseConnectionParam) {
+        baseConnectionParam.setValidationQuery(Constants.ORACLE_VALIDATION_QUERY);
     }
 
 }
