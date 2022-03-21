bee375432805d1af1c18ae64b1e63bb91f272695
songjianet <1778651752@qq.com>
2022-03-21 18:27:18 +0800

    [Fix][UI Next][V1.0.0-Alpha] Fix the monitoring center does not support multiple worker.

--- dolphinscheduler-ui-next/src/locales/modules/zh_CN.ts
+++ dolphinscheduler-ui-next/src/locales/modules/zh_CN.ts
@@ -1107,12 +1107,12 @@ const security = {
     chatId: '频道ID',
     parseMode: '解析类型',
     IntegrationKey: '集成密钥',
-    BotAccessToken:'访问令牌',
-    RoomId:'房间',
-    ToPersonId:'用户',
-    ToPersonEmail:'用户邮箱',
-    AtSomeoneInRoom:'@房间中的成员',
-    Destination:'描述'
+    BotAccessToken: '访问令牌',
+    RoomId: '房间',
+    ToPersonId: '用户',
+    ToPersonEmail: '用户邮箱',
+    AtSomeoneInRoom: '@房间中的成员',
+    Destination: '描述'
   },
   k8s_namespace: {
     create_namespace: '创建命名空间',
--- dolphinscheduler-ui-next/src/views/resource/task-group/option/components/form-modal.tsx
+++ dolphinscheduler-ui-next/src/views/resource/task-group/option/components/form-modal.tsx
@@ -115,21 +115,20 @@ const FormModal = defineComponent({
               placeholder={t('resource.task_group_option.please_enter_name')}
             />
           </NFormItem>
-          { this.status === 0 && (
-              <NFormItem
+          {this.status === 0 && (
+            <NFormItem
               label={t('resource.task_group_option.project_name')}
               path='projectCode'
-              >
+            >
               <NSelect
-              options={projectOptions}
-              v-model:value={this.formData.projectCode}
-              placeholder={t(
-              'resource.task_group_option.please_select_project'
-              )}
+                options={projectOptions}
+                v-model:value={this.formData.projectCode}
+                placeholder={t(
+                  'resource.task_group_option.please_select_project'
+                )}
               />
-              </NFormItem>
-            )
-          }
+            </NFormItem>
+          )}
           <NFormItem
             label={t('resource.task_group_option.resource_pool_size')}
             path='groupSize'
