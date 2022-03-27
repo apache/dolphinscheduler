0841ccacc08bddc0e944a52fee1f49bf70c4bd40
Amy0104 <97265214+Amy0104@users.noreply.github.com>
2022-03-27 11:33:28 +0800

    [Fix][UI Next][V1.0.0-Alpha] Fix the default items display error in SQOOP. (#9211)

--- dolphinscheduler-ui-next/src/components/form/fields/radio.ts
+++ dolphinscheduler-ui-next/src/components/form/fields/radio.ts
@@ -32,8 +32,9 @@ export function renderRadio(item: IJsonItem, fields: { [field: string]: any }) {
   return h(
     NRadioGroup,
     {
+      ...props,
       value: fields[field],
-      onUpdateValue: (value) => void (fields[field] = value)
+      onUpdateValue: (value: any) => void (fields[field] = value)
     },
     () =>
       h(NSpace, null, () =>
