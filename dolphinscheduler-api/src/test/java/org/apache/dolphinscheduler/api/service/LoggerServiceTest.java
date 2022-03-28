32149904398af0a58e96b3c9ee4270027213f3e1
caishunfeng <caishunfeng2021@gmail.com>
2022-03-28 11:36:56 +0800

    [Bug-9045][Server] improve log detail error if task instance host is null (#9205)
    * improve log detail error if task instance host is null
    
    * test
    
    Co-authored-by: caishunfeng <534328519@qq.com>

--- dolphinscheduler-api/src/test/java/org/apache/dolphinscheduler/api/service/LoggerServiceTest.java
+++ dolphinscheduler-api/src/test/java/org/apache/dolphinscheduler/api/service/LoggerServiceTest.java
@@ -92,7 +92,7 @@ public class LoggerServiceTest {
             Assert.assertTrue(true);
             logger.error("testQueryDataSourceList error {}", e.getMessage());
         }
-        Assert.assertEquals(Status.TASK_INSTANCE_NOT_FOUND.getCode(), result.getCode().intValue());
+        Assert.assertEquals(Status.TASK_INSTANCE_HOST_IS_NULL.getCode(), result.getCode().intValue());
 
         //SUCCESS
         taskInstance.setHost("127.0.0.1:8080");
