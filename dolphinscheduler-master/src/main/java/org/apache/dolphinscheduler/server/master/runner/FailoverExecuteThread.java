bdca52c902dda81f081afbe893770b211eff9916
wangrui <7039986@qq.com>
2022-03-30 14:52:26 +0800

    [DS-9263][Improvement][master]optimize failover
    - add FailoverService.java
    - move failover method  from MasterRegistryClient to FailoverService
    - move failover code from FailoverExecuteThread to FailoverService
    
    This closes #9263

--- dolphinscheduler-master/src/main/java/org/apache/dolphinscheduler/server/master/runner/FailoverExecuteThread.java
+++ dolphinscheduler-master/src/main/java/org/apache/dolphinscheduler/server/master/runner/FailoverExecuteThread.java
@@ -18,18 +18,10 @@
 package org.apache.dolphinscheduler.server.master.runner;
 
 import org.apache.dolphinscheduler.common.Constants;
-import org.apache.dolphinscheduler.common.enums.NodeType;
 import org.apache.dolphinscheduler.common.thread.Stopper;
 import org.apache.dolphinscheduler.common.thread.ThreadUtils;
 import org.apache.dolphinscheduler.server.master.config.MasterConfig;
-import org.apache.dolphinscheduler.server.master.registry.MasterRegistryClient;
-import org.apache.dolphinscheduler.service.process.ProcessService;
-import org.apache.dolphinscheduler.service.registry.RegistryClient;
-
-import org.apache.commons.collections4.CollectionUtils;
-
-import java.util.Iterator;
-import java.util.List;
+import org.apache.dolphinscheduler.server.master.service.FailoverService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -42,19 +34,13 @@ public class FailoverExecuteThread extends Thread {
     private static final Logger logger = LoggerFactory.getLogger(FailoverExecuteThread.class);
 
     @Autowired
-    private MasterRegistryClient masterRegistryClient;
-
-    @Autowired
-    private RegistryClient registryClient;
-
-    @Autowired
     private MasterConfig masterConfig;
 
     /**
-     * process service
+     * failover service
      */
     @Autowired
-    private ProcessService processService;
+    private FailoverService failoverService;
 
     @Override
     public synchronized void start() {
@@ -67,44 +53,12 @@ public class FailoverExecuteThread extends Thread {
         while (Stopper.isRunning()) {
             logger.info("failover execute started");
             try {
-                List<String> hosts = getNeedFailoverMasterServers();
-                if (CollectionUtils.isEmpty(hosts)) {
-                    continue;
-                }
-                logger.info("need failover hosts:{}", hosts);
-
-                for (String host : hosts) {
-                    String failoverPath = masterRegistryClient.getFailoverLockPath(NodeType.MASTER, host);
-                    try {
-                        registryClient.getLock(failoverPath);
-                        masterRegistryClient.failoverMaster(host);
-                    } catch (Exception e) {
-                        logger.error("{} server failover failed, host:{}", NodeType.MASTER, host, e);
-                    } finally {
-                        registryClient.releaseLock(failoverPath);
-                    }
-                }
+                failoverService.checkMasterFailover();
             } catch (Exception e) {
                 logger.error("failover execute error", e);
             } finally {
                 ThreadUtils.sleep((long) Constants.SLEEP_TIME_MILLIS * masterConfig.getFailoverInterval() * 60);
             }
         }
-    }
-
-    private List<String> getNeedFailoverMasterServers() {
-        // failover myself && failover dead masters
-        List<String> hosts = processService.queryNeedFailoverProcessInstanceHost();
-
-        Iterator<String> iterator = hosts.iterator();
-        while (iterator.hasNext()) {
-            String host = iterator.next();
-            if (registryClient.checkNodeExists(host, NodeType.MASTER)) {
-                if (!host.equals(masterRegistryClient.getLocalAddress())) {
-                    iterator.remove();
-                }
-            }
-        }
-        return hosts;
     }
 }
