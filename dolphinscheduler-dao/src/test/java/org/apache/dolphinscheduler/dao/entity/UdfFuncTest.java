af69338af0662f4baf0583670abec4021f190fc3
nobolity <nobolity@gmail.com>
2022-04-02 10:05:17 +0800

    fix(udfTest): upgrade the udfTest

--- dolphinscheduler-dao/src/test/java/org/apache/dolphinscheduler/dao/entity/UdfFuncTest.java
+++ dolphinscheduler-dao/src/test/java/org/apache/dolphinscheduler/dao/entity/UdfFuncTest.java
@@ -36,7 +36,7 @@ public class UdfFuncTest {
     udfFunc.setResourceId(2);
     udfFunc.setClassName("org.apache.dolphinscheduler.test.mrUpdate");
 
-    Assert.assertEquals("{\"id\":0,\"userId\":0,\"funcName\":null,\"resourceType\":\"UDF\",\"className\":\"org.apache.dolphinscheduler.test.mrUpdate\",\"argTypes\":null,\"database\":null,"
+    Assert.assertEquals("{\"id\":0,\"userId\":0,\"resourceType\":\"UDF\",\"funcName\":null,\"className\":\"org.apache.dolphinscheduler.test.mrUpdate\",\"argTypes\":null,\"database\":null,"
                     + "\"description\":null,\"resourceId\":2,\"resourceName\":\"dolphin_resource_update\",\"type\":null,\"createTime\":null,\"updateTime\":null}"
             , udfFunc.toString());
   }
