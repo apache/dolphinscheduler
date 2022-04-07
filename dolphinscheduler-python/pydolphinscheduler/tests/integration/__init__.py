3457cee96047ab7ab0371a61852cac3e5a98d991
Jiajie Zhong <zhongjiajie955@hotmail.com>
2022-04-07 14:41:15 +0800

    [python] Migrate pythonGatewayServer into api server (#9372)
    Currently the size of our distribute package is up to
    800MB, this patch is migrate python gateway server into
    api server
    
    The distribute package size before and after this patch is:
    
    ```sh
    # before
    796M   apache-dolphinscheduler-2.0.4-SNAPSHOT-bin.tar.gz
    
    # after
    647M   apache-dolphinscheduler-2.0.4-SNAPSHOT-bin.tar.gz
    ```

--- dolphinscheduler-python/pydolphinscheduler/tests/integration/__init__.py
+++ dolphinscheduler-python/pydolphinscheduler/tests/integration/__init__.py
@@ -15,4 +15,4 @@
 # specific language governing permissions and limitations
 # under the License.
 
-"""Test integration between Python API and PythonGatewayServer."""
+"""Test integration between Python API and PythonGatewayService."""
