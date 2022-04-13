24a0478041477f7d7340b5ccdc2b4a850a1553c4
Tq <tianqitobethefirst@gmail.com>
2022-04-13 23:35:05 +0800

    [FIX][WEBSITE-9224]fix wechat doc and wechat form display (#9439)
    Co-authored-by: Jiajie Zhong <zhongjiajie955@hotmail.com>

--- dolphinscheduler-alert/dolphinscheduler-alert-plugins/dolphinscheduler-alert-wechat/src/main/java/org/apache/dolphinscheduler/plugin/alert/wechat/WeChatType.java
+++ dolphinscheduler-alert/dolphinscheduler-alert-plugins/dolphinscheduler-alert-wechat/src/main/java/org/apache/dolphinscheduler/plugin/alert/wechat/WeChatType.java
@@ -19,8 +19,8 @@
 package org.apache.dolphinscheduler.plugin.alert.wechat;
 
 public enum WeChatType {
-    APP(1, "应用"),
-    APPCHAT(2, "群聊"),
+    APP(1, "APP/应用"),
+    APPCHAT(2, "GROUP CHAT/群聊"),
     ;
 
     private final int code;
