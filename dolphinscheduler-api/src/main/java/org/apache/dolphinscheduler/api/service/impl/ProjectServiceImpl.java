3808013af4d89d14afe379e9a0f689edac031f4e
WangJPLeo <wangjipeng@whaleops.com>
2022-04-20 17:51:12 +0800

    remove unused import

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/impl/ProjectServiceImpl.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/impl/ProjectServiceImpl.java
@@ -38,7 +38,6 @@ import org.apache.dolphinscheduler.dao.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.util.*;
 
 import static org.apache.dolphinscheduler.api.utils.CheckUtils.checkDesc;
