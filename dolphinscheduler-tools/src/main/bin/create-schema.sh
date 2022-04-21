f9ea82a9626c25b4fdc0f4b7b0d5d7038ecb1d92
Eric Gao <ericgao.apache@gmail.com>
2022-04-21 11:01:19 +0800

    [Bug][DAO] Update db init script and soft_version (#9628) (#9637)

--- dolphinscheduler-tools/src/main/bin/create-schema.sh
+++ dolphinscheduler-tools/src/main/bin/create-schema.sh
@@ -18,11 +18,10 @@
 
 BIN_DIR=$(dirname $0)
 DOLPHINSCHEDULER_HOME=${DOLPHINSCHEDULER_HOME:-$(cd $BIN_DIR/../..; pwd)}
-
-source "$DOLPHINSCHEDULER_HOME/bin/env/dolphinscheduler_env.sh"
+source "$DOLPHINSCHEDULER_HOME/tools/bin/dolphinscheduler_env.sh"
 
 JAVA_OPTS=${JAVA_OPTS:-"-server -Duser.timezone=${SPRING_JACKSON_TIME_ZONE} -Xms1g -Xmx1g -Xmn512m -XX:+PrintGCDetails -Xloggc:gc.log -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=dump.hprof"}
 
 java $JAVA_OPTS \
-  -cp "$DOLPHINSCHEDULER_HOME/conf":"$DOLPHINSCHEDULER_HOME/tools/libs/*":"$DOLPHINSCHEDULER_HOME/tools/sql" \
+  -cp "$DOLPHINSCHEDULER_HOME/tools/conf":"$DOLPHINSCHEDULER_HOME/tools/libs/*":"$DOLPHINSCHEDULER_HOME/tools/sql" \
   org.apache.dolphinscheduler.tools.datasource.CreateDolphinScheduler
