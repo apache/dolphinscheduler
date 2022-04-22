619cb2bcde3be1468bc50b5634bc4bdaf9fb48fe
yimaixinchen <yimaixinchen@163.com>
2022-04-22 19:49:22 +0800

    keep the longest flow
    Co-authored-by: Jiajie Zhong <zhongjiajie955@gmail.com>

--- dolphinscheduler-dao/src/test/java/org/apache/dolphinscheduler/dao/utils/DagHelperTest.java
+++ dolphinscheduler-dao/src/test/java/org/apache/dolphinscheduler/dao/utils/DagHelperTest.java
@@ -285,10 +285,11 @@ public class DagHelperTest {
      * 4->3->6
      * 1->2->8->5->7
      * DAG graph:
-     *    8->5->7
-     *    ↑  ↑
-     * 1->2->3->6
-     *    4-↗
+     *      4 ->   -> 6
+     *          \ /
+     * 1 -> 2 -> 3 -> 5 -> 7
+     *       \       /
+     *        -> 8 ->
      *
      * @return dag
      * @throws JsonProcessingException if error throws JsonProcessingException
