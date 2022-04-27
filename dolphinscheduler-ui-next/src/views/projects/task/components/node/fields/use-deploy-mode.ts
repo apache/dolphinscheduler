68cb674dfdd906e53baed0b56cb97b7f3d153897
sq-q <83269577+sq-q@users.noreply.github.com>
2022-04-27 17:44:05 +0800

    [Feature-9772][plugin/ui] support SparkSQL Task (#9790)
    * [refactor] Dolphinscheduler sparkSQL
    
    * [refactor] Dolphinscheduler SparkSQL
    
    * [refactor] Dolphinscheduler SparkSQL
    
    * [refactor] Dolphinscheduler SparkSQL
    
    * [refactor] Dolphinscheduler SparkSQL
    
    * [refactor] Dolphinscheduler SparkSQL
    
    * [refactor] Dolphinscheduler SparkSQL
    
    * [refactor] Refactor ui code and add sparksql test cases
    
    * [refactor] refactor dolphinscheduler SparkSQL
    
    * [refactor] refactor dolphinscheduler plugin-sparkSQL
    
    * [refactor] refactor dolphinscheduler plugin-SparkSQL
    
    * [refactor] dolphinscheduler plugin-SparkTaskTest
    
    * [refactor] dolphinscheduler plugin-SparkTask
    
    * [refactor] dolphinscheduler plugin-Spark
    
    * [refactor] dolphinscheduler plugin-SparkTask-SparkSQL
    
    * [refactor] dolphinscheduler plugin-spark-SparkTask
    
    * [refactor] dolphinscheduler plugin-spark-SparkTask redefine code

--- dolphinscheduler-ui-next/src/views/projects/task/components/node/fields/use-deploy-mode.ts
+++ dolphinscheduler-ui-next/src/views/projects/task/components/node/fields/use-deploy-mode.ts
@@ -14,34 +14,49 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { useI18n } from 'vue-i18n'
-import type { IJsonItem } from '../types'
+import {ref, watchEffect} from 'vue'
+import {useI18n} from 'vue-i18n'
+import type {IJsonItem, IOption} from '../types'
 
-export function useDeployMode(span = 24, showClient = true): IJsonItem {
-  const { t } = useI18n()
+export function useDeployMode(span = 24, showClient = ref(true), showCluster = ref(true)): IJsonItem {
+    const {t} = useI18n()
 
-  return {
-    type: 'radio',
-    field: 'deployMode',
-    name: t('project.node.deploy_mode'),
-    options: DEPLOY_MODES.filter((option) =>
-      option.value === 'client' ? showClient : true
-    ),
-    span
-  }
+    const deployModeOptions = ref(DEPLOY_MODES as IOption[])
+
+    watchEffect(
+        () => {
+            deployModeOptions.value = DEPLOY_MODES.filter((option) => {
+                switch (option.value) {
+                    case 'cluster':
+                        return showCluster.value
+                    case 'client':
+                        return showClient.value
+                    default:
+                        return true
+                }
+            })
+        }
+    )
+    return {
+        type: 'radio',
+        field: 'deployMode',
+        name: t('project.node.deploy_mode'),
+        options: deployModeOptions,
+        span
+    }
 }
 
 export const DEPLOY_MODES = [
-  {
-    label: 'cluster',
-    value: 'cluster'
-  },
-  {
-    label: 'client',
-    value: 'client'
-  },
-  {
-    label: 'local',
-    value: 'local'
-  }
+    {
+        label: 'cluster',
+        value: 'cluster'
+    },
+    {
+        label: 'client',
+        value: 'client'
+    },
+    {
+        label: 'local',
+        value: 'local'
+    }
 ]
