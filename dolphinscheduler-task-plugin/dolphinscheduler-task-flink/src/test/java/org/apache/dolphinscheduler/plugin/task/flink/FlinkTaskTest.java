ae662f620bcaa7c88d1ccd6b57b27a51554d31ee
cxy1122 <cxy1122bl@163.com>
2022-04-29 09:20:19 +0800

    [refactor] flink sql

--- dolphinscheduler-task-plugin/dolphinscheduler-task-flink/src/test/java/org/apache/dolphinscheduler/plugin/task/flink/FlinkTaskTest.java
+++ dolphinscheduler-task-plugin/dolphinscheduler-task-flink/src/test/java/org/apache/dolphinscheduler/plugin/task/flink/FlinkTaskTest.java
@@ -1,9 +1,28 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.dolphinscheduler.plugin.task.flink;
 
 import org.apache.commons.lang.StringUtils;
+
 import org.apache.dolphinscheduler.plugin.task.api.TaskExecutionContext;
 import org.apache.dolphinscheduler.plugin.task.api.model.ResourceInfo;
 import org.apache.dolphinscheduler.spi.utils.JSONUtils;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -33,16 +52,16 @@ public class FlinkTaskTest {
         FlinkTask flinkTask = spy(new FlinkTask(taskExecutionContext));
         flinkTask.init();
         Assert.assertEquals(flinkTask.buildCommand(),
-           "flink run " +
-                  "-m yarn-cluster " +
-                  "-ys 1 " +
-                  "-ynm TopSpeedWindowing " +
-                  "-yjm 1G " +
-                  "-ytm 1G " +
-                  "-yqu default " +
-                  "-p 2 -sae " +
-                  "-c org.apache.flink.streaming.examples.windowing.TopSpeedWindowing " +
-                  "TopSpeedWindowing.jar");
+            "flink run " +
+                "-m yarn-cluster " +
+                "-ys 1 " +
+                "-ynm TopSpeedWindowing " +
+                "-yjm 1G " +
+                "-ytm 1G " +
+                "-yqu default " +
+                "-p 2 -sae " +
+                "-c org.apache.flink.streaming.examples.windowing.TopSpeedWindowing " +
+                "TopSpeedWindowing.jar");
     }
 
     @Test
