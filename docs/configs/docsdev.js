c6fca94cd40d84f4b50b2d3115c2007b868a192f
陈爽 <chens616@qq.com>
2022-05-06 08:55:00 +0800

    [bug] [doc] fix  the data quality link error about the navigation of dolphinscheduler 3.0.0-alpha zh docs  (#9887)
    (cherry picked from commit 15a15de927a11c6cc40d42b5a865b7a02ef9ce76)

--- docs/configs/docsdev.js
+++ docs/configs/docsdev.js
@@ -579,7 +579,7 @@ export default {
                     },
                     {
                         title: '数据质量',
-                        link: '/zh-cn/docs/3.0.0/user_doc/guide /data-quality.html',
+                        link: '/zh-cn/docs/3.0.0/user_doc/guide/data-quality.html',
                     },
                     {
                         title: '资源中心',
