6dc1282f973f271af9741fafa77143002d690127
SbloodyS <460888207@qq.com>
2022-05-12 10:05:59 +0800

    add github actions

--- .github/workflows/cluster-test/deploy.sh
+++ .github/workflows/cluster-test/deploy.sh
@@ -36,8 +36,8 @@ cp $DOLPHINSCHEDULER_HOME/alert-server/libs/$MYSQL_DRIVER $DOLPHINSCHEDULER_HOME
 
 #Create database
 sudo apt install -y mycli
-mysql -uroot -p123456 -e "CREATE DATABASE dolphinscheduler DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;"
-mysql -uroot -p123456 -e "source ${DOLPHINSCHEDULER_HOME}/tools/sql/sql/dolphinscheduler_mysql.sql"
+mysql -h0.0.0.0 -P3306 -uroot -p123456 -e "CREATE DATABASE dolphinscheduler DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;"
+mysql -h0.0.0.0 -P3306 -uroot -p123456 -e "source ${DOLPHINSCHEDULER_HOME}/tools/sql/sql/dolphinscheduler_mysql.sql"
 
 #Sudo
 sudo sed -i '$a'$USER'  ALL=(ALL)  NOPASSWD: NOPASSWD: ALL' /etc/sudoers
