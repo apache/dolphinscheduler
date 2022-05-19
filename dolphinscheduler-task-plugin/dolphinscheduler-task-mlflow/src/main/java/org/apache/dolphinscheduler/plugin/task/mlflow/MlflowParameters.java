1fa67f1b136da252cfcdfc4bc24d2858b0c877fc
JieguangZhou <jieguang_zhou@163.com>
2022-05-19 09:40:31 +0800

    [Feature][Task Plugin] Add mlflow task plugin for MLops scenario. (#10036)
    * [Feature][Task Plugin] Add mlflow task plugin (#9725)
    
    * [Feature][Task Plugin] Add mlflow task plugin UI (#9725)
    
    * [Feature][Task Plugin] fix license header (#9725)
    
    * [Feature][Task Plugin] fix license header (#9725)
    
    * [Feature][Task Plugin] revert unnecessary
    
    * [Feature][Task Plugin] add auto ml to mlflow task plugin
    
    * [DOC] add mlflow document
    
    * [DOC] fix mlflow docs imgs
    
    * [DOC] fix dead link localhost:5000
    
    * [DOC] fix dead link localhost:5000
    
    * [DOC] remove dead link localhost:5000
    
    * Update docs/docs/en/guide/task/mlflow.md
    
    * Update docs/docs/zh/guide/task/mlflow.md
    
    * [DOC] format ui code
    
    * [DOC] remove dead link localhost:5000
    
    * [Feature][Task Plugin] revert unnecessary
    
    * fix some nits
    
    * Move the personal repository to the public repository.
    
    * Run the command directly instead of saving the file
    
    * fix paramsMap initialize
    
    * revert unnecessary
    
    Co-authored-by: Jiajie Zhong <zhongjiajie955@gmail.com>

---
+++ dolphinscheduler-task-plugin/dolphinscheduler-task-mlflow/src/main/java/org/apache/dolphinscheduler/plugin/task/mlflow/MlflowParameters.java
@@ -0,0 +1,191 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.dolphinscheduler.plugin.task.mlflow;
+
+import org.apache.dolphinscheduler.plugin.task.api.parameters.AbstractParameters;
+
+import java.util.HashMap;
+
+public class MlflowParameters extends AbstractParameters {
+
+    /**
+     * common parameters
+     */
+
+    private String params = "";
+
+    private String mlflowJobType = "BasicAlgorithm";
+
+    /**
+     * AutoML parameters
+     */
+    private String automlTool = "FLAML";
+
+
+    /**
+     * basic algorithm parameters
+     */
+
+    private String algorithm = "lightgbm";
+
+    private String searchParams = "";
+
+    private String dataPath;
+
+    /**
+     * mlflow parameters
+     */
+
+    private String experimentName;
+
+    private String modelName = "";
+
+    private String mlflowTrackingUri = "http://127.0.0.1:5000";
+
+
+    public void setAlgorithm(String algorithm) {
+        this.algorithm = algorithm;
+    }
+
+    public String getAlgorithm() {
+        return algorithm;
+    }
+
+    public void setParams(String params) {
+        this.params = params;
+    }
+
+    public String getParams() {
+        return params;
+    }
+
+    public void setSearchParams(String searchParams) {
+        this.searchParams = searchParams;
+    }
+
+    public String getSearchParams() {
+        return searchParams;
+    }
+
+    public void setDataPaths(String dataPath) {
+        this.dataPath = dataPath;
+    }
+
+    public String getDataPath() {
+        return dataPath;
+    }
+
+
+    public void setExperimentNames(String experimentName) {
+        this.experimentName = experimentName;
+    }
+
+    public String getExperimentName() {
+        return experimentName;
+    }
+
+    public void setModelNames(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setMlflowTrackingUris(String mlflowTrackingUri) {
+        this.mlflowTrackingUri = mlflowTrackingUri;
+    }
+
+    public String getMlflowTrackingUri() {
+        return mlflowTrackingUri;
+    }
+
+    public void setMlflowJobType(String mlflowJobType) {
+        this.mlflowJobType = mlflowJobType;
+    }
+
+    public String getMlflowJobType() {
+        return mlflowJobType;
+    }
+
+    public void setAutomlTool(String automlTool) {
+        this.automlTool = automlTool;
+    }
+
+    public String getAutomlTool() {
+        return automlTool;
+    }
+
+    @Override
+    public boolean checkParameters() {
+
+        Boolean checkResult = experimentName != null && mlflowTrackingUri != null;
+        if (mlflowJobType.equals(MlflowConstants.JOB_TYPE_BASIC_ALGORITHM)) {
+            checkResult &= dataPath != null;
+        } else if (mlflowJobType.equals(MlflowConstants.JOB_TYPE_AUTOML)) {
+            checkResult &= dataPath != null;
+            checkResult &= automlTool != null;
+        } else {
+        }
+        return checkResult;
+    }
+
+    public HashMap<String, String> getParamsMap() {
+
+        HashMap<String, String> paramsMap = new HashMap<String, String>();
+        paramsMap.put("params", params);
+        paramsMap.put("data_path", dataPath);
+        paramsMap.put("experiment_name", experimentName);
+        paramsMap.put("model_name", modelName);
+        paramsMap.put("MLFLOW_TRACKING_URI", mlflowTrackingUri);
+        if (mlflowJobType.equals(MlflowConstants.JOB_TYPE_BASIC_ALGORITHM)) {
+            addParamsMapForBasicAlgorithm(paramsMap);
+        } else if (mlflowJobType.equals(MlflowConstants.JOB_TYPE_AUTOML)) {
+            getParamsMapForAutoML(paramsMap);
+        } else {
+        }
+        return paramsMap;
+    }
+
+    private void addParamsMapForBasicAlgorithm(HashMap<String, String> paramsMap) {
+        paramsMap.put("algorithm", algorithm);
+        paramsMap.put("search_params", searchParams);
+        paramsMap.put("repo", MlflowConstants.PRESET_BASIC_ALGORITHM_PROJECT);
+        paramsMap.put("repo_version", MlflowConstants.PRESET_REPOSITORY_VERSION);
+    }
+
+    private void getParamsMapForAutoML(HashMap<String, String> paramsMap) {
+        paramsMap.put("automl_tool", automlTool);
+        paramsMap.put("repo", MlflowConstants.PRESET_AUTOML_PROJECT);
+        paramsMap.put("repo_version", MlflowConstants.PRESET_REPOSITORY_VERSION);
+    }
+
+    public String getScriptPath() {
+        String projectScript;
+        if (mlflowJobType.equals(MlflowConstants.JOB_TYPE_BASIC_ALGORITHM)) {
+            projectScript = MlflowConstants.RUN_PROJECT_BASIC_ALGORITHM_SCRIPT;
+        } else if (mlflowJobType.equals(MlflowConstants.JOB_TYPE_AUTOML)) {
+            projectScript = MlflowConstants.RUN_PROJECT_AUTOML_SCRIPT;
+        } else {
+            throw new IllegalArgumentException();
+        }
+        String scriptPath = MlflowTask.class.getClassLoader().getResource(projectScript).getPath();
+        return scriptPath;
+    }
+
+};
---
+++ dolphinscheduler-task-plugin/dolphinscheduler-task-mlflow/src/test/java/org/apache/dolphinler/plugin/task/mlflow/MlflowTaskTest.java
@@ -0,0 +1,129 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.dolphinler.plugin.task.mlflow;
+
+import java.util.Date;
+import java.util.UUID;
+
+import org.apache.dolphinscheduler.plugin.task.api.TaskExecutionContext;
+import org.apache.dolphinscheduler.plugin.task.api.TaskExecutionContextCacheManager;
+import org.apache.dolphinscheduler.plugin.task.mlflow.MlflowParameters;
+import org.apache.dolphinscheduler.plugin.task.mlflow.MlflowTask;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.apache.dolphinscheduler.spi.utils.PropertyUtils;
+
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.core.classloader.annotations.SuppressStaticInitializationFor;
+import org.apache.dolphinscheduler.spi.utils.JSONUtils;
+
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({
+        JSONUtils.class,
+        PropertyUtils.class,
+})
+@PowerMockIgnore({"javax.*"})
+@SuppressStaticInitializationFor("org.apache.dolphinscheduler.spi.utils.PropertyUtils")
+public class MlflowTaskTest {
+    private static final Logger logger = LoggerFactory.getLogger(MlflowTask.class);
+
+    @Before
+    public void before() throws Exception {
+        PowerMockito.mockStatic(PropertyUtils.class);
+    }
+
+    public TaskExecutionContext createContext(MlflowParameters mlflowParameters){
+        String parameters = JSONUtils.toJsonString(mlflowParameters);
+        TaskExecutionContext taskExecutionContext = Mockito.mock(TaskExecutionContext.class);
+        Mockito.when(taskExecutionContext.getTaskParams()).thenReturn(parameters);
+        Mockito.when(taskExecutionContext.getTaskLogName()).thenReturn("MLflowTest");
+        Mockito.when(taskExecutionContext.getExecutePath()).thenReturn("/tmp/dolphinscheduler_test");
+        Mockito.when(taskExecutionContext.getTaskAppId()).thenReturn(UUID.randomUUID().toString());
+        Mockito.when(taskExecutionContext.getTenantCode()).thenReturn("root");
+        Mockito.when(taskExecutionContext.getStartTime()).thenReturn(new Date());
+        Mockito.when(taskExecutionContext.getTaskTimeout()).thenReturn(10000);
+        Mockito.when(taskExecutionContext.getLogPath()).thenReturn("/tmp/dolphinscheduler_test/log");
+        Mockito.when(taskExecutionContext.getEnvironmentConfig()).thenReturn("export PATH=$HOME/anaconda3/bin:$PATH");
+
+        String userName = System.getenv().get("USER");
+        Mockito.when(taskExecutionContext.getTenantCode()).thenReturn(userName);
+
+        TaskExecutionContextCacheManager.cacheTaskExecutionContext(taskExecutionContext);
+        return taskExecutionContext;
+    }
+
+    @Test
+    public void testInitBasicAlgorithmTask()
+            throws Exception {
+        try {
+            MlflowParameters mlflowParameters = createBasicAlgorithmParameters();
+            TaskExecutionContext taskExecutionContext = createContext(mlflowParameters);
+            MlflowTask mlflowTask = new MlflowTask(taskExecutionContext);
+            mlflowTask.init();
+            mlflowTask.getParameters().setVarPool(taskExecutionContext.getVarPool());
+        } catch (Exception e) {
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testInitAutoMLTask()
+            throws Exception {
+        try {
+            MlflowParameters mlflowParameters = createAutoMLParameters();
+            TaskExecutionContext taskExecutionContext = createContext(mlflowParameters);
+            MlflowTask mlflowTask = new MlflowTask(taskExecutionContext);
+            mlflowTask.init();
+            mlflowTask.getParameters().setVarPool(taskExecutionContext.getVarPool());
+        } catch (Exception e) {
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    private MlflowParameters createBasicAlgorithmParameters() {
+        MlflowParameters mlflowParameters = new MlflowParameters();
+        mlflowParameters.setMlflowJobType("BasicAlgorithm");
+        mlflowParameters.setAlgorithm("xgboost");
+        mlflowParameters.setDataPaths("xxxxxxxxxx");
+        mlflowParameters.setExperimentNames("asbbb");
+        mlflowParameters.setMlflowTrackingUris("http://127.0.0.1:5000");
+        return mlflowParameters;
+    }
+
+    private MlflowParameters createAutoMLParameters() {
+        MlflowParameters mlflowParameters = new MlflowParameters();
+        mlflowParameters.setMlflowJobType("AutoML");
+        mlflowParameters.setAutomlTool("autosklearn");
+        mlflowParameters.setParams("time_left_for_this_task=30");
+        mlflowParameters.setDataPaths("xxxxxxxxxxx");
+        mlflowParameters.setExperimentNames("asbbb");
+        mlflowParameters.setModelNames("asbbb");
+        mlflowParameters.setMlflowTrackingUris("http://127.0.0.1:5000");
+        return mlflowParameters;
+    }
+
+}
