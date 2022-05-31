c5961aed25329b1cbd0aab34fc49cc7cffb39753
HuangWei <hw14@qq.com>
2022-05-31 11:00:21 +0800

    [feat][task] Add OpenMLDB task plugin (#10198)

--- dolphinscheduler-ui/src/views/projects/task/components/node/types.ts
+++ dolphinscheduler-ui/src/views/projects/task/components/node/types.ts
@@ -336,6 +336,9 @@ interface ITaskParams {
   deployType?: string
   deployPort?: string
   deployModelKey?: string
+  zk?: string
+  zkPath?: string
+  executeMode?: string
 }
 
 interface INodeData
