165279b9d6db296fb93e8e845c04ec7b600fd261
ruanwenjun <wenjun@apache.org>
2022-06-04 14:28:08 +0800

    Add license header

--- dolphinscheduler-scheduler/dolphinscheduler-scheduler-api/src/main/java/org/apache/dolphinscheduler/scheduler/api/SchedulerException.java
+++ dolphinscheduler-scheduler/dolphinscheduler-scheduler-api/src/main/java/org/apache/dolphinscheduler/scheduler/api/SchedulerException.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.dolphinscheduler.scheduler.api;
 
 public class SchedulerException extends RuntimeException {
@@ -10,11 +27,4 @@ public class SchedulerException extends RuntimeException {
         super(message, cause);
     }
 
-    public static SchedulerException of(String message) {
-        return new SchedulerException(message);
-    }
-
-    public static SchedulerException of(String message, Throwable cause) {
-        return new SchedulerException(message, cause);
-    }
 }
