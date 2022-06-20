903881931843ca8a39b4002d9692b6aa94941853
qingwli <qingwli@cisco.com>
2022-06-20 13:09:35 +0800

    run ci

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/security/impl/AbstractAuthenticator.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/security/impl/AbstractAuthenticator.java
@@ -106,5 +106,4 @@ public abstract class AbstractAuthenticator implements Authenticator {
         //get user object from session
         return userService.queryUser(session.getUserId());
     }
-
 }
