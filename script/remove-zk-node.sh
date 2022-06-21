ce3def8f190858a82aaeb99fb3bcce8dc22e7f72
Desperado2 <Jack__Mu@163.com>
2022-06-21 16:22:20 +0800

    Update remove-zk-node.sh

--- script/remove-zk-node.sh
+++ script/remove-zk-node.sh
@@ -42,6 +42,7 @@ export DOLPHINSCHEDULER_LIB_JARS=$DOLPHINSCHEDULER_HOME/libs/*
 export DOLPHINSCHEDULER_OPTS="-Xmx1g -Xms1g -Xss512k -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 "
 export STOP_TIMEOUT=5
 
+registryServers=$REGISTRY_ZOOKEEPER_CONNECT_STRING
 CLASS=org.apache.zookeeper.ZooKeeperMain
 
 exec_command="$DOLPHINSCHEDULER_OPTS -classpath $DOLPHINSCHEDULER_CONF_DIR:$DOLPHINSCHEDULER_LIB_JARS $CLASS -server $registryServers rmr $rootNode"
