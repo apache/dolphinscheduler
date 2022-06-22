7047dc15daff740f7face563b6420ead6487ddf4
WangJPLeo <103574007+WangJPLeo@users.noreply.github.com>
2022-06-22 14:28:50 +0800

    [Feature] Serial wait for policy recovery (#10453)

--- dolphinscheduler-master/src/main/java/org/apache/dolphinscheduler/server/master/runner/WorkflowExecuteRunnable.java
+++ dolphinscheduler-master/src/main/java/org/apache/dolphinscheduler/server/master/runner/WorkflowExecuteRunnable.java
@@ -689,14 +689,13 @@ public class WorkflowExecuteRunnable implements Runnable {
 
             if (stateEvent.getExecutionStatus() == ExecutionStatus.STOP) {
                 // serial wait execution type needs to wake up the waiting process
-                if (processDefinition.getExecutionType().typeIsSerialWait()){
+                if (processDefinition.getExecutionType().typeIsSerialWait() || processDefinition.getExecutionType().typeIsSerialPriority()){
                     endProcess();
                     return true;
                 }
                 this.updateProcessInstanceState(stateEvent);
                 return true;
             }
-
             if (processComplementData()) {
                 return true;
             }
@@ -841,7 +840,7 @@ public class WorkflowExecuteRunnable implements Runnable {
      */
     public void endProcess() {
         this.stateEvents.clear();
-        if (processDefinition.getExecutionType().typeIsSerialWait()) {
+        if (processDefinition.getExecutionType().typeIsSerialWait() || processDefinition.getExecutionType().typeIsSerialPriority()) {
             checkSerialProcess(processDefinition);
         }
         if (processInstance.getState().typeIsWaitingThread()) {
@@ -862,6 +861,10 @@ public class WorkflowExecuteRunnable implements Runnable {
         if (nextInstanceId == 0) {
             ProcessInstance nextProcessInstance = this.processService.loadNextProcess4Serial(processInstance.getProcessDefinition().getCode(), ExecutionStatus.SERIAL_WAIT.getCode(), processInstance.getId());
             if (nextProcessInstance == null) {
+                return;
+            }
+            ProcessInstance nextReadyStopProcessInstance = this.processService.loadNextProcess4Serial(processInstance.getProcessDefinition().getCode(), ExecutionStatus.READY_STOP.getCode(), processInstance.getId());
+            if (processDefinition.getExecutionType().typeIsSerialPriority() && nextReadyStopProcessInstance != null) {
                 return;
             }
             nextInstanceId = nextProcessInstance.getId();
