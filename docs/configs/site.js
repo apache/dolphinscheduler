016692413cff09503bb8a56197e1ee845131671f
Devosend <devosend@gmail.com>
2022-07-04 10:05:29 +0800

    [chore] update by release version (#10742)
    * update 3.0.0-beta-2 doc
    
    * update history-versions doc

--- docs/configs/site.js
+++ docs/configs/site.js
@@ -45,7 +45,7 @@ export default {
         children: [
           {
             key: 'docs0',
-            text: 'latest(3.0.0-beta-1)',
+            text: 'latest(3.0.0-beta-2)',
             link: '/en-us/docs/latest/user_doc/about/introduction.html',
           },
           {
@@ -221,7 +221,7 @@ export default {
         children: [
           {
             key: 'docs0',
-            text: '最新版本latest(3.0.0-beta-1)',
+            text: '最新版本latest(3.0.0-beta-2)',
             link: '/zh-cn/docs/latest/user_doc/about/introduction.html',
           },
           {
