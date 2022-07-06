6af302eaedc8b86fca7d3fb19423003198517ef0
xiangzihao <460888207@qq.com>
2022-07-06 14:24:13 +0800

    [API-Test] [Checkstyle] Reformat api-test code and enable checkstyle (#10805)
    * reformat api-test code
    
    * reformat api-test code
    
    * reformat api-test code
    
    * reformat api-test code
    
    * reformat api-test code
    
    * reformat api-test code
    
    * fix checkstyle

--- dolphinscheduler-api-test/dolphinscheduler-api-test-core/src/main/java/org/apache/dolphinscheduler/api/test/core/DolphinSchedulerExtension.java
+++ dolphinscheduler-api-test/dolphinscheduler-api-test-core/src/main/java/org/apache/dolphinscheduler/api/test/core/DolphinSchedulerExtension.java
@@ -27,7 +27,6 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import lombok.SneakyThrows;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
@@ -38,15 +37,15 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 final class DolphinSchedulerExtension implements BeforeAllCallback, AfterAllCallback {
-    private final boolean LOCAL_MODE = Objects.equals(System.getProperty("local"), "true");
+    private final boolean localMode = Objects.equals(System.getProperty("local"), "true");
 
-    private final String SERVICE_NAME = "dolphinscheduler_1";
+    private final String serviceName = "dolphinscheduler_1";
 
     private DockerComposeContainer<?> compose;
 
     @Override
     public void beforeAll(ExtensionContext context) {
-        if (!LOCAL_MODE) {
+        if (!localMode) {
             compose = createDockerCompose(context);
             compose.start();
         }
@@ -63,17 +62,17 @@ final class DolphinSchedulerExtension implements BeforeAllCallback, AfterAllCall
         final Class<?> clazz = context.getRequiredTestClass();
         final DolphinScheduler annotation = clazz.getAnnotation(DolphinScheduler.class);
         final List<File> files = Stream.of(annotation.composeFiles())
-                                       .map(it -> DolphinScheduler.class.getClassLoader().getResource(it))
-                                       .filter(Objects::nonNull)
-                                       .map(URL::getPath)
-                                       .map(File::new)
-                                       .collect(Collectors.toList());
+            .map(it -> DolphinScheduler.class.getClassLoader().getResource(it))
+            .filter(Objects::nonNull)
+            .map(URL::getPath)
+            .map(File::new)
+            .collect(Collectors.toList());
 
         compose = new DockerComposeContainer<>(files)
-                .withPull(true)
-                .withTailChildContainers(true)
-                .withLogConsumer(SERVICE_NAME, outputFrame -> LOGGER.info(outputFrame.getUtf8String()))
-                .waitingFor(SERVICE_NAME, Wait.forHealthcheck().withStartupTimeout(Duration.ofSeconds(Constants.DOCKER_COMPOSE_DEFAULT_TIMEOUT)));
+            .withPull(true)
+            .withTailChildContainers(true)
+            .withLogConsumer(serviceName, outputFrame -> LOGGER.info(outputFrame.getUtf8String()))
+            .waitingFor(serviceName, Wait.forHealthcheck().withStartupTimeout(Duration.ofSeconds(Constants.DOCKER_COMPOSE_DEFAULT_TIMEOUT)));
 
         return compose;
     }
