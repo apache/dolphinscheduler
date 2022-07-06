1a36b44f12ad04d8c9b62ea2634cdbffb23cfe09
songjianet <1778651752@qq.com>
2022-07-06 09:39:02 +0800

    [Refactor][UI] Refactor alarm group manage using NSpace component. (#10803)

--- dolphinscheduler-ui/src/views/security/alarm-group-manage/index.tsx
+++ dolphinscheduler-ui/src/views/security/alarm-group-manage/index.tsx
@@ -22,14 +22,13 @@ import {
   NDataTable,
   NIcon,
   NInput,
-  NPagination
+  NPagination,
+  NSpace
 } from 'naive-ui'
 import { SearchOutlined } from '@vicons/antd'
 import { useI18n } from 'vue-i18n'
 import { useTable } from './use-table'
-import Card from '@/components/card'
 import AlarmGroupModal from './components/alarm-group-modal'
-import styles from './index.module.scss'
 
 const alarmGroupManage = defineComponent({
   name: 'alarm-group-manage',
@@ -102,15 +101,13 @@ const alarmGroupManage = defineComponent({
     } = this
 
     return (
-      <div>
-        <NCard>
-          <div class={styles['search-card']}>
-            <div>
-              <NButton size='small' type='primary' onClick={handleModalChange}>
-                {t('security.alarm_group.create_alarm_group')}
-              </NButton>
-            </div>
-            <div class={styles.box}>
+      <NSpace vertical>
+        <NCard size='small'>
+          <NSpace justify='space-between'>
+            <NButton size='small' type='primary' onClick={handleModalChange}>
+              {t('security.alarm_group.create_alarm_group')}
+            </NButton>
+            <NSpace>
               <NInput
                 size='small'
                 clearable
@@ -126,28 +123,30 @@ const alarmGroupManage = defineComponent({
                   )
                 }}
               </NButton>
-            </div>
-          </div>
+            </NSpace>
+          </NSpace>
         </NCard>
-        <Card class={styles['table-card']}>
-          <NDataTable
-            loading={loadingRef}
-            columns={this.columns}
-            data={this.tableData}
-          />
-          <div class={styles.pagination}>
-            <NPagination
-              v-model:page={this.page}
-              v-model:page-size={this.pageSize}
-              page-count={this.totalPage}
-              show-size-picker
-              page-sizes={[10, 30, 50]}
-              show-quick-jumper
-              onUpdatePage={requestData}
-              onUpdatePageSize={onUpdatePageSize}
+        <NCard size='small'>
+          <NSpace vertical>
+            <NDataTable
+              loading={loadingRef}
+              columns={this.columns}
+              data={this.tableData}
             />
-          </div>
-        </Card>
+            <NSpace justify='center'>
+              <NPagination
+                v-model:page={this.page}
+                v-model:page-size={this.pageSize}
+                page-count={this.totalPage}
+                show-size-picker
+                page-sizes={[10, 30, 50]}
+                show-quick-jumper
+                onUpdatePage={requestData}
+                onUpdatePageSize={onUpdatePageSize}
+              />
+            </NSpace>
+          </NSpace>
+        </NCard>
         <AlarmGroupModal
           showModalRef={this.showModalRef}
           statusRef={this.statusRef}
@@ -155,7 +154,7 @@ const alarmGroupManage = defineComponent({
           onCancelModal={onCancelModal}
           onConfirmModal={onConfirmModal}
         />
-      </div>
+      </NSpace>
     )
   }
 })
