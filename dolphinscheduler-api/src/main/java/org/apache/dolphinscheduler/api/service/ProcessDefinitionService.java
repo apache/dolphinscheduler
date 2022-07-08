c15219e59ee2b874cbfdaacda7488fe63550d806
Jiajie Zhong <zhongjiajie955@gmail.com>
2022-07-08 14:05:54 +0800

    [fix] delete workflow or task used by other tasks
    currently, users can delete process definitions used
    in other sub-process tasks or in other dependent tasks.
    but this change will break the dependence of those task
    and failed DAG, this patch add validation of those
    delete behavior to avoid this error

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/ProcessDefinitionService.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/ProcessDefinitionService.java
@@ -158,7 +158,7 @@ public interface ProcessDefinitionService {
                                                    long targetProjectCode);
 
     /**
-     * update  process definition
+     * update process definition, with whole process definition object including task definition, task relation and location.
      *
      * @param loginUser login user
      * @param projectCode project code
@@ -398,7 +398,7 @@ public interface ProcessDefinitionService {
                                                      ProcessExecutionTypeEnum executionType);
 
     /**
-     * update process definition basic info
+     * update process definition basic info, not including task definition, task relation and location.
      *
      * @param loginUser login user
      * @param projectCode project code
