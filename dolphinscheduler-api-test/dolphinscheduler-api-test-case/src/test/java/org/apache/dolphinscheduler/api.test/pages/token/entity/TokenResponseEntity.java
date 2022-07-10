0ced771f2e75e02b8b5f0e130ed8f7173f4ad6e4
Mr.An <42627702+SnowMoon-Dev@users.noreply.github.com>
2022-07-10 23:06:35 +0800

    add token page

---
+++ dolphinscheduler-api-test/dolphinscheduler-api-test-case/src/test/java/org/apache/dolphinscheduler/api.test/pages/token/entity/TokenResponseEntity.java
@@ -0,0 +1,6 @@
+package org.apache.dolphinscheduler.api.test.pages.token.entity;
+
+import org.apache.dolphinscheduler.api.test.base.AbstractBaseEntity;
+
+public class TokenResponseEntity extends AbstractBaseEntity {
+}
