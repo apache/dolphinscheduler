4563408146cbfe1caa9d040616dab67dd8c7d152
jkhhuse <jiangkang_yewu@cmss.chinamobile.com>
2022-07-13 20:46:23 +0800

    [fix-10724][ui] remove unused attribute in chunjun component
    - add FlinkX (Chunjun) workflow ui component
    
    This closes #10724

--- dolphinscheduler-ui/src/views/projects/task/components/node/fields/use-chunjun.ts
+++ dolphinscheduler-ui/src/views/projects/task/components/node/fields/use-chunjun.ts
@@ -17,167 +17,10 @@
 import { ref, onMounted, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
 import type { IJsonItem } from '../types'
-import { find } from 'lodash'
-import { TypeReq } from '@/service/modules/data-source/types'
-import { queryDataSourceList } from '@/service/modules/data-source'
 import { useChunjunDeployMode } from './'
 
 export function useChunjun(model: { [field: string]: any }): IJsonItem[] {
   const { t } = useI18n()
-
-  const datasourceTypes = [
-    {
-      id: 0,
-      code: 'MYSQL',
-      disabled: false
-    },
-    {
-      id: 1,
-      code: 'POSTGRESQL',
-      disabled: false
-    },
-    {
-      id: 2,
-      code: 'HIVE',
-      disabled: true
-    },
-    {
-      id: 3,
-      code: 'SPARK',
-      disabled: true
-    },
-    {
-      id: 4,
-      code: 'CLICKHOUSE',
-      disabled: false
-    },
-    {
-      id: 5,
-      code: 'ORACLE',
-      disabled: false
-    },
-    {
-      id: 6,
-      code: 'SQLSERVER',
-      disabled: false
-    },
-    {
-      id: 7,
-      code: 'DB2',
-      disabled: true
-    },
-    {
-      id: 8,
-      code: 'PRESTO',
-      disabled: true
-    }
-  ]
-  const datasourceTypeOptions = ref([] as any)
-  const datasourceOptions = ref([] as any)
-  const destinationDatasourceOptions = ref([] as any)
-  const jobSpeedByteOptions: any[] = [
-    {
-      label: `0(${t('project.node.unlimited')})`,
-      value: 0
-    },
-    {
-      label: '1KB',
-      value: 1024
-    },
-    {
-      label: '10KB',
-      value: 10240
-    },
-    {
-      label: '50KB',
-      value: 51200
-    },
-    {
-      label: '100KB',
-      value: 102400
-    },
-    {
-      label: '512KB',
-      value: 524288
-    }
-  ]
-  const jobSpeedRecordOptions: any[] = [
-    {
-      label: `0(${t('project.node.unlimited')})`,
-      value: 0
-    },
-    {
-      label: '500',
-      value: 500
-    },
-    {
-      label: '1000',
-      value: 1000
-    },
-    {
-      label: '1500',
-      value: 1500
-    },
-    {
-      label: '2000',
-      value: 2000
-    },
-    {
-      label: '2500',
-      value: 2500
-    },
-    {
-      label: '3000',
-      value: 3000
-    }
-  ]
-  const loading = ref(false)
-
-  const getDatasourceTypes = async () => {
-    if (loading.value) return
-    loading.value = true
-    datasourceTypeOptions.value = datasourceTypes
-      .filter((item) => !item.disabled)
-      .map((item) => ({ label: item.code, value: item.code }))
-    loading.value = false
-  }
-
-  const getDatasourceInstances = async () => {
-    const params = { type: model.dsType } as TypeReq
-    const res = await queryDataSourceList(params)
-    datasourceOptions.value = []
-    res.map((item: any) => {
-      datasourceOptions.value.push({ label: item.name, value: String(item.id) })
-    })
-    if (datasourceOptions.value && model.dataSource) {
-      const item = find(datasourceOptions.value, {
-        value: String(model.dataSource)
-      })
-      if (!item) {
-        model.dataSource = null
-      }
-    }
-  }
-
-  const getDestinationDatasourceInstances = async () => {
-    const params = { type: model.dtType } as TypeReq
-    const res = await queryDataSourceList(params)
-    destinationDatasourceOptions.value = []
-    res.map((item: any) => {
-      destinationDatasourceOptions.value.push({
-        label: item.name,
-        value: String(item.id)
-      })
-    })
-    if (destinationDatasourceOptions.value && model.dataTarget) {
-      const item = find(destinationDatasourceOptions.value, {
-        value: String(model.dataTarget)
-      })
-      if (!item) {
-        model.dataTarget = null
-      }
-    }
-  }
 
   const jsonEditorSpan = ref(0)
   const customParameterSpan = ref(0)
@@ -193,9 +36,6 @@ export function useChunjun(model: { [field: string]: any }): IJsonItem[] {
   }
 
   onMounted(() => {
-    getDatasourceTypes()
-    getDatasourceInstances()
-    getDestinationDatasourceInstances()
     initConstants()
   })
 
