15c1184616b49cdd18f4108ab529df9a4ebb65bc
labbomb <739955946@qq.com>
2022-07-14 20:44:41 +0800

    feat: Added global trim method (#10975)

--- dolphinscheduler-ui/src/utils/trim.ts
+++ dolphinscheduler-ui/src/utils/trim.ts
@@ -15,27 +15,8 @@
  * limitations under the License.
  */
 
-import { createApp } from 'vue'
-import App from './App'
-import router from './router'
-import { createPinia } from 'pinia'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-import i18n from '@/locales'
-import * as echarts from 'echarts'
-import 'echarts/theme/macarons'
-import 'echarts/theme/dark-bold'
-import './assets/styles/default.scss'
-import trim from './utils/trim'
+const trim = (value: string) => {
+  return !value.startsWith(' ') && !value.endsWith(' ')
+}
 
-const app = createApp(App)
-const pinia = createPinia()
-
-pinia.use(piniaPluginPersistedstate)
-
-app.config.globalProperties.echarts = echarts
-app.config.globalProperties.trim = trim
-
-app.use(router)
-app.use(pinia)
-app.use(i18n)
-app.mount('#app')
+export default trim
