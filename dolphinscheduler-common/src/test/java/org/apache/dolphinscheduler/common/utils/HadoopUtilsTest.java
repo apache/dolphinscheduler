7fec1f57885ce126bef61f3db83133488b4b348f
liyangyang <liyangyang@bizseer.com>
2022-07-22 19:11:04 +0800

    [Improvement-11013][dolphinscheduler-common] shortened string

--- dolphinscheduler-common/src/test/java/org/apache/dolphinscheduler/common/utils/HadoopUtilsTest.java
+++ dolphinscheduler-common/src/test/java/org/apache/dolphinscheduler/common/utils/HadoopUtilsTest.java
@@ -66,7 +66,8 @@ public class HadoopUtilsTest {
     public void getAppAddress() {
         PowerMockito.mockStatic(HttpUtils.class);
         PowerMockito.when(HttpUtils.get("http://ds1:8088/ws/v1/cluster/info")).thenReturn(
-            "{\"clusterInfo\":{\"id\":1657034250022,\"startedOn\":1657034250022,\"state\":\"STARTED\",\"haState\":\"ACTIVE\",\"rmStateStoreName\":\"org.apache.hadoop.yarn.server.resourcemanager.recovery.NullRMStateStore\",\"resourceManagerVersion\":\"2.8.5\",\"resourceManagerBuildVersion\":\"2.8.5 from 0b8464d75227fcee2c6e7f2410377b3d53d3d5f8 by jdu source checksum ba3b9c96362faf6bc5c4cfc9cb53880\",\"resourceManagerVersionBuiltOn\":\"2018-09-10T03:44Z\",\"hadoopVersion\":\"2.8.5\",\"hadoopBuildVersion\":\"2.8.5 from 0b8464d75227fcee2c6e7f2410377b3d53d3d5f8 by jdu source checksum 9942ca5c745417c14e318835f420733\",\"hadoopVersionBuiltOn\":\"2018-09-10T03:32Z\",\"haZooKeeperConnectionState\":\"CONNECTED\"}}");
+            "{\"clusterInfo\":{\"id\":1657034250022,\"startedOn\":1657034250022,\"" +
+                "state\":\"STARTED\",\"haState\":\"ACTIVE\",\"haZooKeeperConnectionState\":\"CONNECTED\"}}");
         logger.info(HadoopUtils.getAppAddress("http://ds1:8088/ws/v1/cluster/apps/%s", "ds1,ds2"));
         Assert.assertTrue(true);
     }
