912003822cd376182aad09378d29dc8cdd8f2b9a
xiangzihao <460888207@qq.com>
2022-07-22 09:49:20 +0800

    add oracle dependency (#11095)

--- docs/configs/docsdev.js
+++ docs/configs/docsdev.js
@@ -822,7 +822,7 @@ export default {
                     },
                     {
                         title: '数据源设置',
-                        link: '/zh-cn/docs/dev/user_doc/guide/howto/general-setting.html',
+                        link: '/zh-cn/docs/dev/user_doc/guide/howto/datasource-setting.html',
                     },
                     {
                         title: '其他',
