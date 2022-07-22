3b617131507e12e484a935e12edebf430bfc591c
liyangyang <liyangyang@bizseer.com>
2022-07-22 13:28:14 +0800

    [Improvement-11013][dolphinscheduler-common] add test code

--- dolphinscheduler-common/src/test/java/org/apache/dolphinscheduler/common/utils/HadoopUtilsTest.java
+++ dolphinscheduler-common/src/test/java/org/apache/dolphinscheduler/common/utils/HadoopUtilsTest.java
@@ -18,14 +18,13 @@
 package org.apache.dolphinscheduler.common.utils;
 
 import org.apache.dolphinscheduler.spi.enums.ResourceType;
-import org.apache.dolphinscheduler.spi.utils.PropertyUtils;
-
-import org.apache.hadoop.security.UserGroupInformation;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.core.classloader.annotations.SuppressStaticInitializationFor;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,7 +33,8 @@ import org.slf4j.LoggerFactory;
  * hadoop utils test
  */
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(value = {PropertyUtils.class, UserGroupInformation.class})
+@PrepareForTest(value = {HadoopUtils.class})
+@SuppressStaticInitializationFor("org.apache.dolphinscheduler.common.utils.HttpUtils")
 public class HadoopUtilsTest {
     private static final Logger logger = LoggerFactory.getLogger(HadoopUtilsTest.class);
 
@@ -59,6 +59,15 @@ public class HadoopUtilsTest {
     @Test
     public void getHdfsFileName() {
         logger.info(HadoopUtils.getHdfsFileName(ResourceType.FILE, "admin", "file_name"));
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void getAppAddress() {
+        PowerMockito.mockStatic(HttpUtils.class);
+        PowerMockito.when(HttpUtils.get("http://ds1:8088/ws/v1/cluster/info")).thenReturn(
+            "{\"clusterInfo\":{\"id\":1657034250022,\"startedOn\":1657034250022,\"state\":\"STARTED\",\"haState\":\"ACTIVE\",\"rmStateStoreName\":\"org.apache.hadoop.yarn.server.resourcemanager.recovery.NullRMStateStore\",\"resourceManagerVersion\":\"2.8.5\",\"resourceManagerBuildVersion\":\"2.8.5 from 0b8464d75227fcee2c6e7f2410377b3d53d3d5f8 by jdu source checksum ba3b9c96362faf6bc5c4cfc9cb53880\",\"resourceManagerVersionBuiltOn\":\"2018-09-10T03:44Z\",\"hadoopVersion\":\"2.8.5\",\"hadoopBuildVersion\":\"2.8.5 from 0b8464d75227fcee2c6e7f2410377b3d53d3d5f8 by jdu source checksum 9942ca5c745417c14e318835f420733\",\"hadoopVersionBuiltOn\":\"2018-09-10T03:32Z\",\"haZooKeeperConnectionState\":\"CONNECTED\"}}");
+        logger.info(HadoopUtils.getAppAddress("http://ds1:8088/ws/v1/cluster/apps/%s", "ds1,ds2"));
         Assert.assertTrue(true);
     }
 
