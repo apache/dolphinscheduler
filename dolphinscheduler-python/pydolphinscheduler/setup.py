72e8081ba7725013cff0fb2b32fdb18f8f4babe7
Jiajie Zhong <zhongjiajie955@gmail.com>
2022-07-27 16:47:13 +0800

    [python] Add support interpreter version 310 and 311
    Add CI to support both python interpreter 3.10 an 3.11

--- dolphinscheduler-python/pydolphinscheduler/setup.py
+++ dolphinscheduler-python/pydolphinscheduler/setup.py
@@ -169,6 +169,8 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Software Development :: User Interfaces",
