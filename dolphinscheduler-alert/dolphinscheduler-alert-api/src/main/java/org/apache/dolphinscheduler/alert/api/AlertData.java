c882c40d65b4984917f108485b57b607413beff1
SongTao Zhuang <51652084+MichaelDeSteven@users.noreply.github.com>
2022-07-28 08:31:44 +0800

    [11130] Refactor alert-api with lombok (#11133)

--- dolphinscheduler-alert/dolphinscheduler-alert-api/src/main/java/org/apache/dolphinscheduler/alert/api/AlertData.java
+++ dolphinscheduler-alert/dolphinscheduler-alert-api/src/main/java/org/apache/dolphinscheduler/alert/api/AlertData.java
@@ -19,177 +19,46 @@
 
 package org.apache.dolphinscheduler.alert.api;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * alert data
  */
+@AllArgsConstructor
+@Builder
+@Data
+@NoArgsConstructor
 public class AlertData {
+
+    /**
+     * alert id
+     */
     private int id;
+
+    /**
+     * alert tile
+     */
     private String title;
+
+    /**
+     * alert content
+     */
     private String content;
+
+    /**
+     * alert log
+     */
     private String log;
+
+    /**
+     * 0 do not send warning;
+     * 1 send if process success;
+     * 2 send if process failed;
+     * 3 send if process ends, whatever the result;
+     */
     private int warnType;
 
-    public AlertData(int id, String title, String content, String log, int warnType) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-        this.log = log;
-        this.warnType = warnType;
-    }
-
-    public AlertData() {
-    }
-
-    public static AlertDataBuilder builder() {
-        return new AlertDataBuilder();
-    }
-
-    public int getId() {
-        return this.id;
-    }
-
-    public AlertData setId(int id) {
-        this.id = id;
-        return this;
-    }
-
-    public String getTitle() {
-        return this.title;
-    }
-
-    public AlertData setTitle(String title) {
-        this.title = title;
-        return this;
-    }
-
-    public String getContent() {
-        return this.content;
-    }
-
-    public AlertData setContent(String content) {
-        this.content = content;
-        return this;
-    }
-
-    public String getLog() {
-        return this.log;
-    }
-
-    public AlertData setLog(String log) {
-        this.log = log;
-        return this;
-    }
-
-    public int getWarnType() {
-        return warnType;
-    }
-
-    public void setWarnType(int warnType) {
-        this.warnType = warnType;
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (o == this) {
-            return true;
-        }
-        if (!(o instanceof AlertData)) {
-            return false;
-        }
-        final AlertData other = (AlertData) o;
-        if (!other.canEqual(this)) {
-            return false;
-        }
-        if (this.getId() != other.getId()) {
-            return false;
-        }
-        if (this.getWarnType() != other.getWarnType()) {
-            return false;
-        }
-        final Object thisTitle = this.getTitle();
-        final Object otherTitle = other.getTitle();
-        if (!Objects.equals(thisTitle, otherTitle)) {
-            return false;
-        }
-        final Object thisContent = this.getContent();
-        final Object otherContent = other.getContent();
-        if (!Objects.equals(thisContent, otherContent)) {
-            return false;
-        }
-        final Object thisLog = this.getLog();
-        final Object otherLog = other.getLog();
-        return Objects.equals(thisLog, otherLog);
-    }
-
-    protected boolean canEqual(final Object other) {
-        return other instanceof AlertData;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 59;
-        int result = 1;
-        result = result * prime + this.getId();
-        result = result * prime + this.getWarnType();
-        final Object title = this.getTitle();
-        result = result * prime + (title == null ? 43 : title.hashCode());
-        final Object content = this.getContent();
-        result = result * prime + (content == null ? 43 : content.hashCode());
-        final Object log = this.getLog();
-        result = result * prime + (log == null ? 43 : log.hashCode());
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "AlertData(id=" + this.getId() + ", title=" + this.getTitle() + ", content=" + this.getContent() + ", log=" + this.getLog() + ", warnType=" + this.getWarnType() + ")";
-    }
-
-    public static class AlertDataBuilder {
-        private int id;
-        private String title;
-        private String content;
-        private String log;
-        private int warnType;
-
-        AlertDataBuilder() {
-        }
-
-        public AlertDataBuilder id(int id) {
-            this.id = id;
-            return this;
-        }
-
-        public AlertDataBuilder title(String title) {
-            this.title = title;
-            return this;
-        }
-
-        public AlertDataBuilder content(String content) {
-            this.content = content;
-            return this;
-        }
-
-        public AlertDataBuilder log(String log) {
-            this.log = log;
-            return this;
-        }
-
-        public AlertDataBuilder warnType(int warnType) {
-            this.warnType = warnType;
-            return this;
-        }
-
-        public AlertData build() {
-            return new AlertData(id, title, content, log, warnType);
-        }
-
-        @Override
-        public String toString() {
-            return "AlertData.AlertDataBuilder(id=" + this.id + ", title=" + this.title + ", content=" + this.content + ", log=" + this.log + ", warnType=" + this.warnType + ")";
-        }
-
-    }
 }
--- dolphinscheduler-alert/dolphinscheduler-alert-api/src/main/java/org/apache/dolphinscheduler/alert/api/AlertInfo.java
+++ dolphinscheduler-alert/dolphinscheduler-alert-api/src/main/java/org/apache/dolphinscheduler/alert/api/AlertInfo.java
@@ -20,111 +20,23 @@
 package org.apache.dolphinscheduler.alert.api;
 
 import java.util.Map;
-import java.util.Objects;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * The alarm information includes the parameters of the alert channel and the alarm data
  */
+@AllArgsConstructor
+@Builder
+@Data
+@NoArgsConstructor
 public class AlertInfo {
+
     private Map<String, String> alertParams;
+
     private AlertData alertData;
 
-    public AlertInfo(Map<String, String> alertParams, AlertData alertData) {
-        this.alertParams = alertParams;
-        this.alertData = alertData;
-    }
-
-    public AlertInfo() {
-    }
-
-    public static AlertInfoBuilder builder() {
-        return new AlertInfoBuilder();
-    }
-
-    public Map<String, String> getAlertParams() {
-        return this.alertParams;
-    }
-
-    public AlertInfo setAlertParams(Map<String, String> alertParams) {
-        this.alertParams = alertParams;
-        return this;
-    }
-
-    public AlertData getAlertData() {
-        return this.alertData;
-    }
-
-    public AlertInfo setAlertData(AlertData alertData) {
-        this.alertData = alertData;
-        return this;
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (o == this) {
-            return true;
-        }
-        if (!(o instanceof AlertInfo)) {
-            return false;
-        }
-        final AlertInfo other = (AlertInfo) o;
-        if (!other.canEqual((Object) this)) {
-            return false;
-        }
-        final Object thisAlertParams = this.getAlertParams();
-        final Object otherAlertParams = other.getAlertParams();
-        if (!Objects.equals(thisAlertParams, otherAlertParams)) {
-            return false;
-        }
-        final Object thisAlertData = this.getAlertData();
-        final Object otherAlertData = other.getAlertData();
-        return Objects.equals(thisAlertData, otherAlertData);
-    }
-
-    protected boolean canEqual(final Object other) {
-        return other instanceof AlertInfo;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 59;
-        int result = 1;
-        final Object alertParams = this.getAlertParams();
-        result = result * prime + (alertParams == null ? 43 : alertParams.hashCode());
-        final Object alertData = this.getAlertData();
-        result = result * prime + (alertData == null ? 43 : alertData.hashCode());
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "AlertInfo(alertParams=" + this.getAlertParams() + ", alertData=" + this.getAlertData() + ")";
-    }
-
-    public static class AlertInfoBuilder {
-        private Map<String, String> alertParams;
-        private AlertData alertData;
-
-        AlertInfoBuilder() {
-        }
-
-        public AlertInfoBuilder alertParams(Map<String, String> alertParams) {
-            this.alertParams = alertParams;
-            return this;
-        }
-
-        public AlertInfoBuilder alertData(AlertData alertData) {
-            this.alertData = alertData;
-            return this;
-        }
-
-        public AlertInfo build() {
-            return new AlertInfo(alertParams, alertData);
-        }
-
-        @Override
-        public String toString() {
-            return "AlertInfo.AlertInfoBuilder(alertParams=" + this.alertParams + ", alertData=" + this.alertData + ")";
-        }
-    }
 }
