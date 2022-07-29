6b4e78a2a2744ee1646f4712d94a271407e25369
Jiajie Zhong <zhongjiajie955@gmail.com>
2022-07-29 12:00:04 +0800

    Fix code style error

--- dolphinscheduler-common/src/main/java/org/apache/dolphinscheduler/common/config/StoreConfiguration.java
+++ dolphinscheduler-common/src/main/java/org/apache/dolphinscheduler/common/config/StoreConfiguration.java
@@ -17,19 +17,19 @@
 
 package org.apache.dolphinscheduler.common.config;
 
+import static org.apache.dolphinscheduler.common.Constants.RESOURCE_STORAGE_TYPE;
+import static org.apache.dolphinscheduler.common.Constants.STORAGE_HDFS;
+import static org.apache.dolphinscheduler.common.Constants.STORAGE_LOCAL;
+import static org.apache.dolphinscheduler.common.Constants.STORAGE_S3;
+
 import org.apache.dolphinscheduler.common.storage.StorageOperate;
 import org.apache.dolphinscheduler.common.utils.HadoopUtils;
 import org.apache.dolphinscheduler.common.utils.PropertyUtils;
 import org.apache.dolphinscheduler.common.utils.S3Utils;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
-
-import static org.apache.dolphinscheduler.common.Constants.RESOURCE_STORAGE_TYPE;
-import static org.apache.dolphinscheduler.common.Constants.STORAGE_HDFS;
-import static org.apache.dolphinscheduler.common.Constants.STORAGE_S3;
-import static org.apache.dolphinscheduler.common.Constants.STORAGE_LOCAL;
-
 
 /**
  * choose the impl of storage by RESOURCE_STORAGE_TYPE
