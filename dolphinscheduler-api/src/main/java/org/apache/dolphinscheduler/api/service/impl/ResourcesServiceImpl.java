2587ebb343447e5c486cafe09ce4094555274615
GavinGYM <gavin-gym@foxmail.com>
2022-08-08 22:41:41 +0800

    Fix bug of authorizedFileWithReadPerm API, the previous code has a bug where the file directory was displayed incorrectly.

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/impl/ResourcesServiceImpl.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/impl/ResourcesServiceImpl.java
@@ -66,17 +66,7 @@ import org.apache.commons.lang3.StringUtils;
 import java.io.IOException;
 import java.rmi.ServerException;
 import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.stream.Collectors;
 
@@ -1693,9 +1683,11 @@ public class ResourcesServiceImpl extends BaseServiceImpl implements ResourcesSe
         }
 
         List<Resource> authedResources = queryResourceList(userId, Constants.AUTHORIZE_READABLE_PERM);
-        for (int i = 0; i < authedResources.size(); i++) {
-            if(authedResources.get(i).isDirectory()){
-                authedResources.remove(i);
+        Iterator<Resource> iterator = authedResources.iterator();
+        while(iterator.hasNext()){
+            Resource currentResource = iterator.next();
+            if(currentResource.isDirectory()){
+                iterator.remove();
             }
         }
 
