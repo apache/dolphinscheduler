16cfa5449d47b4dbc1b5e3d607cd5beb71bf9605
xuhaihui <xuhaihui@cmss.chinamobile.com>
2022-08-08 16:42:32 +0800

    refactor service part

--- dolphinscheduler-api/src/test/java/org/apache/dolphinscheduler/api/service/QueueServiceTest.java
+++ dolphinscheduler-api/src/test/java/org/apache/dolphinscheduler/api/service/QueueServiceTest.java
@@ -41,7 +41,6 @@ import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import org.junit.After;
