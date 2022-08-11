fd33b573baeea4151a5d50334d924d6d346cb599
Jiajie Zhong <zhongjiajie955@hotmail.com>
2022-08-11 23:39:44 +0800

    [python] Add support interpreter version 310 and 311-dev (#11170)
    * Change version of package
    * Fix tox error
    * Change dev status to beta
    * py4j not work on py{10,11} OS Windows

--- dolphinscheduler-python/pydolphinscheduler/setup.py
+++ dolphinscheduler-python/pydolphinscheduler/setup.py
@@ -32,7 +32,7 @@ if sys.version_info[0] < 3:
 
 logger = logging.getLogger(__name__)
 
-version = "2.0.4"
+version = "dev"
 
 # Start package required
 prod = [
@@ -156,7 +156,7 @@ setup(
     platforms=["any"],
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
@@ -169,6 +169,8 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Software Development :: User Interfaces",
