6478cad5714cfb8e8ef6cdeba8b8634bb29f58ee
Amy0104 <amywang0104@163.com>
2022-08-17 09:26:01 +0800

    [Fix][UI] Fix the preTask options are unavailable when creating a tas...

--- dolphinscheduler-ui/src/views/projects/task/components/node/tasks/use-http.ts
+++ dolphinscheduler-ui/src/views/projects/task/components/node/tasks/use-http.ts
@@ -52,24 +52,10 @@ export function useHttp({
     socketTimeout: 60000
   } as INodeData)
 
-  let extra: IJsonItem[] = []
-  if (from === 1) {
-    extra = [
-      Fields.useTaskType(model, readonly),
-      Fields.useProcessName({
-        model,
-        projectCode,
-        isCreate: !data?.id,
-        from,
-        processName: data?.processName
-      })
-    ]
-  }
-
   return {
     json: [
       Fields.useName(from),
-      ...extra,
+      ...Fields.useTaskDefinition({ projectCode, from, readonly, data, model }),
       Fields.useRunFlag(),
       Fields.useDescription(),
       Fields.useTaskPriority(),
