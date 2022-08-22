9467e002267a882cfabe96268b33ee2a01213ebe
冯剑 Jian <jian.feng@jiduauto.com>
2022-08-22 18:42:19 +0800

    [Improvement-#9680][task-plugin] add hive sql log listener, print MapReduce progress log and obtain application_id allows developers to better track tasks

--- dolphinscheduler-task-plugin/dolphinscheduler-task-sql/src/test/java/org/apache/dolphinscheduler/plugin/task/sql/HiveSqlLogThreadTest.java
+++ dolphinscheduler-task-plugin/dolphinscheduler-task-sql/src/test/java/org/apache/dolphinscheduler/plugin/task/sql/HiveSqlLogThreadTest.java
@@ -17,85 +17,63 @@
 
 package org.apache.dolphinscheduler.plugin.task.sql;
 
-import org.apache.dolphinscheduler.plugin.datasource.api.plugin.DataSourceClientProvider;
-import org.apache.dolphinscheduler.plugin.datasource.api.utils.DataSourceUtils;
-import org.apache.dolphinscheduler.plugin.datasource.hive.param.HiveConnectionParam;
+import org.apache.dolphinscheduler.common.utils.LoggerUtils;
 import org.apache.dolphinscheduler.plugin.task.api.TaskExecutionContext;
 import org.apache.dolphinscheduler.plugin.task.api.parameters.SqlParameters;
-import org.apache.dolphinscheduler.spi.datasource.BaseConnectionParam;
-import org.apache.dolphinscheduler.spi.enums.DbType;
 import org.apache.dolphinscheduler.spi.utils.JSONUtils;
+import org.apache.dolphinscheduler.spi.utils.StringUtils;
 
-import org.apache.hadoop.security.UserGroupInformation;
+
+import org.apache.hive.jdbc.HiveStatement;
 
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.modules.junit4.PowerMockRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * hive sql listener test
  */
+@RunWith(PowerMockRunner.class)
+@PowerMockIgnore({"javax.*"})
 public class HiveSqlLogThreadTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(HiveSqlLogThreadTest.class);
+
     @Test
-    public void testHiveSql() throws IOException {
-        String taskJson="{\"type\":\"HIVE\",\"datasource\":1,\"sql\":\"select count(*) from tmp.test_doris\",\"udfs\":\"\",\"sqlType\":\"0\",\"sendEmail\":false,\"displayRows\":10,\"title\":\"\",\"groupId\":null,\"localParams\":[],\"connParams\":\"\",\"preStatements\":[],\"postStatements\":[],\"dependence\":{},\"conditionResult\":{\"successNode\":[],\"failedNode\":[]},\"waitStartTimeout\":{},\"switchResult\":{}}";
-        TaskExecutionContext taskExecutionContext = new TaskExecutionContext();
-        taskExecutionContext.setTaskType("hive");
-        taskExecutionContext.setTaskParams(taskJson);
-        SqlParameters sqlParameters = JSONUtils.parseObject(taskExecutionContext.getTaskParams(), SqlParameters.class);
-        assert sqlParameters != null;
-        String sql = sqlParameters.getSql();
+    public void testHiveSql() throws SQLException, InterruptedException {
+        TaskExecutionContext taskExecutionContext = PowerMockito.mock(TaskExecutionContext.class);
+        PowerMockito.when(taskExecutionContext.getTaskType()).thenReturn("hive");
 
-        String krb5FilePath ="/etc/krb5.conf";
-        String krb5KeyTabPath ="/etc/vulcan.keytab";
-        System.setProperty("java.security.krb5.conf", krb5FilePath);
-        System.setProperty("sun.security.krb5.debug", "true");
-        org.apache.hadoop.conf.Configuration configuration = new org.apache.hadoop.conf.Configuration();
-        configuration.set("hadoop.security.authentication", "Kerberos");
-        configuration.set("keytab.file", krb5KeyTabPath);
-        configuration.set("kerberos.principal", "vulcan@BJ.BAIDU.COM");
+        String sql = "select count(*) from test.table";
 
-        UserGroupInformation.setConfiguration(configuration);
-        UserGroupInformation.loginUserFromKeytab("vulcan", krb5KeyTabPath);
-
-        // create hive connection
-        HiveConnectionParam connectionParam = new HiveConnectionParam();
-        connectionParam.setUser("hive");
-        connectionParam.setDriverClassName("org.apache.hive.jdbc.HiveDriver");
-        connectionParam.setAddress("jdbc:hive2://bjbd-test-bigdata-hive-001:10000");
-        connectionParam.setJdbcUrl("jdbc:hive2://bjbd-test-bigdata-hive-001:10000/default;principal=hive/_HOST@BJ.BAIDU.COM");
-        connectionParam.setPrincipal("hive/_HOST@BJ.BAIDU.COM");
-        connectionParam.setDatabase("default");
-        BaseConnectionParam baseConnectionParam = (BaseConnectionParam) DataSourceUtils.buildConnectionParams(
-            DbType.valueOf(sqlParameters.getType()),JSONUtils.toJsonString(connectionParam)
-            );
-
-
-
-        ResultSet res = null;
-        Connection con = null;
-        Statement stmt = null;
-        try{
-            con = DataSourceClientProvider.getInstance().getConnection(DbType.valueOf(sqlParameters.getType()), baseConnectionParam);
-
-            stmt = con.createStatement();
+        List<String> mockLog = new ArrayList<>();
+        mockLog.add("start hive sql log\napplication_1231_2323");
+        HiveStatement statement = PowerMockito.mock(HiveStatement.class);
+        PowerMockito.when(statement.isClosed()).thenReturn(false);
+        PowerMockito.when(statement.hasMoreLogs()).thenReturn(true);
+        PowerMockito.when(statement.getQueryLog(true,500)).thenReturn(mockLog);
+        try {
             //print process log
-            HiveSqlLogThread queryThread = new HiveSqlLogThread(stmt,LOGGER,taskExecutionContext);
-            queryThread.setName("sql log print");
+            HiveSqlLogThread queryThread = PowerMockito.spy(new HiveSqlLogThread(statement,LOGGER,taskExecutionContext));
             queryThread.start();
-            res = stmt.executeQuery(sql);
-            while (res.next()) {
-                LOGGER.info("---------{}--------",res.getInt(1));
-            }
+
+            Assert.assertEquals(taskExecutionContext.getTaskType(), "hive");
 
         } catch (Exception e) {
-            LOGGER.error(sql+" query failed" , e);
+            LOGGER.error(sql + " query failed" , e);
         }
 
     }
