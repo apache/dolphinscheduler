ec9ec8be78c03dfd054e2532d8a8726ba2c5e074
冯剑 Jian <jian.feng@jiduauto.com>
2022-08-26 19:36:07 +0800

    [Bug-11650][worker] #11650 fix SQL type task, stop task does not take effect

--- dolphinscheduler-worker/src/main/java/org/apache/dolphinscheduler/server/worker/processor/TaskKillProcessor.java
+++ dolphinscheduler-worker/src/main/java/org/apache/dolphinscheduler/server/worker/processor/TaskKillProcessor.java
@@ -46,6 +46,8 @@ import org.apache.dolphinscheduler.service.log.LogClientService;
 
 import org.apache.commons.collections.CollectionUtils;
 
+
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
