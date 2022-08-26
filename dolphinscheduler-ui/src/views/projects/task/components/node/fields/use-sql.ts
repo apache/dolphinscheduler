1e56949d3988728c4ad5bc9bf1c6ff109b77f415
EricGao888 <ericgao.apache@gmail.com>
2022-08-26 13:50:31 +0800

    [Feature][UI] Enable highlight and auto-complete for SQL scripts (#11658)

--- dolphinscheduler-ui/src/views/projects/task/components/node/fields/use-sql.ts
+++ dolphinscheduler-ui/src/views/projects/task/components/node/fields/use-sql.ts
@@ -43,6 +43,10 @@ export function useSql(model: { [field: string]: any }): IJsonItem[] {
         trigger: ['input', 'trigger'],
         required: true,
         message: t('project.node.sql_empty_tips')
+      },
+      props: {
+        type: 'sql',
+        placeholder: t('project.node.sql_statement_tips')
       }
     },
     useUdfs(model),
