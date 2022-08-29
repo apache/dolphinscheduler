7d4b617a892a6473bf3a3e11453907a0281f6303
xingnull <xingnull@126.com>
2022-08-29 23:40:41 +0800

    fix 10340, timeout modify 15e3 to 15e5

--- dolphinscheduler-ui/src/service/service.ts
+++ dolphinscheduler-ui/src/service/service.ts
@@ -43,7 +43,7 @@ const baseRequestConfig: AxiosRequestConfig = {
     import.meta.env.MODE === 'development'
       ? '/dolphinscheduler'
       : import.meta.env.VITE_APP_PROD_WEB_URL + '/dolphinscheduler',
-  timeout: 15000,
+  timeout: 1500000,
   transformRequest: (params) => {
     if (_.isPlainObject(params)) {
       return qs.stringify(params, { arrayFormat: 'repeat' })
