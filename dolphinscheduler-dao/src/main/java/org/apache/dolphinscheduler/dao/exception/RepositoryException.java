571daa8c0af8f26b9fb7c09c2b28d838cc9dfcad
Wenjun Ruan <wenjun@apache.org>
2022-08-31 10:39:57 +0800

    Add comment

--- dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/exception/RepositoryException.java
+++ dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/exception/RepositoryException.java
@@ -1,5 +1,9 @@
 package org.apache.dolphinscheduler.dao.exception;
 
+/**
+ * This exception is used in dao module, when crud from repository failed,
+ * need to throw {@link RepositoryException} instead of {@link RuntimeException}.
+ */
 public class RepositoryException extends Exception {
 
     public RepositoryException(String message) {
