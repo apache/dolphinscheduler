04de80b910f1a4afef4c9cc559f553612664c898
xiangzihao <460888207@qq.com>
2022-09-07 16:40:43 +0800

    fix hdfs defaultFs not working (#11823)

--- dolphinscheduler-common/src/main/java/org/apache/dolphinscheduler/common/Constants.java
+++ dolphinscheduler-common/src/main/java/org/apache/dolphinscheduler/common/Constants.java
@@ -74,6 +74,11 @@ public final class Constants {
 
 
     /**
+     * hdfs defaultFS property name. Should be consistent with the property name in hdfs-site.xml
+     */
+    public static final String HDFS_DEFAULT_FS = "fs.defaultFS";
+
+    /**
      * hadoop configuration
      */
     public static final String HADOOP_RM_STATE_ACTIVE = "ACTIVE";
