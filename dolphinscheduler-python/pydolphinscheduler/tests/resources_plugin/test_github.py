09596b81fd068003155ae01a677782adce4c7d9a
xdu-chenrj <chenrj.xdu@gmail.com>
2022-09-08 19:40:11 +0800

    reformatted tests/resources_plugin/test_github.py

--- dolphinscheduler-python/pydolphinscheduler/tests/resources_plugin/test_github.py
+++ dolphinscheduler-python/pydolphinscheduler/tests/resources_plugin/test_github.py
@@ -195,9 +195,8 @@ def test_github_get_req_url(attr, expected):
     github = GitHub(prefix="prefix")
     assert expected == github.get_req_url(attr)
 
-@pytest.mark.skip(
-    reason="Lack of test environment, need stable repository"
-)
+
+@pytest.mark.skip(reason="Lack of test environment, need stable repository")
 @pytest.mark.parametrize(
     "attr, expected",
     [
@@ -231,9 +230,8 @@ def test_github_read_file(attr, expected):
     )
     assert expected == github.read_file(attr)
 
-@pytest.mark.skip(
-    reason="Lack of test environment, need stable repository"
-)
+
+@pytest.mark.skip(reason="Lack of test environment, need stable repository")
 @pytest.mark.parametrize(
     "attr, expected",
     [
@@ -296,9 +294,8 @@ def test_github_private_rep(attr, expected):
     github = GitHub(**attr)
     assert expected == github.read_file("union.sh")
 
-@pytest.mark.skip(
-    reason="Lack of test environment, need stable repository"
-)
+
+@pytest.mark.skip(reason="Lack of test environment, need stable repository")
 def test_github_file_not_found():
     """Test file does not exist."""
     with pytest.raises(
