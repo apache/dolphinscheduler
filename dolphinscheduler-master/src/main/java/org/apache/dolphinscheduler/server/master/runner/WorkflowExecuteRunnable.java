27b69e608a6e4dd43483922d53afea60352a7b06
caishunfeng <caishunfeng2021@gmail.com>
2022-09-15 10:39:00 +0800

    [Bug] [Master] WorkflowExecuteRunnable will face a infinite loop #11838 (#11864) (#11950)
    * [Bug] [Master] WorkflowExecuteRunnable will face a infinite loop #11838
    
    Co-authored-by: Yann Ann <2993643785@qq.com>

--- dolphinscheduler-master/src/main/java/org/apache/dolphinscheduler/server/master/runner/WorkflowExecuteRunnable.java
+++ dolphinscheduler-master/src/main/java/org/apache/dolphinscheduler/server/master/runner/WorkflowExecuteRunnable.java
@@ -300,7 +300,6 @@ public class WorkflowExecuteRunnable implements Callable<WorkflowSubmitStatue> {
             } finally {
                 LoggerUtils.removeWorkflowAndTaskInstanceIdMDC();
             }
-
         }
     }
 
