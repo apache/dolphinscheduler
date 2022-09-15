81f2d6b2938149daa832a51039184f0441c1497b
冯剑 <35831367+fengjian1129@users.noreply.github.com>
2022-09-15 09:03:47 +0800

    [3.0.1-prepare]cherry-pick[Bug-#11650][worker] #11650 fix SQL type task, stop task does not take effect (#11938)
    * [Bug-11650][worker] #11650 fix SQL type task, stop task does not take effect
    
    Co-authored-by: 冯剑 Jian <jian.feng@jiduauto.com>

--- dolphinscheduler-worker/src/main/java/org/apache/dolphinscheduler/server/worker/processor/TaskKillProcessor.java
+++ dolphinscheduler-worker/src/main/java/org/apache/dolphinscheduler/server/worker/processor/TaskKillProcessor.java
@@ -124,7 +124,9 @@ public class TaskKillProcessor implements NettyRequestProcessor {
     private void sendTaskKillResponseCommand(Channel channel, TaskExecutionContext taskExecutionContext) {
         TaskKillResponseCommand taskKillResponseCommand = new TaskKillResponseCommand();
         taskKillResponseCommand.setStatus(taskExecutionContext.getCurrentExecutionStatus().getCode());
-        taskKillResponseCommand.setAppIds(Arrays.asList(taskExecutionContext.getAppIds().split(TaskConstants.COMMA)));
+        if (taskExecutionContext.getAppIds() != null) {
+            taskKillResponseCommand.setAppIds(Arrays.asList(taskExecutionContext.getAppIds().split(TaskConstants.COMMA)));
+        }
         taskKillResponseCommand.setTaskInstanceId(taskExecutionContext.getTaskInstanceId());
         taskKillResponseCommand.setHost(taskExecutionContext.getHost());
         taskKillResponseCommand.setProcessId(taskExecutionContext.getProcessId());
