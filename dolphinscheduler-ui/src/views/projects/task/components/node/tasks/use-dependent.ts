ffb58c1e3272fa5cffc9cd94cfc1d46842899dfd
Kerwin <37063904+zhuangchong@users.noreply.github.com>
2022-09-19 10:41:06 +0800

    [3.0.1-prepare][cherry-pick]3.0.1 UI 1 (#12033)
    * [Improvement][UI] Unified local parameters UI (#11190)
    
    * [Fix][UI] fix bug where icons are displayed in one line. (#11320)
    
    * cherry pick [Improvement-11386][UI] Concise the logic available for task action buttons
    
    * [Fix][UI] Fix the preTask options are unavailable when creating a tas...
    
    * fix build
    
    Co-authored-by: Chris Ho <realhezean@gmail.com>
    Co-authored-by: Devosend <devosend@gmail.com>
    Co-authored-by: Sheldon <39169452+sketchmind@users.noreply.github.com>
    Co-authored-by: Amy0104 <amywang0104@163.com>

--- dolphinscheduler-ui/src/views/projects/task/components/node/tasks/use-dependent.ts
+++ dolphinscheduler-ui/src/views/projects/task/components/node/tasks/use-dependent.ts
@@ -51,24 +51,10 @@ export function useDependent({
     ...data
   } as INodeData)
 
-  let extra: IJsonItem[] = []
-  if (from === 1) {
-    extra = [
-      Fields.useTaskType(model, readonly),
-      Fields.useProcessName({
-        model,
-        projectCode,
-        isCreate: !data?.id,
-        from,
-        processName: data?.processName
-      })
-    ]
-  }
-
   return {
     json: [
       Fields.useName(from),
-      ...extra,
+      ...Fields.useTaskDefinition({ projectCode, from, readonly, data, model }),
       Fields.useRunFlag(),
       Fields.useDescription(),
       Fields.useTaskPriority(),
