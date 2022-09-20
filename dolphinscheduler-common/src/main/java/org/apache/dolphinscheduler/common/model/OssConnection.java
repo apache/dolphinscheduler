806563897fdb7ad80e74cb9582fd1c495adc561b
EricGao888 <ericgao.apache@gmail.com>
2022-09-20 15:59:18 +0800

    Fix LICENSE and optimize OssConnection.java

--- dolphinscheduler-common/src/main/java/org/apache/dolphinscheduler/common/model/OssConnection.java
+++ dolphinscheduler-common/src/main/java/org/apache/dolphinscheduler/common/model/OssConnection.java
@@ -17,18 +17,15 @@
 
 package org.apache.dolphinscheduler.common.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class OssConnection {
 
     public String accessKeyId;
     public String accessKeySecret;
     public String endPoint;
 
-    public OssConnection(final String accessKeyId, final String accessKeySecret, final String endPoint) {
-        this.accessKeyId = accessKeyId;
-        this.accessKeySecret = accessKeySecret;
-        this.endPoint = endPoint;
-    }
 }
