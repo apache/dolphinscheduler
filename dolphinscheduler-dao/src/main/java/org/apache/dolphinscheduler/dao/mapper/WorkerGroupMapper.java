1194d1e5bc46e6aaac377f367a3472a5dac4d069
Yann Ann <2993643785@qq.com>
2022-10-05 17:08:48 +0800

    just correct wrong word (#12174)

--- dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/mapper/WorkerGroupMapper.java
+++ dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/mapper/WorkerGroupMapper.java
@@ -55,7 +55,7 @@ public interface WorkerGroupMapper extends BaseMapper<WorkerGroup> {
     int updateById(@Param("et") WorkerGroup entity);
 
     /**
-     * query worer grouop by name
+     * query worker group by name
      *
      * @param name name
      * @return worker group list
