cd8a759163ead472802dee605a5b1134162bf8e2
lynn-illumio <101166369+lynn-illumio@users.noreply.github.com>
2022-10-08 01:35:14 -0700

    [Feat][UI] Set up UI setting component in profile page (#11226)
    * Set up UI setting component in profile page
    
    * Add UI setting page to the route, remove UI setting section from the profile
    
    * Add log auto refresh timer store
    
    * Add logTimer to pinia store and cookies
    
    * Read logTimer from pinia store, and delay getLogs by passing the logTimer to setInterval in refreshLogs function
    
    * Add log timer to getLogs
    
    * Fine tune view log, add auto refresh based on time interval set in UI setting
    
    * Add useI18n to ui setting
    
    * Set up UI setting component in profile page
    
    * Add UI setting page to the route, remove UI setting section from the profile
    
    * Add log auto refresh timer store
    
    * Add logTimer to pinia store and cookies
    
    * Read logTimer from pinia store, and delay getLogs by passing the logTimer to setInterval in refreshLogs function
    
    * Add log timer to getLogs
    
    * Fine tune view log, add auto refresh based on time interval set in UI setting
    
    * Add useI18n to ui setting
    
    * [Feat][UI] Add UI setting page in the project.
    
    * [Feat][UI] Add UI setting page in the project.
    
    * Remove logTimer in cookies
    
    * Clear timer id, set VITE_APP_DEV_WEB_URL back to default
    
    * Clear time id in dag
    
    * [Feat][UI] Add license header.
    
    * [Feat][UI] Remove console.
    
    * [Fix][UI] Fix log timer types.
    
    Co-authored-by: songjianet <1778651752@qq.com>

--- dolphinscheduler-ui/src/locales/en_US/menu.ts
+++ dolphinscheduler-ui/src/locales/en_US/menu.ts
@@ -56,5 +56,6 @@ export default {
   task_group_queue: 'Task Group Queue',
   data_quality: 'Data Quality',
   task_result: 'Task Result',
-  rule: 'Rule management'
+  rule: 'Rule management',
+  ui_setting: 'Setting',
 }
