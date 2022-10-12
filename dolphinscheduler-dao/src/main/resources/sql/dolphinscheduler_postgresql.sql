74e5819f3ee1a7c63a496ce16573391946b95fab
EricPyZhou <ericzpy1998@outlook.com>
2022-10-12 12:04:30 -0400

    modify db schema for postgres and h2

--- dolphinscheduler-dao/src/main/resources/sql/dolphinscheduler_postgresql.sql
+++ dolphinscheduler-dao/src/main/resources/sql/dolphinscheduler_postgresql.sql
@@ -698,7 +698,19 @@ CREATE TABLE t_ds_resources (
   CONSTRAINT t_ds_resources_un UNIQUE (full_name, type)
 ) ;
 
-
+--
+-- Table structure for table t_ds_relation_resources_task
+--
+DROP TABLE IF EXISTS t_ds_relation_resources_task;
+CREATE TABLE t_ds_relation_resources_task
+(
+  id SERIAL int NOT NULL,
+  task_id int(11) DEFAULT NULL,
+  full_name varchar(255) DEFAULT NULL,
+  type int DEFAULT NULL,
+  PRIMARY KEY (id),
+);
+CREATE UNIQUE INDEX t_ds_relation_resources_task_un on t_ds_relation_resources_task (task_id, full_name);
 --
 -- Table structure for table t_ds_schedules
 --
