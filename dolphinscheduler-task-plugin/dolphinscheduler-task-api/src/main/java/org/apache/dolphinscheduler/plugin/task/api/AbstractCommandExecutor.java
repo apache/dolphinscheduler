f0ad67f99262fc825a5bcdd4a067012c86f19c52
Kerwin <37063904+zhuangchong@users.noreply.github.com>
2022-10-18 13:24:57 +0800

    cherry-pick source is not available in sh (#12413)
    Co-authored-by: simsicon <simsicon@gmail.com>

--- dolphinscheduler-task-plugin/dolphinscheduler-task-api/src/main/java/org/apache/dolphinscheduler/plugin/task/api/AbstractCommandExecutor.java
+++ dolphinscheduler-task-plugin/dolphinscheduler-task-api/src/main/java/org/apache/dolphinscheduler/plugin/task/api/AbstractCommandExecutor.java
@@ -127,6 +127,7 @@ public abstract class AbstractCommandExecutor {
                 command.add("sudo");
                 command.add("-u");
                 command.add(taskRequest.getTenantCode());
+                command.add("-E");
             }
         }
         command.add(commandInterpreter());
