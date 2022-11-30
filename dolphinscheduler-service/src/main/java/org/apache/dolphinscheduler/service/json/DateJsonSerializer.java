fcf46e52a7e4e96587532dd6b2e76c2d00ec2e75
zicheng.zhu <zicheng.zhu@71360.com>
2022-11-30 13:57:22 +0800

    [fix]fixed java.sql.Date serialize error by convert it to java.util.Date

--- dolphinscheduler-service/src/main/java/org/apache/dolphinscheduler/service/json/DateJsonSerializer.java
+++ dolphinscheduler-service/src/main/java/org/apache/dolphinscheduler/service/json/DateJsonSerializer.java
@@ -35,7 +35,7 @@ public class DateJsonSerializer extends JsonSerializer<Date> {
     @Override
     public void serialize(Date value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
         Date temp = value;
-        // In the internal implementation of JDK, java.sql.Date is inherited from util.Date, and java.sql.Date has
+        // In the internal implementation of JDK, java.sql.Date is inherited from java.util.Date, and java.sql.Date has
         // rewritten many methods in java.util.Date, but it has not been implemented.
         // Later, when formatting, you need to call the method in Date to report an error.
         if (value instanceof java.sql.Date) {
