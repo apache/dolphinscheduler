f5801a0e1c8bebc900d75aab8ca3314bc0410c6b
Wenjun Ruan <wenjun@apache.org>
2022-12-11 22:58:21 +0800

    Fix task group might be rob by multiple task (#13115)

--- dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/mapper/TaskGroupMapper.java
+++ dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/mapper/TaskGroupMapper.java
@@ -35,14 +35,10 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
  */
 public interface TaskGroupMapper extends BaseMapper<TaskGroup> {
 
-    /**
-     * compard and set to update table of task group
-     *
-     * @param id primary key
-     * @return affected rows
-     */
-    int updateTaskGroupResource(@Param("id") int id, @Param("queueId") int queueId,
-                                @Param("queueStatus") int queueStatus);
+    int robTaskGroupResource(@Param("id") int id,
+                             @Param("currentUseSize") int currentUseSize,
+                             @Param("queueId") int queueId,
+                             @Param("queueStatus") int queueStatus);
 
     /**
      * update table of task group
