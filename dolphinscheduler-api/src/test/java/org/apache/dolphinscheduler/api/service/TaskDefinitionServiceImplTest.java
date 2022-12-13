b09b46353211ac21d5eaf9125fafccfa4ca211cf
fanwanlong <fanwanlong@kezaihui.com>
2022-12-13 20:14:54 +0800

    modify code style

--- dolphinscheduler-api/src/test/java/org/apache/dolphinscheduler/api/service/TaskDefinitionServiceImplTest.java
+++ dolphinscheduler-api/src/test/java/org/apache/dolphinscheduler/api/service/TaskDefinitionServiceImplTest.java
@@ -492,6 +492,7 @@ public class TaskDefinitionServiceImplTest {
 
     @Test
     public void testUpdateTaskWithUpstream() {
+
         String taskDefinitionJson = getTaskDefinitionJson();
         TaskDefinition taskDefinition = getTaskDefinition();
         taskDefinition.setFlag(Flag.NO);
