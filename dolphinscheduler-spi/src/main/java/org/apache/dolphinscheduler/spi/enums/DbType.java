f0dd4f081fcd65015d4043f59f1bba6f5f94f82e
Tq <tianqitobethefirst@gmail.com>
2022-12-19 10:27:03 +0800

    [Feature][task-plugins-12820] azure azure-sql datasource plugins (#13209)
    * submit azure sql code
    
    * change mssql-jdbc version
    
    * merge changes to delete other
    
    * merge changes to delete other
    
    * add dependencies

--- dolphinscheduler-spi/src/main/java/org/apache/dolphinscheduler/spi/enums/DbType.java
+++ dolphinscheduler-spi/src/main/java/org/apache/dolphinscheduler/spi/enums/DbType.java
@@ -42,8 +42,10 @@ public enum DbType {
     ATHENA(11, "athena"),
     TRINO(12, "trino"),
     STARROCKS(13, "starrocks"),
-    ;
+    AZURESQL(14, "azuresql");
 
+    private static final Map<Integer, DbType> DB_TYPE_MAP =
+            Arrays.stream(DbType.values()).collect(toMap(DbType::getCode, Functions.identity()));
     @EnumValue
     private final int code;
     private final String descp;
@@ -52,17 +54,6 @@ public enum DbType {
         this.code = code;
         this.descp = descp;
     }
-
-    public int getCode() {
-        return code;
-    }
-
-    public String getDescp() {
-        return descp;
-    }
-
-    private static final Map<Integer, DbType> DB_TYPE_MAP =
-            Arrays.stream(DbType.values()).collect(toMap(DbType::getCode, Functions.identity()));
 
     public static DbType of(int type) {
         if (DB_TYPE_MAP.containsKey(type)) {
@@ -76,12 +67,21 @@ public enum DbType {
                 .orElseThrow(() -> new NoSuchElementException("no such db type"));
     }
 
+    public int getCode() {
+        return code;
+    }
+
+    public String getDescp() {
+        return descp;
+    }
+
     public boolean isHive() {
         return this == DbType.HIVE;
     }
 
     /**
      * support execute multiple segmented statements at a time
+     *
      * @return
      */
     public boolean isSupportMultipleStatement() {
