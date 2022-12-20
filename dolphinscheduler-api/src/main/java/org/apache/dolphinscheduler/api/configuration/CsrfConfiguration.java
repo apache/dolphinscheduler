34fa8d2f76fbdd0bb60ac09f4671122e3a32e5c4
海大一个小神经 <1161623489>
2022-12-21 00:44:00 +0800

    [Improvement][Security]spotless apply(issue-12931)

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/configuration/CsrfConfiguration.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/configuration/CsrfConfiguration.java
@@ -22,7 +22,6 @@ import org.apache.dolphinscheduler.api.security.LoginCsrfTokenRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
