5b502e81afca93e2706ebaba84ccd5e162ca2484
Jay Chung <zhongjiajie955@gmail.com>
2022-12-29 17:38:35 +0800

    Avoid return map in interface setScheduleState

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/SchedulerService.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/SchedulerService.java
@@ -128,9 +128,8 @@ public interface SchedulerService {
      * @param projectCode project code
      * @param id scheduler id
      * @param scheduleStatus schedule status
-     * @return publish result code
      */
-    Map<String, Object> setScheduleState(User loginUser,
+    void setScheduleState(User loginUser,
                                          long projectCode,
                                          Integer id,
                                          ReleaseState scheduleStatus);
