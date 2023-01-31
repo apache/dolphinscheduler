f7cd01f35a28507be0a51e0b9400654a98c446f9
Wenjun Ruan <wenjun@apache.org>
2023-01-31 15:45:38 +0800

    Fix task wake up failed will block the event

--- dolphinscheduler-common/src/main/java/org/apache/dolphinscheduler/common/enums/StateEventType.java
+++ dolphinscheduler-common/src/main/java/org/apache/dolphinscheduler/common/enums/StateEventType.java
@@ -25,7 +25,7 @@ public enum StateEventType {
     TASK_STATE_CHANGE(1, "task state change"),
     PROCESS_TIMEOUT(2, "process timeout"),
     TASK_TIMEOUT(3, "task timeout"),
-    WAIT_TASK_GROUP(4, "wait task group"),
+    WAKE_UP_TASK_GROUP(4, "wait task group"),
     TASK_RETRY(5, "task retry"),
     PROCESS_BLOCKED(6, "process blocked");
 
