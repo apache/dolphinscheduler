86021588a4a786f703bf4a397704af58eba21b2c
xu.xiaojing <xu.xiaojing@datasw.com>
2023-02-01 10:54:30 +0800

    [fix-13472][Api] change Threadpool of Quartz Scheduler to ZeroSizeThreadPool
    This closes issue #13472

---
+++ dolphinscheduler-scheduler-plugin/dolphinscheduler-scheduler-quartz/src/main/java/org/apache/dolphinscheduler/scheduler/quartz/QZeroSizeThreadPool.java
@@ -0,0 +1,14 @@
+package org.apache.dolphinscheduler.scheduler.quartz;
+
+import  org.quartz.simpl.ZeroSizeThreadPool;
+
+/**
+ * fix spring bug : add getter、setter method for threadCount field
+ */
+public class QZeroSizeThreadPool extends ZeroSizeThreadPool {
+
+    public void setThreadCount(int count) {}
+
+    public int getThreadCount() {return -1;}
+}
+
