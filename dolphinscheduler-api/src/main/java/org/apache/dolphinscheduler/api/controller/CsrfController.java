c0ce175e72146d9cfae53910784786cd997faf95
海大一个小神经 <1161623489>
2023-02-02 22:58:05 +0800

    [Improvement][Security]Fix Security Hotspots(issue-12931)

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/controller/CsrfController.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/controller/CsrfController.java
@@ -17,13 +17,13 @@
 package org.apache.dolphinscheduler.api.controller;
 
 import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CsrfController {
 
-    @RequestMapping("/csrf")
+    @GetMapping("/csrf")
     public CsrfToken csrf(CsrfToken token) {
         return token;
     }
