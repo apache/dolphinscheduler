4675131efbaa91d1bcf6feae4bb6de03857cec4a
xu.xiaojing <xu.xiaojing@datasw.com>
2023-02-02 14:19:33 +0800

    [fix-13472][Api] fix the code style

--- dolphinscheduler-scheduler-plugin/dolphinscheduler-scheduler-quartz/src/main/java/org/apache/dolphinscheduler/scheduler/quartz/QZeroSizeThreadPool.java
+++ dolphinscheduler-scheduler-plugin/dolphinscheduler-scheduler-quartz/src/main/java/org/apache/dolphinscheduler/scheduler/quartz/QZeroSizeThreadPool.java
@@ -1,14 +1,16 @@
 package org.apache.dolphinscheduler.scheduler.quartz;
 
-import  org.quartz.simpl.ZeroSizeThreadPool;
+import org.quartz.simpl.ZeroSizeThreadPool;
 
 /**
  * fix spring bug : add getter、setter method for threadCount field
  */
 public class QZeroSizeThreadPool extends ZeroSizeThreadPool {
 
-    public void setThreadCount(int count) {}
+    public void setThreadCount(int count) {
+    }
 
-    public int getThreadCount() {return -1;}
+    public int getThreadCount() {
+        return -1;
+    }
 }
-
