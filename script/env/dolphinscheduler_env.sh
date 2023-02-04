836f3261ebb7ff737f2e44cc3b70bd0822bde18d
xu.xiaojing <xu.xiaojing@datasw.com>
2023-02-04 19:28:39 +0800

    remove controversial env variable JAVA_HOME

--- script/env/dolphinscheduler_env.sh
+++ script/env/dolphinscheduler_env.sh
@@ -18,9 +18,6 @@
 
 # Never put sensitive config such as database password here in your production environment,
 
-# JAVA_HOME, will use it to start DolphinScheduler server
-export JAVA_HOME=${JAVA_HOME:-/opt/soft/java}
-
 #DS will auto load hdfs configuration from environment
 export HADOOP_HOME=${HADOOP_HOME:-/opt/soft/hadoop}
 export HADOOP_CONF_DIR=${HADOOP_CONF_DIR:-/opt/soft/hadoop/etc/hadoop}
