28711581ab992d3bbad4d5e3d7f902370299bf3d
moonkop <moonkop@qq.com>
2023-03-03 20:03:32 +0800

    update use-cell-update.ts

--- dolphinscheduler-ui/src/views/projects/workflow/components/dag/use-cell-update.ts
+++ dolphinscheduler-ui/src/views/projects/workflow/components/dag/use-cell-update.ts
@@ -55,7 +55,7 @@ export function useCellUpdate(options: Options) {
    * @param {string} id
    * @param {string} flag
    */
-  function setNodeFlag(id: string, flag: string) {
+  function setNodeFlag(id: string, flag?: string) {
     const node = graph.value?.getCellById(id);
     if (node) {
       node.attr('rect/fill', flag === 'NO' ? '#f3f3f5' : '#ffffff');
