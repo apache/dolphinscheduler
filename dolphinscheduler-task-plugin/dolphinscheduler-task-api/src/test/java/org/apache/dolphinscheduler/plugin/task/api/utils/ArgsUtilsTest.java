2d11f8ee65eca713a044f34540e531792c3c6882
brian <flyunrain@163.com>
2023-03-09 23:21:33 +0800

    add UT for utils

---
+++ dolphinscheduler-task-plugin/dolphinscheduler-task-api/src/test/java/org/apache/dolphinscheduler/plugin/task/api/utils/ArgsUtilsTest.java
@@ -0,0 +1,22 @@
+package org.apache.dolphinscheduler.plugin.task.api.utils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ArgsUtilsTest {
+    public static final Logger logger = LoggerFactory.getLogger(ArgsUtilsTest.class);
+
+    @Test
+    public void escape() {
+        Assertions.assertEquals(ArgsUtils.escape("23\42"),"2342");
+    }
+
+
+    @Test
+    public void wrapperSingleQuotes(){
+        Assertions.assertEquals(ArgsUtils.wrapperSingleQuotes("23"),"'23'");
+    }
+
+}
---
+++ dolphinscheduler-task-plugin/dolphinscheduler-task-api/src/test/java/org/apache/dolphinscheduler/plugin/task/api/utils/ProcessUtilsTest.java
@@ -0,0 +1,22 @@
+package org.apache.dolphinscheduler.plugin.task.api.utils;
+
+import org.apache.dolphinscheduler.plugin.task.api.TaskExecutionContext;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class ProcessUtilsTest {
+    public static final Logger logger = LoggerFactory.getLogger(ProcessUtilsTest.class);
+
+    @Test
+    public void kill() throws IOException {
+        TaskExecutionContext taskExecutionContext=new TaskExecutionContext();
+        Assertions.assertFalse(ProcessUtils.kill(taskExecutionContext));
+    }
+
+
+
+}
