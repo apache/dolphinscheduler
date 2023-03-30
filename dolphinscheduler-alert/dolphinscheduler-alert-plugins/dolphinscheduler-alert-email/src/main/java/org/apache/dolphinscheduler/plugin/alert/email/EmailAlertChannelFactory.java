784e2fba00a3db64c0cc61a4b288d27c715d236c
Jay Chung <zhongjiajie955@gmail.com>
2023-03-30 18:57:47 +0800

    fix: Alarm instance port verification (#13827)
    close: #13532

--- dolphinscheduler-alert/dolphinscheduler-alert-plugins/dolphinscheduler-alert-email/src/main/java/org/apache/dolphinscheduler/plugin/alert/email/EmailAlertChannelFactory.java
+++ dolphinscheduler-alert/dolphinscheduler-alert-plugins/dolphinscheduler-alert-email/src/main/java/org/apache/dolphinscheduler/plugin/alert/email/EmailAlertChannelFactory.java
@@ -28,10 +28,12 @@ import org.apache.dolphinscheduler.alert.api.AlertConstants;
 import org.apache.dolphinscheduler.alert.api.AlertInputTips;
 import org.apache.dolphinscheduler.alert.api.ShowType;
 import org.apache.dolphinscheduler.spi.params.PasswordParam;
+import org.apache.dolphinscheduler.spi.params.base.DataType;
 import org.apache.dolphinscheduler.spi.params.base.ParamsOptions;
 import org.apache.dolphinscheduler.spi.params.base.PluginParams;
 import org.apache.dolphinscheduler.spi.params.base.Validate;
 import org.apache.dolphinscheduler.spi.params.input.InputParam;
+import org.apache.dolphinscheduler.spi.params.input.number.InputNumberParam;
 import org.apache.dolphinscheduler.spi.params.radio.RadioParam;
 
 import java.util.ArrayList;
@@ -69,11 +71,12 @@ public final class EmailAlertChannelFactory implements AlertChannelFactory {
                         .addValidate(Validate.newBuilder().setRequired(true).build())
                         .build();
 
-        InputParam mailSmtpPort =
-                InputParam.newBuilder(MailParamsConstants.NAME_MAIL_SMTP_PORT, MailParamsConstants.MAIL_SMTP_PORT)
-                        .setValue("25")
+        InputNumberParam mailSmtpPort =
+                InputNumberParam.newBuilder(MailParamsConstants.NAME_MAIL_SMTP_PORT, MailParamsConstants.MAIL_SMTP_PORT)
+                        .setValue(25)
                         .addValidate(Validate.newBuilder()
                                 .setRequired(true)
+                                .setType(DataType.NUMBER.getDataType())
                                 .build())
                         .build();
 
