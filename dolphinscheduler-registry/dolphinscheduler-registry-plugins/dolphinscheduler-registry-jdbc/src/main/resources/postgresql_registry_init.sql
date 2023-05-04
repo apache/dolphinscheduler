0a025224206916bb2fb80dfabe97f0e419cb886e
Wenjun Ruan <wenjun@apache.org>
2023-05-04 22:51:23 +0800

    Fix registry table field (#14043)

--- dolphinscheduler-registry/dolphinscheduler-registry-plugins/dolphinscheduler-registry-jdbc/src/main/resources/postgresql_registry_init.sql
+++ dolphinscheduler-registry/dolphinscheduler-registry-plugins/dolphinscheduler-registry-jdbc/src/main/resources/postgresql_registry_init.sql
@@ -20,15 +20,15 @@ create table t_ds_jdbc_registry_data
 (
     id               serial
         constraint t_ds_jdbc_registry_data_pk primary key,
-    key              varchar                             not null,
-    data             text                                not null,
-    type             int4                                not null,
+    data_key         varchar                             not null,
+    data_value       text                                not null,
+    data_type        int4                                not null,
     last_term        bigint                              not null,
     last_update_time timestamp default current_timestamp not null,
     create_time      timestamp default current_timestamp not null
 );
 
-create unique index t_ds_jdbc_registry_data_key_uindex on t_ds_jdbc_registry_data (key);
+create unique index t_ds_jdbc_registry_data_key_uindex on t_ds_jdbc_registry_data (data_key);
 
 
 DROP TABLE IF EXISTS t_ds_jdbc_registry_lock;
@@ -36,10 +36,10 @@ create table t_ds_jdbc_registry_lock
 (
     id               serial
         constraint t_ds_jdbc_registry_lock_pk primary key,
-    key              varchar                             not null,
+    lock_key         varchar                             not null,
     lock_owner       varchar                             not null,
     last_term        bigint                              not null,
     last_update_time timestamp default current_timestamp not null,
     create_time      timestamp default current_timestamp not null
 );
-create unique index t_ds_jdbc_registry_lock_key_uindex on t_ds_jdbc_registry_lock (key);
+create unique index t_ds_jdbc_registry_lock_key_uindex on t_ds_jdbc_registry_lock (lock_key);
