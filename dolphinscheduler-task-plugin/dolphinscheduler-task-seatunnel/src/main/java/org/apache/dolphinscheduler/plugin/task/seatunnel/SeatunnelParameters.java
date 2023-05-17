bc3555eb9c226496b79d7c61d0f924d634238efa
yuanzhongkai <yuanzk@bjhzwq.com>
2023-05-17 17:46:30 +0800

    FIX: fix issue #14114(https://github.com/apache/dolphinscheduler/issues/14114)

--- dolphinscheduler-task-plugin/dolphinscheduler-task-seatunnel/src/main/java/org/apache/dolphinscheduler/plugin/task/seatunnel/SeatunnelParameters.java
+++ dolphinscheduler-task-plugin/dolphinscheduler-task-seatunnel/src/main/java/org/apache/dolphinscheduler/plugin/task/seatunnel/SeatunnelParameters.java
@@ -20,6 +20,7 @@ package org.apache.dolphinscheduler.plugin.task.seatunnel;
 import org.apache.dolphinscheduler.plugin.task.api.model.ResourceInfo;
 import org.apache.dolphinscheduler.plugin.task.api.parameters.AbstractParameters;
 import org.apache.dolphinscheduler.plugin.task.seatunnel.flink.SeatunnelFlinkParameters;
+import org.apache.dolphinscheduler.plugin.task.seatunnel.self.SeatunnelEngineParameters;
 import org.apache.dolphinscheduler.plugin.task.seatunnel.spark.SeatunnelSparkParameters;
 
 import org.apache.commons.collections4.CollectionUtils;
@@ -35,7 +36,8 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, visible = true, property = "engine")
 @JsonSubTypes({
         @JsonSubTypes.Type(value = SeatunnelFlinkParameters.class, name = "FLINK"),
-        @JsonSubTypes.Type(value = SeatunnelSparkParameters.class, name = "SPARK")
+        @JsonSubTypes.Type(value = SeatunnelSparkParameters.class, name = "SPARK"),
+        @JsonSubTypes.Type(value = SeatunnelEngineParameters.class, name = "SEATUNNEL_ENGINE")
 })
 public class SeatunnelParameters extends AbstractParameters {
 
