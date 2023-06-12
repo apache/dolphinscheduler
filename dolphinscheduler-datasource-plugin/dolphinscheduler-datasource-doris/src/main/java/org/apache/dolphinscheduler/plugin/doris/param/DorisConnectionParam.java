11aa39b8b926086efd810c9ec8cbc5e4b6712603
Xinxing <49302071+xinxingi@users.noreply.github.com>
2023-06-12 15:04:16 +0800

    Update DorisConnectionParam.java
    Strip comment headers

--- dolphinscheduler-datasource-plugin/dolphinscheduler-datasource-doris/src/main/java/org/apache/dolphinscheduler/plugin/doris/param/DorisConnectionParam.java
+++ dolphinscheduler-datasource-plugin/dolphinscheduler-datasource-doris/src/main/java/org/apache/dolphinscheduler/plugin/doris/param/DorisConnectionParam.java
@@ -2,11 +2,6 @@ package org.apache.dolphinscheduler.plugin.doris.param;
 
 import org.apache.dolphinscheduler.spi.datasource.BaseConnectionParam;
 
-/**
- * @author xinxing
- * @description 功能描述
- * @create 2023/6/7 14:47
- */
 public class DorisConnectionParam extends BaseConnectionParam {
     @Override
     public String toString() {
