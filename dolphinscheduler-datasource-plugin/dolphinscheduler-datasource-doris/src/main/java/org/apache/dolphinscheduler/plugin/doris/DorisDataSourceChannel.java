c95691f6781a373ba428158437d2030d275d08d2
Xinxing <49302071+xinxingi@users.noreply.github.com>
2023-06-12 14:54:53 +0800

    Update DorisDataSourceChannel.java
    Remove comment headers

--- dolphinscheduler-datasource-plugin/dolphinscheduler-datasource-doris/src/main/java/org/apache/dolphinscheduler/plugin/doris/DorisDataSourceChannel.java
+++ dolphinscheduler-datasource-plugin/dolphinscheduler-datasource-doris/src/main/java/org/apache/dolphinscheduler/plugin/doris/DorisDataSourceChannel.java
@@ -5,11 +5,7 @@ import org.apache.dolphinscheduler.spi.datasource.DataSourceChannel;
 import org.apache.dolphinscheduler.spi.datasource.DataSourceClient;
 import org.apache.dolphinscheduler.spi.enums.DbType;
 
-/**
- * @author xinxing
- * @description 功能描述
- * @create 2023/6/7 14:43
- */
+
 public class DorisDataSourceChannel implements DataSourceChannel {
     @Override
     public DataSourceClient createDataSourceClient(BaseConnectionParam baseConnectionParam, DbType dbType) {
