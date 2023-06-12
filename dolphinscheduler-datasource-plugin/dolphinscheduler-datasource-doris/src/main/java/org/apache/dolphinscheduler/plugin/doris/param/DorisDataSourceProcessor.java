28e99fd4aad312ee145c5783eb694d0dfffeed97
xinxing <xinxing.work@outlook.com>
2023-06-12 15:20:27 +0800

    Strip comment headers and add license headers

--- dolphinscheduler-datasource-plugin/dolphinscheduler-datasource-doris/src/main/java/org/apache/dolphinscheduler/plugin/doris/param/DorisDataSourceProcessor.java
+++ dolphinscheduler-datasource-plugin/dolphinscheduler-datasource-doris/src/main/java/org/apache/dolphinscheduler/plugin/doris/param/DorisDataSourceProcessor.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.dolphinscheduler.plugin.doris.param;
 
 import com.google.auto.service.AutoService;
