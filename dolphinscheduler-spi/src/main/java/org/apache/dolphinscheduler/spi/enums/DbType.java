3e0acb99d5dea904ba890086095b3af37b2a6af9
Xinxing <49302071+xinxingi@users.noreply.github.com>
2023-06-26 11:47:34 +0800

    Update DbType.java

--- dolphinscheduler-spi/src/main/java/org/apache/dolphinscheduler/spi/enums/DbType.java
+++ dolphinscheduler-spi/src/main/java/org/apache/dolphinscheduler/spi/enums/DbType.java
@@ -50,7 +50,7 @@ public enum DbType {
     DATABEND(19, "databend"),
     SNOWFLAKE(20, "snowflake"),
     VERTICA(21, "vertica"),
-    DORIS(22,"doris");
+    DORIS(22, "doris");
 
     private static final Map<Integer, DbType> DB_TYPE_MAP =
             Arrays.stream(DbType.values()).collect(toMap(DbType::getCode, Functions.identity()));
