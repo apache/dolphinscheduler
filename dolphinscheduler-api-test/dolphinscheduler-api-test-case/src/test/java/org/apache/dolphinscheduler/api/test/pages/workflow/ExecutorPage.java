78c3043d400ad8b468b0de62c44cc0f592065762
EricGao888 <ericgao.apache@gmail.com>
2023-07-10 20:00:16 +0800

    update

--- dolphinscheduler-api-test/dolphinscheduler-api-test-case/src/test/java/org/apache/dolphinscheduler/api/test/pages/workflow/ExecutorPage.java
+++ dolphinscheduler-api-test/dolphinscheduler-api-test-case/src/test/java/org/apache/dolphinscheduler/api/test/pages/workflow/ExecutorPage.java
@@ -22,6 +22,7 @@ package org.apache.dolphinscheduler.api.test.pages.workflow;
 import org.apache.dolphinscheduler.api.test.core.Constants;
 import org.apache.dolphinscheduler.api.test.entity.HttpResponse;
 import org.apache.dolphinscheduler.api.test.utils.RequestClient;
+import org.apache.dolphinscheduler.common.enums.FailureStrategy;
 import org.apache.dolphinscheduler.dao.entity.User;
 
 import java.util.HashMap;
@@ -37,21 +38,21 @@ public class ExecutorPage {
 
     private String sessionId;
 
-    public HttpResponse createSchedule(User loginUser, long projectCode, long processDefinitionCode, String schedule) {
+    public HttpResponse startProcessInstance(User loginUser, long projectCode, long processDefinitionCode, String scheduleTime, FailureStrategy failureStrategy) {
         Map<String, Object> params = new HashMap<>();
         params.put("loginUser", loginUser);
-        params.put("projectCode", projectCode);
         params.put("processDefinitionCode", processDefinitionCode);
-        params.put("schedule", schedule);
+        params.put("scheduleTime", scheduleTime);
+        params.put("failureStrategy", failureStrategy);
         Map<String, String> headers = new HashMap<>();
         headers.put(Constants.SESSION_ID_KEY, sessionId);
 
         RequestClient requestClient = new RequestClient();
-        String url = String.format("/projects/%s/schedules", projectCode);
+        String url = String.format("projects/{projectCode}/executors/start-process-instance", projectCode);
         return requestClient.post(url, headers, params);
     }
 
-    public HttpResponse queryScheduleList(User loginUser, long projectCode) {
+    public HttpResponse queryExecutingWorkflow(User loginUser, long projectCode) {
         Map<String, Object> params = new HashMap<>();
         params.put("loginUser", loginUser);
         params.put("projectCode", projectCode);
@@ -63,50 +64,47 @@ public class ExecutorPage {
         return requestClient.post(url, headers, params);
     }
 
-
-    public HttpResponse publishScheduleOnline(User loginUser, long projectCode, int scheduleId) {
+    public HttpResponse execute(User loginUser, long projectCode) {
         Map<String, Object> params = new HashMap<>();
         params.put("loginUser", loginUser);
+        params.put("projectCode", projectCode);
         Map<String, String> headers = new HashMap<>();
         headers.put(Constants.SESSION_ID_KEY, sessionId);
 
         RequestClient requestClient = new RequestClient();
-        String url = String.format("/projects/%s/schedules/%s/online", projectCode, scheduleId);
+        String url = String.format("/projects/%s/schedules/list", projectCode);
         return requestClient.post(url, headers, params);
     }
 
-    public HttpResponse offlineSchedule(User loginUser, long projectCode, int scheduleId) {
+    public HttpResponse startCheckProcessDefinition(User loginUser, long projectCode) {
         Map<String, Object> params = new HashMap<>();
         params.put("loginUser", loginUser);
+        params.put("projectCode", projectCode);
         Map<String, String> headers = new HashMap<>();
         headers.put(Constants.SESSION_ID_KEY, sessionId);
 
         RequestClient requestClient = new RequestClient();
-        String url = String.format("/projects/%s/schedules/%s/offline", projectCode, scheduleId);
+        String url = String.format("/projects/%s/schedules/list", projectCode);
         return requestClient.post(url, headers, params);
     }
 
-    public HttpResponse updateSchedule(User loginUser, long projectCode, int scheduleId, String schedule) {
+    public HttpResponse executeTask(User loginUser, long projectCode) {
         Map<String, Object> params = new HashMap<>();
         params.put("loginUser", loginUser);
-        params.put("schedule", schedule);
+        params.put("projectCode", projectCode);
         Map<String, String> headers = new HashMap<>();
         headers.put(Constants.SESSION_ID_KEY, sessionId);
 
         RequestClient requestClient = new RequestClient();
-        String url = String.format("/projects/%s/schedules/%s", projectCode, scheduleId);
-        return requestClient.put(url, headers, params);
-    }
-
-    public HttpResponse deleteScheduleById(User loginUser, long projectCode, int scheduleId) {
-        Map<String, Object> params = new HashMap<>();
-        params.put("loginUser", loginUser);
-        Map<String, String> headers = new HashMap<>();
-        headers.put(Constants.SESSION_ID_KEY, sessionId);
-
-        RequestClient requestClient = new RequestClient();
-        String url = String.format("/projects/%s/schedules/%s", projectCode, scheduleId);
-        return requestClient.delete(url, headers, params);
+        String url = String.format("/projects/%s/schedules/list", projectCode);
+        return requestClient.post(url, headers, params);
     }
 
 }
+
+//
+//    startCheckProcessDefinition
+//    execute
+//    queryExecutingWorkflow
+//    executeTask
+//    startProcessInstance
