819827938be00901d03dad8721b1074787055073
calvin <jianghuachinacom@163.com>
2023-07-10 12:05:37 +0800

    ironed out this issue

--- dolphinscheduler-ui/src/views/security/tenant-manage/components/use-modalData.ts
+++ dolphinscheduler-ui/src/views/security/tenant-manage/components/use-modalData.ts
@@ -47,7 +47,7 @@ export function useModalData(
           if (!variables.model.tenantCode) {
             return new Error(t('security.tenant.tenant_code_tips'))
           }
-          if (variables.model.tenantCode.search('[^-._a-zA-Z0-9]')!=-1) {
+          if (variables.model.tenantCode.search('[^-._a-zA-Z0-9]') !== -1) {
             return new Error(t('security.tenant.tenant_code_invalid_tips'))
           }
         }
