9582820d8f13c3b4574d5382da171ea76f102ff8
Wenjun Ruan <wenjun@apache.org>
2023-07-19 17:25:58 +0800

    Support parse ipv6 (#14584)

--- dolphinscheduler-remote/src/test/java/org/apache/dolphinscheduler/remote/utils/HostTest.java
+++ dolphinscheduler-remote/src/test/java/org/apache/dolphinscheduler/remote/utils/HostTest.java
@@ -29,9 +29,13 @@ public class HostTest {
     public void testHost() {
         Host host = Host.of("192.158.2.2:22");
         Assertions.assertEquals(22, host.getPort());
-        host.setAddress("127.0.0.1:8888");
+        host = new Host("127.0.0.1:8888");
         Assertions.assertEquals("127.0.0.1", host.getIp());
         Assertions.assertEquals(8888, host.getPort());
+
+        host = new Host("2001:db8:1::ab9:C0A8:102:5678");
+        Assertions.assertEquals("2001:db8:1::ab9:C0A8:102", host.getIp());
+        Assertions.assertEquals(5678, host.getPort());
     }
 
 }
