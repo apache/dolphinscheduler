541e318cb7ac8e60540f91b63c86308705d2f15e
Chenshuai <785659165@qq.com>
2023-08-20 14:30:47 +0800

    fix: filter (#14770)
    Co-authored-by: Jim Chen <chenshuai19950725@gmail.com>
    Co-authored-by: Rick Cheng <rickchengx@gmail.com>
    Co-authored-by: xiangzihao <460888207@qq.com>

--- dolphinscheduler-ui/src/views/projects/task/components/node/fields/use-child-node.ts
+++ dolphinscheduler-ui/src/views/projects/task/components/node/fields/use-child-node.ts
@@ -72,7 +72,8 @@ export function useChildNode({
     span: 24,
     name: t('project.node.child_node'),
     props: {
-      loading: loading
+      loading: loading,
+      filterable: true
     },
     options: options,
     class: 'select-child-node',
