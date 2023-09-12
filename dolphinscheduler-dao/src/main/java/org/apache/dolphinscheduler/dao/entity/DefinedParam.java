718b3b9a9a030465c12667e64c79d8f4fa71558b
AmriStrong <wanggang1@baseus.com>
2023-09-12 15:12:27 +0800

    [Feature][dolphinscheduler-service] datax component global, parameter configuration #14897

---
+++ dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/entity/DefinedParam.java
@@ -0,0 +1,60 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package org.apache.dolphinscheduler.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+@TableName("t_ds_defined_param")
+public class DefinedParam {
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private int id;
+    private String key;
+    private String value;
+    private String description;
+
+    public DefinedParam(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public DefinedParam() {
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getKey() {
+        return this.key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
