3de8b70663ce7f46624982a1d4cbb9ebb2c1db18
wxn <154544017@qq.com>
2023-10-21 15:57:35 +0800

    sync database schema

--- dolphinscheduler-dao/src/main/resources/sql/upgrade/3.3.0_schema/mysql/dolphinscheduler_ddl.sql
+++ dolphinscheduler-dao/src/main/resources/sql/upgrade/3.3.0_schema/mysql/dolphinscheduler_ddl.sql
@@ -14,3 +14,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
 */
+-- Modify "t_ds_alertgroup" table
+ALTER TABLE `t_ds_alertgroup` AUTO_INCREMENT 3;
+-- Modify "t_ds_alert_plugin_instance" table
+ALTER TABLE `t_ds_alert_plugin_instance`
+    ADD COLUMN `instance_type` int NOT NULL DEFAULT 0, ADD COLUMN `warning_type` int NULL;
+-- Modify "t_ds_fav_task" table
+ALTER TABLE `t_ds_fav_task`
+    ADD COLUMN `task_type` varchar(64) NOT NULL COMMENT "favorite task type name";
+-- Create "t_ds_listener_event" table
+CREATE TABLE `t_ds_listener_event`
+(
+    `id`          int      NOT NULL AUTO_INCREMENT COMMENT "key",
+    `content`     text NULL COMMENT "listener event json content",
+    `sign`        char(40) NOT NULL DEFAULT "" COMMENT "sign=sha1(content)",
+    `post_status` tinyint NULL DEFAULT 0 COMMENT "0:wait running,1:success,2:failed,3:partial success",
+    `event_type`  int NULL COMMENT "listener event type",
+    `log`         text NULL COMMENT "log",
+    `create_time` datetime NULL COMMENT "create time",
+    `update_time` datetime NULL COMMENT "update time",
+    PRIMARY KEY (`id`),
+    INDEX         `idx_sign` (`sign`),
+    INDEX         `idx_status` (`post_status`)
+) CHARSET utf8 COLLATE utf8_bin;
