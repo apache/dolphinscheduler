09db46c6cc97eae0e7a59d3102f74fe13b339adb
xinxing <xinxing.work@outlook.com>
2023-11-06 16:26:27 +0800

    hana fix

--- dolphinscheduler-datasource-plugin/dolphinscheduler-datasource-hana/src/main/java/org/apache/dolphinscheduler/plugin/datasource/hana/param/HanaDataSourceParamDTO.java
+++ dolphinscheduler-datasource-plugin/dolphinscheduler-datasource-hana/src/main/java/org/apache/dolphinscheduler/plugin/datasource/hana/param/HanaDataSourceParamDTO.java
@@ -17,6 +17,9 @@
 
 package org.apache.dolphinscheduler.plugin.datasource.hana.param;
 
+import org.apache.dolphinscheduler.plugin.datasource.api.datasource.BaseDataSourceParamDTO;
+import org.apache.dolphinscheduler.spi.enums.DbType;
+
 public class HanaDataSourceParamDTO extends BaseDataSourceParamDTO {
 
     @Override
