0aa830cef7138f25c191067ebe9400ccdbb6be03
dailidong <dailidong66@gmail.com>
2023-12-06 16:25:03 +0800

    add test

--- dolphinscheduler-common/src/test/java/org/apache/dolphinscheduler/common/utils/HttpUtilsTest.java
+++ dolphinscheduler-common/src/test/java/org/apache/dolphinscheduler/common/utils/HttpUtilsTest.java
@@ -8,12 +8,19 @@ public class HttpUtilsTest {
     @Test
     void testGetRequest() {
         // test HTTP URL
-//        String response = HttpUtils.get("http://www.bing.com/");
-//        assertNotNull(response, "Response should not be null for a http URL");
+        String response1 = HttpUtils.get("http://www.bing.com/");
+        assertNotNull(response1, "Response should not be null for a http URL");
 
-        // test HTTPS URL
-        String response = HttpUtils.get("https://poc.bzzt.net");
-        assertNull(response, "Response should be null for a https URL");
+        // test invalid certification HTTPS URL
+        String response2 = HttpUtils.get("https://poc.bzzt.net");
+        assertNull(response2, "Response should be null for ainvalid certification https URL and throw exception in console");
+
+
+        // test valid certification HTTPS URL
+        String response3 = HttpUtils.get("https://www.google.com/");
+        assertNotNull(response3, "Response should not be null for a valid certification https URL");
+
+
     }
 
 }
