43127ec5797f14644336635b2e16d062f434c950
zzzk1 <madzhou1@gmail.com>
2023-12-06 17:14:11 +0800

    refactor: StatusTest.java use new status

--- dolphinscheduler-api/src/test/java/org/apache/dolphinscheduler/api/enums/StatusTest.java
+++ dolphinscheduler-api/src/test/java/org/apache/dolphinscheduler/api/enums/StatusTest.java
@@ -19,6 +19,8 @@ package org.apache.dolphinscheduler.api.enums;
 import java.util.Locale;
 import java.util.Optional;
 
+import org.apache.dolphinscheduler.api.enums.v2.BaseStatus;
+import org.apache.dolphinscheduler.api.enums.v2.ProjectStatus;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -27,17 +29,17 @@ public class StatusTest {
 
     @Test
     public void testGetCode() {
-        Assertions.assertEquals(0, Status.SUCCESS.getCode());
-        Assertions.assertNotEquals(0, Status.REQUEST_PARAMS_NOT_VALID_ERROR.getCode());
+        Assertions.assertEquals(0, BaseStatus.SUCCESS.getCode());
+        Assertions.assertNotEquals(0, BaseStatus.REQUEST_PARAMS_NOT_VALID_ERROR.getCode());
     }
 
     @Test
     public void testGetMsg() {
         LocaleContextHolder.setLocale(Locale.US);
-        Assertions.assertEquals("success", Status.SUCCESS.getMsg());
+        Assertions.assertEquals("success", BaseStatus.SUCCESS.getMsg());
 
         LocaleContextHolder.setLocale(Locale.SIMPLIFIED_CHINESE);
-        Assertions.assertEquals("成功", Status.SUCCESS.getMsg());
+        Assertions.assertEquals("成功", BaseStatus.SUCCESS.getMsg());
     }
 
     @Test
@@ -49,6 +51,6 @@ public class StatusTest {
         // SUCCESS
         optional = Status.findStatusBy(10018);
         Assertions.assertTrue(optional.isPresent());
-        Assertions.assertEquals(Status.PROJECT_NOT_FOUND, optional.get());
+        Assertions.assertEquals(ProjectStatus.PROJECT_NOT_FOUND, optional.get());
     }
 }
