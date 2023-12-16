286f88fe217a2b7cc6418cbcff5a4f8bf46c2ad1
pegasas <pegasashjy@gmail.com>
2023-12-16 20:34:57 +0800

    [Feature][Api] Refactor org.apache.dolphinscheduler.api.controller.K8sNamespaceController

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/controller/v2/K8sNamespaceV2Controller.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/controller/v2/K8sNamespaceV2Controller.java
@@ -1,30 +1,11 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package org.apache.dolphinscheduler.api.controller.v2;
 
-package org.apache.dolphinscheduler.api.controller;
-
-import static org.apache.dolphinscheduler.api.enums.Status.CREATE_K8S_NAMESPACE_ERROR;
-import static org.apache.dolphinscheduler.api.enums.Status.DELETE_K8S_NAMESPACE_BY_ID_ERROR;
-import static org.apache.dolphinscheduler.api.enums.Status.QUERY_AUTHORIZED_NAMESPACE_ERROR;
-import static org.apache.dolphinscheduler.api.enums.Status.QUERY_CAN_USE_K8S_NAMESPACE_ERROR;
-import static org.apache.dolphinscheduler.api.enums.Status.QUERY_K8S_NAMESPACE_LIST_PAGING_ERROR;
-import static org.apache.dolphinscheduler.api.enums.Status.QUERY_UNAUTHORIZED_NAMESPACE_ERROR;
-import static org.apache.dolphinscheduler.api.enums.Status.VERIFY_K8S_NAMESPACE_ERROR;
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.dolphinscheduler.api.controller.BaseController;
 import org.apache.dolphinscheduler.api.exceptions.ApiException;
 import org.apache.dolphinscheduler.api.service.K8sNamespaceService;
 import org.apache.dolphinscheduler.api.utils.Result;
@@ -32,34 +13,19 @@ import org.apache.dolphinscheduler.common.constants.Constants;
 import org.apache.dolphinscheduler.dao.entity.K8sNamespace;
 import org.apache.dolphinscheduler.dao.entity.User;
 import org.apache.dolphinscheduler.plugin.task.api.utils.ParameterUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import static org.apache.dolphinscheduler.api.enums.Status.*;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.Parameters;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.tags.Tag;
-
-/**
- * k8s namespace controller
- */
 @Tag(name = "K8S_NAMESPACE_TAG")
 @RestController
-@RequestMapping("/k8s-namespace")
-public class K8sNamespaceController extends BaseController {
-
+@RequestMapping("v2/k8s-namespace")
+public class K8sNamespaceV2Controller extends BaseController {
     @Autowired
     private K8sNamespaceService k8sNamespaceService;
 
@@ -110,7 +76,9 @@ public class K8sNamespaceController extends BaseController {
     public Result createNamespace(@Parameter(hidden = true) @RequestAttribute(value = Constants.SESSION_USER) User loginUser,
                                   @RequestParam(value = "namespace") String namespace,
                                   @RequestParam(value = "clusterCode") Long clusterCode) {
-        return returnDataList(k8sNamespaceService.createK8sNamespace(loginUser, namespace, clusterCode));
+        Map<String, Object> result =
+                k8sNamespaceService.createK8sNamespace(loginUser, namespace, clusterCode);
+        return returnDataList(result);
     }
 
     /**
@@ -152,7 +120,8 @@ public class K8sNamespaceController extends BaseController {
     @ApiException(DELETE_K8S_NAMESPACE_BY_ID_ERROR)
     public Result delNamespaceById(@Parameter(hidden = true) @RequestAttribute(value = Constants.SESSION_USER) User loginUser,
                                    @RequestParam(value = "id") int id) {
-        return returnDataList(k8sNamespaceService.deleteNamespaceById(loginUser, id));
+        Map<String, Object> result = k8sNamespaceService.deleteNamespaceById(loginUser, id);
+        return returnDataList(result);
     }
 
     /**
@@ -171,7 +140,8 @@ public class K8sNamespaceController extends BaseController {
     @ApiException(QUERY_UNAUTHORIZED_NAMESPACE_ERROR)
     public Result queryUnauthorizedNamespace(@Parameter(hidden = true) @RequestAttribute(value = Constants.SESSION_USER) User loginUser,
                                              @RequestParam("userId") Integer userId) {
-        return returnDataList(k8sNamespaceService.queryUnauthorizedNamespace(loginUser, userId));
+        Map<String, Object> result = k8sNamespaceService.queryUnauthorizedNamespace(loginUser, userId);
+        return returnDataList(result);
     }
 
     /**
@@ -190,7 +160,8 @@ public class K8sNamespaceController extends BaseController {
     @ApiException(QUERY_AUTHORIZED_NAMESPACE_ERROR)
     public Result queryAuthorizedNamespace(@Parameter(hidden = true) @RequestAttribute(value = Constants.SESSION_USER) User loginUser,
                                            @RequestParam("userId") Integer userId) {
-        return returnDataList(k8sNamespaceService.queryAuthorizedNamespace(loginUser, userId));
+        Map<String, Object> result = k8sNamespaceService.queryAuthorizedNamespace(loginUser, userId);
+        return returnDataList(result);
     }
 
     /**
@@ -204,6 +175,7 @@ public class K8sNamespaceController extends BaseController {
     @ResponseStatus(HttpStatus.OK)
     @ApiException(QUERY_CAN_USE_K8S_NAMESPACE_ERROR)
     public Result queryAvailableNamespaceList(@Parameter(hidden = true) @RequestAttribute(value = Constants.SESSION_USER) User loginUser) {
-        return success(k8sNamespaceService.queryNamespaceAvailable(loginUser));
+        List<K8sNamespace> result = k8sNamespaceService.queryNamespaceAvailable(loginUser);
+        return success(result);
     }
 }
