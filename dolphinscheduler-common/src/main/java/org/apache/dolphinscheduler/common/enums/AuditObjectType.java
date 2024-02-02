7ff0a5042b1d42e4357e31c1c55969e0da6c61cc
Draven <qingwli@cisco.com>
2024-02-02 15:49:06 +0800

    update

--- dolphinscheduler-common/src/main/java/org/apache/dolphinscheduler/common/enums/AuditObjectType.java
+++ dolphinscheduler-common/src/main/java/org/apache/dolphinscheduler/common/enums/AuditObjectType.java
@@ -19,8 +19,8 @@ package org.apache.dolphinscheduler.common.enums;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
