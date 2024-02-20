5213290262117d78dcd8936dd28eec2002a3e538
calvin <jianghuachinacom@163.com>
2024-02-20 17:20:00 +0800

    [Feature-14863][API] Support to manage what worker groups can be used for a project. (#15600)

--- dolphinscheduler-ui/src/views/projects/task/components/node/tasks/use-datasync.ts
+++ dolphinscheduler-ui/src/views/projects/task/components/node/tasks/use-datasync.ts
@@ -68,7 +68,7 @@ export function useDatasync({
       Fields.useCache(),
       Fields.useDescription(),
       Fields.useTaskPriority(),
-      Fields.useWorkerGroup(),
+      Fields.useWorkerGroup(projectCode),
       Fields.useEnvironmentName(model, !data?.id),
       ...Fields.useTaskGroup(model, projectCode),
       ...Fields.useFailed(),
