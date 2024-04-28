050491b1b22152b11c381160b227e2489a5db3bd
calvin <jianghuachinacom@163.com>
2024-04-28 13:36:17 +0800

    merge from dev

--- dolphinscheduler-ui/src/locales/en_US/project.ts
+++ dolphinscheduler-ui/src/locales/en_US/project.ts
@@ -250,7 +250,7 @@ export default {
       'The downstream dependent tasks exists. You can not delete the task.',
     warning_delete_scheduler_dependent_tasks_desc:
       'The downstream dependent tasks exists. Are you sure to delete the scheduler?',
-    warning_too_large_parallelism_number: 'The parallelism number is too large. It\'s better not to be over 10.'
+    warning_too_large_parallelism_number: 'The parallelism number is too large. It is better not to be over 10.'
   },
   task: {
     on_line: 'Online',
