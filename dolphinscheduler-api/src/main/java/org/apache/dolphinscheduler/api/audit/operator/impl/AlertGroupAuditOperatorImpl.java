e93ed78c0fc49fdef96d87e9aeee121da1bb0b29
旺阳 <wang@lqwang.net>
2024-05-28 13:49:19 +0800

    [Fix] Fix audit log parma typo (#16077)

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/audit/operator/impl/AlertGroupAuditOperatorImpl.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/audit/operator/impl/AlertGroupAuditOperatorImpl.java
@@ -31,7 +31,7 @@ public class AlertGroupAuditOperatorImpl extends BaseAuditOperator {
     private AlertGroupMapper alertGroupMapper;
 
     @Override
-    public String getObjectNameFromReturnIdentity(Object identity) {
+    public String getObjectNameFromIdentity(Object identity) {
         Long objId = toLong(identity);
         if (objId == -1) {
             return "";
