87dfc74c63171c286e2e65d193dae14d9a502d55
rockfang <cooldish1245@163.com>
2024-05-30 11:21:14 +0800

    [Fix][UI] ensure number type field has number type value in alarm-instance-manage (#16089)

--- dolphinscheduler-ui/src/views/security/alarm-instance-manage/use-form.ts
+++ dolphinscheduler-ui/src/views/security/alarm-instance-manage/use-form.ts
@@ -115,6 +115,15 @@ export function useForm() {
     state.detailForm.warningType = record.warningType
     if (record.pluginInstanceParams)
       state.json = JSON.parse(record.pluginInstanceParams)
+    // ensure number type field has number type value
+    state.json.forEach((item: any) => {
+      if (item.validate && item.validate.length) {
+        const numberTypeItem = item.validate.find(
+          (v: any) => v.type === 'number'
+        )
+        if (numberTypeItem) item.value = +item.value
+      }
+    })
   }
 
   return {
