f9db86559da282c0ad8ba48ced5196d905df2bcc
zuo <58384836+xxzuo@users.noreply.github.com>
2024-08-12 10:42:42 +0800

    [Fix] fix dingtalk at user Params (#16379)

--- dolphinscheduler-ui/src/locales/en_US/security.ts
+++ dolphinscheduler-ui/src/locales/en_US/security.ts
@@ -263,7 +263,7 @@ export default {
     AtSomeoneInRoom: 'At Someone In Room',
     Destination: 'Destination',
     AtMobiles: 'At User Mobiles',
-    AtUserIds: 'At User Ids',
+    AtDingtalkIds: 'At User Ids',
     MsgType: 'Msg Type',
     // eslint-disable-next-line quotes
     IsAtAll: "{'@'}All",
