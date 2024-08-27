ab40cf01b60c6931233f60f8fe8a5f52f640d13a
soxueren <250029975@qq.com>
2024-08-27 19:12:34 +0800

    Update BaseWindowsShellInterceptorBuilder.java
    Resolve the issue of unresponsive execution of bat in CMD.exe under window

--- dolphinscheduler-task-plugin/dolphinscheduler-task-api/src/main/java/org/apache/dolphinscheduler/plugin/task/api/shell/BaseWindowsShellInterceptorBuilder.java
+++ dolphinscheduler-task-plugin/dolphinscheduler-task-api/src/main/java/org/apache/dolphinscheduler/plugin/task/api/shell/BaseWindowsShellInterceptorBuilder.java
@@ -84,6 +84,10 @@ public abstract class BaseWindowsShellInterceptorBuilder<T extends BaseWindowsSh
         // todo: support tenant in widnows
         List<String> bootstrapCommand = new ArrayList<>();
         bootstrapCommand.add(shellInterpreter());
+        // bug Resolve the issue of unresponsive execution of bat in CMD.exe under window
+        if (SystemUtils.IS_OS_WINDOWS) {
+            bootstrapCommand.add("/c");
+        }
         bootstrapCommand.add(shellAbsolutePath().toString());
         return bootstrapCommand;
     }
