f8aa4932333add3f3a56eee43b33df5b3436648e
Wenjun Ruan <wenjun@apache.org>
2024-09-22 16:27:29 +0800

    [DSIP-75] Support auto add license header by spotless (#16643)

--- dolphinscheduler-registry/dolphinscheduler-registry-plugins/dolphinscheduler-registry-jdbc/src/test/java/org/apache/dolphinscheduler/plugin/registry/jdbc/LockUtilsTest.java
+++ dolphinscheduler-registry/dolphinscheduler-registry-plugins/dolphinscheduler-registry-jdbc/src/test/java/org/apache/dolphinscheduler/plugin/registry/jdbc/LockUtilsTest.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.dolphinscheduler.plugin.registry.jdbc;
 
 import static com.google.common.truth.Truth.assertThat;
