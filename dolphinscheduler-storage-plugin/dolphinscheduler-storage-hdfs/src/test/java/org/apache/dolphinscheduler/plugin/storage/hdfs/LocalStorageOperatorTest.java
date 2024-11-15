89f13edc304a29b279327bcb3d0ab025947af027
SbloodyS <460888207@qq.com>
2024-11-15 09:19:49 +0800

    fix ut

--- dolphinscheduler-storage-plugin/dolphinscheduler-storage-hdfs/src/test/java/org/apache/dolphinscheduler/plugin/storage/hdfs/LocalStorageOperatorTest.java
+++ dolphinscheduler-storage-plugin/dolphinscheduler-storage-hdfs/src/test/java/org/apache/dolphinscheduler/plugin/storage/hdfs/LocalStorageOperatorTest.java
@@ -32,6 +32,7 @@ import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 import lombok.SneakyThrows;
 
@@ -296,14 +297,20 @@ class LocalStorageOperatorTest {
         String resourceFileAbsolutePath = "file:" + baseDir;
         List<StorageEntity> storageEntities =
                 storageOperator.listFileStorageEntityRecursively(resourceFileAbsolutePath);
-        assertThat(storageEntities.size()).isEqualTo(1);
+        assertThat(storageEntities.size()).isEqualTo(3);
 
-        StorageEntity storageEntity2 = storageEntities.get(0);
-        assertThat(storageEntity2.getFullName()).isEqualTo("file:" + Paths.get(baseDir, "sqlDirectory", "demo.sql"));
-        assertThat(storageEntity2.getFileName()).isEqualTo("demo.sql");
-        assertThat(storageEntity2.getPfullName()).isEqualTo("file:" + Paths.get(baseDir, "sqlDirectory"));
-        assertThat(storageEntity2.isDirectory()).isFalse();
-        assertThat(storageEntity2.getType()).isEqualTo(ResourceType.FILE);
+        AtomicReference<StorageEntity> storageEntity2 = new AtomicReference<>();
+
+        storageEntities.stream()
+                .filter(storageEntity -> storageEntity.getFileName().equals("demo.sql"))
+                .findFirst()
+                .ifPresent(storageEntity2::set);;
+        assertThat(storageEntity2.get().getFullName())
+                .isEqualTo("file:" + Paths.get(baseDir, "sqlDirectory", "demo.sql"));
+        assertThat(storageEntity2.get().getFileName()).isEqualTo("demo.sql");
+        assertThat(storageEntity2.get().getPfullName()).isEqualTo("file:" + Paths.get(baseDir, "sqlDirectory"));
+        assertThat(storageEntity2.get().isDirectory()).isFalse();
+        assertThat(storageEntity2.get().getType()).isEqualTo(ResourceType.FILE);
     }
 
     @SneakyThrows
