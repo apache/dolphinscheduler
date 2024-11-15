dadfac10e7bdf6f010d92d570d4e7845ac54c7f9
SbloodyS <460888207@qq.com>
2024-11-15 15:00:43 +0800

    remove data-quality module

---
+++ dolphinscheduler-tools/src/main/java/org/apache/dolphinscheduler/tools/table/DropTable.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.dolphinscheduler.tools.table;
+
+import java.sql.SQLException;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+@ComponentScan("org.apache.dolphinscheduler")
+@Slf4j
+public class DropTable {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DropTable.class, args);
+    }
+
+    @Component
+    @Profile("dropTable")
+    static class DropTableRunner implements CommandLineRunner {
+
+        @Autowired
+        private DropTableService dropTableService;
+
+        @Override
+        public void run(String... args) throws SQLException {
+            log.info("Starting Drop Table...");
+            dropTableService.dropTableOnce();
+        }
+    }
+}
