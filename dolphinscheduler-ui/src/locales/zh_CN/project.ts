a9cd44f99ff1bf061d9e0dafbb13e3e3e6ebe66e
wuchao <sdhzw@163.com>
2025-03-17 11:38:41 +0800

    Improvement datasync json parameter validate

--- dolphinscheduler-ui/src/locales/zh_CN/project.ts
+++ dolphinscheduler-ui/src/locales/zh_CN/project.ts
@@ -894,7 +894,8 @@ export default {
     spark_submit_parameters: 'spark submit parameters',
     spark_submit_parameters_tips: 'spark submit parameters',
     is_production: 'is production',
-    is_production_tips: 'is production'
+    is_production_tips: 'is production',
+    json_format_tips: 'json参数不是一个正确的JSON格式'
   },
   menu: {
     fav: '收藏组件',
