35ce6671e77c4babbfd67d9a6d3843cdccc2e604
zhangyinghong <joy.zhang4@outlook.com>
2025-03-28 17:23:37 +0800

    [Fix-16929][Meter] Fix startup failed when metrics is disabled.

--- dolphinscheduler-meter/src/main/java/org/apache/dolphinscheduler/meter/MeterAutoConfiguration.java
+++ dolphinscheduler-meter/src/main/java/org/apache/dolphinscheduler/meter/MeterAutoConfiguration.java
@@ -18,6 +18,7 @@
 package org.apache.dolphinscheduler.meter;
 
 import org.apache.dolphinscheduler.meter.metrics.DefaultMetricsProvider;
+import org.apache.dolphinscheduler.meter.metrics.EmptyMetricsProvider;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -42,20 +43,28 @@ import io.micrometer.core.instrument.MeterRegistry;
  */
 @Configuration(proxyBeanMethods = false)
 @EnableAspectJAutoProxy
-@ConditionalOnProperty(prefix = "metrics", name = "enabled", havingValue = "true")
 public class MeterAutoConfiguration {
 
     @Bean
+    @ConditionalOnProperty(prefix = "metrics", name = "enabled", havingValue = "true")
     public DefaultMetricsProvider metricsProvider(MeterRegistry meterRegistry) {
         return new DefaultMetricsProvider(meterRegistry);
     }
 
     @Bean
+    @ConditionalOnProperty(prefix = "metrics", name = "enabled", havingValue = "false", matchIfMissing = true)
+    public EmptyMetricsProvider emptyMetricsProvider() {
+        return new EmptyMetricsProvider();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "metrics", name = "enabled", havingValue = "true")
     public TimedAspect timedAspect(MeterRegistry registry) {
         return new TimedAspect(registry);
     }
 
     @Bean
+    @ConditionalOnProperty(prefix = "metrics", name = "enabled", havingValue = "true")
     public CountedAspect countedAspect(MeterRegistry registry) {
         return new CountedAspect(registry);
     }
