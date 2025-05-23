1999c268d580f65d9a39445ccb6ace91c57c1196
Feng Jin <jinfeng1995@gmail.com>
2025-05-23 10:09:02 +0800

    fix ui checkstyle

--- dolphinscheduler-ui/src/views/projects/task/components/node/fields/use-flink-materialized-table.ts
+++ dolphinscheduler-ui/src/views/projects/task/components/node/fields/use-flink-materialized-table.ts
@@ -18,123 +18,125 @@ import { useI18n } from 'vue-i18n'
 import { useCustomParams } from '.'
 import type { IJsonItem } from '../types'
 
-export function useFlinkMaterializedTable(model: { [field: string]: any }): IJsonItem[] {
-    const { t } = useI18n()
+export function useFlinkMaterializedTable(model: {
+  [field: string]: any
+}): IJsonItem[] {
+  const { t } = useI18n()
 
-    return [
-        // mandatory field
-        {
-            type: 'input',
-            field: 'identifier',
-            name: t('project.node.identifier'),
-            class: 'input-identifier',
-            props: {
-                placeholder: t('project.node.identifier_tips')
-            },
-            validate: {
-                trigger: ['input', 'blur'],
-                required: true,
-                validator(validate: any, value: string) {
-                    if (!value) {
-                        return new Error(t('project.node.identifier_tips'))
-                    }
-                }
-            }
-        },
+  return [
+    // mandatory field
+    {
+      type: 'input',
+      field: 'identifier',
+      name: t('project.node.identifier'),
+      class: 'input-identifier',
+      props: {
+        placeholder: t('project.node.identifier_tips')
+      },
+      validate: {
+        trigger: ['input', 'blur'],
+        required: true,
+        validator(validate: any, value: string) {
+          if (!value) {
+            return new Error(t('project.node.identifier_tips'))
+          }
+        }
+      }
+    },
 
-        {
-            type: 'input',
-            field: 'gatewayEndpoint',
-            name: t('project.node.gateway_endpoint'),
-            class: 'input-gateway-endpoint',
-            props: {
-                placeholder: t('project.node.gateway_endpoint_tips')
-            },
-            validate: {
-                trigger: ['input', 'blur'],
-                required: true,
-                validator(validate: any, value: string) {
-                    if (!value) {
-                        return new Error(t('project.node.gateway_endpoint_tips'))
-                    }
-                }
-            }
-        },
+    {
+      type: 'input',
+      field: 'gatewayEndpoint',
+      name: t('project.node.gateway_endpoint'),
+      class: 'input-gateway-endpoint',
+      props: {
+        placeholder: t('project.node.gateway_endpoint_tips')
+      },
+      validate: {
+        trigger: ['input', 'blur'],
+        required: true,
+        validator(validate: any, value: string) {
+          if (!value) {
+            return new Error(t('project.node.gateway_endpoint_tips'))
+          }
+        }
+      }
+    },
 
-        {
-            type: 'input',
-            field: 'dynamicOptions',
-            name: t('project.node.dynamic_options'),
-            class: 'input-dynamic-options',
-            props: {
-                placeholder: t('project.node.dynamic_options_tips')
-            },
-            validate: {
-                trigger: ['input', 'blur'],
-                required: true,
-                validator(validate: any, value: string) {
-                    if (!value) {
-                        return new Error(t('project.node.dynamic_options_tips'))
-                    }
-                }
-            }
-        },
+    {
+      type: 'input',
+      field: 'dynamicOptions',
+      name: t('project.node.dynamic_options'),
+      class: 'input-dynamic-options',
+      props: {
+        placeholder: t('project.node.dynamic_options_tips')
+      },
+      validate: {
+        trigger: ['input', 'blur'],
+        required: true,
+        validator(validate: any, value: string) {
+          if (!value) {
+            return new Error(t('project.node.dynamic_options_tips'))
+          }
+        }
+      }
+    },
 
-        {
-            type: 'input',
-            field: 'staticPartitions',
-            name: t('project.node.static_partitions'),
-            class: 'input-static-partitions',
-            props: {
-                placeholder: t('project.node.static_partitions_tips')
-            },
-            validate: {
-                trigger: ['input', 'blur'],
-                required: true,
-                validator(validate: any, value: string) {
-                    if (!value) {
-                        return new Error(t('project.node.static_partitions_tips'))
-                    }
-                }
-            }
-        },
-        {
-            type: 'input',
-            field: 'initConfig',
-            name: t('project.node.init_config'),
-            class: 'input-init-config',
-            props: {
-                placeholder: t('project.node.init_config_tips')
-            },
-            validate: {
-                trigger: ['input', 'blur'],
-                required: true,
-                validator(validate: any, value: string) {
-                    if (!value) {
-                        return new Error(t('project.node.init_config_tips'))
-                    }
-                }
-            }
-        },
+    {
+      type: 'input',
+      field: 'staticPartitions',
+      name: t('project.node.static_partitions'),
+      class: 'input-static-partitions',
+      props: {
+        placeholder: t('project.node.static_partitions_tips')
+      },
+      validate: {
+        trigger: ['input', 'blur'],
+        required: true,
+        validator(validate: any, value: string) {
+          if (!value) {
+            return new Error(t('project.node.static_partitions_tips'))
+          }
+        }
+      }
+    },
+    {
+      type: 'input',
+      field: 'initConfig',
+      name: t('project.node.init_config'),
+      class: 'input-init-config',
+      props: {
+        placeholder: t('project.node.init_config_tips')
+      },
+      validate: {
+        trigger: ['input', 'blur'],
+        required: true,
+        validator(validate: any, value: string) {
+          if (!value) {
+            return new Error(t('project.node.init_config_tips'))
+          }
+        }
+      }
+    },
 
-        {
-            type: 'input',
-            field: 'executionConfig',
-            name: t('project.node.execution_config'),
-            class: 'input-execution-config',
-            props: {
-                placeholder: t('project.node.execution_config_tips')
-            },
-            validate: {
-                trigger: ['input', 'blur'],
-                required: true,
-                validator(validate: any, value: string) {
-                    if (!value) {
-                        return new Error(t('project.node.execution_config_tips'))
-                    }
-                }
-            }
-        },
-        ...useCustomParams({ model, field: 'localParams', isSimple: false })
-    ]
+    {
+      type: 'input',
+      field: 'executionConfig',
+      name: t('project.node.execution_config'),
+      class: 'input-execution-config',
+      props: {
+        placeholder: t('project.node.execution_config_tips')
+      },
+      validate: {
+        trigger: ['input', 'blur'],
+        required: true,
+        validator(validate: any, value: string) {
+          if (!value) {
+            return new Error(t('project.node.execution_config_tips'))
+          }
+        }
+      }
+    },
+    ...useCustomParams({ model, field: 'localParams', isSimple: false })
+  ]
 }
--- dolphinscheduler-ui/src/views/projects/task/components/node/tasks/use-flink-materialized-table.ts
+++ dolphinscheduler-ui/src/views/projects/task/components/node/tasks/use-flink-materialized-table.ts
@@ -20,54 +20,54 @@ import * as Fields from '../fields/index'
 import type { IJsonItem, INodeData, ITaskData } from '../types'
 
 export function useFlinkMaterializedTable({
-                                             projectCode,
-                                             from = 0,
-                                             readonly,
-                                             data
-                                         }: {
-    projectCode: number
-    from?: number
-    readonly?: boolean
-    data?: ITaskData
+  projectCode,
+  from = 0,
+  readonly,
+  data
+}: {
+  projectCode: number
+  from?: number
+  readonly?: boolean
+  data?: ITaskData
 }) {
-    const model = reactive({
-        name: '',
-        taskType: 'FLINK_MATERIALIZED_TABLE',
-        flag: 'YES',
-        description: '',
-        timeoutFlag: false,
-        localParams: [],
-        environmentCode: null,
-        failRetryInterval: 1,
-        failRetryTimes: 0,
-        workerGroup: 'default',
-        delayTime: 0,
-        timeout: 30,
-        type: 'ALIYUN_SERVERLESS_SPARK',
-        displayRows: 10,
-        timeoutNotifyStrategy: ['WARN'],
-        restEndpoint: '',
-        username: '',
-        password: ''
-    } as INodeData)
+  const model = reactive({
+    name: '',
+    taskType: 'FLINK_MATERIALIZED_TABLE',
+    flag: 'YES',
+    description: '',
+    timeoutFlag: false,
+    localParams: [],
+    environmentCode: null,
+    failRetryInterval: 1,
+    failRetryTimes: 0,
+    workerGroup: 'default',
+    delayTime: 0,
+    timeout: 30,
+    type: 'ALIYUN_SERVERLESS_SPARK',
+    displayRows: 10,
+    timeoutNotifyStrategy: ['WARN'],
+    restEndpoint: '',
+    username: '',
+    password: ''
+  } as INodeData)
 
-    return {
-        json: [
-            Fields.useName(from),
-            ...Fields.useTaskDefinition({ projectCode, from, readonly, data, model }),
-            Fields.useRunFlag(),
-            Fields.useDescription(),
-            Fields.useTaskPriority(),
-            Fields.useWorkerGroup(projectCode),
-            Fields.useEnvironmentName(model, !data?.id),
-            ...Fields.useTaskGroup(model, projectCode),
-            ...Fields.useFailed(),
-            Fields.useDelayTime(model),
-            ...Fields.useTimeoutAlarm(model),
-            // ...Fields.useDatasource(model),
-            ...Fields.useFlinkMaterializedTable(model),
-            Fields.usePreTasks()
-        ] as IJsonItem[],
-        model
-    }
+  return {
+    json: [
+      Fields.useName(from),
+      ...Fields.useTaskDefinition({ projectCode, from, readonly, data, model }),
+      Fields.useRunFlag(),
+      Fields.useDescription(),
+      Fields.useTaskPriority(),
+      Fields.useWorkerGroup(projectCode),
+      Fields.useEnvironmentName(model, !data?.id),
+      ...Fields.useTaskGroup(model, projectCode),
+      ...Fields.useFailed(),
+      Fields.useDelayTime(model),
+      ...Fields.useTimeoutAlarm(model),
+      // ...Fields.useDatasource(model),
+      ...Fields.useFlinkMaterializedTable(model),
+      Fields.usePreTasks()
+    ] as IJsonItem[],
+    model
+  }
 }
