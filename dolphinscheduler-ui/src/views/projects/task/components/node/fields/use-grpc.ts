88af6c078b50bb43bedf5fef7dae26f519e2390e
npofsi | N/P硅 <npofsi@outlook.com>
2025-07-16 21:23:14 +0800

    [Feature] Add fields

--- dolphinscheduler-ui/src/views/projects/task/components/node/fields/use-grpc.ts
+++ dolphinscheduler-ui/src/views/projects/task/components/node/fields/use-grpc.ts
@@ -15,14 +15,26 @@
  * limitations under the License.
  */
 import { useI18n } from 'vue-i18n'
-// import { useCustomParams } from '.'
+import { useCustomParams } from '.'
 import type { IJsonItem } from '../types'
+import { languages } from 'monaco-editor'
 // import { ref } from 'vue'
 
 export function useGrpc(model: { [field: string]: any }): IJsonItem[] {
   //TODO: Implement gRPC specific logic
   const { t } = useI18n()
   // const grpcItems = ref<IJsonItem[]>([])
+
+  const GRPC_CHECK_CONDITIONS = [
+    {
+      label: t('project.node.status_code_default'),
+      value: 'STATUS_CODE_DEFAULT'
+    },
+    {
+      label: t('project.node.status_code_custom'),
+      value: 'STATUS_CODE_CUSTOM'
+    }
+  ]
 
   model.grpcMethod = model.grpcMethod || 'UNARY'
 
@@ -31,7 +43,7 @@ export function useGrpc(model: { [field: string]: any }): IJsonItem[] {
       type: 'input',
       class: 'input-url-name',
       field: 'url',
-      name: '服务器地址', //t('project.node.http_url'),
+      name: '请求地址', //t('project.node.http_url'),
       props: {
         placeholder: t('project.node.http_url_tips')
       },
@@ -47,6 +59,107 @@ export function useGrpc(model: { [field: string]: any }): IJsonItem[] {
           }
         }
       }
-    }
+    },
+    {
+      type: 'editor',
+      class: 'input-url-name',
+      field: 'serviceDefinition',
+      name: 'Protobuf 定义', //t('project.node.http_url'),
+      props: {
+        languages: 'protobuf',
+        placeholder: 'Protobuf Text' //t('project.node.http_url_tips')
+      },
+      validate: {
+        trigger: ['input', 'blur'],
+        required: true,
+        validator(validate: any, value: string) {
+          if (!value) {
+            return new Error(t('project.node.http_url_tips'))
+          }
+          if (value.search(new RegExp(/http[s]{0,1}:\/\/\S*/, 'i'))) {
+            return new Error(t('project.node.http_url_validator'))
+          }
+        }
+      }
+    },
+    {
+      type: 'select',
+      class: 'input-url-name',
+      field: 'methodName',
+      name: '方法名称', //t('project.node.http_url'),
+      props: {
+        placeholder: '服务/方法' //t('project.node.http_url_tips')
+      },
+      validate: {
+        trigger: ['input', 'blur'],
+        required: true,
+        validator(validate: any, value: string) {
+          if (!value) {
+            return new Error(t('project.node.http_url_tips'))
+          }
+          if (value.search(new RegExp(/http[s]{0,1}:\/\/\S*/, 'i'))) {
+            return new Error(t('project.node.http_url_validator'))
+          }
+        }
+      }
+    },
+    {
+      type: 'editor',
+      class: 'input-url-name',
+      field: 'requestMessage',
+      name: 'Message 内容', //t('project.node.http_url'),
+      props: {
+        languages: 'json',
+        placeholder: 'Compose Message' //t('project.node.http_url_tips')
+      },
+      validate: {
+        trigger: ['input', 'blur'],
+        required: true,
+        validator(validate: any, value: string) {
+          if (!value) {
+            //TODO if JSON is not valid
+            return new Error(t('project.node.http_url_tips'))
+          }
+          if (value.search(new RegExp(/http[s]{0,1}:\/\/\S*/, 'i'))) {
+            //TODO if JSON is not compliant to proto format
+            return new Error(t('project.node.http_url_validator'))
+          }
+        }
+      }
+    },
+    {
+      type: 'select',
+      field: 'grpcCheckCondition',
+      name: t('project.node.http_check_condition'),
+      options: GRPC_CHECK_CONDITIONS
+    },
+    {
+      type: 'input-number',
+      field: 'connectTimeout',
+      name: t('project.node.connect_timeout'),
+      span: 12,
+      props: {
+        max: Math.pow(7, 10) - 1
+      },
+      slots: {
+        suffix: () => t('project.node.ms')
+      },
+      validate: {
+        trigger: ['input', 'blur'],
+        validator(validate: any, value: string) {
+          if (!Number.isInteger(parseInt(value))) {
+            return new Error(
+              t('project.node.connect_timeout') +
+                t('project.node.positive_integer_tips')
+            )
+          }
+        }
+      }
+    },
+    ...useCustomParams({
+      model,
+      field: 'localParams',
+      isSimple: true
+    })
   ]
 }
