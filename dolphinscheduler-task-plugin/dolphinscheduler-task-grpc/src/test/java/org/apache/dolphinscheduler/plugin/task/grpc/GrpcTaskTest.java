ad5f85df470d51d8a5c43e906dcd67aaa2029d87
npofsi | N/P硅 <npofsi@outlook.com>
2025-08-01 15:43:48 +0800

    Test: add test server

--- dolphinscheduler-task-plugin/dolphinscheduler-task-grpc/src/test/java/org/apache/dolphinscheduler/plugin/task/grpc/GrpcTaskTest.java
+++ dolphinscheduler-task-plugin/dolphinscheduler-task-grpc/src/test/java/org/apache/dolphinscheduler/plugin/task/grpc/GrpcTaskTest.java
@@ -18,14 +18,16 @@
 package org.apache.dolphinscheduler.plugin.task.grpc;
 
 import org.apache.dolphinscheduler.plugin.task.api.TaskExecutionContext;
+
 import org.mockito.Mockito;
 
 public class GrpcTaskTest {
-    private GrpcTask generateGrpcTask(){
+
+    private GrpcTask generateGrpcTask() {
         return genrateGrpcTaskWithJSONDefinition();
     }
 
-    private GrpcTask genrateGrpcTaskWithJSONDefinition(){
+    private GrpcTask genrateGrpcTaskWithJSONDefinition() {
         TaskExecutionContext taskExecutionContext = Mockito.mock(TaskExecutionContext.class);
         return new GrpcTask(taskExecutionContext);
     }
