5d3a2a7c9772be058d06e8c7b754bd441eccd4e0
lile <lile@Lile-PC.>
2025-08-05 15:55:10 +0800

    update constants

--- dolphinscheduler-common/src/main/java/org/apache/dolphinscheduler/common/constants/CommandKeyConstants.java
+++ dolphinscheduler-common/src/main/java/org/apache/dolphinscheduler/common/constants/CommandKeyConstants.java
@@ -25,19 +25,19 @@ public class CommandKeyConstants {
     /**
      * command parameter keys
      */
-    public static final String CMD_PARAM_RECOVER_WORKFLOW_ID_STRING = "ProcessInstanceId";
+    public static final String CMD_PARAM_RECOVER_WORKFLOW_ID_STRING = "WorkflowInstanceId";
 
     public static final String CMD_PARAM_RECOVERY_START_NODE_STRING = "StartNodeIdList";
 
     public static final String CMD_PARAM_RECOVERY_WAITING_THREAD = "WaitingThreadInstanceId";
 
-    public static final String CMD_PARAM_SUB_WORKFLOW = "processInstanceId";
+    public static final String CMD_PARAM_SUB_WORKFLOW = "workflowInstanceId";
 
     public static final String CMD_PARAM_EMPTY_SUB_PROCESS = "0";
 
-    public static final String CMD_PARAM_SUB_WORKFLOW_PARENT_INSTANCE_ID = "parentProcessInstanceId";
+    public static final String CMD_PARAM_SUB_WORKFLOW_PARENT_INSTANCE_ID = "parentWorkflowInstanceId";
 
-    public static final String CMD_PARAM_SUB_WORKFLOW_DEFINITION_CODE = "processDefinitionCode";
+    public static final String CMD_PARAM_SUB_WORKFLOW_DEFINITION_CODE = "workflowDefinitionCode";
 
     public static final String CMD_PARAM_START_NODES = "StartNodeList";
 
