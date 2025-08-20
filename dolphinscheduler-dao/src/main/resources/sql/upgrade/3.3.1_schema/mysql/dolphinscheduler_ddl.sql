27cad60f3bb3e4e8eec1a4514ed348c4f630400f
luxl@chinatelecom.cn <lindaluo83@126.com>
2025-08-20 11:37:51 +0800

    [Fix-17431] init-job failed to execute the 3.3.1 SQL (#17434)

--- dolphinscheduler-dao/src/main/resources/sql/upgrade/3.3.1_schema/mysql/dolphinscheduler_ddl.sql
+++ dolphinscheduler-dao/src/main/resources/sql/upgrade/3.3.1_schema/mysql/dolphinscheduler_ddl.sql
@@ -14,9 +14,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
 */
-
+-- id is AUTO_INCREMENT MODIFY id;
+ALTER TABLE t_ds_workflow_definition MODIFY id INT NOT NULL;
 ALTER TABLE t_ds_workflow_definition DROP PRIMARY KEY;
 ALTER TABLE t_ds_workflow_definition ADD PRIMARY KEY(id);
+-- recover AUTO_INCREMENT
+ALTER TABLE t_ds_workflow_definition MODIFY id INT NOT NULL AUTO_INCREMENT;
 ALTER TABLE t_ds_workflow_definition ADD UNIQUE KEY uniq_workflow_definition_code (code);
 ALTER TABLE t_ds_command DROP COLUMN test_flag;
 ALTER TABLE t_ds_error_command DROP COLUMN test_flag;
