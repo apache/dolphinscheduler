a35fc81ec90da9a23a96dec8cbd17f0f2e60eda5
npofsi koi | N/P硅 <npofsi@outlook.com>
2025-09-11 13:23:10 +0900

    [Chore] Use JSONUtil

--- dolphinscheduler-task-plugin/dolphinscheduler-task-grpc/src/main/java/org/apache/dolphinscheduler/plugin/task/grpc/protobufjs/JSONDescriptorHelper.java
+++ dolphinscheduler-task-plugin/dolphinscheduler-task-grpc/src/main/java/org/apache/dolphinscheduler/plugin/task/grpc/protobufjs/JSONDescriptorHelper.java
@@ -17,25 +17,20 @@
 
 package org.apache.dolphinscheduler.plugin.task.grpc.protobufjs;
 
+import org.apache.dolphinscheduler.common.utils.JSONUtils;
 import org.apache.dolphinscheduler.plugin.task.grpc.protobufjs.types.Root;
 
-import lombok.val;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.google.protobuf.Descriptors;
 
 public class JSONDescriptorHelper {
 
     public static Root ProtobufFromJSON(String json) throws JsonProcessingException {
-        ObjectMapper mapper = JsonMapper.builder()
-                .build();
-        return mapper.readValue(json, Root.class);
+        return JSONUtils.parseObject(json, Root.class);
     }
 
     public static Descriptors.FileDescriptor FileDescFromJSON(String json) throws JsonProcessingException, Descriptors.DescriptorValidationException {
-        val parser = new JSONDescriptorParser();
+        JSONDescriptorParser parser = new JSONDescriptorParser();
         return parser.buildDescriptor(ProtobufFromJSON(json));
     }
 }
