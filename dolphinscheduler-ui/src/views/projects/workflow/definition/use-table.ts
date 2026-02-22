bbbfd659eeb6e611adbfd455455a0b796970b8e8
asadjan4611 <asadjan4611@gmail.com>
2026-02-22 19:56:06 +0500

    [Fix-17984][UI] Release workflow definition list loading lock on request failure

--- dolphinscheduler-ui/src/views/projects/workflow/definition/use-table.ts
+++ dolphinscheduler-ui/src/views/projects/workflow/definition/use-table.ts
@@ -506,15 +506,27 @@ export function useTable() {
   const getTableData = (params: IDefinitionParam) => {
     if (variables.loadingRef) return
     variables.loadingRef = true
-    const { state } = useAsyncState(
-      queryListPaging({ ...params }, variables.projectCode).then((res: any) => {
+    // Always release loading lock, even when request fails.
+    const queryStatePromise = queryListPaging(
+      { ...params },
+      variables.projectCode
+    )
+      .then((res: any) => {
         variables.totalCount = res.total
         variables.totalPage = res.totalPage
         variables.tableData = res.totalList.map((item: any) => {
           return { ...item }
         })
+      })
+      .catch((err: Error) => {
+        window.$message.error(err?.message || 'Request failed')
+      })
+      .finally(() => {
         variables.loadingRef = false
-      }),
+      })
+
+    const { state } = useAsyncState(
+      queryStatePromise,
       { total: 0, table: [] }
     )
     return state
