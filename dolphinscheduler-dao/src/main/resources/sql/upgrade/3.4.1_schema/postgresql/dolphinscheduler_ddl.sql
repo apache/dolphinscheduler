3170c57ebca087c45f5edbbea3f9d7f8b60a55a6
Divyansh <iampratap7997@gmail.com>
2026-02-23 20:01:27 +0530

    Remove PostgreSQL DDL change

--- dolphinscheduler-dao/src/main/resources/sql/upgrade/3.4.1_schema/postgresql/dolphinscheduler_ddl.sql
+++ dolphinscheduler-dao/src/main/resources/sql/upgrade/3.4.1_schema/postgresql/dolphinscheduler_ddl.sql
@@ -19,6 +19,3 @@ CREATE SEQUENCE IF NOT EXISTS t_ds_task_instance_context_id_seq;
 ALTER TABLE t_ds_task_instance_context
 ALTER COLUMN id SET DEFAULT nextval('t_ds_task_instance_context_id_seq'::regclass);
 
--- Change column type to BIGint
-ALTER TABLE t_ds_serial_command
-    ALTER COLUMN workflow_definition_code TYPE BIGINT;
