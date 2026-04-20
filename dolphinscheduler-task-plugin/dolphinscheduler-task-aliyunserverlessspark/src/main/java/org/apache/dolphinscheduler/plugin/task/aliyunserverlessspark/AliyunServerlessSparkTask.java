7713ad8ef595e86ac594daf7620f7bf8f6190627
includetts <117636492+includetts@users.noreply.github.com>
2026-04-20 15:10:17 +0800

    fix e.getMessage() and e are duplicated. You just need to keep one
    Update exception message to include the exception object instead of the message.

--- dolphinscheduler-task-plugin/dolphinscheduler-task-aliyunserverlessspark/src/main/java/org/apache/dolphinscheduler/plugin/task/aliyunserverlessspark/AliyunServerlessSparkTask.java
+++ dolphinscheduler-task-plugin/dolphinscheduler-task-aliyunserverlessspark/src/main/java/org/apache/dolphinscheduler/plugin/task/aliyunserverlessspark/AliyunServerlessSparkTask.java
@@ -224,7 +224,7 @@ public class AliyunServerlessSparkTask extends AbstractRemoteTask {
                                 aliyunServerlessSparkParameters.getWorkspaceId(), jobRunId,
                                 cancelJobRunRequest);
                     } catch (Exception e) {
-                        throw new AliyunServerlessSparkTaskException("Failed to cancel job run! " + e.getMessage(), e);
+                        throw new AliyunServerlessSparkTaskException("Failed to cancel job run! " + e);
                     }
                 }, retryPolicy);
     }
