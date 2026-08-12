3c840904f4c2a6c50aadb4f1bf22758e616f6d85
liangwenjie2021 <liangwenjie2021@163.com>
2026-08-12 14:33:37 +0800

    fix(schedule): remove invalid source characters

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/impl/SchedulerServiceImpl.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/impl/SchedulerServiceImpl.java
@@ -177,7 +177,7 @@ public class SchedulerServiceImpl extends BaseServiceImpl implements SchedulerSe
         scheduleObj.setCrontab(scheduleParam.getCrontab());
         validateMissedFirePolicy(scheduleParam);
         scheduleObj.setMissedFirePolicy(scheduleParam.getMissedFirePolicy());
-        scheduleObj.setTriggerType(`r`n                scheduleParam.getTriggerType() == null ? ScheduleTriggerType.CRON : scheduleParam.getTriggerType());
+        scheduleObj.setTriggerType(scheduleParam.getTriggerType() == null ? ScheduleTriggerType.CRON : scheduleParam.getTriggerType());
         scheduleObj.setTimezoneId(scheduleParam.getTimezoneId());
         scheduleObj.setWarningType(warningType);
         scheduleObj.setWarningGroupId(warningGroupId);
@@ -595,7 +595,7 @@ public class SchedulerServiceImpl extends BaseServiceImpl implements SchedulerSe
             if (scheduleParam.isMissedFirePolicySet() && scheduleParam.getMissedFirePolicy() != null) {
                 schedule.setMissedFirePolicy(scheduleParam.getMissedFirePolicy());
             }
-            schedule.setTriggerType(`r`n                    scheduleParam.getTriggerType() == null ? ScheduleTriggerType.CRON : scheduleParam.getTriggerType());
+            schedule.setTriggerType(scheduleParam.getTriggerType() == null ? ScheduleTriggerType.CRON : scheduleParam.getTriggerType());
             schedule.setTimezoneId(scheduleParam.getTimezoneId());
         }
 
